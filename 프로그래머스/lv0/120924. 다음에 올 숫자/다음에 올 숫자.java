9c673db444815eefbf5423cf56772a1c89d746e9
CHOICORE <41681972+CHOICORE@users.noreply.github.com>
2023-03-27 18:28:53 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        
+        if (common[1] - common[0] == common[common.length - 1] - common[common.length - 2]) {
+            // 등차수열인 경우
+            answer = common[common.length - 1] + (common[1] - common[0]);
+        } else {
+            // 등비수열인 경우
+            answer = common[common.length - 1] * (common[1] / common[0]);
+        }
+        
+        return answer;
+    }
+}
