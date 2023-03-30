a767384f39978a1721cfe64a890eb0c129a0b70e
CHOICORE <41681972+CHOICORE@users.noreply.github.com>
2023-03-30 11:49:02 +0900

    Time: 10 ms (50.85%), Space: 44.1 MB (6.07%) - LeetHub

--- LeetCode/Medium/0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ LeetCode/Medium/0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,33 +1,21 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> result = new ArrayList<>();
-        Map<Character, Integer> frequencyMap = new HashMap<>();
+        int[] frequencyMap = new int[26];
         for (char c : p.toCharArray()) {
-            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+            frequencyMap[c - 'a']++;
         }
         int left = 0, right = 0;
         int count = p.length();
         while (right < s.length()) {
-            char curr = s.charAt(right++);
-            if (frequencyMap.containsKey(curr)) {
-                int freq = frequencyMap.get(curr);
-                if (freq > 0) {
-                    count--;
-                }
-                frequencyMap.put(curr, freq - 1);
+            if (frequencyMap[s.charAt(right++) - 'a']-- >= 1) {
+                count--;
             }
             if (count == 0) {
                 result.add(left);
             }
-            if (right - left == p.length()) {
-                char prev = s.charAt(left++);
-                if (frequencyMap.containsKey(prev)) {
-                    int freq = frequencyMap.get(prev);
-                    if (freq >= 0) {
-                        count++;
-                    }
-                    frequencyMap.put(prev, freq + 1);
-                }
+            if (right - left == p.length() && frequencyMap[s.charAt(left++) - 'a']++ >= 0) {
+                count++;
             }
         }
         return result;
