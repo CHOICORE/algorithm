069fa0fe7effc887658f9f831437b955a3c63815
CHOICORE <41681972+CHOICORE@users.noreply.github.com>
2023-04-02 15:36:40 +0900

    Time: 0 ms (100.00%), Space: 40.1 MB (47.07%) - LeetHub

--- LeetCode/Easy/1046-last-stone-weight/1046-last-stone-weight.java
+++ LeetCode/Easy/1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,22 +1,42 @@
-import java.util.PriorityQueue;
-
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a); // max heap
-        
-        for (int stone : stones) {
-            pq.offer(stone);
+        int n = stones.length;
+        for (int i = (n / 2) - 1; i >= 0; i--) {
+            heapify(stones, n, i);
         }
-        
-        while (pq.size() > 1) {
-            int stone1 = pq.poll();
-            int stone2 = pq.poll();
-            
-            if (stone1 != stone2) {
-                pq.offer(Math.abs(stone1 - stone2));
+        while (n > 1) {
+            int stone1 = stones[0];
+            stones[0] = stones[--n];
+            stones[n] = stone1;
+            heapify(stones, n, 0);
+            int stone2 = stones[0];
+            stones[0] = stones[--n];
+            stones[n] = stone2;
+            heapify(stones, n, 0);
+            int diff = Math.abs(stone1 - stone2);
+            if (diff > 0) {
+                stones[n++] = diff;
+                heapify(stones, n, n - 1);
             }
         }
-        
-        return pq.isEmpty() ? 0 : pq.poll();
+        return n == 0 ? 0 : stones[0];
+    }
+    
+    private void heapify(int[] stones, int n, int i) {
+        int maxIdx = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        if (left < n && stones[left] > stones[maxIdx]) {
+            maxIdx = left;
+        }
+        if (right < n && stones[right] > stones[maxIdx]) {
+            maxIdx = right;
+        }
+        if (maxIdx != i) {
+            int temp = stones[i];
+            stones[i] = stones[maxIdx];
+            stones[maxIdx] = temp;
+            heapify(stones, n, maxIdx);
+        }
     }
 }
