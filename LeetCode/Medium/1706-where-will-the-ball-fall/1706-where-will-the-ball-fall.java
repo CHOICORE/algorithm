448ec2ffee735c2473c233e33495bcb4dd34012c
CHOICORE <41681972+CHOICORE@users.noreply.github.com>
2023-04-03 11:15:23 +0900

    Time: 2 ms (56.80%), Space: 43.9 MB (38.09%) - LeetHub

---
+++ LeetCode/Medium/1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] findBall(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            int row = 0, col = i;
+            while (row < m) {
+                if (grid[row][col] == 1 && col < n - 1 && grid[row][col+1] == 1) {
+                    col++;
+                    row++;
+                } else if (grid[row][col] == -1 && col > 0 && grid[row][col-1] == -1) {
+                    col--;
+                    row++;
+                } else {
+                    answer[i] = -1;
+                    break;
+                }
+            }
+            if (row == m) {
+                answer[i] = col;
+            }
+        }
+        return answer;
+    }
+}
