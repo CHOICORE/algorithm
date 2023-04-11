abc0371d5904ad7df09c004c3720cbc275fc121b
CHOICORE <41681972+CHOICORE@users.noreply.github.com>
2023-04-11 09:15:15 +0900

    Time: 189 ms (63.93%), Space: 44.4 MB (58.05%) - LeetHub

--- LeetCode/Medium/2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ LeetCode/Medium/2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -2,12 +2,11 @@ class Solution {
     public String removeStars(String s) {
         Stack<Character> st = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '*') {
-                if (!st.isEmpty()) {
-                    st.pop();
-                }
+            char c = s.charAt(i);
+            if (c == '*') {
+                st.pop();
             } else {
-                st.push(s.charAt(i));
+                st.push(c);
             }
         }
 
