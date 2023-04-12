df3e8ccf1a0e9fb8fc601410def410acbc2c5326
CHOICORE <41681972+CHOICORE@users.noreply.github.com>
2023-04-12 10:02:31 +0900

    Time: 9 ms (39.29%), Space: 43.2 MB (93.51%) - LeetHub

--- LeetCode/Medium/0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ LeetCode/Medium/0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,51 +1,68 @@
 class Solution {
-    private int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    
+    private int m, n;
+    private int[][] heights;
+    private boolean[][] visitedP, visitedA;
+    private int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
+    private List<List<Integer>> res = new ArrayList<>();
+
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (heights == null || heights.length == 0 || heights[0].length == 0) {
-            return res;
-        }
-        
-        int m = heights.length, n = heights[0].length;
-        boolean[][] canReachPac = new boolean[m][n];
-        boolean[][] canReachAtl = new boolean[m][n];
-        
-        // DFS from cells on Pacific coast
+        m = heights.length;
+        n = heights[0].length;
+        this.heights = heights;
+        visitedP = new boolean[m][n];
+        visitedA = new boolean[m][n];
+
+        // 탐색을 위한 큐
+        Queue<int[]> queueP = new LinkedList<>();
+        Queue<int[]> queueA = new LinkedList<>();
+
+        // 탐색을 위해 바다의 가장자리에 있는 좌표를 큐에 추가
         for (int i = 0; i < m; i++) {
-            dfs(heights, canReachPac, i, 0);
+            queueP.offer(new int[]{i, 0});
+            queueA.offer(new int[]{i, n-1});
+            visitedP[i][0] = true;
+            visitedA[i][n-1] = true;
         }
-        for (int j = 0; j < n; j++) {
-            dfs(heights, canReachPac, 0, j);
+        for (int i = 0; i < n; i++) {
+            queueP.offer(new int[]{0, i});
+            queueA.offer(new int[]{m-1, i});
+            visitedP[0][i] = true;
+            visitedA[m-1][i] = true;
         }
-        
-        // DFS from cells on Atlantic coast
-        for (int i = 0; i < m; i++) {
-            dfs(heights, canReachAtl, i, n - 1);
-        }
-        for (int j = 0; j < n; j++) {
-            dfs(heights, canReachAtl, m - 1, j);
-        }
-        
-        // Add cells that can reach both coasts to result
+
+        // 큐를 이용한 BFS 탐색
+        bfs(queueP, visitedP);
+        bfs(queueA, visitedA);
+
+        // 결과 리스트 생성
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (canReachPac[i][j] && canReachAtl[i][j]) {
+                if (visitedP[i][j] && visitedA[i][j]) {
                     res.add(Arrays.asList(i, j));
                 }
             }
         }
-        
+
         return res;
     }
-    
-    private void dfs(int[][] heights, boolean[][] canReach, int i, int j) {
-        int m = heights.length, n = heights[0].length;
-        canReach[i][j] = true;
-        for (int[] dir : dirs) {
-            int x = i + dir[0], y = j + dir[1];
-            if (x >= 0 && x < m && y >= 0 && y < n && !canReach[x][y] && heights[x][y] >= heights[i][j]) {
-                dfs(heights, canReach, x, y);
+
+    private void bfs(Queue<int[]> queue, boolean[][] visited) {
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int row = curr[0], col = curr[1];
+
+            for (int[] d : dir) {
+                int newRow = row + d[0];
+                int newCol = col + d[1];
+
+                // 유효한 좌표인지, 아직 방문하지 않은 육지인지, 현재 위치보다 높이가 높은지 확인
+                if (newRow < 0 || newRow >= m || newCol < 0 || newCol >= n 
+                        || visited[newRow][newCol] || heights[newRow][newCol] < heights[row][col]) {
+                    continue;
+                }
+
+                visited[newRow][newCol] = true;
+                queue.offer(new int[]{newRow, newCol});
             }
         }
     }
