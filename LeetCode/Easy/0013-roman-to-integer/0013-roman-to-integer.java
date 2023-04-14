409c0e274e0de9f188c6476999ea47d1ff282a8b
CHOICORE <41681972+CHOICORE@users.noreply.github.com>
2023-04-14 19:47:41 +0900

    Time: 4 ms (94.12%), Space: 42.5 MB (86.96%) - LeetHub

---
+++ LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int romanToInt(String s) {
+        int result = 0;
+        int size = s.length();
+        for (int i = 0; i < size; i++) {
+            int value = getSymbolValue(s.charAt(i));
+            if (i < size - 1 && value < getSymbolValue(s.charAt(i+1))) {
+                result -= value;
+            } else {
+                result += value;
+            }
+        }
+        
+        return result;
+    }
+
+    private int getSymbolValue(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return -1;
+        }
+    }
+}
