85883d87492ff1dc77516240b14baf7fbace2df9
CHOICORE <41681972+CHOICORE@users.noreply.github.com>
2023-04-14 19:48:19 +0900

    Time: 6 ms (50.25%), Space: 43.6 MB (7.26%) - LeetHub

--- LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.java
+++ LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,37 +1,23 @@
 class Solution {
     public int romanToInt(String s) {
+        Map<Character, Integer> symbolValues = new HashMap<>();
+        symbolValues.put('I', 1);
+        symbolValues.put('V', 5);
+        symbolValues.put('X', 10);
+        symbolValues.put('L', 50);
+        symbolValues.put('C', 100);
+        symbolValues.put('D', 500);
+        symbolValues.put('M', 1000);
+        
         int result = 0;
-        int size = s.length();
-        for (int i = 0; i < size; i++) {
-            int value = getSymbolValue(s.charAt(i));
-            if (i < size - 1 && value < getSymbolValue(s.charAt(i + 1))) {
+        for (int i = 0; i < s.length(); i++) {
+            int value = symbolValues.get(s.charAt(i));
+            if (i < s.length() - 1 && value < symbolValues.get(s.charAt(i+1))) {
                 result -= value;
             } else {
                 result += value;
             }
         }
-
         return result;
-    }
-
-    private int getSymbolValue(char c) {
-        switch (c) {
-            case 'I':
-                return 1;
-            case 'V':
-                return 5;
-            case 'X':
-                return 10;
-            case 'L':
-                return 50;
-            case 'C':
-                return 100;
-            case 'D':
-                return 500;
-            case 'M':
-                return 1000;
-            default:
-                return -1;
-        }
     }
 }
