ca9c23ad145f6c80c9c38b8a021d47a00cf8268a
CHOICORE <41681972+CHOICORE@users.noreply.github.com>
2023-04-14 14:25:52 +0900

    Time: 104 ms (79.70%), Space: 51.9 MB (75.19%) - LeetHub

--- LeetCode/Medium/2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
+++ LeetCode/Medium/2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
@@ -5,7 +5,8 @@
  */
 var checkIfInstanceOf = function(obj, classFunction) {
     if(obj === null || obj === undefined || typeof classFunction !== 'function') return false;
-    return Object(obj) instanceof classFunction;};
+    return Object(obj) instanceof classFunction;
+};
 
 /**
  * checkIfInstanceOf(new Date(), Date); // true
