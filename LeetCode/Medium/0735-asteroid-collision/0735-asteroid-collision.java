7ea7c6c5e3de7223e8cb00e5ca26379a0c89c4f6
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-04-21 09:29:55 +0900

    Time: 11 ms (64.59%), Space: 43.5 MB (40.90%) - LeetHub

--- LeetCode/Medium/0735-asteroid-collision/0735-asteroid-collision.java
+++ LeetCode/Medium/0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,37 +1,25 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        if(asteroids==null || asteroids.length<=1)
-            return asteroids;
-        
-        Stack<Integer> astStack = new Stack<Integer>();
-        boolean blows = false;
-        for(int ast: asteroids)
-        {   blows = false;
-            while(!astStack.isEmpty() && ast< 0 && astStack.peek() > 0 && !blows)
-            {
-                if(-ast < astStack.peek())
-                       blows = true;
-                else
-                    if(-ast == astStack.peek())
-                    {
-                        blows = true;
-                        astStack.pop();
+        Stack<Integer> stack = new Stack();
+        for (int ast: asteroids) {
+            collision: {
+                while (!stack.isEmpty() && ast < 0 && 0 < stack.peek()) {
+                    if (stack.peek() < -ast) {
+                        stack.pop();
+                        continue;
+                    } else if (stack.peek() == -ast) {
+                        stack.pop();
                     }
-                else 
-                    astStack.pop();
+                    break collision;
+                }
+                stack.push(ast);
             }
-            
-            if(!blows)
-                astStack.push(ast);
-            
         }
-        
-        int[] result = new int[astStack.size()];
-        for(int i=result.length-1 ; i>=0 ;i--)
-        {
-            result[i] = astStack.pop();
+
+        int[] ans = new int[stack.size()];
+        for (int t = ans.length - 1; t >= 0; --t) {
+            ans[t] = stack.pop();
         }
-        
-        return result;
+        return ans;
     }
 }
