860cfacb1cbad70727309ad22e33327e5d60f8de
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-04-23 10:46:06 +0900

    Time: 67 ms (68.97%), Space: 42.5 MB (100.00%) - LeetHub

--- LeetCode/Hard/1416-restore-the-array/1416-restore-the-array.java
+++ LeetCode/Hard/1416-restore-the-array/1416-restore-the-array.java
@@ -1,22 +1,25 @@
 class Solution {
     public int numberOfArrays(String s, int k) {
+        long[] dp = new long[10];
+        int c = 9;
+        int d = 1_000_000_007;
         int sz = s.length();
-        int[] m = new int[sz + 1];
-        m[sz] = 1;
         for (int i = sz - 1; i >= 0; i--) {
-            if (s.charAt(i) == '0') {
-                m[i] = 0;
-                continue;
-            }
-            long r = 0;
+            long cur = 0;
+            long cr = 0;
             for (int j = i; j < sz; j++) {
-                r = r * 10 + (s.charAt(j) - '0');
-                if (r > k) {
+                cur = cur * 10 + s.charAt(j) - '0';
+                if (cur > k || (cur == 0))
                     break;
-                }
-                m[i] = (m[i] + m[j + 1]) % 1_000_000_007;
+                if (j == sz - 1)
+                    cr += 1 % d;
+                else
+                    cr += dp[(j - i + c + 1) % 10] % d;
             }
+            dp[c] = cr % d;
+            c--;
+            c = c == -1 ? 9 : c;
         }
-        return m[0];
+        return (int) dp[(c + 1) % 10] % d;
     }
 }
