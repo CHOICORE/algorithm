dcc1d4380a49b5484e522844b8e25946db076d68
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-04-23 10:50:06 +0900

    Time: 0 ms (100.00%), Space: 41 MB (10.21%) - LeetHub

--- LeetCode/Medium/0054-spiral-matrix/0054-spiral-matrix.java
+++ LeetCode/Medium/0054-spiral-matrix/0054-spiral-matrix.java
@@ -6,25 +6,21 @@ class Solution {
         }
         int top = 0, bottom = matrix.length - 1, left = 0, right = matrix[0].length - 1;
         while (top <= bottom && left <= right) {
-            // Traverse the top row
             for (int i = left; i <= right; i++) {
                 result.add(matrix[top][i]);
             }
             top++;
-            // Traverse the right column
             for (int i = top; i <= bottom; i++) {
                 result.add(matrix[i][right]);
             }
             right--;
             if (top <= bottom) {
-                // Traverse the bottom row
                 for (int i = right; i >= left; i--) {
                     result.add(matrix[bottom][i]);
                 }
                 bottom--;
             }
             if (left <= right) {
-                // Traverse the left column
                 for (int i = bottom; i >= top; i--) {
                     result.add(matrix[i][left]);
                 }
