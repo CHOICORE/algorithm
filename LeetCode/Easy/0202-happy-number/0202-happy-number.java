046b560560c89c1ca7ea4233c2e60bde319dce2b
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-04-23 10:48:06 +0900

    Time: 1 ms (92.33%), Space: 40.4 MB (13.31%) - LeetHub

--- LeetCode/Easy/0202-happy-number/0202-happy-number.java
+++ LeetCode/Easy/0202-happy-number/0202-happy-number.java
@@ -1,18 +1,20 @@
 class Solution {
+    private int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
     public boolean isHappy(int n) {
         Set<Integer> seen = new HashSet<>();
-        do {
-            int sum = 0;
-            while (n > 0) {
-                int digit = n % 10;
-                sum += digit * digit;
-                n /= 10;
-            }
-            if (sum == 1) {
-                return true;
-            }
-            n = sum;
-        } while (!seen.contains(n) && seen.add(n));
-        return false;
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
     }
 }
