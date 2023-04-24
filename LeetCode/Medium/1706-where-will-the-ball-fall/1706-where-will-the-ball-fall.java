76dda6b0ac777e302ea26e220fe74c17f549c26a
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-04-24 09:56:57 +0900

    Time: 0 ms (100.00%), Space: 43.6 MB (93.30%) - LeetHub

--- LeetCode/Medium/1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
+++ LeetCode/Medium/1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -1,25 +1,33 @@
 class Solution {
     public int[] findBall(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        int[] answer = new int[n];
-        for (int i = 0; i < n; i++) {
-            int row = 0, col = i;
-            while (row < m) {
-                if (grid[row][col] == 1 && col + 1 < n && grid[row][col+1] == 1) {
-                    col++;
-                    row++;
-                } else if (grid[row][col] == -1 && col > 0 && grid[row][col-1] == -1) {
-                    col--;
-                    row++;
-                } else {
-                    answer[i] = -1;
-                    break;
-                }
+        int[] result = new int[grid[0].length];
+        for(int i =0;i<grid[0].length;i++){
+            result[i] = dropBall(i, grid);
+        }
+
+        return result;
+    }
+
+    private int dropBall(int startPoint, int[][] grid){
+        int j = startPoint;
+        for(int i=0;i<grid.length;i++){
+            if(j<0 || j == grid[0].length){
+                return -1;
             }
-            if (row == m) {
-                answer[i] = col;
+            if(grid[i][j] == 1){
+                if(j == grid[0].length -1 || grid[i][j+1] == -1){
+                    return -1;
+                }
+                j++;
+            } else{
+                if(j == 0 || grid[i][j-1] == 1){
+                    return -1;
+                }
+                j--;
             }
         }
-        return answer;
+
+        return j;
+
     }
 }
