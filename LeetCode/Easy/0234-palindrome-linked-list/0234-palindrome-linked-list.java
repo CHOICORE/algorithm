6ec4e0522868c36b4cb537a7aec9c21098a0740f
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-04-26 09:41:09 +0900

    Time: 2 ms (100.00%), Space: 62.9 MB (47.45%) - LeetHub

--- LeetCode/Easy/0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ LeetCode/Easy/0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,44 +9,46 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-        if (head == null || head.next == null) {
-            return true;
+    ListNode findMiddle(ListNode head) {
+        ListNode t = head;
+        ListNode h = head;
+
+        if (h.next != null && h.next.next != null) {
+            h = h.next.next;
+            t = t.next;
         }
-        
-        ListNode slow = head, fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        
-        if (fast != null) {
-            slow = slow.next;
-        }
-        
-        ListNode reversed = reverse(slow);
-        while (reversed != null) {
-            if (head.val != reversed.val) {
-                return false;
-            }
-            head = head.next;
-            reversed = reversed.next;
-        }
-        
-        return true;
+        return t;
     }
-    
-    private ListNode reverse(ListNode head) {
+
+    ListNode reverse(ListNode head) {
+
         ListNode prev = null;
         ListNode curr = head;
-        
+
         while (curr != null) {
-            ListNode next = curr.next;
+            ListNode nxt = curr.next;
             curr.next = prev;
             prev = curr;
-            curr = next;
+            curr = nxt;
         }
-        
         return prev;
+
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null)
+            return true;
+
+
+        ListNode middle = findMiddle(head);
+        ListNode second = reverse(middle.next);
+
+        ListNode start = head;
+        while (second != null && start != null) {
+            if (start.val != second.val) return false;
+            start = start.next;
+            second = second.next;
+        }
+        return true;
     }
 }
