7ab842f50ad5168fcaf89a8dca356558cca99619
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-04-27 09:27:04 +0900

    Time: 1 ms (100.00%), Space: 44 MB (37.68%) - LeetHub

--- LeetCode/Medium/0621-task-scheduler/0621-task-scheduler.java
+++ LeetCode/Medium/0621-task-scheduler/0621-task-scheduler.java
@@ -1,31 +1,21 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        Map<Character, Integer> freqMap = new HashMap<>();
-        for (char task : tasks) {
-            freqMap.put(task, freqMap.getOrDefault(task, 0) + 1);
-        }
-        
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
-        maxHeap.addAll(freqMap.values());
-        
-        int cycles = 0;
-        while (!maxHeap.isEmpty()) {
-            List<Integer> cooldownList = new ArrayList<>();
-            for (int i = 0; i < n + 1; i++) {
-                if (!maxHeap.isEmpty()) {
-                    int freq = maxHeap.poll();
-                    if (freq > 1) {
-                        cooldownList.add(freq - 1);
-                    }
-                }
-                cycles++;
-                if (maxHeap.isEmpty() && cooldownList.isEmpty()) {
-                    break;
-                }
+        int[] map = new int[26];
+        for (char task : tasks)
+            map[task - 'A']++;
+
+        int maxCount = 0, numOfMaxCount = 0;
+        for (int num : map) {
+            if (num > maxCount) {
+                maxCount = num;
+                numOfMaxCount = 1;
+            } else if (num == maxCount) {
+                numOfMaxCount++;
             }
-            maxHeap.addAll(cooldownList);
         }
-        
-        return cycles;
+
+        int res = (n + 1) * (maxCount - 1) + numOfMaxCount;
+
+        return Math.max(res, tasks.length);
     }
 }
