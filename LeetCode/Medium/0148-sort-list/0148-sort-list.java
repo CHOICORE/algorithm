369efbc36ab69549a6ba4586613bd8c932ab81cf
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-04-27 09:23:20 +0900

    Time: 3 ms (99.11%), Space: 50.5 MB (59.74%) - LeetHub

--- LeetCode/Medium/0148-sort-list/0148-sort-list.java
+++ LeetCode/Medium/0148-sort-list/0148-sort-list.java
@@ -10,40 +10,40 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null)
-            return head;
-        ListNode mid = getMid(head);
-        ListNode left = sortList(head);
-        ListNode right = sortList(mid);
-        return merge(left, right);
+        return sortList(head, null);
     }
 
-    ListNode merge(ListNode list1, ListNode list2) {
-        ListNode dummyHead = new ListNode();
-        ListNode tail = dummyHead;
-        while (list1 != null && list2 != null) {
-            if (list1.val < list2.val) {
-                tail.next = list1;
-                list1 = list1.next;
-                tail = tail.next;
-            } else {
-                tail.next = list2;
-                list2 = list2.next;
-                tail = tail.next;
+    ListNode sortList(ListNode p, ListNode q) {
+        if (p == null || p == q) {
+            return p;
+        }
+        boolean sorted = true;
+        ListNode curr = p;
+        while (curr.next != null && curr != q) {
+            if (curr.val > curr.next.val) {
+                sorted = false;
+                break;
             }
+            curr = curr.next;
         }
-        tail.next = (list1 != null) ? list1 : list2;
-        return dummyHead.next;
-    }
-
-    ListNode getMid(ListNode head) {
-        ListNode midPrev = null;
-        while (head != null && head.next != null) {
-            midPrev = (midPrev == null) ? head : midPrev.next;
-            head = head.next.next;
+        if (sorted) {
+            return p;
         }
-        ListNode mid = midPrev.next;
-        midPrev.next = null;
-        return mid;
+        ListNode pre = p;
+        ListNode head = p;
+        curr = pre.next;
+        while (curr != null && curr != q) {
+            ListNode next = curr.next;
+            if (curr.val < p.val) {
+                pre.next = next;
+                curr.next = head;
+                head = curr;
+            } else {
+                pre = curr;
+            }
+            curr = next;
+        }
+        p.next = sortList(p.next, q);
+        return sortList(head, p);
     }
 }
