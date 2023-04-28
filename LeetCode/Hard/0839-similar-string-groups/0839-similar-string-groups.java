23fd3a7cd50714118cfb7406fd5abef5e952c097
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-04-28 09:11:27 +0900

    Time: 167 ms (13.36%), Space: 42.2 MB (64.12%) - LeetHub

--- LeetCode/Hard/0839-similar-string-groups/0839-similar-string-groups.java
+++ LeetCode/Hard/0839-similar-string-groups/0839-similar-string-groups.java
@@ -1,22 +1,4 @@
 class Solution {
-    public void bfs(int node, Map<Integer, List<Integer>> adj, boolean[] visit) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(node);
-        visit[node] = true;
-        while (!q.isEmpty()) {
-            node = q.poll();
-            if (!adj.containsKey(node)) {
-                continue;
-            }
-            for (int neighbor : adj.get(node)) {
-                if (!visit[neighbor]) {
-                    visit[neighbor] = true;
-                    q.offer(neighbor);
-                }
-            }
-        }
-    }
-
     public boolean isSimilar(String a, String b) {
         int diff = 0;
         for (int i = 0; i < a.length(); i++) {
@@ -29,27 +11,50 @@ class Solution {
 
     public int numSimilarGroups(String[] strs) {
         int n = strs.length;
-        Map<Integer, List<Integer>> adj = new HashMap<>();
+        UnionFind dsu = new UnionFind(n);
+        int count = n;
         // Form the required graph from the given strings array.
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                if (isSimilar(strs[i], strs[j])) {
-                    adj.computeIfAbsent(i, k -> new ArrayList<Integer>()).add(j);
-                    adj.computeIfAbsent(j, k -> new ArrayList<Integer>()).add(i);
+                if (isSimilar(strs[i], strs[j]) && dsu.find(i) != dsu.find(j)) {
+                    count--;
+                    dsu.union_set(i, j);
                 }
             }
         }
 
-        boolean[] visit = new boolean[n];
-        int count = 0;
-        // Count the number of connected components.
-        for (int i = 0; i < n; i++) {
-            if (!visit[i]) {
-                bfs(i, adj, visit);
-                count++;
-            }
-        }
-
         return count;
+    }
+}
+
+class UnionFind {
+    int[] parent;
+    int[] rank;
+
+    public UnionFind(int size) {
+        parent = new int[size];
+        for (int i = 0; i < size; i++)
+            parent[i] = i;
+        rank = new int[size];
+    }
+
+    public int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    public void union_set(int x, int y) {
+        int xset = find(x), yset = find(y);
+        if (xset == yset) {
+            return;
+        } else if (rank[xset] < rank[yset]) {
+            parent[xset] = yset;
+        } else if (rank[xset] > rank[yset]) {
+            parent[yset] = xset;
+        } else {
+            parent[yset] = xset;
+            rank[xset]++;
+        }
     }
 }
