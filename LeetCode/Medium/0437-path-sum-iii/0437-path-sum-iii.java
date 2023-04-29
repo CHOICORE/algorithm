5c58029b086203e1e7e1e63b3462c3fbb04243e5
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-04-29 10:21:26 +0900

    Time: 3 ms (98.94%), Space: 42.3 MB (62.25%) - LeetHub

--- LeetCode/Medium/0437-path-sum-iii/0437-path-sum-iii.java
+++ LeetCode/Medium/0437-path-sum-iii/0437-path-sum-iii.java
@@ -1,40 +1,42 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
-    int count;
-    Map<Long, Integer> freq;
-
-    public void dfs(TreeNode root, int targetSum, long sum) {
-        if (root == null) return;
-        
-        long tmp = sum + root.val;
-        if (freq.containsKey(tmp - targetSum)) {
-            count += freq.get(tmp - targetSum);
-        }
-        freq.put(tmp, freq.getOrDefault(tmp, 0) + 1);
-        dfs(root.left, targetSum, tmp);
-        dfs(root.right, targetSum, tmp);
-        freq.put(tmp, freq.get(tmp) - 1);
-    }
+    int total = 0;
 
     public int pathSum(TreeNode root, int targetSum) {
-        count = 0;
-        freq = new HashMap<>();
-        freq.put(0L, 1);
-        dfs(root, targetSum, 0L);
-        return count;
+        if (root == null)
+            return 0;
+        HashMap<Long, Integer> map = new HashMap<>();
+        map.put(0L, 1);
+        findSum(root, 0L, (long) targetSum, map);
+        return total;
+
+    }
+
+    private void findSum(TreeNode curr, Long sum, Long target, HashMap<Long, Integer> hm) {
+        if (curr == null) return;
+        
+        sum += curr.val;
+        if (hm.containsKey(sum - target)) {
+            total += hm.get(sum - target);
+        }
+        hm.put(sum, hm.getOrDefault(sum, 0) + 1);
+        findSum(curr.left, sum, target, hm);
+        findSum(curr.right, sum, target, hm);
+        hm.put(sum, hm.get(sum) - 1);
+        return;
     }
 }
