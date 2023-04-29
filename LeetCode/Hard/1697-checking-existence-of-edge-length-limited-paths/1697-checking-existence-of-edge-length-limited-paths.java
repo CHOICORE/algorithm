790ad9655f7d5fb1e3ecd411676c4cf80a763b31
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-04-29 10:19:38 +0900

    Time: 51 ms (100.00%), Space: 88.7 MB (28.69%) - LeetHub

--- LeetCode/Hard/1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.java
+++ LeetCode/Hard/1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.java
@@ -1,91 +1,54 @@
-class UnionFind {
-    private int[] group;
-    private int[] rank;
+class Solution {
+    int[] parent;
+    int[] rank;
+    int[] weight;
 
-    UnionFind(int size) {
-        group = new int[size];
-        rank = new int[size];
-        for (int i = 0; i < size; ++i) {
-            group[i] = i;
-        }
+    public boolean[] distanceLimitedPathsExist(int n, int[][] E, int[][] Q) {
+        parent = new int[n];
+        rank = new int[n];
+        weight = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
+
+        Arrays.sort(E, (a, b) -> a[2] - b[2]);
+        for (var e : E) union(e[0], e[1], e[2]);
+
+        boolean[] res = new boolean[Q.length];
+        for (int i = 0; i < Q.length; i++)
+            res[i] = query(Q[i][0], Q[i][1], Q[i][2]);
+
+        return res;
     }
 
-    public int find(int node) {
-        if (group[node] != node) {
-            group[node] = find(group[node]);
-        }
-        return group[node];
+    public boolean query(int p, int q, int limit) {
+        return find(p, limit) == find(q, limit);
     }
 
-    public void join(int node1, int node2) {
-        int group1 = find(node1);
-        int group2 = find(node2);
-        
-        if (group1 == group2) {
+    private int find(int x, int limit) {
+        while (x != parent[x]) {
+            if (weight[x] >= limit) {
+                break;
+            }
+            x = parent[x];
+        }
+        return x;
+    }
+
+    private void union(int x, int y, int limit) {
+        int x_ref = find(x, Integer.MAX_VALUE);
+        int y_ref = find(y, Integer.MAX_VALUE);
+        if (x_ref == y_ref) {
             return;
         }
-
-        if (rank[group1] > rank[group2]) {
-            group[group2] = group1;
-        } else if (rank[group1] < rank[group2]) {
-            group[group1] = group2;
+        if (rank[x_ref] < rank[y_ref]) {
+            parent[x_ref] = y_ref;
+            weight[x_ref] = limit;
+            ;
         } else {
-            group[group1] = group2;
-            rank[group2] += 1;
-        }
-    }
-    
-    public boolean areConnected(int node1, int node2) {
-        int group1 = find(node1);
-        int group2 = find(node2);
-        return group1 == group2;
-    }
-};
-
-class Solution {
-    private void sort(int[][] array) {
-        Arrays.sort(array, new Comparator<int[]>() {
-          @Override
-          public int compare(int[] a, int[] b) {
-            return a[2] - b[2];
-          }
-        });
-    }
-    
-    public boolean[] distanceLimitedPathsExist(int n, int[][] edgeList, int[][] queries) {
-        UnionFind uf = new UnionFind(n);
-        int queriesCount = queries.length;
-        boolean[] answer = new boolean[queriesCount];
-        
-        int[][] queriesWithIndex = new int[queriesCount][4];
-        for (int i = 0; i < queriesCount; ++i) {
-            queriesWithIndex[i][0] = queries[i][0];
-            queriesWithIndex[i][1] = queries[i][1];
-            queriesWithIndex[i][2] = queries[i][2];
-            queriesWithIndex[i][3] = i;
-        }
-        
-        sort(edgeList);
-        sort(queriesWithIndex);
-         
-        int edgesIndex = 0;
-        
-        for (int queryIndex = 0; queryIndex < queriesCount; queryIndex += 1) {
-            int p = queriesWithIndex[queryIndex][0];
-            int q = queriesWithIndex[queryIndex][1];
-            int limit = queriesWithIndex[queryIndex][2];
-            int queryOriginalIndex = queriesWithIndex[queryIndex][3];
-            
-            while (edgesIndex < edgeList.length && edgeList[edgesIndex][2] < limit) {
-                int node1 = edgeList[edgesIndex][0];
-                int node2 = edgeList[edgesIndex][1];
-                uf.join(node1, node2);
-                edgesIndex += 1;
+            parent[y_ref] = x_ref;
+            weight[y_ref] = limit;
+            if (rank[x_ref] == rank[y_ref]) {
+                rank[x_ref]++;
             }
-            
-            answer[queryOriginalIndex] = uf.areConnected(p, q);
         }
-        
-        return answer;
     }
 }
