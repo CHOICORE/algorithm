763a8340faec8f82a4ebcd8db6d06ad9a665d088
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-04-29 10:20:39 +0900

    Time: 0 ms (100.00%), Space: 42.6 MB (32.66%) - LeetHub

--- LeetCode/Easy/0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ LeetCode/Easy/0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -1,30 +1,31 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
-    public int diameterOfBinaryTree(TreeNode root) {
-        if (root == null) return 0;
-        int diameter = 0;
-        diameter = Math.max(diameter, depth(root.left) + depth(root.right));
-        diameter = Math.max(diameter, diameterOfBinaryTree(root.left));
-        diameter = Math.max(diameter, diameterOfBinaryTree(root.right));
-        return diameter;
+    public static int diameterOfBinaryTree(TreeNode root) {
+        int[] max = new int[1];
+        helperFunction(root, max);
+        return max[0];
     }
-    
-    private int depth(TreeNode node) {
-        if (node == null) return 0;
-        return Math.max(depth(node.left), depth(node.right)) + 1;
+
+    public static int helperFunction(TreeNode root, int[] max) {
+        if (root == null) return 0;
+
+        int leftHeight = helperFunction(root.left, max);
+        int rightHeight = helperFunction(root.right, max);
+        max[0] = Math.max(max[0], leftHeight + rightHeight);
+        return 1 + Math.max(leftHeight, rightHeight);
     }
 }
