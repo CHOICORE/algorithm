e3cfc2e803ff3f04ca238f0297911877a2bcc711
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-03 09:14:09 +0900

    Time: 3 ms (99.30%), Space: 44.2 MB (7.45%) - LeetHub

--- LeetCode/Medium/0210-course-schedule-ii/0210-course-schedule-ii.java
+++ LeetCode/Medium/0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,46 +1,63 @@
 class Solution {
+    boolean hasCycle = false;
+    boolean[] onPath;
+    boolean[] visited;
+    List<Integer> res = new ArrayList<>();
+
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> graph = new ArrayList<>();
-        int[] inDegree = new int[numCourses];
-        Queue<Integer> queue = new LinkedList<>();
+        List<Integer>[] graph = buildGraph(numCourses, prerequisites);
+
+        visited = new boolean[numCourses];
+        onPath = new boolean[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+
+            traverse(graph, i);
+            if (hasCycle == true) return new int[]{};
+        }
+
+
+        Collections.reverse(res);
         int[] result = new int[numCourses];
-        int index = 0;
-        
-        // 1. 그래프 초기화
-        for (int i = 0; i < numCourses; i++) {
-            graph.add(new ArrayList<>());
+        for (int i = 0; i < result.length; i++) {
+            result[i] = res.get(i);
         }
-        for (int[] prerequisite : prerequisites) {
-            int course = prerequisite[0];
-            int pre = prerequisite[1];
-            graph.get(pre).add(course);
-            inDegree[course]++;
+
+        return result;
+    }
+
+    private void traverse(List<Integer>[] graph, int v) {
+
+        if (onPath[v] == true) {
+            hasCycle = true;
         }
-        
-        // 2. 진입 차수가 0인 노드 큐에 추가
-        for (int i = 0; i < numCourses; i++) {
-            if (inDegree[i] == 0) {
-                queue.offer(i);
-            }
+
+        if (visited[v] == true || hasCycle == true) {
+            return;
         }
-        
-        // 3. 위상 정렬
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
-            result[index++] = current;
-            for (int next : graph.get(current)) {
-                inDegree[next]--;
-                if (inDegree[next] == 0) {
-                    queue.offer(next);
-                }
-            }
+
+        visited[v] = true;
+        onPath[v] = true;
+        for (int node : graph[v]) {
+            traverse(graph, node);
         }
-        
-        // 4. 모든 노드를 방문했는지 체크
-        if (index == numCourses) {
-            return result;
-        } else {
-            return new int[0];
+    
+        res.add(v);
+        onPath[v] = false;
+    }
+
+
+    private List<Integer>[] buildGraph(int n, int[][] arr) {
+        List<Integer>[] graph = new List[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<Integer>();
         }
+
+        for (int[] edge : arr) {
+            int from = edge[1];
+            int to = edge[0];
+            graph[from].add(to);
+        }
+        return graph;
     }
 }
