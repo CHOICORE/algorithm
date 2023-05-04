a4ef1cff7e7e1f20eed6b128ca2fc1be0d78457d
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-04 11:49:53 +0900

    Time: 8 ms (84.09%), Space: 42.6 MB (80.11%) - LeetHub

--- LeetCode/Medium/0649-dota2-senate/0649-dota2-senate.java
+++ LeetCode/Medium/0649-dota2-senate/0649-dota2-senate.java
@@ -1,64 +1,27 @@
 class Solution {
-
-    public boolean ban(StringBuilder senateArray, char toBan, int startAt) {
-
-        boolean loopAround = false;
-        int pointer = startAt;
-
-        while (true) {
-            if (pointer == 0) {
-                loopAround = true;
-            }
-            if (senateArray.charAt(pointer) == toBan) {
-                senateArray.deleteCharAt(pointer);
-                break;
-            }
-            pointer = (pointer + 1) % senateArray.length();
-        }
-
-        return loopAround;
-    }
-
     public String predictPartyVictory(String senate) {
+        int rCount = 0, dCount = 0;
+        boolean[] vote = new boolean[senate.length()];
 
-        StringBuilder senateArray = new StringBuilder(senate);
+        while (rCount < senate.length() && dCount < senate.length()) {
+            for (int i = 0; i < senate.length() && rCount < senate.length() && dCount < senate.length(); i++) {
+                // this voter has been banned already.
+                if (vote[i])
+                    continue;
 
-        int rCount = 0;
-        int dCount = 0;
-        for (int i = 0; i < senateArray.length(); i++) {
-            if (senateArray.charAt(i) == 'R') {
-                rCount++;
-            } else {
-                dCount++;
+                if (senate.charAt(i) == 'R') {
+                    if (rCount < dCount) vote[i] = true;
+                    rCount++;
+                } else {
+                    if (dCount < rCount) vote[i] = true;
+                    dCount++;
+                }
             }
         }
 
-        int turn = 0;
-
-        while (rCount > 0 && dCount > 0) {
-
-   
-            if (senateArray.charAt(turn) == 'R') {
-                boolean bannedSenatorBefore = ban(senateArray, 'D', (turn + 1) % senateArray.length());
-                if (bannedSenatorBefore) {
-                    turn--;
-                }
-                dCount--;
-            } else {
-                boolean bannedSenatorBefore = ban(senateArray, 'R', (turn + 1) % senateArray.length());
-                if (bannedSenatorBefore) {
-                    turn--;
-                }
-                rCount--;
-            }
-
-            turn = (turn + 1) % senateArray.length();
-        }
-
-        if (rCount > 0) {
+        if (rCount == senate.length())
             return "Radiant";
-        } else {
+        else
             return "Dire";
-        }
     }
 }
