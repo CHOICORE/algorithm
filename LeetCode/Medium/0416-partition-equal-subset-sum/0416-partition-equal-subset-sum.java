ced62e50c8dc9ed25320bc9338f4bdb335bab2ba
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-05 09:55:25 +0900

    Time: 8 ms (99.86%), Space: 42.3 MB (84.87%) - LeetHub

--- LeetCode/Medium/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ LeetCode/Medium/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,25 +1,22 @@
 class Solution {
-    public boolean canPartition(int[] nums) {
+    public static boolean canPartition(int[] nums){
+        if (nums == null || nums.length == 0){
+            return true;
+        }
+
         int sum = 0;
-        for (int num : nums) {
+        for (int num : nums){
             sum += num;
         }
-        if (sum % 2 != 0) {
+        if ((sum & 1) == 1){
             return false;
         }
-        sum /= 2;
-        boolean[][] dp = new boolean[nums.length + 1][sum + 1];
-        for (int i = 0; i <= nums.length; i++) {
-            dp[i][0] = true;
+        sum >>>= 1;
+        java.math.BigInteger map = new java.math.BigInteger("1");
+        map = map.shiftLeft(sum);
+        for (int num : nums){
+            map = map.or(map.shiftRight(num));
         }
-        for (int i = 1; i <= nums.length; i++) {
-            for (int j = 1; j <= sum; j++) {
-                dp[i][j] = dp[i - 1][j];
-                if (j >= nums[i - 1]) {
-                    dp[i][j] |= dp[i - 1][j - nums[i - 1]];
-                }
-            }
-        }
-        return dp[nums.length][sum];
+        return map.testBit(0);
     }
 }
