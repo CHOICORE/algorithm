f00393289c1f63e10d4d8e54eb74da52ca246aa6
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-06 18:40:28 +0900

    Time: 33 ms (100.00%), Space: 51.1 MB (68.24%) - LeetHub

--- LeetCode/Medium/1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ LeetCode/Medium/1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -1,42 +1,18 @@
 class Solution {
-    public static int binarySearchRightmost(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] <= target) {
-                left = mid + 1;
+    public int numSubseq(int[] A, int target) {
+        Arrays.sort(A);
+        int res = 0, n = A.length, l = 0, r = n - 1, mod = (int) 1e9 + 7;
+        int[] pows = new int[n];
+        pows[0] = 1;
+        for (int i = 1; i < n; ++i)
+            pows[i] = pows[i - 1] * 2 % mod;
+        while (l <= r) {
+            if (A[l] + A[r] > target) {
+                r--;
             } else {
-                right = mid - 1;
+                res = (res + pows[r - l++]) % mod;
             }
         }
-        return left;
-    }
-    
-    public int numSubseq(int[] nums, int target) {
-        int n = nums.length;
-        int mod = 1_000_000_007;
-        Arrays.sort(nums);
-        
-        int[] power = new int[n];
-        power[0] = 1;
-        for (int i = 1; i < n; ++i) {
-            power[i] = (power[i - 1] * 2) % mod;
-        }
-        
-        int answer = 0;
-
-        // Iterate over each left pointer.
-        for (int left = 0; left < n; left++) {
-            // Find the insertion position for `target - nums[left]`
-            // 'right' equals the insertion index minus 1.
-            int right = binarySearchRightmost(nums, target - nums[left]) - 1;
-            if (right >= left) {
-                answer += power[right - left];
-                answer %= mod;
-            }
-            
-        }
-        
-        return answer;
+        return res;
     }
 }
