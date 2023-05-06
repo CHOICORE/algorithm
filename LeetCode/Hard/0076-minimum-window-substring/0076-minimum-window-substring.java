717cc13e33780e9c26d588c2c6442c89686c9e58
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-06 18:42:38 +0900

    Time: 2 ms (100.00%), Space: 42.6 MB (84.61%) - LeetHub

--- LeetCode/Hard/0076-minimum-window-substring/0076-minimum-window-substring.java
+++ LeetCode/Hard/0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,30 +1,33 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int[] required = new int[128];
-        for (char c : t.toCharArray()) {
-            required[c]++;
-        }
-        int left = 0, right = 0, count = t.length(), minLength = Integer.MAX_VALUE, minLeft = 0;
-        while (right < s.length()) {
-            char c = s.charAt(right);
-            if (required[c] > 0) {
-                count--;
+        if (t.length() > s.length()) return "";
+
+        int[] tCounts = new int[128];
+        for (char c : t.toCharArray()) tCounts[c]++;
+
+
+        char[] sc = s.toCharArray();
+        int scLen = sc.length;
+        int neededCount = t.length();
+        int minWindowLen = Integer.MAX_VALUE;
+        int minWindowLeft = 0;
+        int minWindowPast = 0;
+        int leftIdx = 0;
+        while (leftIdx < scLen && tCounts[sc[leftIdx]] == 0) leftIdx++;
+        int pastIdx = leftIdx;
+        while (pastIdx < scLen || neededCount == 0) {
+            if (neededCount == 0) {
+                if (++tCounts[sc[leftIdx++]] > 0) neededCount++;
+            } else {
+                if (--tCounts[sc[pastIdx++]] >= 0) neededCount--;
             }
-            required[c]--;
-            while (count == 0) {
-                if (right - left + 1 < minLength) {
-                    minLength = right - left + 1;
-                    minLeft = left;
-                }
-                char d = s.charAt(left);
-                if (required[d] == 0) {
-                    count++;
-                }
-                required[d]++;
-                left++;
+            if (neededCount == 0 && pastIdx - leftIdx < minWindowLen) {
+                minWindowLen = pastIdx - leftIdx;
+                minWindowLeft = leftIdx;
+                minWindowPast = pastIdx;
             }
-            right++;
         }
-        return minLength == Integer.MAX_VALUE ? "" : s.substring(minLeft, minLeft + minLength);
+
+        return s.substring(minWindowLeft, minWindowPast);
     }
 }
