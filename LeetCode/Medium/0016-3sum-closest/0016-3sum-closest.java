c22c5e254b8cfddfdfeb08b8d839b2eb2d77b102
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-06 18:41:40 +0900

    Time: 3 ms (99.87%), Space: 42.5 MB (35.35%) - LeetHub

--- LeetCode/Medium/0016-3sum-closest/0016-3sum-closest.java
+++ LeetCode/Medium/0016-3sum-closest/0016-3sum-closest.java
@@ -1,22 +1,48 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
+        if (nums.length < 3) {
+            return 0;
+        }
         Arrays.sort(nums);
-        int n = nums.length;
-        int closestSum = nums[0] + nums[1] + nums[2];
-        for (int i = 0; i < n - 2; i++) {
-            int left = i + 1, right = n - 1;
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
-                    closestSum = sum;
+        int minDiff = Integer.MAX_VALUE, result = 0;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i - 1] == nums[i]) {
+                continue;
+            }
+            if (nums[i] * 3 >= target) {
+                int sum = nums[i] + nums[i + 1] + nums[i + 2];
+                if (sum - target < minDiff) {
+                    result = sum;
                 }
-                if (sum < target) {
-                    left++;
-                } else {
+                break;
+            }
+            int sum = nums[i] + nums[nums.length - 1] + nums[nums.length - 2];
+            if (sum <= target) {
+                if (target - sum < minDiff) {
+                    minDiff = target - sum;
+                    result = sum;
+                }
+                continue;
+            }
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                sum = nums[i] + nums[left] + nums[right];
+                if (sum == target) {
+                    return sum;
+                }
+                if (Math.abs(sum - target) < minDiff) {
+                    result = sum;
+                    minDiff = Math.abs(sum - target);
+                }
+                if (sum > target) {
                     right--;
+                } else {
+                    left++;
                 }
             }
         }
-        return closestSum;
+
+        return result;
     }
 }
