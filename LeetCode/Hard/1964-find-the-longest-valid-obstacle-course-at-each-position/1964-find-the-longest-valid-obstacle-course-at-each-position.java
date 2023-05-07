efafcb03f481b8332b502729a23cc91e9b2df2fa
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-07 09:34:31 +0900

    Time: 82 ms (98.41%), Space: 58.7 MB (26.98%) - LeetHub

--- LeetCode/Hard/1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.java
+++ LeetCode/Hard/1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.java
@@ -1,34 +1,24 @@
 class Solution {
-    List<Integer> answer;
-    private int bisectRight(int[] A, int target, int right) {
-        if (right == 0)
-            return 0;
-        int left = 0;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (A[mid] <= target)
-                left = mid + 1;
-            else
-                right = mid;
-        }
-        return left;
-    }
-    
     public int[] longestObstacleCourseAtEachPosition(int[] obstacles) {
-        int n = obstacles.length, lisLength = 0;
-        
-        int[] answer = new int[n], lis = new int[n];
-
-        for (int i = 0; i < n; ++i) {
-            int height = obstacles[i];
-            
-            int idx = bisectRight(lis, height, lisLength);
-            if (idx == lisLength)
-                lisLength++;
-
-            lis[idx] = height;
-            answer[i] = idx + 1;
+        List<Integer> l = new ArrayList<>();
+        int[] res = new int[obstacles.length];
+        for (int i = 0; i < obstacles.length; ++i) {
+            if (l.size() == 0 || obstacles[i] >= l.get(l.size() - 1)) {
+                l.add(obstacles[i]);
+                res[i] = l.size();
+                continue;
+            }
+            int lo = 0, hi = l.size() - 1;
+            while (lo < hi) {
+                int mid = (lo + hi) / 2;
+                if (l.get(mid) > obstacles[i])
+                    hi = mid;
+                else
+                    lo = mid + 1;
+            }
+            l.set(lo, obstacles[i]);
+            res[i] = lo + 1;
         }
-        return answer;
+        return res;
     }
 }
