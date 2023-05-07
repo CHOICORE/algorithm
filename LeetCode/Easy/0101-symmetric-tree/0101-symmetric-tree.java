fe3863f3d83875e1753ef9716de231098fd8d4da
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-07 09:35:38 +0900

    Time: 0 ms (100.00%), Space: 41 MB (49.98%) - LeetHub

--- LeetCode/Easy/0101-symmetric-tree/0101-symmetric-tree.java
+++ LeetCode/Easy/0101-symmetric-tree/0101-symmetric-tree.java
@@ -15,19 +15,19 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-        return isMirror(root.left, root.right);
+        return helper(root.left, root.right);
     }
-    
-    private boolean isMirror(TreeNode p, TreeNode q) {
+
+    public boolean helper(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
             return true;
         }
+
         if (p == null || q == null) {
             return false;
         }
-        return p.val == q.val && isMirror(p.left, q.right) && isMirror(p.right, q.left);
+
+        return p.val == q.val && helper(p.left, q.right)
+               && helper(p.right, q.left);
     }
 }
