8509689915816fdba203baab08e1de3a42489f5c
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-09 09:24:23 +0900

    Time: 0 ms (100.00%), Space: 44.9 MB (43.39%) - LeetHub

--- LeetCode/Medium/0057-insert-interval/0057-insert-interval.java
+++ LeetCode/Medium/0057-insert-interval/0057-insert-interval.java
@@ -1,40 +1,24 @@
 class Solution {
+
     public int[][] insert(int[][] intervals, int[] newInterval) {
         int len = intervals.length;
-        if (len == 0)
-            return new int[][]{newInterval};
-
-        int insertionPointStart = Arrays.binarySearch(intervals, 0, len, new int[]{newInterval[0], newInterval[0]}, Comparator.comparingInt(a -> a[0]));
-        insertionPointStart = Math.max(insertionPointStart, -insertionPointStart - 1);
-
-        int insertionPointEnd = Arrays.binarySearch(intervals, 0, len, new int[]{newInterval[1] + 1, newInterval[1] + 1}, Comparator.comparingInt(a -> a[0]));
-        insertionPointEnd = Math.max(insertionPointEnd, -insertionPointEnd - 1);
-
-        List<int[]> res = new ArrayList<>();
-        for (int i = 0; i < len; i++) {
-            if (i < insertionPointStart - 1 || i >= insertionPointEnd) {
-                if (i == 0 && insertionPointEnd == 0) {
-                    res.add(newInterval);
-                }
-                res.add(intervals[i]);
-            } else {
-                if (i == insertionPointStart - 1) {
-                    if (intervals[i][1] >= newInterval[0]) {
-                        newInterval[0] = intervals[i][0];
-                    } else {
-                        res.add(intervals[i]);
-                    }
-                } else if (i >= insertionPointStart) {
-                    newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
-                }
-
-                if (i == Math.max(0, insertionPointEnd - 1)) {
-                    newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
-                    res.add(newInterval);
-                }
-            }
+        int left = 0;
+        int right = len - 1;
+        while (left < len && newInterval[0] > intervals[left][1]) {
+            left++;
         }
-
-        return res.toArray(new int[1][1]);
+        while (right >= 0 && newInterval[1] < intervals[right][0]) {
+            right--;
+        }
+        int[][] res = new int[left + len - right][2];
+        for (int i = 0; i < left; i++) {
+            res[i] = Arrays.copyOf(intervals[i], intervals[i].length);
+        }
+        res[left][0] = Math.min(newInterval[0], left == len ? newInterval[0] : intervals[left][0]);
+        res[left][1] = Math.max(newInterval[1], right == -1 ? newInterval[1] : intervals[right][1]);
+        for (int i = left + 1, j = right + 1; j < len; i++, j++) {
+            res[i] = intervals[j];
+        }
+        return res;
     }
 }
