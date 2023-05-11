79313ce850f60c37ebb7df63af7e624292fa44af
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-11 09:13:09 +0900

    Time: 8 ms (26.05%), Space: 42.1 MB (67.74%) - LeetHub

--- LeetCode/Medium/1035-uncrossed-lines/1035-uncrossed-lines.java
+++ LeetCode/Medium/1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -1,31 +1,51 @@
 class Solution {
-    private int solve(int i, int j, int[] nums1, int[] nums2, int[][] memo) {
-        if (i <= 0 || j <= 0) {
+    public static int ansBU(int[] arr1, int[] arr2) {
+        int[][] dp = new int[arr1.length + 1][arr2.length + 1];
+
+        for (int i = arr1.length - 1; i >= 0; i--) {
+            for (int j = arr2.length - 1; j >= 0; j--) {
+                if (arr1[i] == arr2[j]) {
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+
+    public static int ansTD(int[] arr1, int[] arr2, int i, int j, int[][] dp) {
+        if (i >= arr1.length || j >= arr2.length) {
             return 0;
-        }
-
-        if (memo[i][j] != -1) {
-            return memo[i][j];
-        }
-
-        if (nums1[i - 1] == nums2[j - 1]) {
-            memo[i][j] = 1 + solve(i - 1, j - 1, nums1, nums2, memo);
+        } else if (dp[i][j] != -1) {
+            return dp[i][j];
         } else {
-            memo[i][j] =
-                Math.max(solve(i, j - 1, nums1, nums2, memo), solve(i - 1, j, nums1, nums2, memo));
+            if (arr1[i] == arr2[j]) {
+                return 1 + ansTD(arr1, arr2, i + 1, j + 1, dp);
+            } else {
+                return dp[i][j] = Math.max(ansTD(arr1, arr2, i + 1, j, dp), ansTD(arr1, arr2, i, j + 1, dp));
+            }
         }
-        return memo[i][j];
+    }
+
+    public static int ans(int[] arr1, int[] arr2, int i, int j) {
+        if (i >= arr1.length || j >= arr2.length) {
+            return 0;
+        } else {
+            if (arr1[i] == arr2[j]) {
+                return 1 + ans(arr1, arr2, i + 1, j + 1);
+            } else {
+                return Math.max(ans(arr1, arr2, i + 1, j), ans(arr1, arr2, i, j + 1));
+            }
+        }
     }
 
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
 
-        int[][] memo = new int[n1 + 1][n2 + 1];
-        for (int[] row : memo) {
-            Arrays.fill(row, -1);
+        int[][] dp = new int[nums1.length][nums2.length];
+        for (int[] arr : dp) {
+            Arrays.fill(arr, -1);
         }
-
-        return solve(n1, n2, nums1, nums2, memo);
+        return ansTD(nums1, nums2, 0, 0, dp);
     }
 }
