fb9fa741920d4cd9b402f795e6d1a8bb8de27e61
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-11 09:13:26 +0900

    Time: 6 ms (42.43%), Space: 42.3 MB (41.94%) - LeetHub

--- LeetCode/Medium/1035-uncrossed-lines/1035-uncrossed-lines.java
+++ LeetCode/Medium/1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -1,51 +1,20 @@
 class Solution {
-    public static int ansBU(int[] arr1, int[] arr2) {
-        int[][] dp = new int[arr1.length + 1][arr2.length + 1];
-
-        for (int i = arr1.length - 1; i >= 0; i--) {
-            for (int j = arr2.length - 1; j >= 0; j--) {
-                if (arr1[i] == arr2[j]) {
-                    dp[i][j] = 1 + dp[i + 1][j + 1];
-                } else {
-                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
-                }
-            }
-        }
-        return dp[0][0];
-    }
-
-    public static int ansTD(int[] arr1, int[] arr2, int i, int j, int[][] dp) {
-        if (i >= arr1.length || j >= arr2.length) {
-            return 0;
-        } else if (dp[i][j] != -1) {
-            return dp[i][j];
-        } else {
-            if (arr1[i] == arr2[j]) {
-                return 1 + ansTD(arr1, arr2, i + 1, j + 1, dp);
-            } else {
-                return dp[i][j] = Math.max(ansTD(arr1, arr2, i + 1, j, dp), ansTD(arr1, arr2, i, j + 1, dp));
-            }
-        }
-    }
-
-    public static int ans(int[] arr1, int[] arr2, int i, int j) {
-        if (i >= arr1.length || j >= arr2.length) {
-            return 0;
-        } else {
-            if (arr1[i] == arr2[j]) {
-                return 1 + ans(arr1, arr2, i + 1, j + 1);
-            } else {
-                return Math.max(ans(arr1, arr2, i + 1, j), ans(arr1, arr2, i, j + 1));
-            }
-        }
-    }
 
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
-
-        int[][] dp = new int[nums1.length][nums2.length];
-        for (int[] arr : dp) {
-            Arrays.fill(arr, -1);
+        int n = nums1.length;
+        int m = nums2.length;
+        int[][] dp = new int[n + 1][m + 1];
+        int ans = 0;
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+                ans = Math.max(ans, dp[i][j]);
+            }
         }
-        return ansTD(nums1, nums2, 0, 0, dp);
+        return ans;
     }
 }
