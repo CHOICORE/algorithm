7de985584bd85309b1cfd3ee1c5069d472e57880
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-12 09:34:13 +0900

    Time: 7 ms (63.49%), Space: 93.2 MB (92.59%) - LeetHub

--- LeetCode/Medium/2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
+++ LeetCode/Medium/2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
@@ -1,21 +1,18 @@
 class Solution {
     public long mostPoints(int[][] questions) {
         int n = questions.length;
-        long[][] dp = new long[n][2];
-        dp[n - 1][0] = 0;
-        dp[n - 1][1] = questions[n - 1][0];
-        long sol = questions[n - 1][0];
-        for (int i = n - 2; i >= 0; i--) {
-            dp[i][0] = Math.max(dp[i + 1][0], dp[i + 1][1]);
-            int back = i + questions[i][1] + 1;
-            if (back < n) {
-                dp[i][1] = Math.max(dp[back][0], dp[back][1]) + questions[i][0];
-            } else {
-                dp[i][1] = questions[i][0];
-            }
-            sol = Math.max(dp[i][0], dp[i][1]);
-        }
-        return sol;
+        long[] dp = new long[n];
 
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = questions[i][0];
+            if (i + questions[i][1] + 1 < n) {
+                dp[i] = Math.max(dp[i], (long) questions[i][0] + dp[i + questions[i][1] + 1]);
+            }
+            if (i + 1 < n) {
+                dp[i] = Math.max(dp[i], dp[i + 1]);
+            }
+        }
+
+        return dp[0];
     }
 }
