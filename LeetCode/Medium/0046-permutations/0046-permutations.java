184d45995c35790183e38379c96af9fddd2d8b6a
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-12 09:38:27 +0900

    Time: 3 ms (10.23%), Space: 43.3 MB (8.08%) - LeetHub

--- LeetCode/Medium/0046-permutations/0046-permutations.java
+++ LeetCode/Medium/0046-permutations/0046-permutations.java
@@ -1,32 +1,34 @@
-class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> finalList = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        helper(0, list, finalList, nums);
-        return finalList;
+public class Solution {
+    LinkedHashSet<List<Integer>> al;
+    List<List<Integer>> al1;
+    int n;
 
-    }
+    void permutate(List<Integer> a, int index) {
 
-    public void helper(int index, List<Integer> list, List<List<Integer>> finalList, int[] nums) {
-        if (index == nums.length) {
-            for (int i = 0; i < nums.length; i++) {
-                list.add(nums[i]);
-            }
-            finalList.add(new ArrayList<>(list));
-            list.clear();
-            return;
-        }
-        for (int i = index; i < nums.length; i++) {
+        for (int i = index; i < n; i++) {
+            List<Integer> b = new ArrayList<Integer>(a);
+            int temp = b.get(index);
+            b.set(index, b.get(i));
+            b.set(i, temp);
+            al.add(b);
+            permutate(b, index + 1);
 
-            swap(i, index, nums);
-            helper(index + 1, list, finalList, nums);
-            swap(i, index, nums);
         }
     }
 
-    public void swap(int i, int j, int[] arr) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    public List<List<Integer>> permute(int[] a) {
+        ArrayList<Integer> A = new ArrayList<>();
+        for (int i : a) {
+            A.add(i);
+        }
+        al = new LinkedHashSet<>();
+        n = A.size();
+        permutate(A, 0);
+        al1 = new ArrayList<>();
+        for (List<Integer> integers : al) {
+            List<Integer> l = new ArrayList<>(integers);
+            al1.add(l);
+        }
+        return al1;
     }
 }
