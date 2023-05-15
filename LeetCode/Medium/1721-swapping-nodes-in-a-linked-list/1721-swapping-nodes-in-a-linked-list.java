e7ff735f28d2b4b384906c6fc3541bcf19775fcc
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-15 09:50:50 +0900

    Time: 2 ms (99.56%), Space: 57.3 MB (77.49%) - LeetHub

--- LeetCode/Medium/1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ LeetCode/Medium/1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -10,44 +10,23 @@
  */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-
-        if (head == null || head.next == null) return head;
-
-        ListNode len = head;
-        int length = 0;
-
-        // calculate length
-        while (len != null) {
-            len = len.next;
-            length++;
+        ListNode temp = head;
+        ListNode rtemp = head;
+        ListNode ctemp = head;
+        int cnt = 1;
+        while (cnt < k) {
+            rtemp = rtemp.next;
+            ctemp = ctemp.next;
+            cnt++;
         }
-
-
-        ListNode curr1 = head;
-        ListNode curr2 = head;
-
-        int l = length - k;
-
-        //finding left node
-        while (k-- > 1) {
-            curr1 = curr1.next;
+        while (rtemp.next != null) {
+            temp = temp.next;
+            rtemp = rtemp.next;
         }
-
-        // finding right node
-        while (l-- > 0) {
-            curr2 = curr2.next;
-        }
-
-        // edge cases like k is the middle node then no need to swap
-        if (curr1 == curr2) return head;
-
-
-        // Swapping Values of both indexes
-
-        curr1.val = curr1.val + curr2.val;
-        curr2.val = curr1.val - curr2.val;
-        curr1.val = curr1.val - curr2.val;
-
+        int d = temp.val;
+        temp.val = ctemp.val;
+        ctemp.val = d;
         return head;
+
     }
 }
