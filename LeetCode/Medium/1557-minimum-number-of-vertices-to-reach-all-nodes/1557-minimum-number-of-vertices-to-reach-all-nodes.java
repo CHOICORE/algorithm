c397279062641d11775625ee293bf35daac9ed52
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-18 09:02:47 +0900

    Time: 10 ms (96.80%), Space: 79.2 MB (63.22%) - LeetHub

--- LeetCode/Medium/1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
+++ LeetCode/Medium/1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
@@ -1,17 +1,20 @@
 class Solution {
-  public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
-    boolean[] isIncomingEdgeExists = new boolean[n];
-    for (List<Integer> edge : edges) {
-      isIncomingEdgeExists[edge.get(1)] = true;
-    }
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+        List<Integer> ans = new ArrayList<>();
+        int[] check = new int[n];
+        for (int i = 0; i < n; i++) {
+            check[i] = 0;
+        }
+        for (List<Integer> l : edges) {
+            check[l.get(1)]++;
+        }
+        for (int i = 0; i < n; i++) {
+            if (check[i] == 0) {
+                ans.add(i);
+            }
+        }
+        return ans;
 
-    List<Integer> requiredNodes = new ArrayList();
-    for (int i = 0; i < n; i++) {
-      if (!isIncomingEdgeExists[i]) {
-        requiredNodes.add(i);
-      }
-    }
 
-    return requiredNodes;
-  }
+    }
 }
