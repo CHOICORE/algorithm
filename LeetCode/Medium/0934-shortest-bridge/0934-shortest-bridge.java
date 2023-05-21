b1e43ddea09d6417569b2468726f7a7d465dc9e7
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-21 10:56:49 +0900

    Time: 8 ms (86.74%), Space: 44.4 MB (5.97%) - LeetHub

--- LeetCode/Medium/0934-shortest-bridge/0934-shortest-bridge.java
+++ LeetCode/Medium/0934-shortest-bridge/0934-shortest-bridge.java
@@ -1,8 +1,10 @@
 class Solution {
+
+    int[][] directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+
     public int shortestBridge(int[][] grid) {
         int n = grid.length;
         int firstX = -1, firstY = -1;
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
@@ -12,54 +14,41 @@ class Solution {
                 }
             }
         }
-
         List<int[]> bfsQueue = new ArrayList<>();
-        List<int[]> secondBfsQueue = new ArrayList<>();
-        bfsQueue.add(new int[]{firstX, firstY});
-        secondBfsQueue.add(new int[]{firstX, firstY});
-        grid[firstX][firstY] = 2;
-
-        while (!bfsQueue.isEmpty()) {
-            List<int[]> newBfs = new ArrayList<>();
-            for (int[] cell : bfsQueue) {
-                int x = cell[0];
-                int y = cell[1];
-                for (int[] next : new int[][]{{x + 1, y}, {x - 1, y}, {x, y + 1}, {x, y - 1}}) {
-                    int curX = next[0];
-                    int curY = next[1];
-                    if (curX >= 0 && curX < n && curY >= 0 && curY < n && grid[curX][curY] == 1) {
-                        newBfs.add(new int[]{curX, curY});
-                        secondBfsQueue.add(new int[]{curX, curY});
-                        grid[curX][curY] = 2;
-                    }
-                }
-            }
-            bfsQueue = newBfs;
-        }
+        dfs(grid, firstX, firstY, n, bfsQueue);
 
         int distance = 0;
-        while (!secondBfsQueue.isEmpty()) {
-            List<int[]> newBfs = new ArrayList<>();
-            for (int[] cell : secondBfsQueue) {
-                int x = cell[0];
-                int y = cell[1];
-                for (int[] next : new int[][]{{x + 1, y}, {x - 1, y}, {x, y + 1}, {x, y - 1}}) {
-                    int curX = next[0];
-                    int curY = next[1];
-                    if (curX >= 0 && curX < n && curY >= 0 && curY < n) {
-                        if (grid[curX][curY] == 1) {
+        while (!bfsQueue.isEmpty()) {
+            List<int[]> newBFSQueue = new ArrayList<>();
+            for (int[] pair : bfsQueue) {
+                for (int[] dir : directions) {
+                    int x = pair[0] + dir[0], y = pair[1] + dir[1];
+                    if (x >= 0 && x < n && y >= 0 && y < n) {
+                        if (grid[x][y] == 1) {
                             return distance;
-                        } else if (grid[curX][curY] == 0) {
-                            newBfs.add(new int[]{curX, curY});
-                            grid[curX][curY] = -1;
+                        } else if (grid[x][y] == 0) {
+                            int[] water = {x, y};
+                            newBFSQueue.add(water);
+                            grid[x][y] = -1;
                         }
                     }
                 }
             }
-
-            secondBfsQueue = newBfs;
+            bfsQueue = newBFSQueue;
             distance++;
         }
         return distance;
+    }
+
+    private void dfs(int[][] grid, int x, int y, int n, List<int[]> bfsQueue) {
+        grid[x][y] = 2;
+        bfsQueue.add(new int[]{x, y});
+        for (int[] dir : directions) {
+            int newX = x + dir[0];
+            int newY = y + dir[1];
+            if (newX >= 0 && newX < n && newY >= 0 && newY < n && grid[newX][newY] == 1) {
+                dfs(grid, newX, newY, n, bfsQueue);
+            }
+        }
     }
 }
