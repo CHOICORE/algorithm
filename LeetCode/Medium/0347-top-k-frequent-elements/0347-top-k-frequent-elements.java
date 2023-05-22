2efe7f58b566b542483ab546eb0bf677878eaf53
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-22 09:21:12 +0900

    Time: 11 ms (94.75%), Space: 48.1 MB (5.00%) - LeetHub

--- LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,52 +1,24 @@
 class Solution {
-        class Pair {
-            private Integer freq;
-            private Integer value;
+    public int[] topKFrequent(int[] nums, int k) {
+        List<Integer> bucket[] = new List[nums.length + 1];
+        Map<Integer, Integer> map = new HashMap<>();
 
-            public Pair(Integer freq, Integer value) {
-                this.freq = freq;
-                this.value = value;
-            }
+        for (int n : nums) map.put(n, map.getOrDefault(n, 0) + 1);
 
-            @Override
-            public String toString() {
-                return "Pair{" +
-                        "freq=" + freq +
-                        ", value=" + value +
-                        '}';
-            }
+        for (int key : map.keySet()) {
+            int frequency = map.get(key);
+            if (bucket[frequency] == null) bucket[frequency] = new ArrayList<Integer>();
+            bucket[frequency].add(key);
         }
-
-        public int[] topKFrequent(int[] nums, int k) {
-            final int[] res = new int[k];
-            if (nums == null) {
-                return nums;
-            }
-            if (nums.length == 1 && k == 1) {
-                return nums;
-            }
-
-            Map<Integer, Integer> map = new HashMap<>();
-            Queue<Pair> heap = new PriorityQueue<>((o1, o2) -> o1.freq.compareTo(o2.freq));
-            for (int i : nums) {
-                if (map.containsKey(i)) {
-                    map.put(i, map.get(i) + 1);
-                } else {
-                    map.put(i, 1);
+        int[] ans = new int[k];
+        int index = 0;
+        for (int i = bucket.length - 1; i >= 0 && index < k; i--) {
+            if (bucket[i] != null) {
+                for (Integer integer : bucket[i]) {
+                    ans[index++] = integer;
                 }
             }
-            map.forEach((key, value) -> {
-                heap.offer(new Pair(value, key));
-                if (heap.size() > k) {
-                    heap.poll();
-                }
-            });
-
-            int i = 0;
-            while (!heap.isEmpty()) {
-                res[i] = heap.poll().value;
-                i++;
-            }
-            return res;
         }
+        return ans;
     }
+}
