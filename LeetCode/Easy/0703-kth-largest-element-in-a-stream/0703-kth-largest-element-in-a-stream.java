b2b499939313c9abc8c868519dffba5341c4bc9a
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-23 09:44:13 +0900

    Time: 12 ms (100.00%), Space: 47.2 MB (16.97%) - LeetHub

--- LeetCode/Easy/0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
+++ LeetCode/Easy/0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -1,62 +1,89 @@
 class KthLargest {
-    private List<Integer> heap;
-    private int k;
+    int[] a;
+    int size = 0;
+    int k = 0;
 
-    KthLargest(int k, int[] nums) {
-        this.k = k;
-        this.heap = new ArrayList<>(k);
+    private int left(int i) {
+        return i * 2;
+    }
 
-        for (int n : nums)
-            add(n);
+    private int right(int i) {
+        return i * 2 + 1;
+    }
+
+    private int parent(int i) {
+        return i / 2;
+    }
+
+    private void swap(int i, int j) {
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
     }
 
     private void siftdown(int i) {
-        int n = heap.size();
-        int left = (2 * i) + 1, right = (2 * i) + 2;
-        while ((left < n && heap.get(i) > heap.get(left))
-                || (right < n && heap.get(i) > heap.get(right))) {
-            int smallest = right >= n || heap.get(left) < heap.get(right) ? left : right;
-            Collections.swap(heap, i, smallest);
-            i = smallest;
-            left = (2 * i) + 1;
-            right = (2 * i) + 2;
+        siftdown(i, a.length);
+    }
+
+    private void siftdown(int i, int n) {
+        while (2 * i < n) {
+            int j = i;
+            if (a[2 * i] < a[j]) {
+                j = 2 * i;
+            }
+            if (2 * i + 1 < n && a[2 * i + 1] < a[j]) {
+                j = 2 * i + 1;
+            }
+            if (i == j) {
+                break;
+            }
+            swap(i, j);
+            i = j;
         }
     }
 
     private void siftup(int i) {
-        int parent = (i - 1) / 2;
-        while (i != 0 && heap.get(i) < heap.get(parent)) {
-            Collections.swap(heap, i, parent);
-            i = parent;
-            parent = (i - 1) / 2;
+        while (i / 2 >= 1) {
+            if (a[i / 2] <= a[i]) {
+                break;
+            }
+            swap(i / 2, i);
+            i = i / 2;
         }
     }
 
-    int peek() {
-        return heap.get(0);
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        a = new int[k + 1];
+        heapifyAndInsert(nums);
     }
 
-    int poll() {
-        Collections.swap(heap, 0, heap.size() - 1);
-        int min = heap.remove(heap.size() - 1);
-        siftdown(0);
-        return min;
-    }
-
-    void offer(int val) {
-        heap.add(val);
-        siftup(heap.size() - 1);
+    private void heapifyAndInsert(int[] nums) {
+        size = Math.min(nums.length, k);
+        for (int i = 0; i < size; i++) {
+            a[i + 1] = nums[i];
+        }
+        int currK = Math.min(nums.length, k);
+        for (int i = currK / 2; i >= 1; i--) {
+            siftdown(i, currK + 1);
+        }
+        for (int i = size; i < nums.length; i++) {
+            add(nums[i]);
+        }
     }
 
     public int add(int val) {
-        if (k > heap.size())
-            offer(val);
-        else if (val > peek()) {
-            poll();
-            offer(val);
+        if (size == k) {
+            if (val > a[1]) {
+                a[1] = val;
+                siftdown(1);
+            }
+        } else {
+            size++;
+            a[size] = val;
+            siftup(size);
         }
-
-        return peek();
+        return a[1];
     }
 }
 
