76cf97a0012d38a53abbd2e3c96b62924d2c7d19
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-23 09:42:32 +0900

    Time: 21 ms (12.35%), Space: 47.1 MB (25.33%) - LeetHub

--- LeetCode/Easy/0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
+++ LeetCode/Easy/0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -1,27 +1,62 @@
 class KthLargest {
-    private static int k;
-    private PriorityQueue<Integer> heap;
+    private List<Integer> heap;
+    private int k;
 
-    public KthLargest(int k, int[] nums) {
+    KthLargest(int k, int[] nums) {
         this.k = k;
-        heap = new PriorityQueue<>();
+        this.heap = new ArrayList<>(k);
 
-        for (int num : nums) {
-            heap.offer(num);
-        }
+        for (int n : nums)
+            add(n);
+    }
 
-        while (heap.size() > k) {
-            heap.poll();
+    private void siftdown(int i) {
+        int n = heap.size();
+        int left = (2 * i) + 1, right = (2 * i) + 2;
+        while ((left < n && heap.get(i) > heap.get(left))
+                || (right < n && heap.get(i) > heap.get(right))) {
+            int smallest = right >= n || heap.get(left) < heap.get(right) ? left : right;
+            Collections.swap(heap, i, smallest);
+            i = smallest;
+            left = (2 * i) + 1;
+            right = (2 * i) + 2;
         }
     }
 
+    private void siftup(int i) {
+        int parent = (i - 1) / 2;
+        while (i != 0 && heap.get(i) < heap.get(parent)) {
+            Collections.swap(heap, i, parent);
+            i = parent;
+            parent = (i - 1) / 2;
+        }
+    }
+
+    int peek() {
+        return heap.get(0);
+    }
+
+    int poll() {
+        Collections.swap(heap, 0, heap.size() - 1);
+        int min = heap.remove(heap.size() - 1);
+        siftdown(0);
+        return min;
+    }
+
+    void offer(int val) {
+        heap.add(val);
+        siftup(heap.size() - 1);
+    }
+
     public int add(int val) {
-        heap.offer(val);
-        if (heap.size() > k) {
-            heap.poll();
+        if (k > heap.size())
+            offer(val);
+        else if (val > peek()) {
+            poll();
+            offer(val);
         }
 
-        return heap.peek();
+        return peek();
     }
 }
 
