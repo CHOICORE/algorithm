2e565f5b406e60729e576a4ec1c2f9f49bc3fa42
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-24 12:51:36 +0900

    Time: 82 ms (98.85%), Space: 57.7 MB (71.26%) - LeetHub

--- LeetCode/Medium/2542-maximum-subsequence-score/2542-maximum-subsequence-score.java
+++ LeetCode/Medium/2542-maximum-subsequence-score/2542-maximum-subsequence-score.java
@@ -1,28 +1,47 @@
 class Solution {
     public long maxScore(int[] nums1, int[] nums2, int k) {
-        int n = nums1.length;
-        int[][] pairs = new int[n][2];
-        for (int i = 0; i < n; ++i) {
-            pairs[i] = new int[]{nums1[i], nums2[i]};
+        Pair[] arr = new Pair[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            arr[i] = new Pair(nums1[i], nums2[i]);
         }
-        Arrays.sort(pairs, (a, b) -> b[1] - a[1]);
-
-        PriorityQueue<Integer> topKHeap = new PriorityQueue<>(k, (a, b) -> a - b);
-        long topKSum = 0;
-        for (int i = 0; i < k; ++i) {
-            topKSum += pairs[i][0];
-            topKHeap.add(pairs[i][0]);
+        Arrays.sort(arr, new Comparator<Pair>() {
+            public int compare(Pair p1, Pair p2) {
+                return p2.n2 - p1.n2;
+            }
+        });
+        long ans = -1;
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        long s = 0;
+        for (int i = 0; i < k; i++) {
+            q.add(arr[i].n1);
+            s += arr[i].n1;
         }
+        ans = Math.max(ans, (s * arr[k - 1].n2));
 
-        long answer = topKSum * pairs[k - 1][1];
+        for (int i = k; i < arr.length; i++) {
+            if (arr[i].n1 > q.peek()) {
+                int r = q.remove();
+                s = s - r;
+                q.add(arr[i].n1);
+                s += arr[i].n1;
+            }
+            ans = Math.max(ans, (s * arr[i].n2));
 
-        for (int i = k; i < n; ++i) {
-            topKSum += pairs[i][0] - topKHeap.poll();
-            topKHeap.add(pairs[i][0]);
 
-            answer = Math.max(answer, topKSum * pairs[i][1]);
         }
+        return ans;
 
-        return answer;
+
+    }
+}
+
+class Pair {
+    int n1;
+    int n2;
+
+    Pair(int n1, int n2) {
+        this.n1 = n1;
+        this.n2 = n2;
+
     }
 }
