caa86c56064c0883ef85d263832db19877d625a1
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-26 09:03:53 +0900

    Time: 3 ms (100.00%), Space: 41.8 MB (92.66%) - LeetHub

--- LeetCode/Medium/1140-stone-game-ii/1140-stone-game-ii.java
+++ LeetCode/Medium/1140-stone-game-ii/1140-stone-game-ii.java
@@ -1,37 +1,23 @@
 class Solution {
-
     public int stoneGameII(int[] piles) {
-        int n = piles.length;
-        int[][][] memo = new int[2][n + 1][n + 1];
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j <= n; j++) {
-                Arrays.fill(memo[i][j], -1);
-            }
-        }
-        return play(piles, 0, 0, 1, memo);
+        int len = piles.length;
+        int[] preSum = Arrays.copyOf(piles, len);
+        for (int i = len - 2; i >= 0; i--)
+            preSum[i] += preSum[i + 1];
+        return dfs(preSum, 1, 0, new int[len][len]);
     }
 
-    private int play(int[] piles, int player, int index, int M, int[][][] memo) {
-        if (index == piles.length) {
-            return 0;
+    private int dfs(int[] preSum, int m, int p, int[][] memo) {
+        if (p + 2 * m >= preSum.length)
+            return preSum[p];
+        if (memo[p][m] > 0)
+            return memo[p][m];
+        int result = 0, take = 0;
+        for (int i = 1; i <= 2 * m; i++) {
+            take = preSum[p] - preSum[p + i];
+            result = Math.max(result, take + preSum[p + i] - dfs(preSum, Math.max(i, m), p + i, memo));
         }
-
-        if (memo[player][index][M] != -1) {
-            return memo[player][index][M];
-        }
-
-        int score = player == 0 ? 0 : Integer.MAX_VALUE;
-        int rocksGained = 0;
-        for (int x = 1; x <= Math.min(piles.length - index, 2 * M); x++) {
-            rocksGained += piles[index + x - 1];
-            if (player == 0) {
-                score = Math.max(score, rocksGained + play(piles, 1 - player, index + x, Math.max(x, M), memo));
-            } else {
-                score = Math.min(score, play(piles, 1 - player, index + x, Math.max(x, M), memo));
-            }
-        }
-        memo[player][index][M] = score;
-        return score;
+        memo[p][m] = result;
+        return result;
     }
-
 }
