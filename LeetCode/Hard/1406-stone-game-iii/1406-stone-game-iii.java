77713ef997f99e6aaf7cacfb58ae63e3017e4ce8
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-27 11:05:07 +0900

    Time: 22 ms (52.26%), Space: 55.6 MB (45.23%) - LeetHub

--- LeetCode/Hard/1406-stone-game-iii/1406-stone-game-iii.java
+++ LeetCode/Hard/1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,14 +1,16 @@
 class Solution {
     public String stoneGameIII(int[] stoneValue) {
         int n = stoneValue.length;
-        int[] dp = new int [n + 1];
+        int[] dp = new int [4];
         for (int i = n - 1; i >= 0; i--) {
-            dp[i] = stoneValue[i] - dp[i + 1];
+            dp[i % 4] = stoneValue[i] - dp[(i + 1) % 4];
             if (i + 2 <= n) {
-                dp[i] = Math.max(dp[i], stoneValue[i] + stoneValue[i + 1] - dp[i + 2]);
+                dp[i % 4] = Math.max(dp[i % 4], stoneValue[i] + stoneValue[i + 1]
+                    - dp[(i + 2) % 4]);
             }
             if (i + 3 <= n) {
-                dp[i] = Math.max(dp[i], stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - dp[i + 3]);
+                dp[i % 4] = Math.max(dp[i % 4], stoneValue[i] + stoneValue[i + 1]
+                    + stoneValue[i + 2] - dp[(i + 3) % 4]);
             }
         }
         if (dp[0] > 0) {
