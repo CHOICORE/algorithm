13eb386d31c851d59cb89ab8dea888bd9d47f331
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-05-29 10:20:34 +0900

    Time: 9 ms (56.91%), Space: 44.6 MB (6.49%) - LeetHub

--- LeetCode/Easy/1603-design-parking-system/1603-design-parking-system.java
+++ LeetCode/Easy/1603-design-parking-system/1603-design-parking-system.java
@@ -1,14 +1,25 @@
 class ParkingSystem {
-
-    int[] empty;
+    int small;
+    int medium;
+    int big;
 
     public ParkingSystem(int big, int medium, int small) {
-        empty = new int[]{big, medium, small};
+        this.small = small;
+        this.medium = medium;
+        this.big = big;
     }
 
     public boolean addCar(int carType) {
-        return empty[carType - 1]-- > 0;
-
+        if (carType == 3) {
+            small--;
+            return small >= 0;
+        } else if (carType == 2) {
+            medium--;
+            return medium >= 0;
+        } else {
+            big--;
+            return big >= 0;
+        }
     }
 }
 /**
