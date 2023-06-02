60771e24dfb05a350f47866bc3807421e180e47e
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-06-02 09:30:10 +0900

    Time: 11 ms (99.74%), Space: 44 MB (9.80%) - LeetHub

--- LeetCode/Medium/2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.java
+++ LeetCode/Medium/2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.java
@@ -1,41 +1,46 @@
 class Solution {
     public int maximumDetonation(int[][] bombs) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        int n = bombs.length;
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int xi = bombs[i][0], yi = bombs[i][1], ri = bombs[i][2];
-                int xj = bombs[j][0], yj = bombs[j][1];
-                
-                if ((long)ri * ri >= (long)(xi - xj) * (xi - xj) + (long)(yi - yj) * (yi - yj)) {
-                    graph.computeIfAbsent(i, k -> new ArrayList<>()).add(j);
-                }
-            }
+        if (bombs == null || bombs.length == 0) return 0;
+        List<List<Integer>> graph = new ArrayList<>();
+        int len = bombs.length;
+        for (int i = 0; i < len; i++) {
+            graph.add(new ArrayList<Integer>());
+            build(i, bombs, graph);
         }
-        
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            answer = Math.max(answer, bfs(i, graph));
+        int res = 0;
+        for (int i = 0; i < len; i++) {
+            res = Math.max(res, dfs(i, graph, new boolean[len]));
+            if (res == len) return res;
         }
-        
-        return answer;
+        return res;
     }
-    
-    private int bfs(int i, Map<Integer, List<Integer>> graph) {
-        Deque<Integer> queue = new ArrayDeque<>();
-        Set<Integer> visited = new HashSet<>();
-        queue.offer(i);
-        visited.add(i);
-        while (!queue.isEmpty()) {
-            int cur = queue.poll();
-            for (int neib : graph.getOrDefault(cur, new ArrayList<>())) {
-                if (!visited.contains(neib)) {
-                    visited.add(neib);
-                    queue.offer(neib);
-                }
+
+    private int dfs(int start, List<List<Integer>> graph, boolean[] seen) {
+        if (seen[start]) return 0;
+        seen[start] = true;
+        int count = 1;
+        for (int next : graph.get(start)) {
+            count += dfs(next, graph, seen);
+        }
+        return count;
+    }
+
+    private void build(int idx, int[][] bombs, List<List<Integer>> graph) {
+        int x = bombs[idx][0];
+        int y = bombs[idx][1];
+        long r = bombs[idx][2];
+        for (int i = 0; i < bombs.length; i++) {
+            if (i == idx) continue;
+            int x1 = bombs[i][0];
+            int y1 = bombs[i][1];
+            if (dis(x1, y1, x, y) <= r * r) {
+                graph.get(idx).add(i);
             }
         }
-        return visited.size();
+        return;
+    }
+
+    private long dis(int x2, int y2, int x1, int y1) {
+        return (long) (x2 - x1) * (x2 - x1) + (long) (y2 - y1) * (y2 - y1);
     }
 }
