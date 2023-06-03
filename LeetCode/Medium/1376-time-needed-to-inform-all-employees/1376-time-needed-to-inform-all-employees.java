5546b984e49245d2870b9c193e5e0739b02bb525
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-06-03 14:53:18 +0900

    Time: 71 ms (68.53%), Space: 61.6 MB (47.93%) - LeetHub

--- LeetCode/Medium/1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
+++ LeetCode/Medium/1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -1,28 +1,37 @@
 class Solution {
-    int maxTime = Integer.MIN_VALUE;
-
-    void DFS(ArrayList<ArrayList<Integer>> adjList, int[] informTime, int curr, int time) {
-        maxTime = Math.max(maxTime, time);
-
-        for (int adjacent : adjList.get(curr)) {
-            DFS(adjList, informTime, adjacent, time + informTime[curr]);
-        }
-    }
-
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-        ArrayList<ArrayList<Integer>> adjList = new ArrayList<ArrayList<Integer>>(n);
+        List<Integer>[] arr = new ArrayList[n];
+        int ans = 0;
 
         for (int i = 0; i < n; i++) {
-            adjList.add(new ArrayList<Integer>());
+            arr[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < n; i++) {
             if (manager[i] != -1) {
-                adjList.get(manager[i]).add(i);
+                arr[manager[i]].add(i);
             }
         }
 
-        DFS(adjList, informTime, headID, 0);
-        return maxTime;
+        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+        q.offer(new Pair<>(headID, informTime[headID]));
+
+        while (!q.isEmpty()) {
+            int si = q.size();
+
+            for (int i = 0; i < si; i++) {
+                Pair<Integer, Integer> t = q.poll();
+
+                for (int x : arr[t.getKey()]) {
+                    if (informTime[x] == 0) {
+                        ans = Math.max(ans, t.getValue());
+                    } else {
+                        q.offer(new Pair<>(x, t.getValue() + informTime[x]));
+                    }
+                }
+            }
+        }
+
+        return ans;
     }
 }
