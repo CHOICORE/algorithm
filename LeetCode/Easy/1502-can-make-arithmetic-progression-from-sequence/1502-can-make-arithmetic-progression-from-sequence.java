7cf0fd42d73b1ee8e8e8aa97eb9677b740ce6ae7
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-06-06 13:19:26 +0900

    Time: 2 ms (81.19%), Space: 40.9 MB (88.83%) - LeetHub

--- LeetCode/Easy/1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
+++ LeetCode/Easy/1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -1,11 +1,9 @@
 class Solution {
-	public boolean canMakeArithmeticProgression(int[] nums) {
-		Arrays.sort(nums);
-		for (int i = 2; i < nums.length; i++) {
-			if (nums[i] - nums[i - 1] != nums[i - 1] - nums[i - 2]) {
-				return false;
-			}
-		}
-		return true;
-	}
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+        if (arr.length <= 2) return true;
+        int sum = arr[1] - arr[0];
+        for (int i = 0; i < arr.length - 1; i++) if (arr[i + 1] - arr[i] != sum) return false;
+        return true;
+    }
 }
