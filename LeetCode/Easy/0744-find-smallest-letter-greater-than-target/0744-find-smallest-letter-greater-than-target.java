84288e48e4eb9280d6abfe0b1fc1fcfa2023476d
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-06-09 09:29:04 +0900

    Time: 2 ms (100.00%), Space: 41.7 MB (99.58%) - LeetHub

--- LeetCode/Easy/0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
+++ LeetCode/Easy/0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
@@ -1,10 +1,21 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        for (char letter : letters) {
-            if (letter > target) {
-                return letter;
+        int l = 0;
+        int n = letters.length;
+        int h = n - 1;
+        char ans = letters[0];
+
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+
+            if (letters[mid] <= target) {
+                l = mid + 1;
+            } else {
+                ans = letters[mid];
+                h = mid - 1;
             }
         }
-        return letters[0];
+        System.gc();
+        return ans;
     }
 }
