8e766fc0a50097fee66b12dbd9096f17ce5d57e5
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-06-11 22:09:14 +0900

    Time: 40 ms (100.00%), Space: 81.8 MB (39.14%) - LeetHub

--- LeetCode/Medium/1146-snapshot-array/1146-snapshot-array.java
+++ LeetCode/Medium/1146-snapshot-array/1146-snapshot-array.java
@@ -1,30 +1,48 @@
 class SnapshotArray {
-    private List<Integer>[] arr;
-    private int snap;
+
+    List<int[]> listOfArray = new ArrayList<>();
+    int[] inArray;
+    int length = 0;
+    int size = 1;
+    boolean needsNewSnapshot = false;
 
     public SnapshotArray(int length) {
-        this.arr = new List[length];
-        this.snap = 0;
+        inArray = new int[length];
+        this.length = length;
     }
 
     public void set(int index, int val) {
-        if (arr[index] == null)
-            arr[index] = new ArrayList<>();
-        while (arr[index].size() < snap)
-            arr[index].add(arr[index].isEmpty() ? 0 : arr[index].get(arr[index].size() - 1));
-        if (arr[index].size() > snap)
-            arr[index].set(snap, val);
-        else arr[index].add(val);
+        inArray[index] = val;
+        // System.out.println("set "+Arrays.toString(inArray));
+        size = Math.max(size, index + 1);
+        needsNewSnapshot = true;
     }
 
     public int snap() {
-        return snap++;
+        int[] snap = null;
+        if (needsNewSnapshot) {
+            snap = new int[size];
+            System.arraycopy(inArray, 0, snap, 0, size);
+        } else {
+            snap = listOfArray.size() > 0 ? listOfArray.get(listOfArray.size() - 1) : new int[0];
+        }
+        listOfArray.add(snap);
+        needsNewSnapshot = false;
+        return listOfArray.size() - 1;
     }
 
     public int get(int index, int snap_id) {
-        if (arr[index] == null)
-            return 0;
-        return arr[index].size() <= snap_id ? arr[index].get(arr[index].size() - 1) : arr[index].get(snap_id);
+        int[] snapshot = listOfArray.get(snap_id);
+        // for(int[] in : listOfArray){
+        //     System.out.println("snap: "+Arrays.toString(in));
+        // }
+        // System.out.println(Arrays.toString(snapshot));
+        // System.out.println(Arrays.toString(inArray));
+
+        if (index < snapshot.length) {
+            return snapshot[index];
+        }
+        return 0;
     }
 }
 
