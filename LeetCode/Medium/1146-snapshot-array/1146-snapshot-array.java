485939eead449dea734094d7100567607202e2e3
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-06-11 22:08:25 +0900

    Time: 48 ms (99.86%), Space: 76 MB (74.14%) - LeetHub

--- LeetCode/Medium/1146-snapshot-array/1146-snapshot-array.java
+++ LeetCode/Medium/1146-snapshot-array/1146-snapshot-array.java
@@ -1,24 +1,37 @@
 class SnapshotArray {
-    int snapId = 0;
-    TreeMap<Integer, Integer>[] historyRecords;
-    
+    private List<Integer>[] arr;
+    private int snap;
+
     public SnapshotArray(int length) {
-        historyRecords = new TreeMap[length];
-        for (int i = 0; i < length; i++) {
-            historyRecords[i] = new TreeMap<Integer, Integer>();
-            historyRecords[i].put(0, 0);
-        }
+        this.arr = new List[length];
+        this.snap = 0;
     }
 
     public void set(int index, int val) {
-        historyRecords[index].put(snapId, val);
+        if (arr[index] == null)
+            arr[index] = new ArrayList<>();
+        while (arr[index].size() < snap)
+            arr[index].add(arr[index].isEmpty() ? 0 : arr[index].get(arr[index].size() - 1));
+        if (arr[index].size() > snap)
+            arr[index].set(snap, val);
+        else arr[index].add(val);
     }
 
     public int snap() {
-        return snapId++;
+        return snap++;
     }
 
-    public int get(int index, int snapId) {
-        return historyRecords[index].floorEntry(snapId).getValue();
+    public int get(int index, int snap_id) {
+        if (arr[index] == null)
+            return 0;
+        return arr[index].size() <= snap_id ? arr[index].get(arr[index].size() - 1) : arr[index].get(snap_id);
     }
 }
+
+/**
+ * Your SnapshotArray object will be instantiated and called as such:
+ * SnapshotArray obj = new SnapshotArray(length);
+ * obj.set(index,val);
+ * int param_2 = obj.snap();
+ * int param_3 = obj.get(index,snap_id);
+ */
