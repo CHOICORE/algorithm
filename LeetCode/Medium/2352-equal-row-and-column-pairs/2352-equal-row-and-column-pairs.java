eab25660e530426f48d94d62551968e3ea2dabea
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-06-13 09:08:22 +0900

    Time: 4 ms (97.41%), Space: 47.8 MB (89.14%) - LeetHub

--- LeetCode/Medium/2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ LeetCode/Medium/2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,37 +1,22 @@
 class Solution {
     public int equalPairs(int[][] grid) {
-
-        int n = grid.length;
-        long[] rowSums = new long[n];
-        long[] colSums = new long[n];
-
-        for (int i = 0; i < n; i++) {
-            long sum = 0;
-            for (int j = 0; j < n; j++) {
-                sum = sum * 10 + grid[i][j];
+        HashMap<Long, Integer> rcs = new HashMap<>();
+        int same = 0;
+        for (int i = 0; i < grid.length; i++) {
+            long row = 0;
+            for (int j = 0; j < grid[0].length; j++) {
+                row = row * 10 + grid[i][j];
             }
-            rowSums[i] = sum;
+            rcs.put(row, rcs.getOrDefault(row, 0) + 1);
         }
 
-        for (int i = 0; i < n; i++) {
-            long sum = 0;
-            for (int j = 0; j < n; j++) {
-                sum = sum * 10 + grid[j][i];
+        for (int j = 0; j < grid[0].length; j++) {
+            long col = 0;
+            for (int i = 0; i < grid.length; i++) {
+                col = col * 10 + grid[i][j];
             }
-            colSums[i] = sum;
+            same += rcs.getOrDefault(col, 0);
         }
-
-        int count = 0;
-
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (rowSums[i] == colSums[j]) {
-                    count++;
-                }
-            }
-        }
-
-        return count;
+        return same;
     }
 }
