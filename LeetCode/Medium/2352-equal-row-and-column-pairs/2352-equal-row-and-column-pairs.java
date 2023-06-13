65b70289eeb9766efa70a52c83ebd1f88effe00c
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-06-13 09:09:42 +0900

    Time: 10 ms (94.10%), Space: 52.7 MB (13.52%) - LeetHub

--- LeetCode/Medium/2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ LeetCode/Medium/2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,37 +1,19 @@
 class Solution {
     public int equalPairs(int[][] grid) {
-
         int n = grid.length;
-        long[] rowSums = new long[n];
-        long[] colSums = new long[n];
-
+        int m = grid[0].length;
+        int[][] reverse = new int[m][n];
         for (int i = 0; i < n; i++) {
-            long sum = 0;
-            for (int j = 0; j < n; j++) {
-                sum = sum * 10 + grid[i][j];
-            }
-            rowSums[i] = sum;
-        }
-
-        for (int i = 0; i < n; i++) {
-            long sum = 0;
-            for (int j = 0; j < n; j++) {
-                sum = sum * 10 + grid[j][i];
-            }
-            colSums[i] = sum;
-        }
-
-        int count = 0;
-
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (rowSums[i] == colSums[j]) {
-                    count++;
-                }
+            for (int j = 0; j < m; j++) {
+                reverse[j][i] = grid[i][j];
             }
         }
-
-        return count;
+        int ans = 0;
+        for (int[] row : grid) {
+            for (int[] col : reverse) {
+                if (Arrays.equals(row, col)) ans++;
+            }
+        }
+        return ans;
     }
 }
