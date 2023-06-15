55ad619b92180407c6a605e40203f73930c9b3a2
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-06-15 10:26:06 +0900

    Time: 11 ms (27.57%), Space: 46.6 MB (16.74%) - LeetHub

--- LeetCode/Medium/1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
+++ LeetCode/Medium/1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
@@ -14,33 +14,42 @@
  * }
  */
 class Solution {
-    int[] sum = new int[10_001];
-    int levels;
-
     public int maxLevelSum(TreeNode root) {
-        levels = 1;
-        visit(root, 1);
-        int max = Integer.MIN_VALUE;
+        if (root == null) {
+            return 0;
+        }
+        int curSum = 0;
+        int maxSum = root.val;
+        int curLevel = 1;
         int maxLevel = 1;
-        for (int level = 1; level <= levels; level++) {
-            if (sum[level] > max) {
-                max = sum[level];
-                maxLevel = level;
+        int curNodes = 1;
+        int nextNodes = 0;
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode cur = q.poll();
+            curNodes--;
+            curSum += cur.val;
+            if (cur.left != null) {
+                q.add(cur.left);
+                nextNodes++;
+            }
+            if (cur.right != null) {
+                q.add(cur.right);
+                nextNodes++;
+            }
+            if (curNodes == 0) {
+                // System.out.println("level: "+curLevel+" | sum: "+curSum);
+                if (maxSum < curSum) {
+                    maxSum = curSum;
+                    maxLevel = curLevel;
+                }
+                curNodes = nextNodes;
+                nextNodes = 0;
+                curSum = 0;
+                curLevel++;
             }
         }
         return maxLevel;
-    }
-
-    private void visit(TreeNode node, int level) {
-        if (level > levels) {
-            levels = level;
-        }
-        sum[level] += node.val;
-        if (node.left != null) {
-            visit(node.left, level + 1);
-        }
-        if (node.right != null) {
-            visit(node.right, level + 1);
-        }
     }
 }
