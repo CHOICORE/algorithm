00844aab258194bbe017f3ecfc8307815e17018e
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-06-16 09:04:13 +0900

    Time: 138 ms (64.06%), Space: 54.4 MB (75.00%) - LeetHub

--- LeetCode/Hard/1569-number-of-ways-to-reorder-array-to-get-same-bst/1569-number-of-ways-to-reorder-array-to-get-same-bst.java
+++ LeetCode/Hard/1569-number-of-ways-to-reorder-array-to-get-same-bst/1569-number-of-ways-to-reorder-array-to-get-same-bst.java
@@ -1,42 +1,54 @@
-class Solution {
-    private long mod = (long)1e9 + 7;
-    private long[][] table;
+import java.math.BigInteger;
 
-    public int numOfWays(int[] nums) {
-        int m = nums.length;
-        
-        // Table of Pascal's triangle
-        table = new long[m][m];
-        for (int i = 0; i < m; ++i) {
-            table[i][0] = table[i][i] = 1;
+class Solution {
+    static int MOD = 1_000_000_007;
+    static Map<Long, Long> F = new HashMap();
+
+    static {
+        F.put(1L, 1L);
+        for (var i = 2L; i < 100_001L; i++) {
+            F.put(i, (F.get(i - 1) * i) % MOD);
         }
-        for (int i = 2; i < m; i++) {
-            for (int j = 1; j < i; j++) {
-                table[i][j] = (table[i - 1][j - 1] + table[i - 1][j]) % mod;
-            }
-        }
-        List<Integer> arrList = Arrays.stream(nums).boxed().collect(Collectors.toList());
-        return (int)((dfs(arrList) - 1) % mod);
     }
 
-    private long dfs(List<Integer> nums) {
-        int m = nums.size();
-        if (m < 3) {
-            return 1;
-        }
+    Map<Integer, Set<Integer>> G = new HashMap();
 
-        List<Integer> leftNodes = new ArrayList<>();
-        List<Integer> rightNodes = new ArrayList<>();
-        for (int i = 1; i < m; ++i) {
-            if (nums.get(i) < nums.get(0)) {
-                leftNodes.add(nums.get(i));
-            } else {
-                rightNodes.add(nums.get(i));
+    static long inv(long n) {
+        return BigInteger.valueOf(F.get(n)).modInverse(BigInteger.valueOf(MOD)).longValue();
+    }
+
+    static long nCk(long n, long k) {
+        return n <= k ? 1 : F.get(n) * inv(k) % MOD * inv(n - k) % MOD;
+    }
+
+    int numOfWays(int[] A) {
+        build(-1, A);
+        return (int) post(A[0])[1] - 1;
+    }
+
+    void build(int p, int[] A) {
+        if (0 < A.length) {
+            var A_i = A[0];
+            if (p != -1) {
+                G.computeIfAbsent(p, __ -> new HashSet()).add(A_i);
             }
+            build(A_i, Arrays.stream(A).filter(A_j -> A_i > A_j).toArray());
+            build(A_i, Arrays.stream(A).filter(A_j -> A_i < A_j).toArray());
         }
-        long leftWays = dfs(leftNodes) % mod;
-        long rightWays = dfs(rightNodes) % mod;
+    }
 
-        return (((leftWays * rightWays) % mod) * table[m - 1][leftNodes.size()]) % mod;
+    long[] post(int u) {
+        long[] ustate = {1, 1};
+        for (var v : G.getOrDefault(u, new HashSet<>())) {
+            var vstate = post(v);
+            ustate[0] += vstate[0];
+            ustate[0] %= MOD;
+
+            ustate[1] *= vstate[1];
+            ustate[1] %= MOD;
+            ustate[1] *= nCk(ustate[0] - 1, vstate[0]);
+            ustate[1] %= MOD;
+        }
+        return ustate;
     }
 }
