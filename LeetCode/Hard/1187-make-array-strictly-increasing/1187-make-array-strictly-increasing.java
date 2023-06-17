3ee6a776c7c2a2d19dcb1278d26476d8759e7bd6
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-06-17 11:54:45 +0900

    Time: 20 ms (90.48%), Space: 41.4 MB (97.96%) - LeetHub

--- LeetCode/Hard/1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.java
+++ LeetCode/Hard/1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.java
@@ -1,42 +1,77 @@
 class Solution {
     public int makeArrayIncreasing(int[] arr1, int[] arr2) {
-        Map<Integer, Integer> dp = new HashMap<>();
-        Arrays.sort(arr2);
-        int n = arr2.length;
-        dp.put(-1, 0);
+        int L1 = arr1.length, L2 = arr2.length;
+        int[][] stack = new int[L1 + 1][2];
+        stack[0][0] = -1000;
+        stack[0][1] = 0;
+        int top = 0;
 
-        for (int i = 0; i < arr1.length; i++) {
-            Map<Integer, Integer> newDp = new HashMap<>();
-            for (int prev : dp.keySet()) {
-                if (arr1[i] > prev) {
-                    newDp.put(arr1[i], Math.min(newDp.getOrDefault(arr1[i], Integer.MAX_VALUE), dp.get(prev)));
+        int[][] tmp = new int[L1 + 1][2];
+        int t2 = -1;
+
+        Arrays.sort(arr2);
+
+        for (int k : arr1) {
+
+            int i2 = 0, j = 0;
+            while (i2 < L2 && j <= top) {
+                int cur = arr2[i2];
+
+                if (cur <= stack[j][0]) {
+                    ++i2;
+                    continue;
                 }
-                int idx = bisectRight(arr2, prev);
-                if (idx < n) {
-                    newDp.put(arr2[idx], Math.min(newDp.getOrDefault(arr2[idx], Integer.MAX_VALUE), 1 + dp.get(prev)));
-                }
+
+                while (j + 1 <= top && cur > stack[j + 1][0]) ++j;
+                ++t2;
+                tmp[t2][0] = cur;
+                tmp[t2][1] = stack[j][1] + 1;
+
+                ++i2;
+                ++j;
             }
-            dp = newDp;
-        }
-        
-        int answer = Integer.MAX_VALUE;
-        for (int value : dp.values()) {
-            answer = Math.min(answer, value);
-        }
-        
-        return answer == Integer.MAX_VALUE ? -1 : answer;
-    }
-    
-    private static int bisectRight(int[] arr, int value) {
-        int left = 0, right = arr.length;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (arr[mid] <= value) {
-                left = mid + 1;
+
+            while (top >= 0 && stack[top][0] >= k) --top;
+            if (top >= 0) {
+                int x = stack[top][1];
+
+                int u = 0;
+                top = -1;
+
+                while (u <= t2 && tmp[u][0] < k) {
+                    ++top;
+                    stack[top][0] = tmp[u][0];
+                    stack[top][1] = tmp[u][1];
+                    ++u;
+                }
+
+                ++top;
+                stack[top][0] = k;
+                stack[top][1] = x;
+
+                while (u <= t2 && tmp[u][1] >= x) ++u;
+
+                while (u <= t2) {
+                    ++top;
+                    stack[top][0] = tmp[u][0];
+                    stack[top][1] = tmp[u][1];
+                    ++u;
+                }
             } else {
-                right = mid;
+                // top==-1
+                for (int u = 0; u <= t2; u++) {
+                    ++top;
+                    stack[top][0] = tmp[u][0];
+                    stack[top][1] = tmp[u][1];
+                }
+
             }
+
+            t2 = -1;
+
+            if (top < 0) break;
         }
-        return left;
+
+        return top >= 0 ? stack[top][1] : -1;
     }
 }
