c4269fe09b46c7f28e2e4ec26c3a9cd669dfa780
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-06-18 11:55:32 +0900

    Time: 32 ms (100.00%), Space: 59.7 MB (48.92%) - LeetHub

--- LeetCode/Hard/2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.java
+++ LeetCode/Hard/2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.java
@@ -1,42 +1,46 @@
 class Solution {
-    public int countPaths(int[][] grid) {
-        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        int m = grid.length, n = grid[0].length;
-        int mod = 1_000_000_007;
-        
-        int[][] dp = new int[m][n];
-        Arrays.stream(dp).forEach(row -> Arrays.fill(row, 1));
-        
-        int[][] cellList = new int[m * n][2];
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                int index = i * n + j;
-                cellList[index][0] = i;
-                cellList[index][1] = j;
-            }      
-        }
-        Arrays.sort(cellList, (a, b) -> grid[a[0]][a[1]] - grid[b[0]][b[1]]);
-        
-        for (int[] cell : cellList) {
-            int i = cell[0], j = cell[1];
+    private int[][] matrix;
+    private int rowLength, columnLength;
+    private int[][] dp;
+    private final int mod = (int) (1e9 + 7);
 
-            for (int[] d : directions) {
-                int currI = i + d[0], currJ = j + d[1];
-                if (0 <= currI && currI < m && 0 <= currJ && currJ < n
-                   && grid[currI][currJ] > grid[i][j]) {
-                    dp[currI][currJ] += dp[i][j];
-                    dp[currI][currJ] %= mod;
-                }
+    public int longestIncreasingPath(int row, int col) {
+        int longestPath = 1;
+        if (dp[row][col] > 0) {
+            return dp[row][col];
+        }
+        if (row + 1 < this.rowLength && this.matrix[row + 1][col] > this.matrix[row][col]) {
+            longestPath = (longestPath + longestIncreasingPath(row + 1, col)) % mod;
+        }
+
+        if (row - 1 >= 0 && this.matrix[row - 1][col] > this.matrix[row][col]) {
+            longestPath = (longestPath + longestIncreasingPath(row - 1, col)) % mod;
+        }
+
+        if (col + 1 < this.columnLength && this.matrix[row][col + 1] > this.matrix[row][col]) {
+            longestPath = (longestPath + longestIncreasingPath(row, col + 1)) % mod;
+        }
+
+        if (col - 1 >= 0 && this.matrix[row][col - 1] > this.matrix[row][col]) {
+            longestPath = (longestPath + longestIncreasingPath(row, col - 1)) % mod;
+        }
+
+        dp[row][col] = longestPath;
+        return longestPath;
+    }
+
+    public int countPaths(int[][] grid) {
+        this.matrix = grid;
+        this.rowLength = matrix.length;
+        this.columnLength = matrix[0].length;
+        this.dp = new int[this.rowLength][this.columnLength];
+
+        int ans = 0;
+        for (int i = 0; i < this.rowLength; i++) {
+            for (int j = 0; j < this.columnLength; j++) {
+                ans = (ans + longestIncreasingPath(i, j)) % mod;
             }
         }
-        
-        int answer = 0;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                answer += dp[i][j];
-                answer %= mod;
-            }
-        }
-        return answer;
+        return ans;
     }
 }
