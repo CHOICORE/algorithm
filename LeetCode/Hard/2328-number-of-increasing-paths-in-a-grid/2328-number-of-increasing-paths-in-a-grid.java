0efe2e231a838cc0b568c38a16637e3fca5f6f0d
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-06-18 11:56:26 +0900

    Time: 34 ms (97.12%), Space: 59.8 MB (47.48%) - LeetHub

--- LeetCode/Hard/2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.java
+++ LeetCode/Hard/2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.java
@@ -1,46 +1,46 @@
 class Solution {
-    private int[][] matrix;
-    private int rowLength, columnLength;
-    private int[][] dp;
-    private final int mod = (int) (1e9 + 7);
-
-    public int longestIncreasingPath(int row, int col) {
-        int longestPath = 1;
-        if (dp[row][col] > 0) {
-            return dp[row][col];
-        }
-        if (row + 1 < this.rowLength && this.matrix[row + 1][col] > this.matrix[row][col]) {
-            longestPath = (longestPath + longestIncreasingPath(row + 1, col)) % mod;
-        }
-
-        if (row - 1 >= 0 && this.matrix[row - 1][col] > this.matrix[row][col]) {
-            longestPath = (longestPath + longestIncreasingPath(row - 1, col)) % mod;
-        }
-
-        if (col + 1 < this.columnLength && this.matrix[row][col + 1] > this.matrix[row][col]) {
-            longestPath = (longestPath + longestIncreasingPath(row, col + 1)) % mod;
-        }
-
-        if (col - 1 >= 0 && this.matrix[row][col - 1] > this.matrix[row][col]) {
-            longestPath = (longestPath + longestIncreasingPath(row, col - 1)) % mod;
-        }
-
-        dp[row][col] = longestPath;
-        return longestPath;
-    }
+    int mod = 1_000_000_007;
 
     public int countPaths(int[][] grid) {
-        this.matrix = grid;
-        this.rowLength = matrix.length;
-        this.columnLength = matrix[0].length;
-        this.dp = new int[this.rowLength][this.columnLength];
-
-        int ans = 0;
-        for (int i = 0; i < this.rowLength; i++) {
-            for (int j = 0; j < this.columnLength; j++) {
-                ans = (ans + longestIncreasingPath(i, j)) % mod;
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] dp = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        int ans = n * m;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                ans += real(i, j, n, m, grid, dp);
+                ans %= mod;
             }
         }
         return ans;
+
+    }
+
+    int real(int i, int j, int n, int m, int[][] grid, int[][] dp) {
+
+        if (i < 0 || i >= n || j < 0 || j >= m) {
+            return 0;
+        }
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int c = 0;
+        if (i - 1 >= 0 && grid[i][j] < grid[i - 1][j]) {
+            c += 1 + real(i - 1, j, n, m, grid, dp);
+        }
+        if (j - 1 >= 0 && grid[i][j] < grid[i][j - 1]) {
+            c += 1 + real(i, j - 1, n, m, grid, dp);
+        }
+        if (i + 1 < n && grid[i][j] < grid[i + 1][j]) {
+            c += 1 + real(i + 1, j, n, m, grid, dp);
+        }
+        if (j + 1 < m && grid[i][j] < grid[i][j + 1]) {
+            c += 1 + real(i, j + 1, n, m, grid, dp);
+        }
+        c %= mod;
+        return dp[i][j] = c;
+
     }
 }
