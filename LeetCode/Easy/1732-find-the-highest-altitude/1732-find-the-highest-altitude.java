fd3dbe72c97b8e07c00c8a4d71586a47a69b9380
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-06-19 09:08:21 +0900

    Time: 0 ms (100.00%), Space: 40.6 MB (33.84%) - LeetHub

--- LeetCode/Easy/1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ LeetCode/Easy/1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -2,13 +2,12 @@ class Solution {
     public int largestAltitude(int[] gain) {
         int sum = 0;
         int max = 0;
-        int i = 0;
-        while (i < gain.length) {
+        for (int i = 0; i < gain.length; i++) {
             sum += gain[i];
             if (sum > max) {
                 max = sum;
             }
-            i++;
+
         }
         return max;
 
