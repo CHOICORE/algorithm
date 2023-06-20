b0554d1e18750bbc1b463d3c6782dde66836f657
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-06-20 09:16:08 +0900

    Time: 9 ms (97.74%), Space: 59.6 MB (84.60%) - LeetHub

--- LeetCode/Medium/2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
+++ LeetCode/Medium/2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -1,29 +1,34 @@
 class Solution {
     public int[] getAverages(int[] nums, int k) {
+        int left = 0;
+        int right = k * 2 + 1;
+        int len = nums.length;
+        int[] ans = new int[len];
+        long cur = 0;
+
+        Arrays.fill(ans, -1);
+
         if (k == 0) {
             return nums;
         }
 
-        int n = nums.length;
-        int[] averages = new int[n];
-        Arrays.fill(averages, -1);
-
-        if (2 * k + 1 > n) {
-            return averages;
+        if ((k * 2 + 1) > len) {
+            return ans;
         }
 
-        long[] prefix = new long[n + 1];
-        for (int i = 0; i < n; ++i) {
-            prefix[i + 1] = prefix[i] + nums[i];
+        for (int i = 0; i < (k * 2 + 1); i++) {
+            cur += nums[i]; 
         }
 
-        for (int i = k; i < (n - k); ++i) {
-            int leftBound = i - k, rightBound = i + k;
-            long subArraySum = prefix[rightBound + 1] - prefix[leftBound];
-            int average = (int) (subArraySum / (2 * k + 1));
-            averages[i] = average;
-        }
+        ans[k] = (int) (cur / (k * 2 + 1));
 
-        return averages;
+        for (int i = right; i < len; i++) {
+            cur -= nums[i - (k * 2 + 1)];
+            cur += nums[i];
+            ans[i - k] = (int) (cur / (k * 2 + 1));
+        }
+        return ans;
     }
 }
+
+
