7811514ac270a091d5b4d6843259283c1bf89e6a
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-06-21 09:27:39 +0900

    Time: 12 ms (100.00%), Space: 55 MB (78.46%) - LeetHub

--- LeetCode/Hard/2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.java
+++ LeetCode/Hard/2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.java
@@ -1,31 +1,37 @@
 class Solution {
-    
+    int[] nums;
+    int[] cost;
+
     public long minCost(int[] nums, int[] cost) {
-        int l = Integer.MAX_VALUE;
-        int r = Integer.MIN_VALUE;
-        for (int i : nums) {
-            l = Math.min(l, i);
-            r = Math.max(r, i);
-        }
-        long ans = 0;
-        while (l < r) {
-            int m = l + (r - l) / 2;
-            long cost1 = find(nums, cost, m);
-            long cost2 = find(nums, cost, m + 1);
-            ans = Math.min(cost1, cost2);
-            if (cost1 <= cost2) {
-                r = m;
-            } else {
-                l = m + 1;
-            }
-        }
-        return ans;
+        this.nums = nums;
+        this.cost = cost;
+        int val = bs(0, 1_000_000);
+        return find(val);
     }
 
-    long find(int[] nums, int[] cost, int target) {
-        long abs = 0;
-        for (int i = 0; i < nums.length; ++i)
-            abs += Math.abs(nums[i] - target) * (long) cost[i];
-        return abs;
+    int bs(int st, int e) {
+
+        int mid = (st + e + 1) / 2;
+        if (st == e) {
+            return st;
+        }
+        long cmid = find(mid);
+        long pmid = find(mid - 1);
+
+
+        if (cmid >= pmid) {
+            return bs(st, mid - 1);
+        }
+
+        return bs(mid, e);
+
+    }
+
+    long find(int val) {
+        long ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ans += (long) Math.abs(nums[i] - val) * cost[i];
+        }
+        return ans;
     }
 }
