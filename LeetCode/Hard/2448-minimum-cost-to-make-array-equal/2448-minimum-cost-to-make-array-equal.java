6e7f01432cc5e8158ba2a6c84df5989343887581
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-06-21 09:13:39 +0900

    Time: 14 ms (98.46%), Space: 55.2 MB (69.23%) - LeetHub

--- LeetCode/Hard/2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.java
+++ LeetCode/Hard/2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.java
@@ -1,31 +1,31 @@
 class Solution {
     
-    private long getCost(int[] nums, int[] cost, int base) {
-        long result = 0L;
-        for (int i = 0; i < nums.length; ++i)
-            result += 1L * Math.abs(nums[i] - base) * cost[i];
-        return result;
-    }
-    
     public long minCost(int[] nums, int[] cost) {
-        int left = 1000001, right = 0;
-        for (int num : nums) {
-            left = Math.min(left, num);
-            right = Math.max(right, num);
+        int l = Integer.MAX_VALUE;
+        int r = Integer.MIN_VALUE;
+        for (int i : nums) {
+            l = Math.min(l, i);
+            r = Math.max(r, i);
         }
-        long answer = getCost(nums, cost, nums[0]);
-        
-        while (left < right) {
-            int mid = (right + left) / 2;
-            long cost1 = getCost(nums, cost, mid);
-            long cost2 = getCost(nums, cost, mid + 1);
-            answer = Math.min(cost1, cost2);
-            
-            if (cost1 > cost2) 
-                left = mid + 1;
-            else
-                right = mid;
+        long ans = 0;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            long cost1 = find(nums, cost, m);
+            long cost2 = find(nums, cost, m + 1);
+            ans = Math.min(cost1, cost2);
+            if (cost1 <= cost2) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
         }
-        return answer;
+        return ans;
+    }
+
+    long find(int[] nums, int[] cost, int target) {
+        long abs = 0;
+        for (int i = 0; i < nums.length; ++i)
+            abs += Math.abs(nums[i] - target) * (long) cost[i];
+        return abs;
     }
 }
