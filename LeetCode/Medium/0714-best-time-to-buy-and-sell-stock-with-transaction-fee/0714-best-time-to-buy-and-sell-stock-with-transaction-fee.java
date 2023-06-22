e034a6ee4d0ac9148592bab52acc9dfef1d1ef70
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-06-22 14:23:55 +0900

    Time: 4 ms (99.03%), Space: 54.4 MB (65.07%) - LeetHub

--- LeetCode/Medium/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ LeetCode/Medium/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,14 +1,16 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
-        int n = prices.length;
-        int free = 0, hold = -prices[0];
-        
-        for (int i = 1; i < n; i++) {
-            int tmp = hold;
-            hold = Math.max(hold, free - prices[i]);
-            free = Math.max(free, tmp + prices[i] - fee);
+        int buy = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            } else if (prices[i] > buy + fee) {
+                profit += prices[i] - buy - fee;
+                buy = prices[i] - fee;
+            }
         }
-        
-        return free;
+        return profit;
     }
 }
