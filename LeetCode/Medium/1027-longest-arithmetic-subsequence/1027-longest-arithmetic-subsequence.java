918eb5f637fdca0ea1aa3460e3c9dca4ecca5646
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-06-23 16:10:16 +0900

    Time: 38 ms (98.67%), Space: 54.1 MB (79.66%) - LeetHub

--- LeetCode/Medium/1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.java
+++ LeetCode/Medium/1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.java
@@ -1,19 +1,17 @@
 class Solution {
-    public int longestArithSeqLength(int[] nums) {
-        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
-        for (int num : nums) {
-            min = Math.min(min, num);
-            max = Math.max(max, num);
-        }
-        int diff = max - min, ans = 0;
-        for (int d = -diff; d <= diff; d++) {
-            int[] dp = new int[max + 1];
-            for (int num : nums) {
-                int prev = num - d;
-                dp[num] = prev >= min && prev <= max ? dp[prev] + 1 : 1;
-                ans = Math.max(ans, dp[num]);
+    public static int longestArithSeqLength(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][1001];
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                int diff = nums[j] - nums[i] + 500;
+                dp[j][diff] = dp[i][diff] + 1;
+
+                res = Math.max(res, dp[j][diff]);
             }
         }
-        return ans;
+        return res + 1;
     }
 }
