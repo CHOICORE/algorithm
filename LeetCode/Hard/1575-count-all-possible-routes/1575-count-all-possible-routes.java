6ac0db35cd9ca406d91390b2f07ec036aeaa7810
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-06-25 13:05:42 +0900

    Time: 13 ms (100.00%), Space: 43 MB (57.33%) - LeetHub

--- LeetCode/Hard/1575-count-all-possible-routes/1575-count-all-possible-routes.java
+++ LeetCode/Hard/1575-count-all-possible-routes/1575-count-all-possible-routes.java
@@ -1,24 +1,57 @@
 class Solution {
-    public  int countRoutes(int[] locations, int start, int finish, int fuel) {
-        int n = locations.length;
-        int dp[][] = new int[n][fuel + 1];
-        Arrays.fill(dp[finish], 1);
+    private static final int modulo = (int) 1e9 + 7;
 
-        int ans = 0;
-        for (int j = 0; j <= fuel; j++) {
-            for (int i = 0; i < n; i++) {
-                for (int k = 0; k < n; k++) {
-                    if (k == i) {
-                        continue;
-                    }
-                    if (Math.abs(locations[i] - locations[k]) <= j) {
-                        dp[i][j] = (dp[i][j] + dp[k][j - Math.abs(locations[i] - locations[k])]) %
-                                   1_000_000_007;
-                    }
+    public int countRoutes(int[] locations, int start, int finish, int fuel) {
+        int n = locations.length;
+        int startPosition = locations[start];
+        int finishPosition = locations[finish];
+
+        Arrays.sort(locations);
+
+        for (int i = 0; i < n; ++i) {
+
+            if (startPosition == locations[i]) {
+                start = i;
+            }
+
+            if (finishPosition == locations[i]) {
+                finish = i;
+            }
+        }
+
+        int[][] dpLeft = new int[n][fuel + 1];
+        int[][] dpRight = new int[n][fuel + 1];
+        dpLeft[start][0] = dpRight[start][0] = 1;
+
+        for (int used = 0; used <= fuel; ++used) {
+            for (int city = n - 2; city >= 0; --city) {
+                int delta = locations[city + 1] - locations[city];
+
+                if (used >= delta) {
+                    dpLeft[city][used] = ((used == delta ? 0 : dpLeft[city + 1][used - delta]) * 2 % modulo + dpRight[city + 1][used - delta]) % modulo;
+                }
+            }
+
+            for (int city = 1; city < n; ++city) {
+                int delta = locations[city] - locations[city - 1];
+
+                if (used >= delta) {
+                    dpRight[city][used] = ((used == delta ? 0 : dpRight[city - 1][used - delta]) * 2 % modulo + dpLeft[city - 1][used - delta]) % modulo;
                 }
             }
         }
 
-        return dp[start][fuel];
+        int result = 0;
+
+        for (int used = 0; used <= fuel; ++used) {
+            result += (dpLeft[finish][used] + dpRight[finish][used]) % modulo;
+            result %= modulo;
+        }
+
+        if (start == finish) {
+            result = (result + modulo - 1) % modulo;
+        }
+
+        return result;
     }
 }
