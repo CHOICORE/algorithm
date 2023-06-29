ae20d8aa1c34def5f330c086650edacc11166d47
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-06-29 10:16:57 +0900

    Time: 15 ms (97.04%), Space: 42.9 MB (92.31%) - LeetHub

--- LeetCode/Hard/0864-shortest-path-to-get-all-keys/0864-shortest-path-to-get-all-keys.java
+++ LeetCode/Hard/0864-shortest-path-to-get-all-keys/0864-shortest-path-to-get-all-keys.java
@@ -1,72 +1,96 @@
 class Solution {
     public int shortestPathAllKeys(String[] grid) {
-        int m = grid.length, n = grid[0].length();
-        Queue<int[]> queue = new LinkedList<>();
-        int[][] moves = new int[][] {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-        
-        Map<Integer, Set<Pair<Integer, Integer>>> seen = new HashMap<>();
-        
-        Set<Character> keySet = new HashSet<>();
-        Set<Character> lockSet = new HashSet<>();
+        int[] directionRow = new int[]{1, -1, 0, 0};
+        int[] directionColumn = new int[]{0, 0, 1, -1};
+        int rows = grid.length;
+        int columns = grid[0].length();
+        int points = rows * columns;
         int allKeys = 0;
-        int startR = -1, startC = -1;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                char cell = grid[i].charAt(j);
-                if (cell >= 'a' && cell <= 'f') {
-                    allKeys += (1 << (cell - 'a'));
-                    keySet.add(cell);
+        int startPoint = 0;
+
+        for (int r = 0; r < rows; r++) {
+            String row = grid[r];
+
+            for (int c = 0; c < columns; c++) {
+                char v = row.charAt(c);
+
+                if (v == '@') {
+                    startPoint = r * columns + c;
                 }
-                if (cell >= 'A' && cell <= 'F') {
-                    lockSet.add(cell);
-                }
-                if (cell == '@') {
-                    startR = i;
-                    startC = j;
+                boolean isKey = v >= 'a' && v <= 'f';
+                if (isKey) {
+                    allKeys = (allKeys << 1) | 1;
                 }
             }
         }
+        int states = points * (allKeys + 1);
+        int[] queue = new int[states];
+        int queueHead = 0;
+        int queueTail = 0;
+        boolean[] visited = new boolean[states];
+        int distance = 0;
+        int noKeys = 0;
+        int startState = noKeys * points + startPoint;
+        queue[queueHead++] = startState;
 
-        queue.offer(new int[] {startR, startC, 0, 0});
-        seen.put(0, new HashSet<>());
-        seen.get(0).add(new Pair<>(startR, startC));    
-        
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int curR = cur[0], curC = cur[1], keys = cur[2], dist = cur[3];
-            for (int[] move : moves) {
-                int newR = curR + move[0], newC = curC + move[1];
-                
-                if (newR >= 0 && newR < m && newC >= 0 && newC < n && grid[newR].charAt(newC) != '#') {
-                    char cell = grid[newR].charAt(newC);
-                    
-                    if (keySet.contains(cell)) {
-                        if (((1 << (cell - 'a')) & keys) != 0) {
-                            continue;
-                        }
-                        
-                        int newKeys = (keys | (1 << (cell - 'a')));
-                        
-                        if (newKeys == allKeys) {
-                            return dist + 1;
-                        }
-                        seen.putIfAbsent(newKeys, new HashSet<>());
-                        seen.get(newKeys).add(new Pair<>(newR, newC));
-                        queue.offer(new int[] {newR, newC, newKeys, dist + 1});
-                    }
-                    
-                    if (lockSet.contains(cell) && ((keys & (1 << (cell - 'A'))) == 0)) {
+        visited[startState] = true;
+
+        while (true) {
+            int size = queueHead - queueTail;
+            if (size == 0) {
+                break;
+            }
+            while (--size >= 0) {
+                int state = queue[queueTail++];
+                int keys = state / points;
+                int point = state % points;
+                int row = point / columns;
+                int column = point % columns;
+
+                if (keys == allKeys) {
+                    return distance;
+                }
+                for (int i = 0; i < 4; i++) {
+                    int nextRow = row + directionRow[i];
+                    int nextColumn = column + directionColumn[i];
+                    if (nextRow < 0 || nextColumn < 0 || nextRow >= rows || nextColumn >= columns) {
                         continue;
                     }
-                        
-                    else if (!seen.get(keys).contains(new Pair<>(newR, newC))) {
-                        seen.get(keys).add(new Pair<>(newR, newC));
-                        queue.offer(new int[] {newR, newC, keys, dist + 1});
+                    char v = grid[nextRow].charAt(nextColumn);
+                    if (v == '#') {
+                        continue;
+                    }
+                    int nextKey = keys;
+                    if (v != '.') {
+                        boolean isLock = v >= 'A' && v <= 'F';
+                        if (isLock && !isOpen(keys, v)) {
+                            continue;
+                        }
+                        boolean isKey = v >= 'a' && v <= 'f';
+                        if (isKey) {
+                            nextKey = addKey(nextKey, v);
+                        }
+                    }
+                    int nextPoint = nextRow * columns + nextColumn;
+                    int nextState = nextKey * points + nextPoint;
+                    if (!visited[nextState]) {
+                        queue[queueHead++] = nextState;
+                        visited[nextState] = true;
                     }
                 }
             }
+            distance++;
         }
-        
         return -1;
+    }
+
+    public int addKey(int keys, char key) {
+        int keyId = 1 << (key - 'a');
+        return keys | keyId;
+    }
+
+    public boolean isOpen(int keys, char lock) {
+        int lockId = 1 << (lock - 'A');
+        return (keys & lockId) > 0;
     }
 }
