d37b7fab849d1a51bafdb098731a8d54b200cd23
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-07-02 16:08:04 +0900

    Time: 15 ms (100.00%), Space: 40.4 MB (54.55%) - LeetHub

--- LeetCode/Hard/1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.java
+++ LeetCode/Hard/1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.java
@@ -1,36 +1,31 @@
 class Solution {
+    int answer = 0;
+
     public int maximumRequests(int n, int[][] requests) {
-        int answer = 0;
+        int[] degree = new int[n];
+        permits(requests, 0, degree, 0);
+        return answer;
+    }
 
-        for (int mask = 0; mask < (1 << requests.length); mask++) {
-            int[] indegree = new int[n];
-            int pos = requests.length - 1;
-            int bitCount = Integer.bitCount(mask);
-
-            if (bitCount <= answer) {
-                continue;
+    void permits(int[][] req, int ind, int[] degree, int temp) {
+        if (ind == req.length) {
+            for (int j : degree) {
+                if (j != 0)
+                    return;
             }
-
-            for (int curr = mask; curr > 0; curr >>= 1, pos--) {
-                if ((curr & 1) == 1) {
-                    indegree[requests[pos][0]]--;
-                    indegree[requests[pos][1]]++;
-                }
-            }
-
-            boolean flag = true;
-            for (int i = 0; i < n; i++) {
-                if (indegree[i] != 0) {
-                    flag = false;
-                    break;
-                }
-            }
-
-            if (flag)  {
-                answer = bitCount;
-            }
+            answer = Math.max(answer, temp);
+            return;
         }
 
-        return answer;
+        int i = req[ind][0];
+        int j = req[ind][1];
+
+        degree[i] = degree[i] - 1;
+        degree[j] = degree[j] + 1;
+        permits(req, ind + 1, degree, temp + 1);
+
+        degree[i] = degree[i] + 1;
+        degree[j] = degree[j] - 1;
+        permits(req, ind + 1, degree, temp);
     }
 }
