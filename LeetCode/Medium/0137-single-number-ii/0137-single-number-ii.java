9c638cdac9963c7a7c4d37a06d7df09330d65da3
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-07-04 09:31:40 +0900

    Time: 0 ms (100.00%), Space: 44 MB (59.60%) - LeetHub

--- LeetCode/Medium/0137-single-number-ii/0137-single-number-ii.java
+++ LeetCode/Medium/0137-single-number-ii/0137-single-number-ii.java
@@ -1,11 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int o = 0;
-        int t = 0;
-        for (int num : nums) {
-            o = (num ^ o) & ~t;
-            t = (num ^ t) & ~o;
+        int ones = 0, twos = 0;
+        for (int e : nums) {
+            ones = ((ones ^ e) & (~twos));
+            twos = ((twos ^ e) & (~ones));
         }
-        return o;
+        return ones;
     }
 }
