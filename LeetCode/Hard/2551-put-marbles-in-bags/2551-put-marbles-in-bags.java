63ea94da6c80fb4283e00e0e305de261eba93e83
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-07-08 17:49:14 +0900

    Time: 37 ms (97.73%), Space: 55.9 MB (81.82%) - LeetHub

--- LeetCode/Hard/2551-put-marbles-in-bags/2551-put-marbles-in-bags.java
+++ LeetCode/Hard/2551-put-marbles-in-bags/2551-put-marbles-in-bags.java
@@ -1,17 +1,15 @@
 class Solution {
     public long putMarbles(int[] weights, int k) {
-        int n = weights.length;
-        int[] pairWeights = new int[n - 1];
-        for (int i = 0; i < n - 1; ++i) {
-            pairWeights[i] = weights[i] + weights[i + 1];
+        int[] costs = new int[weights.length - 1];
+        for (int i = 0; i < weights.length - 1; i++) {
+            costs[i] = weights[i] + weights[i + 1];
         }
-        Arrays.sort(pairWeights, 0, n - 1);
-        
-        long answer = 0l;
-        for (int i = 0; i < k - 1; ++i) {
-            answer += pairWeights[n - 2 - i] - pairWeights[i];
+        Arrays.sort(costs);
+        long min = 0, max = 0;
+        for (int i = 0; i < k - 1; i++) {
+            min += costs[i];
+            max += costs[costs.length - 1 - i];
         }
-
-        return answer;
+        return max - min;
     }
 }
