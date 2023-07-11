9f085d7622ab8ba8ca72dc0c759905696d01cf52
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-07-11 09:13:28 +0900

    Time: 14 ms (21.97%), Space: 43.7 MB (6.98%) - LeetHub

--- LeetCode/Medium/0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
+++ LeetCode/Medium/0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -8,46 +8,52 @@
  * }
  */
 class Solution {
-    Map<Integer, List<Integer>> graph;
-    List<Integer> answer;
-    Set<Integer> visited;
-
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        graph = new HashMap<>();
-        buildGraph(root, null);
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        dfsBuild(root, null, graph);
 
-        answer = new ArrayList<>();
-        visited = new HashSet<>();
+        List<Integer> answer = new ArrayList<>();
+        Set<Integer> visited = new HashSet<>();
+        Queue<int[]> queue = new LinkedList<>();
+
+        queue.add(new int[]{target.val, 0});
         visited.add(target.val);
 
-        dfs(target.val, 0, k);
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int node = cur[0], distance = cur[1];
+
+            if (distance == k) {
+                answer.add(node);
+                continue;
+            }
+
+            for (int neighbor : graph.getOrDefault(node, new ArrayList<>())) {
+                if (!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    queue.add(new int[]{neighbor, distance + 1});
+                }
+            }
+        }
 
         return answer;
     }
 
-    private void buildGraph(TreeNode cur, TreeNode parent) {
+    private void dfsBuild(TreeNode cur, TreeNode parent, Map<Integer, List<Integer>> graph) {
         if (cur != null && parent != null) {
-            graph.computeIfAbsent(cur.val, k -> new ArrayList<>()).add(parent.val);
-            graph.computeIfAbsent(parent.val, k -> new ArrayList<>()).add(cur.val);
+            int curVal = cur.val, parentVal = parent.val;
+            graph.putIfAbsent(curVal, new ArrayList<>());
+            graph.putIfAbsent(parentVal, new ArrayList<>());
+            graph.get(curVal).add(parentVal);
+            graph.get(parentVal).add(curVal);
         }
-        if (cur.left != null) {
-            buildGraph(cur.left, cur);
-        }
-        if (cur.right != null) {
-            buildGraph(cur.right, cur);
-        }
-    }
 
-    private void dfs(int cur, int distance, int k) {
-        if (distance == k) {
-            answer.add(cur);
-            return;
+        if (cur != null && cur.left != null) {
+            dfsBuild(cur.left, cur, graph);
         }
-        for (int neighbor : graph.getOrDefault(cur, new ArrayList<>())) {
-            if (!visited.contains(neighbor)) {
-                visited.add(neighbor);
-                dfs(neighbor, distance + 1, k);
-            }
+
+        if (cur != null && cur.right != null) {
+            dfsBuild(cur.right, cur, graph);
         }
     }
 }
