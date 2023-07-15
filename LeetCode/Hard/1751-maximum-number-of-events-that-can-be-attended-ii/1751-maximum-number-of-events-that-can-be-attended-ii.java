5503a80bd12e35125ebfa3742d59eed1ad8236c6
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-07-15 10:48:51 +0900

    Time: 70 ms (40.19%), Space: 96.4 MB (35.51%) - LeetHub

--- LeetCode/Hard/1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.java
+++ LeetCode/Hard/1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.java
@@ -1,5 +1,6 @@
 class Solution {
     private int[][] dp;
+    private int[] nextIndices;
 
     public static int bisectRight(int[][] events, int target) {
         int left = 0, right = events.length;
@@ -17,6 +18,10 @@ class Solution {
     public int maxValue(int[][] events, int k) {
         Arrays.sort(events, (a, b) -> a[0] - b[0]);
         int n = events.length;
+        nextIndices = new int[n];
+        for (int curIndex = 0; curIndex < n; ++curIndex) {
+            nextIndices[curIndex] = bisectRight(events, events[curIndex][1]);
+        }
 
         dp = new int[k + 1][n];
         for (int[] row : dp) {
@@ -33,7 +38,7 @@ class Solution {
         if (dp[count][curIndex] != -1) {
             return dp[count][curIndex];
         }
-        int nextIndex = bisectRight(events, events[curIndex][1]);
+        int nextIndex = nextIndices[curIndex];
         dp[count][curIndex] = Math.max(dfs(curIndex + 1, count, events), events[curIndex][2] + dfs(nextIndex, count - 1, events));
         return dp[count][curIndex];
     }
