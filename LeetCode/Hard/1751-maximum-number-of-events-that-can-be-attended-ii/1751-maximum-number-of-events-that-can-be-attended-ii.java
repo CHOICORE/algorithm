49cd58b53249f0d8f74d8cebe54b3c102e808588
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-07-15 10:48:26 +0900

    Time: 65 ms (56.07%), Space: 96 MB (41.12%) - LeetHub

--- LeetCode/Hard/1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.java
+++ LeetCode/Hard/1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.java
@@ -1,29 +1,6 @@
 class Solution {
-    public int maxValue(int[][] events, int k) {
-        Arrays.sort(events, (a, b) -> a[0] - b[0]);
-        int n = events.length;
-        
-        dp = new int[k + 1][n];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
-        
-        return dfs(0, k, events);
-    }    
-    
     private int[][] dp;
-    private int dfs(int curIndex, int count, int[][] events) {
-        if (count == 0 || curIndex == events.length) {
-            return 0;
-        }
-        if (dp[count][curIndex] != -1) {
-            return dp[count][curIndex];
-        }
-        int nextIndex = bisectRight(events, events[curIndex][1]);
-        dp[count][curIndex] = Math.max(dfs(curIndex + 1, count, events), events[curIndex][2] + dfs(nextIndex, count - 1, events));
-        return dp[count][curIndex];
-    }
-    
+
     public static int bisectRight(int[][] events, int target) {
         int left = 0, right = events.length;
         while (left < right) {
@@ -35,5 +12,29 @@ class Solution {
             }
         }
         return left;
-    }   
+    }
+
+    public int maxValue(int[][] events, int k) {
+        Arrays.sort(events, (a, b) -> a[0] - b[0]);
+        int n = events.length;
+
+        dp = new int[k + 1][n];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        return dfs(0, k, events);
+    }
+
+    private int dfs(int curIndex, int count, int[][] events) {
+        if (count == 0 || curIndex == events.length) {
+            return 0;
+        }
+        if (dp[count][curIndex] != -1) {
+            return dp[count][curIndex];
+        }
+        int nextIndex = bisectRight(events, events[curIndex][1]);
+        dp[count][curIndex] = Math.max(dfs(curIndex + 1, count, events), events[curIndex][2] + dfs(nextIndex, count - 1, events));
+        return dp[count][curIndex];
+    }
 }
