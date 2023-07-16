0795309f58a0b11f9f502fcb6ae0f0c114314a00
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-07-16 14:54:33 +0900

    Time: 8 ms (97.09%), Space: 41.1 MB (95.15%) - LeetHub

--- LeetCode/Hard/1125-smallest-sufficient-team/1125-smallest-sufficient-team.java
+++ LeetCode/Hard/1125-smallest-sufficient-team/1125-smallest-sufficient-team.java
@@ -1,62 +1,39 @@
 class Solution {
-    List<Integer> ans = new ArrayList<>();
+    List<Integer> sol = new ArrayList<>();
 
-    public int[] smallestSufficientTeam(String[] skills, List<List<String>> people) {
-        int n = skills.length, m = people.size();
-        Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            map.put(skills[i], i);
-        }
-        int[] arr = new int[m];
-        for (int i = 0; i < m; i++) {
-            List<String> list = people.get(i);
-            int val = 0;
-            for (String skill : list) {
-                val |= 1 << map.get(skill);
-            }
-            arr[i] = val;
-        }
-        boolean[] banned = new boolean[m];
-        for (int i = 0; i < m; i++) {
-            for (int j = i + 1; j < m; j++) {
-                int val = arr[i] | arr[j];
-                if (val == arr[i]) {
-                    banned[j] = true;
-                } else if (val == arr[j]) {
-                    banned[i] = true;
-                }
+    public int[] smallestSufficientTeam(String[] req_skills, List<List<String>> people) {
+        Map<String, Integer> idx = new HashMap<>();
+        int n = 0;
+        for (String s : req_skills) idx.put(s, n++);
+        int[] pe = new int[people.size()];
+        for (int i = 0; i < pe.length; i++) {
+            for (String p : people.get(i)) {
+                int skill = idx.get(p);
+                pe[i] += 1 << skill;
             }
         }
-        solve(0, n, arr, new ArrayList<>(), banned);
-        int[] res = new int[ans.size()];
-        for (int i = 0; i < res.length; i++) {
-            res[i] = ans.get(i);
-        }
-        return res;
+        search(0, pe, new ArrayList<>(), n);
+        int[] ans = new int[sol.size()];
+        for (int i = 0; i < sol.size(); i++) ans[i] = sol.get(i);
+        return ans;
     }
 
-    private void solve(int cur, int n, int[] arr, List<Integer> list, boolean[] banned) {
+    public void search(int cur, int[] pe, List<Integer> onesol, int n) {
         if (cur == (1 << n) - 1) {
-            if (ans.isEmpty() || ans.size() > list.size()) {
-                ans = new ArrayList<>(list);
+            if (sol.size() == 0 || onesol.size() < sol.size()) {
+                sol = new ArrayList<>(onesol);
             }
             return;
         }
-        if (!ans.isEmpty() && list.size() >= ans.size()) {
-            return;
-        }
-        int zero = 0;
-        while (((cur >> zero) & 1) == 1) {
-            zero++;
-        }
-        for (int i = 0; i < arr.length; i++) {
-            if (banned[i]) {
-                continue;
-            }
-            if (((arr[i] >> zero) & 1) == 1) {
-                list.add(i);
-                solve(cur | arr[i], n, arr, list, banned);
-                list.remove(list.size() - 1);
+        if (sol.size() != 0 && onesol.size() >= sol.size()) return;
+        int zeroBit = 0;
+        while (((cur >> zeroBit) & 1) == 1) zeroBit++;
+        for (int i = 0; i < pe.length; i++) {
+            int per = pe[i];
+            if (((per >> zeroBit) & 1) == 1) {
+                onesol.add(i);
+                search(cur | per, pe, onesol, n);
+                onesol.remove(onesol.size() - 1);
             }
         }
     }
