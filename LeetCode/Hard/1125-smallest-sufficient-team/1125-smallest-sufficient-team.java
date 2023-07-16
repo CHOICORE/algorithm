8aa678e8b4d883d3f601992d023509d3f435c32a
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-07-16 14:53:48 +0900

    Time: 2 ms (100.00%), Space: 41.3 MB (89.32%) - LeetHub

--- LeetCode/Hard/1125-smallest-sufficient-team/1125-smallest-sufficient-team.java
+++ LeetCode/Hard/1125-smallest-sufficient-team/1125-smallest-sufficient-team.java
@@ -1,38 +1,63 @@
 class Solution {
-    public int[] smallestSufficientTeam(String[] req_skills, List<List<String>> people) {
-        int n = people.size(), m = req_skills.length;
-        HashMap<String, Integer> skillId = new HashMap<String, Integer>();
-        for (int i = 0; i < m; i++) {
-            skillId.put(req_skills[i], i);
-        }
-        int[] skillsMaskOfPerson = new int[n];
+    List<Integer> ans = new ArrayList<>();
+
+    public int[] smallestSufficientTeam(String[] skills, List<List<String>> people) {
+        int n = skills.length, m = people.size();
+        Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            for (String skill : people.get(i)) {
-                skillsMaskOfPerson[i] |= 1 << skillId.get(skill);
-            }
+            map.put(skills[i], i);
         }
-        long[] dp = new long[1 << m];
-        Arrays.fill(dp, (1L << n) - 1);
-        dp[0] = 0;
-        for (int skillsMask = 1; skillsMask < (1 << m); skillsMask++) {
-            for (int i = 0; i < n; i++) {
-                int smallerSkillsMask = skillsMask & ~skillsMaskOfPerson[i];
-                if (smallerSkillsMask != skillsMask) {
-                    long peopleMask = dp[smallerSkillsMask] | (1L << i);
-                    if (Long.bitCount(peopleMask) < Long.bitCount(dp[skillsMask])) {
-                        dp[skillsMask] = peopleMask;
-                    }
+        int[] arr = new int[m];
+        for (int i = 0; i < m; i++) {
+            List<String> list = people.get(i);
+            int val = 0;
+            for (String skill : list) {
+                val |= 1 << map.get(skill);
+            }
+            arr[i] = val;
+        }
+        boolean[] banned = new boolean[m];
+        for (int i = 0; i < m; i++) {
+            for (int j = i + 1; j < m; j++) {
+                int val = arr[i] | arr[j];
+                if (val == arr[i]) {
+                    banned[j] = true;
+                } else if (val == arr[j]) {
+                    banned[i] = true;
                 }
             }
         }
-        long answerMask = dp[(1 << m) - 1];
-        int[] ans = new int[Long.bitCount(answerMask)];
-        int ptr = 0;
-        for (int i = 0; i < n; i++) {
-            if (((answerMask >> i) & 1) == 1) {
-                ans[ptr++] = i;
+        solve(0, n, arr, new ArrayList<>(), banned);
+        int[] res = new int[ans.size()];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = ans.get(i);
+        }
+        return res;
+    }
+
+    private void solve(int cur, int n, int[] arr, List<Integer> list, boolean[] banned) {
+        if (cur == (1 << n) - 1) {
+            if (ans.isEmpty() || ans.size() > list.size()) {
+                ans = new ArrayList<>(list);
+            }
+            return;
+        }
+        if (!ans.isEmpty() && list.size() >= ans.size()) {
+            return;
+        }
+        int zero = 0;
+        while (((cur >> zero) & 1) == 1) {
+            zero++;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (banned[i]) {
+                continue;
+            }
+            if (((arr[i] >> zero) & 1) == 1) {
+                list.add(i);
+                solve(cur | arr[i], n, arr, list, banned);
+                list.remove(list.size() - 1);
             }
         }
-        return ans;
     }
 }
