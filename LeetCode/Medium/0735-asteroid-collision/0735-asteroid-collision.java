7835665256b52024af9baa4cf8c74034527caf13
CHOICORE <choijaehyeong93@gmail.com>
2023-07-20 09:22:17 +0900

    Time: 1 ms (100.00%), Space: 44.3 MB (33.94%) - LeetHub

--- LeetCode/Medium/0735-asteroid-collision/0735-asteroid-collision.java
+++ LeetCode/Medium/0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,36 +1,14 @@
 class Solution {
-    public int[] asteroidCollision(int[] asteroids) {
-        Deque<Integer> queue = new ArrayDeque<>();
-
-        for (int val : asteroids) {
-            queue.addLast(val);
-            boolean shouldBreak = false;
-
-            while (queue.size() > 1 && !shouldBreak) {
-                int last = queue.pollLast();
-                int first = queue.pollLast();
-                // collide
-                if (first > 0 && last < 0) {
-                    if (first == Math.abs(last))
-                        shouldBreak = true;
-                    else if (first > Math.abs(last)) {
-                        queue.addLast(first);
-                        shouldBreak = true;
-                    } else
-                        queue.addLast(last);
-                } else {
-                    queue.addLast(first);
-                    queue.addLast(last);
-                    shouldBreak = true;
-                }
+    public int[] asteroidCollision(int[] a) {
+        int top = -1;
+        for (int x : a) {
+            boolean stillAlive = true;
+            while (stillAlive && x < 0 && top >= 0 && a[top] > 0) {
+                stillAlive = a[top] + x < 0;
+                if (a[top] + x <= 0) top--;
             }
+            if (stillAlive) a[++top] = x;
         }
-
-        int[] ans = new int[queue.size()];
-
-        for (int i = 0; i < ans.length; i++)
-            ans[i] = queue.pollFirst();
-
-        return ans;
+        return Arrays.copyOf(a, top + 1);
     }
 }
