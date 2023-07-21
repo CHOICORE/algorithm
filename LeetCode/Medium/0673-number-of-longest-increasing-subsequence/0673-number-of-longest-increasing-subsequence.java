94b05bd7f6e7e9ca8ba3b404d698122c3fc064f0
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-07-21 09:07:11 +0900

    Time: 5 ms (100.00%), Space: 43.5 MB (23.22%) - LeetHub

--- LeetCode/Medium/0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
+++ LeetCode/Medium/0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
@@ -1,39 +1,58 @@
 class Solution {
     public int findNumberOfLIS(int[] nums) {
-        int n = nums.length;
-        int[] length = new int[n];
-        int[] count = new int[n];
+        int num = nums.length;
+        List<int[]>[] len = new ArrayList[num];
+        for (int i = 0; i < num; i++) {
+            len[i] = new ArrayList<>();
+        }
+        int size = 0;
+        for (int n : nums) {
+            int index = bSearchLength(len, size, n);
+            int count = 1;
 
-        Arrays.fill(length, 1);
-        Arrays.fill(count, 1);
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    if (length[j] + 1 > length[i]) {
-                        length[i] = length[j] + 1;
-                        count[i] = 0;
-                    }
-                    if (length[j] + 1 == length[i]) {
-                        count[i] += count[j];
-                    }
+            if (index > 0) {
+                List<int[]> t = len[index - 1];
+                int p = bSearchIndex(t, n);
+                count = t.get(t.size() - 1)[1] - (p == 0 ? 0 : t.get(p - 1)[1]);
+            }
+            if (len[index].size() == 0) {
+                len[index].add(new int[]{n, count});
+                size++;
+            } else {
+                List<int[]> t = len[index];
+                int[] last = t.get(t.size() - 1);
+                int ch = last[1] + count;
+                if (last[0] == n) {
+                    last[1] += count;
+                } else {
+                    t.add(new int[]{n, last[1] + count});
                 }
             }
         }
+        return len[size - 1].get(len[size - 1].size() - 1)[1];
+    }
 
-        int maxLength = 0;
-        int result = 0;
-
-        for (int len : length) {
-            maxLength = Math.max(maxLength, len);
+    public int bSearchLength(List<int[]>[] len, int right, int n) {
+        int left = 0;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (n > len[mid].get(len[mid].size() - 1)[0])
+                left = mid + 1;
+            else
+                right = mid;
         }
+        return left;
+    }
 
-        for (int i = 0; i < n; i++) {
-            if (length[i] == maxLength) {
-                result += count[i];
-            }
+    public int bSearchIndex(List<int[]> t, int num) {
+        int left = 0, right = t.size() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (num <= t.get(mid)[0])
+                left = mid + 1;
+            else
+                right = mid;
         }
-
-        return result;
+        return left;
     }
 }
