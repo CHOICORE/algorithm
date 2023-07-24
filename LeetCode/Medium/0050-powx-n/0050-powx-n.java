94a1fb8e7375a9ff34ec23c41a6a6e432ea12c54
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-07-24 14:01:30 +0900

    Time: 0 ms (100.00%), Space: 40.5 MB (91.63%) - LeetHub

--- LeetCode/Medium/0050-powx-n/0050-powx-n.java
+++ LeetCode/Medium/0050-powx-n/0050-powx-n.java
@@ -3,17 +3,22 @@ class Solution {
         if (n == 0) {
             return 1;
         }
-       
+
         if (n < 0) {
-            return 1.0 / binaryExp(x, -1 * n);
+            n = -1 * n;
+            x = 1.0 / x;
         }
-       
-        if (n % 2 == 1) {
-            return x * binaryExp(x * x, (n - 1) / 2);
+
+        double result = 1;
+        while (n != 0) {
+            if (n % 2 == 1) {
+                result = result * x;
+                n -= 1;
+            }
+            x = x * x;
+            n = n / 2;
         }
-        else {
-            return binaryExp(x * x, n / 2);
-        }
+        return result;
     }
 
     public double myPow(double x, int n) {
