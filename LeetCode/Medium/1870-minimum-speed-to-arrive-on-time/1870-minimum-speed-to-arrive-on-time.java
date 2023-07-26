aac2c1d65efeb5d5f0e402e32213ba2586e13774
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-07-26 18:12:21 +0900

    Time: 37 ms (100.00%), Space: 59 MB (56.40%) - LeetHub

--- LeetCode/Medium/1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
+++ LeetCode/Medium/1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
@@ -1,28 +1,25 @@
 class Solution {
     public int minSpeedOnTime(int[] dist, double hour) {
-        if (dist.length - 1 + 1e-7 > hour) {
-            return -1;
-        }
-        int left = 1;
-        int right = (int) 1e7;
-        while (left <= right) {
-            int mid = (left + right) >>> 1;
-            if (isCheck(dist, mid, hour)) {
-                left = mid + 1;
+        int left = 1, right = (int) 1e7, n = dist.length;
+
+        if (n - 1 >= hour) return -1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int hours = 0;
+
+            for (int i = 0; i < n - 1; i++) {
+                hours += (dist[i] + mid - 1) / mid;
+            }
+            double hoursSpent = hours + (1d * dist[n - 1]) / mid;
+
+            if (hoursSpent <= hour) {
+                right = mid;
             } else {
-                right = mid - 1;
+                left = mid + 1;
             }
         }
-        return left;
-    }
 
-    private boolean isCheck(int[] dist, int mid, double hour) {
-        int count = 0;
-        int n = dist.length;
-        for (int i = 0; i < n - 1; i++) {
-            count += (dist[i] + (mid - 1)) / mid;
-        }
-        double dCount = count + (1d * dist[n - 1]) / mid;
-        return dCount > hour;
+        return left;
     }
 }
