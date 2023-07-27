76bd114cd3801023722fc6ed2bea508921ced5cc
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-07-27 10:18:40 +0900

    Time: 12 ms (100.00%), Space: 55.7 MB (88.18%) - LeetHub

--- LeetCode/Hard/2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
+++ LeetCode/Hard/2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
@@ -1,11 +1,17 @@
 class Solution {
     public long maxRunTime(int n, int[] batteries) {
-        int bn = batteries.length;
-        if (bn < n) return 0;
+        
+        int length = batteries.length;
+        
+        if (length < n) return 0;
+        
         long l = 0, r = 0;
+        
         for (int b : batteries) r += b;
         r /= n;
+        
         long res = 0;
+        
         while (l <= r) {
             long mid = l + (r - l) / 2;
             if (possible(n, batteries, mid)) {
@@ -17,8 +23,11 @@ class Solution {
     }
 
     private boolean possible(int n, int[] batteries, long target) {
+        
         long totalTarget = target * n;
+        
         for (int b : batteries) totalTarget -= Math.min(target, b);
+        
         return totalTarget <= 0;
     }
 }
