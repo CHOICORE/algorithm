bc31314368d119fd41c3a40fd2fc24105aeb6fc6
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-07-28 09:43:49 +0900

    Time: 0 ms (100.00%), Space: 40.2 MB (30.09%) - LeetHub

--- LeetCode/Medium/0486-predict-the-winner/0486-predict-the-winner.java
+++ LeetCode/Medium/0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,18 +1,18 @@
 class Solution {
-    private int maxDiff(int[] nums, int left, int right, int n) {
-        if (left == right) {
-            return nums[left];
-        }
-
-        int scoreByLeft = nums[left] - maxDiff(nums, left + 1, right, n);
-        int scoreByRight = nums[right] - maxDiff(nums, left, right - 1, n);
-
-        return Math.max(scoreByLeft, scoreByRight);
-    }
-
     public boolean PredictTheWinner(int[] nums) {
         int n = nums.length;
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
 
-        return maxDiff(nums, 0, n - 1, n) >= 0;
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+            }
+        }
+
+        return dp[0][n - 1] >= 0;
+
     }
 }
