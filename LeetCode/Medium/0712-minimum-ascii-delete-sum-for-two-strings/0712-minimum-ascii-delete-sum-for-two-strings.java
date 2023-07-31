86b0a656b8ab14f66bed0720ae63cdca0e13bc93
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-07-31 11:11:56 +0900

    Time: 7 ms (99.68%), Space: 40.4 MB (99.57%) - LeetHub

--- LeetCode/Medium/0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ LeetCode/Medium/0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -1,30 +1,28 @@
 class Solution {
-    public int minimumDeleteSum(String s1, String s2) {
-
-        int m = s1.length(), n = s2.length();
-        int[][] computeCost = new int[m + 1][n + 1];
-
-        for (int i = 1; i <= m; i++) {
-            computeCost[i][0] = computeCost[i - 1][0] + s1.charAt(i - 1);
-        }
-        
-        for (int j = 1; j <= n; j++) {
-            computeCost[0][j] = computeCost[0][j - 1] + s2.charAt(j - 1);
-        }
-        
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    computeCost[i][j] = computeCost[i - 1][j - 1];
-                } else {
-                    computeCost[i][j] = Math.min(
-                            s1.charAt(i - 1) + computeCost[i - 1][j],
-                            s2.charAt(j - 1) + computeCost[i][j - 1]
-                    );
-                }
+    public int minimumDeleteSum(String string1, String string2) {
+        if (string1.length() < string2.length()) return minimumDeleteSum(string2, string1);
+        char[] s1 = string1.toCharArray(), s2 = string2.toCharArray();
+        int[] memo = getData(s2);
+        int[] ps = getData(s1);
+        for (int i = s1.length - 1; i > -1; i--) {
+            int last = ps[i], prevLast = i < s1.length - 1 ? ps[i + 1] : 0;
+            for (int j = s2.length - 1; j > -1; j--) {
+                int tmp = memo[j];
+                if (s1[i] == s2[j]) last = memo[j] = prevLast;
+                else last = memo[j] = Math.min(memo[j] + s1[i], last + s2[j]);
+                prevLast = tmp;
             }
         }
+        return memo[0];
+    }
 
-        return computeCost[m][n];
+    private int[] getData(char[] str) {
+        int strLen = str.length;
+        int[] data = new int[strLen];
+        data[data.length - 1] = str[strLen - 1];
+        for (int i = data.length - 2; i > -1; i--) {
+            data[i] += data[i + 1] + str[i];
+        }
+        return data;
     }
 }
