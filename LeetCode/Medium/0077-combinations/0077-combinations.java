f79aa2ed6c8032a6ee0f8a882312ac79bd1f93be
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-08-01 11:18:25 +0900

    Time: 1 ms (100.00%), Space: 45.6 MB (22.97%) - LeetHub

--- LeetCode/Medium/0077-combinations/0077-combinations.java
+++ LeetCode/Medium/0077-combinations/0077-combinations.java
@@ -1,29 +1,28 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> ans = new ArrayList<>();
-
-        getCombinations(1, n, k, new ArrayList<>(), ans);
+        helper(n, k, ans, new ArrayList<Integer>());
         return ans;
     }
 
-
-    private void getCombinations(int i, int n, int k, List<Integer> curr, List<List<Integer>> ans) {
-        if (i > n) {
-            if (curr.size() == k) {
-                ans.add(new ArrayList<>(curr));
-            }
+    public void helper(int n, int k, List<List<Integer>> ans, List<Integer> temp) {
+        if (k == 0) {
+            ans.add(new ArrayList<>(temp));
+            return;
+        } else if (n < 1) {
             return;
         }
 
-        //choose
-        if (curr.size() < k) {
-            curr.add(i);
-            getCombinations(i + 1, n, k, curr, ans);
-            curr.remove(curr.size() - 1);
+        temp.add(n);
+        helper(n - 1, k - 1, ans, temp);
+        temp.remove(temp.size() - 1);
+
+        // trimming
+        if (k <= n - 1) {
+            helper(n - 1, k, ans, temp);
         }
 
-        // not choose
-        getCombinations(i + 1, n, k, curr, ans);
+        return;
 
     }
 }
