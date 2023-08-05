56832f21820a511d8059c75684a0d2adddf21825
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-08-05 09:58:48 +0900

    Time: 0 ms (100.00%), Space: 43.5 MB (87.96%) - LeetHub

--- LeetCode/Medium/0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
+++ LeetCode/Medium/0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
@@ -14,33 +14,35 @@
  * }
  */
 class Solution {
-    public List<TreeNode> allPossibleBST(int start, int end,
-                                         Map<Pair<Integer, Integer>, List<TreeNode>> memo) {
-        List<TreeNode> res = new ArrayList<>();
-        if (start > end) {
+    private List<TreeNode> [][] dp;
+    public List<TreeNode> generateTrees(int n) {
+        dp = new ArrayList[n+1][n+1];
+        int low = 1;
+        int high = n;
+        return generateTrees(low, high);
+    }
+    public List<TreeNode> generateTrees(int low, int high){
+        final List<TreeNode> res = new ArrayList<>();
+        if(low > high){
             res.add(null);
             return res;
         }
-        if (memo.containsKey(new Pair<>(start, end))) {
-            return memo.get(new Pair<>(start, end));
+        if(dp[low][high] != null){
+            return dp[low][high];
         }
-        for (int i = start; i <= end; ++i) {
-            List<TreeNode> leftSubTrees = allPossibleBST(start, i - 1, memo);
-            List<TreeNode> rightSubTrees = allPossibleBST(i + 1, end, memo);
-
-            for (TreeNode left : leftSubTrees) {
-                for (TreeNode right : rightSubTrees) {
-                    TreeNode root = new TreeNode(i, left, right);
+        for(int i = low; i <= high; i++){
+            final List<TreeNode> left = generateTrees(low, i - 1);
+            final List<TreeNode> right = generateTrees(i+1, high);
+            for(int l = 0; l < left.size(); l++){
+                for(int r = 0; r < right.size(); r++){
+                    final TreeNode root = new TreeNode(i);
+                    root.left = left.get(l);
+                    root.right = right.get(r);
                     res.add(root);
                 }
             }
         }
-        memo.put(new Pair<>(start, end), res);
+        dp[low][high] = res;
         return res;
-    }
-
-    public List<TreeNode> generateTrees(int n) {
-        Map<Pair<Integer, Integer>, List<TreeNode>> memo = new HashMap<>();
-        return allPossibleBST(1, n, memo);
     }
 }
