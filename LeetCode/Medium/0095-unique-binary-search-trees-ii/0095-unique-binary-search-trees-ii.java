876abcdcebf5613f55c56fecd5d95f4215195917
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-08-05 09:59:06 +0900

    Time: 1 ms (99.88%), Space: 43.6 MB (66.87%) - LeetHub

--- LeetCode/Medium/0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
+++ LeetCode/Medium/0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
@@ -14,27 +14,29 @@
  * }
  */
 class Solution {
-    private List<TreeNode> [][] dp;
+    private List<TreeNode>[][] dp;
+
     public List<TreeNode> generateTrees(int n) {
-        dp = new ArrayList[n+1][n+1];
+        dp = new ArrayList[n + 1][n + 1];
         int low = 1;
         int high = n;
         return generateTrees(low, high);
     }
-    public List<TreeNode> generateTrees(int low, int high){
+
+    public List<TreeNode> generateTrees(int low, int high) {
         final List<TreeNode> res = new ArrayList<>();
-        if(low > high){
+        if (low > high) {
             res.add(null);
             return res;
         }
-        if(dp[low][high] != null){
+        if (dp[low][high] != null) {
             return dp[low][high];
         }
-        for(int i = low; i <= high; i++){
+        for (int i = low; i <= high; i++) {
             final List<TreeNode> left = generateTrees(low, i - 1);
-            final List<TreeNode> right = generateTrees(i+1, high);
-            for(int l = 0; l < left.size(); l++){
-                for(int r = 0; r < right.size(); r++){
+            final List<TreeNode> right = generateTrees(i + 1, high);
+            for (int l = 0; l < left.size(); l++) {
+                for (int r = 0; r < right.size(); r++) {
                     final TreeNode root = new TreeNode(i);
                     root.left = left.get(l);
                     root.right = right.get(r);
