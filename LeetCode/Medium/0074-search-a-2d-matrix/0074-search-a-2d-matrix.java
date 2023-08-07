1c7142a082191f6a8177f5334c3b8ab073e41b1d
CHOICORE <choijaehyeong93@gmail.com>
2023-08-07 11:55:05 +0900

    Time: 0 ms (100.00%), Space: 40.8 MB (91.38%) - LeetHub

--- LeetCode/Medium/0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ LeetCode/Medium/0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,19 +1,13 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int left = 0;
-        int right = m * n - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            int row = mid / n;
-            int col = mid % n;
-            if (matrix[row][col] == target) {
-                return true;
-            } else if (matrix[row][col] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        for (final int[] ints : matrix) {
+            for (int j = 0; j < cols; j++) {
+                if (ints[j] == target) {
+                    return true;
+                }
             }
         }
         return false;
