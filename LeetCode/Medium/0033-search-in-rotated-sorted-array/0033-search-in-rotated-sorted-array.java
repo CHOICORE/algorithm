e1b23a10e893fa4a78e2f4c4b5945ccca30b811a
CHOICORE <choijaehyeong93@gmail.com>
2023-08-08 15:28:56 +0900

    Time: 0 ms (100.00%), Space: 40.7 MB (92.66%) - LeetHub

--- LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,41 +1,34 @@
 class Solution {
     public int search(int[] nums, int target) {
         int n = nums.length;
-        int pivot = findPivot(nums, 0, n-1);
-        if (pivot == -1) {
-            return binarySearch(nums, 0, n-1, target);
-        } else if (nums[0] <= target && target <= nums[pivot]) {
-            return binarySearch(nums, 0, pivot, target);
-        } else {
-            return binarySearch(nums, pivot+1, n-1, target);
-        }
-    }
-    
-    private int findPivot(int[] nums, int lo, int hi) {
-        if (lo > hi) {
-            return -1;
-        }
-        int mid = lo + (hi - lo) / 2;
-        if (mid < hi && nums[mid] > nums[mid+1]) {
-            return mid;
-        } else if (mid > lo && nums[mid] < nums[mid-1]) {
-            return mid - 1;
-        } else if (nums[mid] <= nums[lo]) {
-            return findPivot(nums, lo, mid-1);
-        } else {
-            return findPivot(nums, mid+1, hi);
-        }
-    }
-    
-    private int binarySearch(int[] nums, int lo, int hi, int target) {
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                lo = mid + 1;
+        int left = 0, right = n - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[n - 1]) {
+                left = mid + 1;
             } else {
-                hi = mid - 1;
+                right = mid - 1;
+            }
+        }
+
+        return shiftedBinarySearch(nums, left, target);
+    }
+
+    private int shiftedBinarySearch(int[] nums, int pivot, int target) {
+        int n = nums.length;
+        int shift = n - pivot;
+        int left = (pivot + shift) % n;
+        int right = (pivot - 1 + shift) % n;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[(mid - shift + n) % n] == target) {
+                return (mid - shift + n) % n;
+            } else if (nums[(mid - shift + n) % n] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
         return -1;
