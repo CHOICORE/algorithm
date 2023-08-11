13f2b87973eb877c2a6f20858199080a1e10d90e
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-08-11 14:27:56 +0900

    Time: 3 ms (91.90%), Space: 39.7 MB (96.51%) - LeetHub

--- LeetCode/Medium/0518-coin-change-ii/0518-coin-change-ii.java
+++ LeetCode/Medium/0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,26 +1,12 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int n = coins.length;
-        int[][] dp = new int[n][amount + 1];
-        for (int i = 0; i < n; i++)
-            Arrays.fill(dp[i], -1);
-        return helper(amount, n - 1, coins, dp);
-    }
-
-    int helper(int amount, int x, int[] coins, int[][] dp) {
-        if (x == 0) {
-            if (amount % coins[x] == 0)
-                return 1;
-            else
-                return 0;
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = coins[i]; j < dp.length; j++) {
+                dp[j] += dp[j - coins[i]];
+            }
         }
-        if (dp[x][amount] != -1)
-            return dp[x][amount];
-        int take = 0;
-        if (coins[x] <= amount) {
-            take = helper(amount - coins[x], x, coins, dp);
-        }
-        int nottake = helper(amount, x - 1, coins, dp);
-        return dp[x][amount] = take + nottake;
+        return dp[amount];
     }
 }
