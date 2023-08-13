61951b24b85dd2aea367eeee56ad6d0f825a6b09
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-08-13 14:24:12 +0900

    Time: 5 ms (98.06%), Space: 56.6 MB (78.64%) - LeetHub

--- LeetCode/Medium/2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
+++ LeetCode/Medium/2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
@@ -1,29 +1,23 @@
 class Solution {
-    Map<Integer, Boolean> memo = new HashMap<>();
-
-    boolean prefixIsValid(int[] nums, int i) {
-        if (memo.containsKey(i)) {
-            return memo.get(i);
-        }
-        boolean ans = false;
-
-        if (i > 0 && nums[i] == nums[i - 1]) {
-            ans |= prefixIsValid(nums, i - 2);
-        }
-        if (i > 1 && nums[i] == nums[i - 1] && nums[i - 1] == nums[i - 2]) {
-            ans |= prefixIsValid(nums, i - 3);
-        }
-        if (i > 1 && nums[i] == nums[i - 1] + 1 && nums[i - 1] == nums[i - 2] + 1) {
-            ans |= prefixIsValid(nums, i - 3);
-        }
-        memo.put(i, ans);
-        return ans;
-    }
-
     public boolean validPartition(int[] nums) {
         int n = nums.length;
-        memo.put(-1, true);
 
-        return prefixIsValid(nums, n - 1);
+        if (n < 2) return false;
+
+        boolean pre3 = true;
+        boolean pre2 = false;
+        boolean pre1 = nums[0] == nums[1];
+        boolean cur;
+        for (int i = 2; i < n; ++i) {
+            cur = (pre2 && nums[i] == nums[i - 1]) || (pre3 && (nums[i - 2] == nums[i - 1]
+                    && nums[i - 1] == nums[i] || nums[i - 2] + 1 == nums[i - 1] && nums[i - 1]
+                    + 1 == nums[i]));
+
+            pre3 = pre2;
+            pre2 = pre1;
+            pre1 = cur;
+        }
+
+        return pre1;
     }
 }
