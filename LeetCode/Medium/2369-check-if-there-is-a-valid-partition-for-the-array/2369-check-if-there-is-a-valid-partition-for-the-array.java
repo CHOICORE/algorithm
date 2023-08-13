8413de10b5684bf2430b38b87416312690e90628
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-08-13 14:24:37 +0900

    Time: 8 ms (75.73%), Space: 56.6 MB (67.96%) - LeetHub

--- LeetCode/Medium/2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
+++ LeetCode/Medium/2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
@@ -1,23 +1,23 @@
 class Solution {
     public boolean validPartition(int[] nums) {
         int n = nums.length;
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
 
-        if (n < 2) return false;
+        for (int i = 0; i < n; i++) {
+            int dpIndex = i + 1;
 
-        boolean pre3 = true;
-        boolean pre2 = false;
-        boolean pre1 = nums[0] == nums[1];
-        boolean cur;
-        for (int i = 2; i < n; ++i) {
-            cur = (pre2 && nums[i] == nums[i - 1]) || (pre3 && (nums[i - 2] == nums[i - 1]
-                    && nums[i - 1] == nums[i] || nums[i - 2] + 1 == nums[i - 1] && nums[i - 1]
-                    + 1 == nums[i]));
-
-            pre3 = pre2;
-            pre2 = pre1;
-            pre1 = cur;
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                dp[dpIndex] |= dp[dpIndex - 2];
+            }
+            if (i > 1 && nums[i] == nums[i - 1] && nums[i] == nums[i - 2]) {
+                dp[dpIndex] |= dp[dpIndex - 3];
+            }
+            if (i > 1 && nums[i] == nums[i - 1] + 1 && nums[i] == nums[i - 2] + 2) {
+                dp[dpIndex] |= dp[dpIndex - 3];
+            }
         }
 
-        return pre1;
+        return dp[n];
     }
 }
