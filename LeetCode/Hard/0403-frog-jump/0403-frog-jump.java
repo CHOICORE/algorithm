e061341cdf45da429e910699827bfcdc15ef9c68
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-08-27 10:10:11 +0900

    Time: 151 ms (12.12%), Space: 88.6 MB (5.01%) - LeetHub

--- LeetCode/Hard/0403-frog-jump/0403-frog-jump.java
+++ LeetCode/Hard/0403-frog-jump/0403-frog-jump.java
@@ -1,7 +1,7 @@
 class Solution {
     HashMap<Integer, Integer> mark = new HashMap<>();
     int dp[][] = new int[2001][2001];
-    
+
     boolean solve(int[] stones, int n, int index, int prevJump) {
         if (index == n - 1) {
             return true;
@@ -10,7 +10,7 @@ class Solution {
         if (dp[index][prevJump] != -1) {
             return dp[index][prevJump] == 1;
         }
-        
+
         boolean ans = false;
 
         for (int nextJump = prevJump - 1; nextJump <= prevJump + 1; nextJump++) {
@@ -19,20 +19,20 @@ class Solution {
             }
         }
 
-        dp[index][prevJump] = (ans ? 1 :0);
+        dp[index][prevJump] = (ans ? 1 : 0);
         return ans;
     }
-    
+
     public boolean canCross(int[] stones) {
 
-        for (int i = 0 ; i < stones.length; i++) {
+        for (int i = 0; i < stones.length; i++) {
             mark.put(stones[i], i);
         }
-        
+
         for (int i = 0; i < 2000; i++) {
             Arrays.fill(dp[i], -1);
         }
-        
+
         return solve(stones, stones.length, 0, 0);
     }
 }
