cbd0bfae28401c58094f8b372011fd6eb385fd9f
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-08-31 09:03:18 +0900

    Time: 3 ms (98.79%), Space: 44.3 MB (37.04%) - LeetHub

--- LeetCode/Hard/1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.java
+++ LeetCode/Hard/1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.java
@@ -3,10 +3,10 @@ class Solution {
         int[] maxReach = new int[n + 1];
 
         for (int i = 0; i < ranges.length; i++) {
-            int start = Math.max(0, i - ranges[i]);
-            int end = Math.min(n, i + ranges[i]);
+            int s = Math.max(0, i - ranges[i]);
+            int e = i + ranges[i];
 
-            maxReach[start] = Math.max(maxReach[start], end);
+            maxReach[s] = e;
         }
 
         int taps = 0;
@@ -15,12 +15,10 @@ class Solution {
 
         for (int i = 0; i <= n; i++) {
             if (i > nextEnd) return -1;
-
             if (i > currEnd) {
                 taps++;
                 currEnd = nextEnd;
             }
-
             nextEnd = Math.max(nextEnd, maxReach[i]);
         }
 
