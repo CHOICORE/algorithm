b82a893315680b5daf58648d235b3b50ba833b41
CHOICORE <choijaehyeong93@gmail.com>
2023-09-03 10:13:18 +0900

    Time: 0 ms (100.00%), Space: 38.7 MB (97.79%) - LeetHub

--- LeetCode/Medium/0062-unique-paths/0062-unique-paths.java
+++ LeetCode/Medium/0062-unique-paths/0062-unique-paths.java
@@ -2,7 +2,6 @@ class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
         
-        // 첫 번째 열,행  1로 초기화
         for (int i = 0; i < m; i++) {
             dp[i][0] = 1;
         }
@@ -10,7 +9,6 @@ class Solution {
             dp[0][j] = 1;
         }
         
-        // 나머지 칸들의 값을 계산
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
