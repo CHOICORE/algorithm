c9626fb687934bf3bc43851048e40c03f6b42a53
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-09-07 09:05:34 +0900

    Time: 0 ms (100.00%), Space: 40.3 MB (30.75%) - LeetHub

--- LeetCode/Medium/0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ LeetCode/Medium/0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -1,60 +1,20 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        if (head == null) return null;
-        if (head.next == null) return head;
-        int index = 0;
-        ListNode leftNode = null, leftBehind = null, rightNode = null;
-        ListNode ans = new ListNode();
-        ans.next = head;
-        ListNode dummy = ans.next;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
 
-        while (dummy != null) {
-            index++;
-            if (index + 1 == left)
-                leftBehind = dummy;
-            if (index == left)
-                leftNode = dummy;
-            if (index == right)
-                rightNode = dummy;
-            dummy = dummy.next;
+        for (int i = 0; i < left - 1; i++)
+            prev = prev.next;
+
+        ListNode curr = prev.next;
+        
+        for (int i = 0; i < right - left; i++) {
+            ListNode forw = curr.next;
+            curr.next = forw.next;
+            forw.next = prev.next;
+            prev.next = forw;
         }
-        if (leftBehind != null)
-            leftBehind.next = null;
-        ListNode rightFront = (rightNode.next != null) ? rightNode.next : null;
-        if (rightFront != null)
-            rightNode.next = null;
-        ListNode reverse = reverseList(leftNode);
-        if (leftBehind != null)
-            leftBehind.next = reverse;
-        if (rightFront != null)
-            leftNode.next = rightFront;
-        if (left == 1)
-            return reverse;
-        return ans.next;
-    }
-
-    public ListNode reverseList(ListNode head) {
-        ListNode currentNext = null;
-        ListNode previous = null;
-        ListNode current = head;
-
-        while (current != null) {
-            currentNext = current.next;
-            current.next = previous;
-            previous = current;
-            current = currentNext;
-        }
-
-        return previous;
+        return dummy.next;
     }
 }
