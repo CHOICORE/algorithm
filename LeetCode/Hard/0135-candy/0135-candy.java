4bcfa2c7d3a27e9880f3dabc4b2d8acafb4c6eaf
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-09-13 09:29:19 +0900

    Time: 2 ms (99.93%), Space: 43.9 MB (44.79%) - LeetHub

--- LeetCode/Hard/0135-candy/0135-candy.java
+++ LeetCode/Hard/0135-candy/0135-candy.java
@@ -1,27 +1,19 @@
 class Solution {
-	public int candy(int[] ratings) {
-		int n = ratings.length;
-		int[] candies = new int[n];
+	public int candy(int[] a) {
+		int n = a.length;
 
-		for (int i = 1; i < n; i++) {
-			if (ratings[i - 1] < ratings[i] && candies[i - 1] >= candies[i]) {
-				candies[i] = candies[i - 1] + 1;
-			}
-		}
+		int[] candy = new int[n];
 
+		Arrays.fill(candy, 1);
 
-		for (int i = n - 2; i >= 0; i--) {
-			if (ratings[i + 1] < ratings[i] && candies[i + 1] >= candies[i]) {
-				candies[i] = candies[i + 1] + 1;
-			}
-		}
+		for (int i = 1; i < n; i++) if (a[i] > a[i - 1]) candy[i] = candy[i - 1] + 1;
 
-		int totalCandies = 0;
+		for (int i = n - 1; i > 0; i--) if (a[i - 1] > a[i] && candy[i - 1] <= candy[i]) candy[i - 1] = candy[i] + 1;
 
-		for (int i = 0; i < n; i++) {
-			totalCandies += candies[i] + 1;
-		}
-
-		return totalCandies;
+		int res = 0;
+        
+		for (int i : candy) res += i;
+        
+		return res;
 	}
 }
