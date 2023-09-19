1644e8b2887441153cfbfbcb11c9bc633fea658d
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-09-19 12:06:49 +0900

    Time: 4 ms (87.14%), Space: 56.4 MB (56.16%) - LeetHub

--- LeetCode/Medium/0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ LeetCode/Medium/0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,12 +1,17 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        HashSet<Integer> seen = new HashSet<>();
-        for (int num : nums) {
-            if (seen.contains(num)) {
-                return num;
-            }
-            seen.add(num);
+        int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
         }
-        return -1;
+        while (slow != fast);
+        fast = nums[0];
+        while (fast != slow) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
     }
 }
