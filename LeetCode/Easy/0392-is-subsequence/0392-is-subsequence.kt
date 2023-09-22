66e040b9375f7d0ea6c54b6400adc0acacdf340c
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-09-22 10:02:21 +0900

    Time: 115 ms (98.32%), Space: 34.3 MB (29.16%) - LeetHub

--- LeetCode/Easy/0392-is-subsequence/0392-is-subsequence.kt
+++ LeetCode/Easy/0392-is-subsequence/0392-is-subsequence.kt
@@ -1,19 +1,17 @@
 class Solution {
     fun isSubsequence(s: String, t: String): Boolean {
-        var i = 0
-        var j = 0
-        val n: Int = t.length
-        val m: Int = s.length
-        val sCharArray: CharArray = s.toCharArray()
-        val tCharArray: CharArray = t.toCharArray()
-        if (m < 1) return true
-        while (i < n) {
-            if (tCharArray[i] == sCharArray[j]) {
-                j++
-            }
-            i++
-            if (j == m) return true
+        if (s.isEmpty()) {
+            return true
+        } else if (t.isEmpty()) {
+            return false
         }
-        return false
+        var sIndex = 0
+        var tIndex = 0
+        while (tIndex < t.length && sIndex < s.length) {
+            if (s[sIndex] == t[tIndex++]) {
+                sIndex++
+            }
+        }
+        return sIndex == s.length
     }
 }
