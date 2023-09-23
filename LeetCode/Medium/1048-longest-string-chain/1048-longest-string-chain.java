c6ceef6a7883ea97b7fb930e817f2055ebfdf461
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-09-23 09:55:01 +0900

    Time: 13 ms (100.00%), Space: 43.1 MB (80.02%) - LeetHub

--- LeetCode/Medium/1048-longest-string-chain/1048-longest-string-chain.java
+++ LeetCode/Medium/1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,18 +1,45 @@
 class Solution {
     public int longestStrChain(String[] words) {
-        Arrays.sort(words, Comparator.comparingInt(String::length));
-        HashMap<String, Integer> dp = new HashMap<>();
-        int maxChain = 0;
-        for (String word : words) {
-            dp.put(word, 1);
-            for (int i = 0; i < word.length(); i++) {
-                String prev = word.substring(0, i) + word.substring(i + 1);
-                if (dp.containsKey(prev)) {
-                    dp.put(word, Math.max(dp.get(word), dp.get(prev) + 1));
-                }
-            }
-            maxChain = Math.max(maxChain, dp.get(word));
+        List<String>[] wordLists = new List[17]; // 1~16
+        List<Integer>[] dp = new List[17];
+        for (int i = 1; i < 17; i++) {
+            wordLists[i] = new ArrayList<>();
+            dp[i] = new ArrayList<>();
         }
-        return maxChain;
+        for (String word : words) {
+            wordLists[word.length()].add(word);
+        }
+        for (int i = 0; i < wordLists[1].size(); i++) {
+            dp[1].add(1);
+        }
+        int longestChain = 1;
+        for (int i = 2; i < 17; i++) {
+            List<String> curr = wordLists[i];
+            List<String> prev = wordLists[i - 1];
+            List<Integer> currCache = dp[i];
+            List<Integer> prevCache = dp[i - 1];
+            for (String word : curr) {
+                int maxLength = Integer.MIN_VALUE;
+                for (int k = 0; k < prev.size(); k++) {
+                    if (isPredecessor(prev.get(k), word)) maxLength = Math.max(maxLength, prevCache.get(k));
+                }
+                if (maxLength == Integer.MIN_VALUE) maxLength = 1;
+                else maxLength++;
+                currCache.add(maxLength);
+                longestChain = Math.max(longestChain, maxLength);
+            }
+        }
+        return longestChain;
+    }
+
+    private boolean isPredecessor(String word1, String word2) {
+        boolean misMatch = false;
+        for (int i = 0, j = 0; i < word1.length(); i++, j++) {
+            if (word1.charAt(i) == word2.charAt(j)) continue;
+            if (misMatch) return false;
+            misMatch = true;
+            i--;
+        }
+        return true;
     }
 }
