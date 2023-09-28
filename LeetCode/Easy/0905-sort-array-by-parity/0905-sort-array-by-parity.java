0f1555f516bd9044997531cfcab4385848624a58
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-09-28 16:07:09 +0900

    Time: 0 ms (100.00%), Space: 44.4 MB (19.53%) - LeetHub

--- LeetCode/Easy/0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ LeetCode/Easy/0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -1,15 +1,20 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int i = 0, j = nums.length - 1;
-
+        int i = 0;
+        int j = nums.length - 1;
         while (i < j) {
-            while (i < j && nums[i] % 2 == 0) i++;
-            while (i < j && nums[j] % 2 == 1) j--;
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-        }
+            if (nums[i] % 2 == 0) i++;
 
+            else if (nums[j] % 2 != 0) {
+                j--;
+            } else {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
+            }
+        }
         return nums;
     }
 }
