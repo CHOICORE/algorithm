4ed5e14e370383f64c2c55f4b4b77e3cbc030aed
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-09-29 09:16:11 +0900

    Time: 1 ms (99.82%), Space: 55.6 MB (59.26%) - LeetHub

--- LeetCode/Easy/0896-monotonic-array/0896-monotonic-array.java
+++ LeetCode/Easy/0896-monotonic-array/0896-monotonic-array.java
@@ -1,21 +1,19 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        int i = 0;
-        while (i < nums.length - 2 && nums[i] == nums[i + 1]) i++;
-
-        Boolean cond = (i <= nums.length - 2 && nums[i] <= nums[i + 1]) ? true : false;
-
-        for (i = 0; i < nums.length - 1; i++) {
-            if (cond) {
-                if (!(nums[i] <= nums[i + 1]))
+        int len = nums.length;
+        if (nums[0] < nums[len - 1]) {
+            for (int i = 1; i < len; i++) {
+                if (nums[i] < nums[i - 1]) {
                     return false;
-
-            } else {
-                if (!(nums[i] >= nums[i + 1]))
+                }
+            }
+        } else {
+            for (int i = 1; i < len; i++) {
+                if (nums[i] > nums[i - 1]) {
                     return false;
+                }
             }
         }
         return true;
-
     }
 }
