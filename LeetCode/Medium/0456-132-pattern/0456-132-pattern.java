f97398d907d95a714c801419967ad705d92ae881
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-09-30 09:47:12 +0900

    Time: 7 ms (94.96%), Space: 62.4 MB (94.12%) - LeetHub

--- LeetCode/Medium/0456-132-pattern/0456-132-pattern.java
+++ LeetCode/Medium/0456-132-pattern/0456-132-pattern.java
@@ -1,26 +1,22 @@
 class Solution {
     public boolean find132pattern(int[] nums) {
-        if (nums.length < 3) return false;
-        
-        Stack<Integer> stack = new Stack<>();
+        if (nums.length < 3)
+            return false;
         int[] min = new int[nums.length];
         min[0] = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
+        for (int i = 1; i < nums.length; i++)
             min[i] = Math.min(min[i - 1], nums[i]);
-        }
-
-        for (int i = nums.length - 1; i >= 0; --i) {
-            if (nums[i] > min[i]) {
-                while (!stack.isEmpty() && stack.peek() <= min[i]) {
-                    stack.pop();
-                }
-                if (!stack.isEmpty() && stack.peek() < nums[i]) {
+        for (int j = nums.length - 1, k = nums.length; j >= 0; j--) {
+            if (nums[j] > min[j]) {
+                k = Arrays.binarySearch(nums, k, nums.length, min[j] + 1);
+                if (k < 0)
+                    k = -1 - k;
+                if (k < nums.length && nums[k] < nums[j])
                     return true;
-                }
-                stack.push(nums[i]);
+                nums[--k] = nums[j];
             }
         }
         return false;
+
     }
 }
