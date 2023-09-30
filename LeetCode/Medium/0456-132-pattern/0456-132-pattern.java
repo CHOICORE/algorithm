870cf8730e1e03f8fe1d91c12d13e00f7b6a36e8
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-09-30 09:47:47 +0900

    Time: 3 ms (98.13%), Space: 62.6 MB (88.62%) - LeetHub

--- LeetCode/Medium/0456-132-pattern/0456-132-pattern.java
+++ LeetCode/Medium/0456-132-pattern/0456-132-pattern.java
@@ -1,21 +1,16 @@
 class Solution {
     public boolean find132pattern(int[] nums) {
         if (nums.length < 3) return false;
-        int[] min = new int[nums.length];
-        min[0] = nums[0];
-        for (int i = 1; i < nums.length; i++)
-            min[i] = Math.min(min[i - 1], nums[i]);
-        for (int j = nums.length - 1, k = nums.length; j >= 0; j--) {
-            if (nums[j] > min[j]) {
-                k = Arrays.binarySearch(nums, k, nums.length, min[j] + 1);
-                if (k < 0)
-                    k = -1 - k;
-                if (k < nums.length && nums[k] < nums[j])
-                    return true;
-                nums[--k] = nums[j];
+        int min = Integer.MIN_VALUE;
+        int peak = nums.length;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] < min) return true;
+            while (peak < nums.length && nums[i] > nums[peak]) {
+                min = nums[peak++];
             }
+            peak--;
+            nums[peak] = nums[i];
         }
         return false;
-
     }
 }
