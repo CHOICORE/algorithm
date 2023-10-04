fd129bd781a05ef4fefed945a25f3ac357591e90
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-10-04 09:10:30 +0900

    Time: 32 ms (24.67%), Space: 54.6 MB (11.90%) - LeetHub

--- LeetCode/Easy/0706-design-hashmap/0706-design-hashmap.java
+++ LeetCode/Easy/0706-design-hashmap/0706-design-hashmap.java
@@ -1,43 +1,24 @@
 class MyHashMap {
-    ArrayList<Integer> keys;
-    ArrayList<Integer> values;
+    int[] arr;
 
     public MyHashMap() {
-        keys = new ArrayList<>();
-        values = new ArrayList<>();
-
+        arr = new int[1_000_001];
+        Arrays.fill(arr, -1);
     }
 
     public void put(int key, int value) {
-        if (keys.contains(key)) {
-            int i = keys.indexOf(key);
-            values.set(i, value);
-        } else {
-            keys.add(key);
-            values.add(value);
-        }
+        arr[key] = value;
     }
 
     public int get(int key) {
-        if (!keys.contains(key)) {
-            return -1;
-        } else {
-            int i = keys.indexOf(key);
-            return values.get(i);
-        }
+        return arr[key];
 
     }
 
     public void remove(int key) {
-        if (keys.contains(key)) {
-            int i = keys.indexOf(key);
-            keys.remove(i);
-            values.remove(i);
-        }
-
+        arr[key] = -1;
     }
 }
-
 /**
  * Your MyHashMap object will be instantiated and called as such:
  * MyHashMap obj = new MyHashMap();
