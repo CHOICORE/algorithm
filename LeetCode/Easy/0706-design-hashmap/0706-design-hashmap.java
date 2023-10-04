c66c4e7bc0fb8a5e895c9eca90fbb6435685ab6d
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-10-04 09:12:18 +0900

    Time: 14 ms (90.39%), Space: 49.8 MB (51.06%) - LeetHub

--- LeetCode/Easy/0706-design-hashmap/0706-design-hashmap.java
+++ LeetCode/Easy/0706-design-hashmap/0706-design-hashmap.java
@@ -1,22 +1,88 @@
 class MyHashMap {
-    int[] arr;
+    Node[] nodes;
+    int size = 1_000;
 
     public MyHashMap() {
-        arr = new int[1_000_001];
-        Arrays.fill(arr, -1);
+        this.nodes = new Node[size];
+    }
+
+    public int hash(int key) {
+        return key % size;
     }
 
     public void put(int key, int value) {
-        arr[key] = value;
+        Node newNode = new Node(key, value);
+        int index = hash(key);
+        Node current = nodes[index];
+
+        if (current == null) {
+            nodes[index] = newNode;
+            return;
+        }
+
+        if (current.key == key) {
+            current.value = value;
+            return;
+        }
+
+        while (current.next != null) {
+
+            if (current.next.key == key) {
+                current.next.value = value;
+                return;
+            }
+
+            current = current.next;
+
+        }
+        current.next = newNode;
+
     }
 
     public int get(int key) {
-        return arr[key];
+        int index = hash(key);
+        Node current = nodes[index];
 
+        if (current == null) return -1;
+
+        while (current != null) {
+            if (current.key == key) return current.value;
+            current = current.next;
+        }
+        return -1;
     }
 
     public void remove(int key) {
-        arr[key] = -1;
+        int index = hash(key);
+        Node current = nodes[index];
+        Node previous = null;
+
+        if (current == null) {
+            return;
+        }
+
+        while (current != null) {
+            if (current.key == key) {
+                if (previous == null) {
+                    nodes[index] = current.next;
+                } else previous.next = current.next;
+            }
+            previous = current;
+            current = current.next;
+
+        }
+    }
+
+    static class Node {
+        int key, value;
+        Node next;
+
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
+
     }
 }
 
