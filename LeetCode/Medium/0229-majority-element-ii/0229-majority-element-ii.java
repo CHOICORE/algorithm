0e4c0ff5d2c32717b22fb9becc100069268121d2
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-10-05 20:49:52 +0900

    Time: 2 ms (98.52%), Space: 46.4 MB (64.44%) - LeetHub

--- LeetCode/Medium/0229-majority-element-ii/0229-majority-element-ii.java
+++ LeetCode/Medium/0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,32 +1,48 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
+        int num1 = 0, count1 = 0, num2 = 0, count2 = 0;
 
         for (int num : nums) {
-            if (candidate1 == num) count1++;
-            else if (candidate2 == num) count2++;
-            else if (count1 == 0) {
-                candidate1 = num;
+
+            if (num == num1) {
+                ++count1;
+            } else if (num == num2) {
+                ++count2;
+            } else if (count1 == 0) {
+                num1 = num;
                 count1 = 1;
             } else if (count2 == 0) {
-                candidate2 = num;
+                num2 = num;
                 count2 = 1;
             } else {
-                count1--;
-                count2--;
+                --count1;
+                --count2;
             }
         }
 
-        count1 = count2 = 0;
+        count1 = 0;
+        count2 = 0;
+
         for (int num : nums) {
-            if (num == candidate1) count1++;
-            else if (num == candidate2) count2++;
+            if (num == num1) {
+                ++count1;
+            } else if (num == num2) {
+                ++count2;
+            }
         }
 
-        List<Integer> ans = new ArrayList<>();
-        if (count1 > nums.length / 3) ans.add(candidate1);
-        if (count2 > nums.length / 3) ans.add(candidate2);
+        int required = nums.length / 3;
 
-        return ans;
+        List<Integer> result = new ArrayList<Integer>();
+
+        if (count1 > required) {
+            result.add(num1);
+        }
+
+        if (count2 > required) {
+            result.add(num2);
+        }
+
+        return result;
     }
 }
