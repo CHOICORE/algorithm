67f952c41cffd0f2c986f0f3905b2795477c4be3
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-10-05 20:50:12 +0900

    Time: 0 ms (100.00%), Space: 46.7 MB (47.18%) - LeetHub

--- LeetCode/Medium/0229-majority-element-ii/0229-majority-element-ii.java
+++ LeetCode/Medium/0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,48 +1,46 @@
 class Solution {
+    List<Integer> resp = new ArrayList<>();
+    int pivot = 0;
+
     public List<Integer> majorityElement(int[] nums) {
-        int num1 = 0, count1 = 0, num2 = 0, count2 = 0;
+        int len = nums.length;
+        pivot = len / 3;
+        quickSort(nums, 0, nums.length - 1);
+        return resp;
+    }
 
-        for (int num : nums) {
-
-            if (num == num1) {
-                ++count1;
-            } else if (num == num2) {
-                ++count2;
-            } else if (count1 == 0) {
-                num1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                num2 = num;
-                count2 = 1;
+    public void quickSort(int[] nums, int start, int end) {
+        if (end - start < pivot) {
+            return;
+        }
+        int left = start;
+        int right = end;
+        int i = start + 1;
+        while (right >= i) {
+            if (nums[left] > nums[i]) {
+                swap(nums, left, i);
+                i++;
+                left++;
+            } else if (nums[left] < nums[i]) {
+                if (nums[i] > nums[right]) {
+                    swap(nums, i, right);
+                }
+                right--;
             } else {
-                --count1;
-                --count2;
+                i++;
             }
         }
-
-        count1 = 0;
-        count2 = 0;
-
-        for (int num : nums) {
-            if (num == num1) {
-                ++count1;
-            } else if (num == num2) {
-                ++count2;
-            }
+        if (right - left >= pivot) {
+            resp.add(nums[right]);
         }
+        quickSort(nums, start, left - 1);
+        quickSort(nums, right + 1, end);
 
-        int required = nums.length / 3;
+    }
 
-        List<Integer> result = new ArrayList<Integer>();
-
-        if (count1 > required) {
-            result.add(num1);
-        }
-
-        if (count2 > required) {
-            result.add(num2);
-        }
-
-        return result;
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
