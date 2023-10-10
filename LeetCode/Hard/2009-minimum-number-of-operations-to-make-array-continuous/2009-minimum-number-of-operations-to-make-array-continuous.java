bdc2e707d0011d40713c1007897c145b9e344c5b
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-10-10 10:17:21 +0900

    Time: 36 ms (99.02%), Space: 57.8 MB (51.96%) - LeetHub

--- LeetCode/Hard/2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.java
+++ LeetCode/Hard/2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.java
@@ -1,27 +1,20 @@
 class Solution {
     public int minOperations(int[] nums) {
-        int N = nums.length;
-        int ans = N;
-        int j = 0;
-
+        int n = nums.length;
         Arrays.sort(nums);
-
-        int M = 1;
-        for (int i = 1; i < N; ++i) {
+        int m = 1;
+        for (int i = 1; i < n; ++i) {
             if (nums[i] != nums[i - 1]) {
-                nums[M] = nums[i];
-                M++;
+                nums[m++] = nums[i];
             }
         }
-
-        for (int i = 0; i < M; ++i) {
-            while (j < M && nums[j] < nums[i] + N) {
+        int ans = n;
+        for (int i = 0, j = 0; i < m; ++i) {
+            while (j < m && nums[j] - nums[i] <= n - 1) {
                 ++j;
             }
-
-            ans = Math.min(ans, N - j + i);
+            ans = Math.min(ans, n - (j - i));
         }
-
         return ans;
     }
 }
