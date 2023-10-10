f2942462b7355a0dbb551bff42333cddb115434e
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-10-10 10:16:54 +0900

    Time: 36 ms (99.02%), Space: 58.2 MB (35.29%) - LeetHub

--- LeetCode/Hard/2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.java
+++ LeetCode/Hard/2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.java
@@ -1,46 +1,27 @@
 class Solution {
     public int minOperations(int[] nums) {
-        int n = nums.length;
-        int ans = n;
+        int N = nums.length;
+        int ans = N;
+        int j = 0;
 
-        HashSet<Integer> unique = new HashSet<>();
-        for (int num : nums) {
-            unique.add(num);
-        }
+        Arrays.sort(nums);
 
-        int[] newNums = new int[unique.size()];
-        int index = 0;
-
-        for (int num : unique) {
-            newNums[index++] = num;
-        }
-
-        Arrays.sort(newNums);
-
-        for (int i = 0; i < newNums.length; i++) {
-            int left = newNums[i];
-            int right = left + n - 1;
-            int j = binarySearch(newNums, right);
-            int count = j - i;
-            ans = Math.min(ans, n - count);
-        }
-
-        return ans;
-    }
-
-    public int binarySearch(int[] newNums, int target) {
-        int left = 0;
-        int right = newNums.length;
-
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (target < newNums[mid]) {
-                right = mid;
-            } else {
-                left = mid + 1;
+        int M = 1;
+        for (int i = 1; i < N; ++i) {
+            if (nums[i] != nums[i - 1]) {
+                nums[M] = nums[i];
+                M++;
             }
         }
 
-        return left;
+        for (int i = 0; i < M; ++i) {
+            while (j < M && nums[j] < nums[i] + N) {
+                ++j;
+            }
+
+            ans = Math.min(ans, N - j + i);
+        }
+
+        return ans;
     }
 }
