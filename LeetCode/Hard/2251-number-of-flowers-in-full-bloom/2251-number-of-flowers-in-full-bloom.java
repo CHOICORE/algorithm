f877b78c72fbdda339f63acaf68c1ca7f6da4652
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-10-11 09:28:09 +0900

    Time: 21 ms (100.00%), Space: 78.6 MB (6.92%) - LeetHub

--- LeetCode/Hard/2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.java
+++ LeetCode/Hard/2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.java
@@ -1,31 +1,52 @@
 class Solution {
     public int[] fullBloomFlowers(int[][] flowers, int[] people) {
-        int[] sortedPeople = Arrays.copyOf(people, people.length);
-        Arrays.sort(sortedPeople);
 
-        Arrays.sort(flowers, (a, b) -> Arrays.compare(a, b));
-        Map<Integer, Integer> dic = new HashMap();
-        PriorityQueue<Integer> heap = new PriorityQueue();
-
-        int i = 0;
-        for (int person : sortedPeople) {
-            while (i < flowers.length && flowers[i][0] <= person) {
-                heap.add(flowers[i][1]);
-                i++;
-            }
-
-            while (!heap.isEmpty() && heap.peek() < person) {
-                heap.remove();
-            }
-
-            dic.put(person, heap.size());
+        int m = flowers.length;
+        int[] starts = new int[m], ends = new int[m];
+        for (int i = 0; i < m; i++) {
+            starts[i] = flowers[i][0];
+            ends[i] = flowers[i][1];
         }
+        Arrays.sort(starts);
+        Arrays.sort(ends);
 
-        int[] ans = new int[people.length];
-        for (int j = 0; j < people.length; j++) {
-            ans[j] = dic.get(people[j]);
+        int n = people.length;
+
+        int[] indices = new int[n];
+        for (int i = 0; i < n; i++)
+            indices[i] = i;
+
+        quickSort(indices, people, 0, n - 1);
+
+        int[] result = new int[n];
+        int index1 = 0, index2 = 0;
+        for (int index : indices) {
+            int arrival = people[index];
+            while (index1 < m && starts[index1] <= arrival) index1++;
+            while (index2 < m && ends[index2] < arrival) index2++;
+            result[index] = index1 - index2;
         }
+        return result;
 
-        return ans;
+    }
+
+    public void quickSort(int[] indices, int[] people, int low, int high) {
+
+        if (low >= high) return;
+
+        int left = low - 1, right = high + 1, mid = low + high >> 1;
+        int pivot = people[indices[mid]];
+
+        while (left < right) {
+            while (people[indices[++left]] < pivot) continue;
+            while (people[indices[--right]] > pivot) continue;
+            if (left < right) {
+                int temp = indices[left];
+                indices[left] = indices[right];
+                indices[right] = temp;
+            }
+        }
+        quickSort(indices, people, low, right);
+        quickSort(indices, people, right + 1, high);
     }
 }
