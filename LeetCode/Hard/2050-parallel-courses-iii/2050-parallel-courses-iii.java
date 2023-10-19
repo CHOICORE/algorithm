1794b2eaa35d6590635265b915bac21f7319f8f8
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-10-19 09:02:02 +0900

    Time: 13 ms (99.56%), Space: 67.4 MB (64.00%) - LeetHub

--- LeetCode/Hard/2050-parallel-courses-iii/2050-parallel-courses-iii.java
+++ LeetCode/Hard/2050-parallel-courses-iii/2050-parallel-courses-iii.java
@@ -1,41 +1,62 @@
 class Solution {
-    Map<Integer, List<Integer>> graph = new HashMap<>();
-    Map<Integer, Integer> memo = new HashMap<>();
-    
-    public int minimumTime(int n, int[][] relations, int[] time) {
+    private int[] startNodes;
+    private int[][] graph;
+    private int[] time;
+    private boolean[] visited;
+    private int[] ans;
+
+    private void convert(final int[][] edges, final int n) {
+        final int[] incoming = new int[n], outgoing = new int[n];
+        for (final int[] e : edges) {
+            outgoing[e[0] - 1]++;
+            incoming[e[1] - 1]++;
+        }
+        int startCnt = 0;
+        for (final int i : incoming) {
+            if (i == 0) {
+                startCnt++;
+            }
+        }
+        startNodes = new int[startCnt];
+        for (int sni = 0, i = 0; sni < startNodes.length; i++) {
+            if (incoming[i] == 0) {
+                startNodes[sni++] = i;
+            }
+        }
+        graph = new int[n][];
         for (int i = 0; i < n; i++) {
-            graph.put(i, new ArrayList<>());
+            graph[i] = new int[outgoing[i]];
         }
-        
-        for (int[] edge: relations) {
-            int x = edge[0] - 1;
-            int y = edge[1] - 1;
-            graph.get(x).add(y);
+        for (final int[] e : edges) {
+            graph[e[0] - 1][--outgoing[e[0] - 1]] = e[1] - 1;
         }
-        
-        int ans = 0;
-        for (int node = 0; node < n; node++) {
-            ans = Math.max(ans, dfs(node, time));
+    }
+
+    private int fill(final int node) {
+        if (ans[node] > 0) {
+            return ans[node];
         }
 
-        return ans;
+        int worstPrereq = 0;
+        visited[node] = true;
+        for (final int child : graph[node]) {
+            if (!visited[child]) {
+                worstPrereq = Math.max(fill(child), worstPrereq);
+            }
+        }
+        visited[node] = false;
+        return ans[node] = worstPrereq + time[node];
     }
-    
-    public int dfs(int node, int[] time) {
-        if (memo.containsKey(node)) {
-            return memo.get(node);
+
+    public int minimumTime(int n, int[][] relations, int[] time) {
+        convert(relations, n);
+        this.time = time;
+        ans = new int[n];
+        visited = new boolean[n];
+        int longest = 0;
+        for (final int node : startNodes) {
+            longest = Math.max(longest, fill(node));
         }
-        
-        if (graph.get(node).size() == 0) {
-            return time[node];
-        }
-        
-        int ans = 0;
-        for (int neighbor: graph.get(node)) {
-            ans = Math.max(ans, dfs(neighbor, time));
-        }
-        
-        memo.put(node, time[node] + ans);
-        return time[node] + ans;
+        return longest;
     }
 }
