1d314cae160e1744770a1b07a62d9e7458768be2
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-10-22 17:15:51 +0900

    Time: 2 ms (100.00%), Space: 59.2 MB (32.41%) - LeetHub

--- LeetCode/Hard/1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.java
+++ LeetCode/Hard/1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.java
@@ -1,55 +1,21 @@
 class Solution {
     public int maximumScore(int[] nums, int k) {
-        int ans = solve(nums, k);
-        for (int i = 0; i < nums.length / 2; i++) {
-            int temp = nums[i];
-            nums[i] = nums[nums.length - i - 1];
-            nums[nums.length - i - 1] = temp;
-        }
-
-        return Math.max(ans, solve(nums, nums.length - k - 1));
-    }
-
-    public int solve(int[] nums, int k) {
         int n = nums.length;
-        int[] left = new int[k];
-        int currMin = Integer.MAX_VALUE;
-        for (int i = k - 1; i >= 0; i--) {
-            currMin = Math.min(currMin, nums[i]);
-            left[i] = currMin;
-        }
-
-        List<Integer> right = new ArrayList<>();
-        currMin = Integer.MAX_VALUE;
-        for (int i = k; i < n; i++) {
-            currMin = Math.min(currMin, nums[i]);
-            right.add(currMin);
-        }
-
-        int ans = 0;
-        for (int j = 0; j < right.size(); j++) {
-            currMin = right.get(j);
-            int i = binarySearch(left, currMin);
-            int size = (k + j) - i + 1;
-            ans = Math.max(ans, currMin * size);
-        }
-
-        return ans;
-    }
-
-    public int binarySearch(int[] nums, int num) {
-        int left = 0;
-        int right = nums.length;
-
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] < num) {
-                left = mid + 1;
+        int start = k - 1, end = k + 1, min = nums[k];
+        while (start >= 0 && nums[start] >= min) start--;
+        while (end < n && nums[end] >= min) end++;
+        int max = min * (end - start - 1);
+        while (start >= 0 || end < n) {
+            if (start < 0 || end < n && nums[end] >= nums[start]) {
+                min = nums[end];
+                while (end < n && nums[end] >= min) end++;
+                max = Math.max(max, min * (end - start - 1));
             } else {
-                right = mid;
+                min = nums[start];
+                while (start >= 0 && nums[start] >= min) start--;
+                max = Math.max(max, min * (end - start - 1));
             }
         }
-
-        return left;
+        return max;
     }
 }
