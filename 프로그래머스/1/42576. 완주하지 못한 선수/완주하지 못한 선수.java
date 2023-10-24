f74345cfd6fe8ef440a5d9a76518aeacf47062cd
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-10-24 17:10:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 105.78 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -5,17 +5,18 @@ import java.util.stream.Collectors;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Map<String, Long> participantMap = Arrays.stream(participant)
+        // 선수 이름으로 그룹핑,
+        Map<String, Long> participants = Arrays.stream(participant)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-        Arrays.stream(completion).forEach(c -> participantMap.put(c, participantMap.get(c) - 1));
-        
-        return participantMap
-            .entrySet()
-            .stream()
-            .dropWhile(entry -> entry.getValue() < 1)
-            .findFirst()
-            .get()
-            .getKey();
+        // 완주한 선수 이름으로 계산,
+        for (String key : completion) {
+            participants.computeIfPresent(key, (k, v) -> {
+                if (v == 1) return null;
+                return --v;
+            });
+        }
+        // 완주하지 못한 선수 이름 반환
+        return participants.keySet().iterator().next();
     }
 }
