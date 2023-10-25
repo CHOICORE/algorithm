b3690ce10e7d35948f3ce86ae2a9ffb6cca0a370
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-10-25 09:04:57 +0900

    [level 1] Title: K번째수, Time: 3.81 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,8 +3,10 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         return Arrays.stream(commands)
+            // commands의 [0], [1] 인덱스로 부분 스트림을 자르고 정렬한 후 [2]의 크기만큼의 배열을 반환
             .mapToInt(command -> Arrays.stream(array, command[0] - 1, command[1])
                       .sorted()
+//                      .peek(System.out::println)
                       .toArray()[command[2] - 1])
             .toArray();
     }
