8549475eae057a39222d239d7a8381ff9e94e702
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-10-26 17:28:33 +0900

    [level 2] Title: 주식가격, Time: 19.37 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,20 +3,15 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int size = prices.length;
-        int[] answer = new int[size]; // 결과를 저장할 배열
+        int[] answer = new int[size];
 
         for (int i = 0; i < size; i++) {
             for (int j = i + 1; j < size; j++) {
-                if (prices[i] > prices[j]) {
-                    answer[i] = j - i; // 가격이 떨어지면 해당 기간을 저장하고 종료
+                answer[i]++;
+                if (prices[i] > prices[j])
                     break;
-                }
-            }
-            if (answer[i] == 0) {
-                answer[i] = size - i - 1; // 끝까지 가격이 떨어지지 않으면 남은 기간을 저장
             }
         }
-
         return answer;
     }
 }
