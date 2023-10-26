87d584c019b9f22da8a0cf0a7c436745b03fccd3
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-10-26 15:54:27 +0900

    [level 2] Title: 프로세스, Time: 0.53 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -3,40 +3,29 @@ import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-
-        // 대기 큐에 있는 프로세스의 정보를 저장할 Queue 생성
+        int size = priorities.length - 1;
         Queue<Integer> queue = new LinkedList<>();
+
         for (int priority : priorities) {
             queue.offer(priority);
         }
 
-        // 우선순위가 높은 프로세스가 실행될 때까지 반복
+        Arrays.sort(priorities);
+
         while (!queue.isEmpty()) {
-            int current = queue.poll(); // 대기 큐에서 프로세스 추출
+            int current = queue.poll();
 
-            // 현재 프로세스가 가장 우선순위가 높은지 여부를 저장할 변수
-            boolean isMax = queue
-                    .stream()
-                    .mapToInt(priority -> priority)
-                    .noneMatch(priority -> current < priority);
-
-            // 우선순위가 더 높은 프로세스가 존재하므로 isMax를 false로 설정
-            if (isMax) { // 현재 프로세스가 가장 우선순위가 높은 경우
-                answer++; // 실행된 프로세스의 개수 증가
-                if (location == 0) break; // 반복문 종료
-
-            } else { // 현재 프로세스가 가장 우선순위가 높지 않은 경우
-                queue.offer(current); // 대기 큐의 맨 뒤에 현재 프로세스 추가
-            }
-
-            // location 값 업데이트
-            if (location == 0) {
-                location = queue.size() - 1;
+            if (current == priorities[size - answer]) {
+                answer++;
+                if (location == 0) break;
             } else {
-                location--;
+                queue.offer(current);
+                if (location == 0) location = queue.size();
             }
+
+            location--;
         }
-        
+
         return answer;
     }
 }
