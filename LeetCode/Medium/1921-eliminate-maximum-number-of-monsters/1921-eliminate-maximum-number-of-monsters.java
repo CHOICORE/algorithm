2072eec2a6a610390b55c99909be9995d4ad4803
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-11-07 09:06:34 +0900

    Time: 16 ms (100.00%), Space: 55.1 MB (58.33%) - LeetHub

--- LeetCode/Medium/1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.java
+++ LeetCode/Medium/1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.java
@@ -1,19 +1,11 @@
 class Solution {
     public int eliminateMaximum(int[] dist, int[] speed) {
-        PriorityQueue<Double> heap = new PriorityQueue<>();
-        for (int i = 0; i < dist.length; i++) {
-            heap.add((double) dist[i] / speed[i]);
-        }
-
-        int result = 0;
-        while (!heap.isEmpty()) {
-            if (heap.remove() <= result) {
-                break;
-            }
-
-            result++;
-        }
-
-        return result;
+        for (int i = 0; i < dist.length; ++i)
+            dist[i] = (dist[i] - 1) / speed[i];
+        Arrays.sort(dist);
+        for (int i = 0; i < dist.length; ++i)
+            if (i > dist[i])
+                return i;
+        return dist.length;
     }
 }
