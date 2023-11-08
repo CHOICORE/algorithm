a40e7a6f5fe9e357bf653518404a345870230043
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-11-08 12:00:00 +0900

    Time: 1 ms (95.30%), Space: 40.1 MB (18.34%) - LeetHub

--- LeetCode/Medium/2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.java
+++ LeetCode/Medium/2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
-        int width = Math.abs(sx - fx);
-        int height = Math.abs(sy - fy);
-        if (width == 0 && height == 0 && t == 1) {
+        int x = Math.abs(sx - fx);
+        int y = Math.abs(sy - fy);
+
+        if (x == 0 && y == 0 && t == 1)
             return false;
-        }
-        return t >= Math.max(width, height);
+
+        return t >= Math.max(x, y);
     }
 }
