925487c709a994c307de8b4410b781b9b56f0cbc
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-11-19 14:05:57 +0900

    Time: 7 ms (100.00%), Space: 54.7 MB (45.39%) - LeetHub

--- LeetCode/Medium/1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.java
+++ LeetCode/Medium/1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.java
@@ -1,17 +1,16 @@
 class Solution {
     public int reductionOperations(int[] nums) {
-        Arrays.sort(nums);
-        int answer = 0;
-        int up = 0;
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i - 1]) {
-                up++;
-            }
-
-            answer += up;
+        int[] freq = new int[50001];
+        for (int num : nums) {
+            freq[num]++;
         }
-
+        int answer = 0, operations = 0;
+        for (int i = 50000; i >= 1; i--) {
+            if (freq[i] > 0) {
+                operations += freq[i];
+                answer += operations - freq[i];
+            }
+        }
         return answer;
     }
 }
