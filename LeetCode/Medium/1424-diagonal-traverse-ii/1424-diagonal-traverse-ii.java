e8a5b91f5237cc8d548f3e769fabe9825b7bc8c4
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-11-22 10:02:18 +0900

    Time: 12 ms (100.00%), Space: 59.5 MB (99.89%) - LeetHub

--- LeetCode/Medium/1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.java
+++ LeetCode/Medium/1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.java
@@ -1,32 +1,55 @@
 class Solution {
     public int[] findDiagonalOrder(List<List<Integer>> nums) {
-        Map<Integer, List<Integer>> groups = new HashMap<>();
-        int n = 0;
-        for (int row = nums.size() - 1; row >= 0; row--) {
-            for (int col = 0; col < nums.get(row).size(); col++) {
-                int diagonal = row + col;
-                if (!groups.containsKey(diagonal)) {
-                    groups.put(diagonal, new ArrayList<>());
-                }
-
-                groups.get(diagonal).add(nums.get(row).get(col));
-                n++;
+        if (nums.size() == 1) {
+            List<Integer> list = nums.get(0);
+            int[] a = new int[list.size()];
+            for (int i = 0; i < list.size(); i++) {
+                a[i] = list.get(i);
             }
+            return a;
         }
-
-        int[] ans = new int[n];
+        int[] previousRow = new int[nums.size()];
+        int size = 0;
+        int max = 0;
         int i = 0;
-        int cur = 0;
-
-        while (groups.containsKey(cur)) {
-            for (int num : groups.get(cur)) {
-                ans[i] = num;
-                i++;
+        previousRow[0] = -1;
+        for (List<Integer> num : nums) {
+            size += num.size();
+            max = Math.max(max, num.size());
+            if (i > 0) {
+                if (nums.get(i - 1).size() > 1) {
+                    previousRow[i] = i - 1;
+                } else {
+                    previousRow[i] = previousRow[i - 1];
+                }
             }
-
-            cur++;
+            i++;
         }
 
-        return ans;
+        int[] result = new int[size];
+        int n = nums.size();
+        int m = max;
+
+        int idx = 0;
+        result[idx++] = nums.get(0).get(0);
+
+        for (int rowIdx = 1; rowIdx < n + m - 1; rowIdx++) {
+            int r = rowIdx < n ? rowIdx : n - 1;
+            int diff = rowIdx < n ? 0 : rowIdx + 1 - n;
+            int c = diff;
+
+            while (r >= 0 && c < m) {
+                List<Integer> row = nums.get(r);
+                if (row.size() > c) {
+                    result[idx++] = row.get(c);
+                }
+                int key = previousRow[r];
+                diff = r - key;
+                r = key;
+                c += diff;
+            }
+        }
+
+        return result;
     }
 }
