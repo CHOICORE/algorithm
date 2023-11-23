31d2842a59f6a90ace64a544405e7dbaf3bd8c69
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-11-23 09:55:37 +0900

    Time: 3 ms (100.00%), Space: 44.3 MB (61.81%) - LeetHub

--- LeetCode/Medium/1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
+++ LeetCode/Medium/1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
@@ -1,43 +1,45 @@
 class Solution {
     
-    public Boolean check(int[] arr) {
-        int minElement = Integer.MAX_VALUE;
-        int maxElement = Integer.MIN_VALUE;
-        Set<Integer> arrSet = new HashSet<>();
-
-        for (int num : arr) {
-            minElement = Math.min(minElement, num);
-            maxElement = Math.max(maxElement, num);
-            arrSet.add(num);
+    public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+        List<Boolean> result = new ArrayList<>(l.length);
+        for (int i = 0; i < l.length; i++) {
+            result.add(isArithmetic(nums, l[i], r[i]));
         }
-
-        if ((maxElement - minElement) % (arr.length - 1) != 0) {
-            return false;
-        }
-
-        int diff = (maxElement - minElement) / (arr.length - 1);
-        int curr = minElement + diff;
-
-        while (curr < maxElement) {
-            if (!arrSet.contains(curr)) {
-                return false;
-            }
-
-            curr += diff;
-        }
-
-        return true;
+        return result;
     }
 
-    public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
-        List<Boolean> ans = new ArrayList<>();
-        for (int i = 0; i < l.length; i++) {
-            int[] arr = new int[r[i] - l[i] + 1];
-            System.arraycopy(nums, l[i], arr, 0, arr.length);
 
-            ans.add(check(arr));
+    public boolean isArithmetic(int[] nums, int l, int r) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = l; i <= r; i++) {
+            max = Math.max(nums[i], max);
+            min = Math.min(nums[i], min);
         }
 
-        return ans;
+        int len = r - l + 1;
+
+        if ((max - min) % (len - 1) != 0)
+            return false;
+
+        int diff = (max - min) / (len - 1);
+        if (diff == 0)
+            return true;
+
+        boolean[] visited = new boolean[len];
+
+        for (int i = l; i <= r; i++) {
+            int val = nums[i];
+            if ((val - min) % diff != 0)
+                return false;
+            else {
+                int pos = (val - min) / diff;
+                if (visited[pos])
+                    return false;
+                visited[pos] = true;
+            }
+        }
+        return true;
     }
 }
