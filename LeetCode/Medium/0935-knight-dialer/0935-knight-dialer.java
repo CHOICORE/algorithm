01b8a50b219719161b9838e02653d4b22582a6a5
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-11-27 09:01:27 +0900

    Time: 1 ms (100.00%), Space: 39.6 MB (94.29%) - LeetHub

--- LeetCode/Medium/0935-knight-dialer/0935-knight-dialer.java
+++ LeetCode/Medium/0935-knight-dialer/0935-knight-dialer.java
@@ -1,46 +1,36 @@
 class Solution {
-    int[][] memo;
-    int n;
-    int MOD = (int) 1e9 + 7;
-    int[][] jumps = {
+    static final int mod = (int) 1e9 + 7;
+    static final int[][] MOVES = {
             {4, 6},
             {6, 8},
             {7, 9},
             {4, 8},
-            {3, 9, 0},
+            {0, 3, 9},
             {},
-            {1, 7, 0},
+            {0, 1, 7},
             {2, 6},
             {1, 3},
             {2, 4}
     };
-
-    public int dp(int remain, int square) {
-        if (remain == 0) {
-            return 1;
-        }
-
-        if (memo[remain][square] != 0) {
-            return memo[remain][square];
-        }
-
-        int ans = 0;
-        for (int nextSquare : jumps[square]) {
-            ans = (ans + dp(remain - 1, nextSquare)) % MOD;
-        }
-
-        memo[remain][square] = ans;
-        return ans;
-    }
+    static final int[][] cache = new int[5001][10];
 
     public int knightDialer(int n) {
-        this.n = n;
-        memo = new int[n + 1][10];
-        int ans = 0;
-        for (int square = 0; square < 10; square++) {
-            ans = (ans + dp(n - 1, square)) % MOD;
-        }
+        return knightDialer(n, new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
+    }
 
-        return ans;
+    int knightDialer(int remaining, int[] nextNumbers) {
+        if (remaining == 1) return nextNumbers.length;
+        int count = 0;
+
+        for (int nextNumber : nextNumbers) {
+            int cur = cache[remaining][nextNumber];
+            if (cur == 0) {
+                cur = knightDialer(remaining - 1, MOVES[nextNumber]);
+                cache[remaining][nextNumber] = cur;
+            }
+            count += cur;
+            count %= mod;
+        }
+        return count;
     }
 }
