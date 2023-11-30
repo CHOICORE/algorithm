1bb631f4049dde6e56631eeef6725050b6daf2ec
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-11-30 20:14:26 +0900

    [level unrated] Title: 암호 해독, Time: 0.03 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/unrated/120892. 암호 해독/암호 해독.java
@@ -1,17 +1,12 @@
 class Solution {
     public String solution(String cipher, int code) {
         
-        int length = cipher.length();
-        
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i = 1; i <= length; i++) {
-            int index = code * i;
-            if (index <= length) {
-                sb.append(cipher.charAt(index - 1));
-            }
+        StringBuilder answer = new StringBuilder();
 
+        for (int i = code; i <= cipher.length(); i = i + code) {
+            answer.append(cipher.charAt(i - 1));
         }
-        return sb.toString();
+
+        return answer.toString();
     }
 }
