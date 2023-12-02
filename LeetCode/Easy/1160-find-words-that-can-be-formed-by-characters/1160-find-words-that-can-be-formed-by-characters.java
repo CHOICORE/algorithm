8c2c44e1705215158b77f84867bb55ae6282d61e
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-12-02 09:22:17 +0900

    Time: 7 ms (71.48%), Space: 44.6 MB (15.19%) - LeetHub

--- LeetCode/Easy/1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
+++ LeetCode/Easy/1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
@@ -1,30 +1,25 @@
 class Solution {
     public int countCharacters(String[] words, String chars) {
-        Map<Character, Integer> counts = new HashMap<>();
-        for (Character c : chars.toCharArray()) {
-            counts.put(c, counts.getOrDefault(c, 0) + 1);
+        int[] counts = new int[26];
+        for (int i = 0; i < chars.length(); i++) {
+            counts[chars.charAt(i) - 'a']++;
         }
-
-        int ans = 0;
-        for (String word : words) {
-            Map<Character, Integer> wordCount = new HashMap<>();
-            for (Character c : word.toCharArray()) {
-                wordCount.put(c, wordCount.getOrDefault(c, 0) + 1);
-            }
-
-            boolean good = true;
-            for (Character c : wordCount.keySet()) {
-                if (counts.getOrDefault(c, 0) < wordCount.get(c)) {
-                    good = false;
+        int res = 0;
+        for (String s : words) {
+            boolean result = true;
+            int[] c = new int[26];
+            for (int i = 0; i < s.length(); i++) {
+                int x = s.charAt(i) - 'a';
+                c[x]++;
+                if (c[x] > counts[x]) {
+                    result = false;
                     break;
                 }
             }
-
-            if (good) {
-                ans += word.length();
-            }
+            if (result)
+                res += s.length();
         }
-
-        return ans;
+        return res;
     }
+
 }
