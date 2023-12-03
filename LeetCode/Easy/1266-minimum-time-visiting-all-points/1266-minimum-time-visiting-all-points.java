4fd306e48857cf7078fff7db8438a805e98051be
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-12-03 12:39:27 +0900

    Time: 0 ms (100.00%), Space: 43.6 MB (6.01%) - LeetHub

--- LeetCode/Easy/1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
+++ LeetCode/Easy/1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
@@ -1,14 +1,19 @@
 class Solution {
+
+    public int toTime(int[] from, int[] to) {
+        int xDiff = Math.abs(from[0] - to[0]);
+        int yDiff = Math.abs(from[1] - to[1]);
+
+        return Math.max(xDiff, yDiff);
+    }
+
     public int minTimeToVisitAllPoints(int[][] points) {
-        int ans = 0;
-        for (int i = 0; i < points.length - 1; i++) {
-            int currX = points[i][0];
-            int currY = points[i][1];
-            int targetX = points[i + 1][0];
-            int targetY = points[i + 1][1];
-            ans += Math.max(Math.abs(targetX - currX), Math.abs(targetY - currY));
+        int time = 0;
+
+        for (int i = 1; i < points.length; i++) {
+            time += toTime(points[i - 1], points[i]);
         }
 
-        return ans;
+        return time;
     }
 }
