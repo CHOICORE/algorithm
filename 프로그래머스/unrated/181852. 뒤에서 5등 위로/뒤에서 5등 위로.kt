0a2d6d1224a4d94ad93380044ffb900815f4a4ca
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-12-04 21:25:17 +0900

    [level unrated] Title: 뒤에서 5등 위로, Time: 20.50 ms, Memory: 63.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.kt
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.kt
@@ -1,5 +1,3 @@
 class Solution {
-    fun solution(num_list: IntArray): IntArray {
-        return num_list.sorted().slice(5..num_list.size - 1).toIntArray()
-    }
+    fun solution(num_list: IntArray): IntArray = num_list.sorted().drop(5).toIntArray()
 }
