c321d7da1f0f817e5e1600e4b4455c66758bf129
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-12-07 11:47:24 +0900

    Time: 1 ms (100.00%), Space: 44.5 MB (47.65%) - LeetHub

--- LeetCode/Easy/1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.java
+++ LeetCode/Easy/1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.java
@@ -1,11 +1,19 @@
+import java.math.BigInteger;
+
 class Solution {
     public String largestOddNumber(String num) {
-        for (int i = num.length() - 1; i >= 0; i--) {
-            if (Character.getNumericValue(num.charAt(i)) % 2 != 0) {
-                return num.substring(0, i + 1);
-            }
+
+        int number = 0;
+        final int size = num.length();
+        for (int i = size - 1; i >= 0; i--) {
+            number = (int) num.codePointAt(i) - 48;
+            if ((number ^ 1) == (number + 1))
+                continue;
+
+            return num.substring(0, i + 1);
         }
 
         return "";
+
     }
 }
