96ee89c342490744512785d45084ef5e8204c2c7
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-12-13 16:32:21 +0900

    Time: 0 ms (100.00%), Space: 44.3 MB (24.24%) - LeetHub

--- LeetCode/Easy/1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
+++ LeetCode/Easy/1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
@@ -1,30 +1,35 @@
 class Solution {
     public int numSpecial(int[][] mat) {
-        int m = mat.length;
-        int n = mat[0].length;
-        int[] rowCount = new int[m];
-        int[] colCount = new int[n];
-
-        for (int row = 0; row < m; row++) {
-            for (int col = 0; col < n; col++) {
-                if (mat[row][col] == 1) {
-                    rowCount[row]++;
-                    colCount[col]++;
-                }
-            }
-        }
 
         int answer = 0;
-        for (int row = 0; row < m; row++) {
-            for (int col = 0; col < n; col++) {
-                if (mat[row][col] == 1) {
-                    if (rowCount[row] == 1 && colCount[col] == 1) {
-                        answer++;
-                    }
-                }
-            }
+
+        for (int i = 0; i < mat.length; i++) {
+            int index = checkRow(mat, i);
+            if (index >= 0 && checkColumn(mat, i, index))
+                answer++;
         }
 
         return answer;
+    }
+
+    private int checkRow(int[][] mat, int i) {
+        int index = -1;
+        for (int j = 0; j < mat[0].length; j++) {
+            if (mat[i][j] == 1) {
+                if (index >= 0)
+                    return -1;
+                else
+                    index = j;
+            }
+        }
+        return index;
+    }
+
+    private boolean checkColumn(int[][] mat, int i, int index) {
+        for (int j = 0; j < mat.length; j++) {
+            if (mat[j][index] == 1 && j != i)
+                return false;
+        }
+        return true;
     }
 }
