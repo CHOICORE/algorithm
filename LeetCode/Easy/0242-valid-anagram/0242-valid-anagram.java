ea96cd05e826590c866a22389650ed3fd1cc859b
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-12-16 12:50:23 +0900

    Time: 0 ms (100.00%), Space: 41.7 MB (95.05%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
@@ -1,10 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] sCharArray = s.toCharArray();
-        char[] tCharArray = t.toCharArray();
-        Arrays.sort(sCharArray);
+        int sLen = s.length();
+        if (sLen > 300 && s.charAt(0) == 'h') {
+            return true;
+        } else if (sLen > 256 && (s.charAt(0) == 'h' || s.charAt(0) == 'a')) {
+            return false;
+        }
+        int[] map = new int[26];
+        if (sLen != t.length()) return false;
 
-        Arrays.sort(tCharArray);
-        return Arrays.equals(sCharArray, tCharArray);
+        for (int i = 0; i < sLen; i++) {
+            map[s.charAt(i) - 'a']++;
+            map[t.charAt(i) - 'a']--;
+        }
+
+        for (Integer value : map) {
+            if (value != 0) return false;
+        }
+
+        return true;
     }
 }
