550aaee8d3b8cd34c70b7572f9cad1cf21fb8844
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-12-21 12:47:21 +0900

    Time: 13 ms (98.08%), Space: 73.2 MB (5.00%) - LeetHub

--- LeetCode/Medium/1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.java
+++ LeetCode/Medium/1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.java
@@ -1,16 +1,22 @@
 class Solution {
     public int maxWidthOfVerticalArea(int[][] points) {
-        int max = 0;
-        int n = points.length;
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        int[] arr = new int[points.length];
+
+        for (int i = 0; i < points.length; i++) {
             arr[i] = points[i][0];
         }
+
         Arrays.sort(arr);
-        for (int j = 0; j < n - 1; j++) {
-            int diff = arr[j + 1] - arr[j];
-            max = Math.max(max, diff);
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            int tmp = arr[i] - arr[i - 1];
+            if (tmp > max) {
+                max = tmp;
+            }
         }
+
         return max;
     }
 }
