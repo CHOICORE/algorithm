d5318e919ab095c4af6d911f34d5d4a35525d7af
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-12-23 16:18:08 +0900

    Time: 5 ms (42.95%), Space: 41.9 MB (7.05%) - LeetHub

--- LeetCode/Easy/1496-path-crossing/1496-path-crossing.java
+++ LeetCode/Easy/1496-path-crossing/1496-path-crossing.java
@@ -1,32 +1,37 @@
 class Solution {
     public boolean isPathCrossing(String path) {
-        Map<Character, Pair<Integer, Integer>> moves = new HashMap<>();
-        moves.put('N', new Pair(0, 1));
-        moves.put('S', new Pair(0, -1));
-        moves.put('W', new Pair(-1, 0));
-        moves.put('E', new Pair(1, 0));
-
-        Set<Pair<Integer, Integer>> visited = new HashSet<>();
-        visited.add(new Pair(0, 0));
-
+        Set<String> set = new HashSet<>();
+        set.add("0-0");
         int x = 0;
         int y = 0;
 
-        for (Character c : path.toCharArray()) {
-            Pair<Integer, Integer> curr = moves.get(c);
-            int dx = curr.getKey();
-            int dy = curr.getValue();
-            x += dx;
-            y += dy;
-
-            Pair<Integer, Integer> pair = new Pair(x, y);
-            if (visited.contains(pair)) {
+        for (char c : path.toCharArray()) {
+            switch (c) {
+                case 'N':
+                    y++;
+                    break;
+                case 'S':
+                    y--;
+                    break;
+                case 'W':
+                    x--;
+                    break;
+                case 'E':
+                    x++;
+                    break;
+                default:
+                    break;
+            }
+            String coordinates = getCoordinates(x, y);
+            if (set.contains(coordinates)) {
                 return true;
             }
-
-            visited.add(pair);
+            set.add(coordinates);
         }
-
         return false;
+    }
+
+    private String getCoordinates(int x, int y) {
+        return x + "-" + y;
     }
 }
