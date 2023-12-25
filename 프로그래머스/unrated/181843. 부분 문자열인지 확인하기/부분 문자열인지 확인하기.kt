1a8e34825d2c572a818c592b09cc5550414b2fee
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-12-25 10:44:37 +0900

    [level unrated] Title: 부분 문자열인지 확인하기, Time: 6.31 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.kt
+++ 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.kt
@@ -1,5 +1,3 @@
 class Solution {
-    fun solution(my_string: String, target: String): Int {
-        return if (my_string.contains(target)) 1 else 0
-    }
+    fun solution(my_string: String, target: String): Int = if (my_string.contains(target)) 1 else 0
 }
