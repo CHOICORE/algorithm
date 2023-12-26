ace2927e901a12622257e1718bc711821bfd6f25
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-12-26 10:19:58 +0900

    Time: 3 ms (99.64%), Space: 40.9 MB (54.45%) - LeetHub

--- LeetCode/Medium/1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ LeetCode/Medium/1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,26 +1,32 @@
-import java.util.Arrays;
-
 class Solution {
-    final int mod = (int) Math.pow(10, 9) + 7;
+    int[][] dp;
+    int MOD = (int) 1e9 + 7;
 
     public int numRollsToTarget(int n, int k, int target) {
-        int[][] dp = new int[n + 1][target + 1];
-        for (int[] d : dp) {
-            Arrays.fill(d, -1);
-        }
-        return recursion(dp, n, k, target);
+        if (n == 0 && target == 0)
+            return 1;
+        if (target < n || n * k < target)
+            return 0;
+        dp = new int[n + 1][target + 1];
+        return f(n, k, target);
     }
 
-    private int recursion(int[][] dp, int n, int k, int target) {
-        if (target == 0 && n == 0) return 1;
-        if (n == 0 || target <= 0) return 0;
-
-        if (dp[n][target] != -1) return (int) (dp[n][target] % mod);
-        int ways = 0;
-        for (int i = 1; i <= k; i++) {
-            ways = (ways + recursion(dp, n - 1, k, target - i)) % mod;
+    public int f(int n, int k, int target) {
+        if (n == 0 && target == 0) {
+            return 1;
         }
-        dp[n][target] = ways % mod;
-        return dp[n][target];
+        if (target < n || n * k < target) {
+            return 0;
+        }
+        if (dp[n][target] != 0) {
+            return dp[n][target];
+        }
+        int answer = 0;
+        for (int i = 1; i <= k; i++) {
+            if (target < i) break;
+            answer = (answer + f(n - 1, k, target - i) % MOD) % MOD;
+        }
+        dp[n][target] = answer;
+        return answer;
     }
 }
