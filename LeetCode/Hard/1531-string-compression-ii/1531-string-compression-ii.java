7830b4cfeedaaf79bfcf6deb11e86799b977a0cc
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2023-12-28 16:43:25 +0900

    Time: 25 ms (84.78%), Space: 42.6 MB (73.91%) - LeetHub

--- LeetCode/Hard/1531-string-compression-ii/1531-string-compression-ii.java
+++ LeetCode/Hard/1531-string-compression-ii/1531-string-compression-ii.java
@@ -1,29 +1,60 @@
 class Solution {
+    int result = Integer.MAX_VALUE;
+    private int[][] dp;
+    private char[] chars;
+    private int n;
+
     public int getLengthOfOptimalCompression(String s, int k) {
-        int n = s.length();
-        int[][] dp = new int[110][110];
-        for (int i = 0; i <= n; i++)
-            for (int j = 0; j <= n; j++)
-                dp[i][j] = 9_999;
-        dp[0][0] = 0;
+        this.chars = s.toCharArray();
+        this.n = s.length();
+        this.dp = new int[n][k + 1];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        return dp(0, k);
+    }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= k; j++) {
-                int count = 0, del = 0;
-                for (int l = i; l >= 1; l--) {
-                    if (s.charAt(l - 1) == s.charAt(i - 1))
-                        count++;
-                    else
-                        del++;
-
-                    if (j - del >= 0) {
-                        dp[i][j] = Math.min(dp[i][j], dp[l - 1][j - del] + 1 + (count >= 100 ? 3 : count >= 10 ? 2 : count >= 2 ? 1 : 0));
-                    }
+    private void backtracking(List<Integer> data, int index, int k) {
+        if (index == data.size() || k == 0) {
+            int tmp = 0;
+            for (int i = 0; i < data.size(); i++) {
+                if (data.get(i) != 0) {
+                    tmp += (1 + ((data.get(i) != 1) ? String.valueOf(data.get(i)).length() : 0));
                 }
-                if (j > 0)
-                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1]);
+            }
+            result = Math.min(result, tmp);
+            return;
+        }
+        for (int i = 0; i <= Math.min(k, data.get(index)); i++) {
+            if (k - i >= 0) {
+                int count = data.get(index);
+                data.set(index, count - i);
+                backtracking(data, index + 1, k - i);
+                data.set(index, count + i);
             }
         }
-        return dp[n][k];
+    }
+
+    private int dp(int i, int k) {
+        if (k < 0) return n;
+        if (n <= i + k) return 0;
+
+        int result = dp[i][k];
+        if (result != -1) return result;
+        result = dp(i + 1, k - 1);
+        int length = 0, same = 0, diff = 0;
+
+        for (int j = i; j < n && diff <= k; j++) {
+
+            if (chars[j] == chars[i]) {
+                same++;
+                if (same <= 2 || same == 10 || same == 100) length++;
+            } else {
+                diff++;
+            }
+            result = Math.min(result, length + dp(j + 1, k - diff));
+        }
+        dp[i][k] = result;
+        return result;
     }
 }
