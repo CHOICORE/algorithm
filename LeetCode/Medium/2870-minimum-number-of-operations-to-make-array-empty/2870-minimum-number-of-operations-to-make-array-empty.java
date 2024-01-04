67008daedf87e26545d6f7665adeb915ac782967
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-01-04 11:15:41 +0900

    Time: 5 ms (100.00%), Space: 57.9 MB (34.72%) - LeetHub

--- LeetCode/Medium/2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
+++ LeetCode/Medium/2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
@@ -1,16 +1,25 @@
+import java.util.Arrays;
+
 class Solution {
     public int minOperations(int[] nums) {
-        var counter = new HashMap<Integer, Integer>();
-        for (int num : nums) {
-            counter.put(num, counter.getOrDefault(num, 0) + 1);
-        }
+        Arrays.sort(nums);
+
         int answer = 0;
-        for (int c : counter.values()) {
-            if (c == 1) {
-                return -1;
+        int tmp = 0;
+        while (tmp < nums.length) {
+            int i = tmp;
+
+            while (i < nums.length && nums[i] == nums[tmp]) {
+                i++;
             }
-            answer += (int) Math.ceil((double) c / 3);
+            int count = i - tmp;
+            if (count == 1) return -1;
+            answer += count / 3;
+
+            if (count % 3 != 0) answer++;
+            tmp = i;
         }
         return answer;
+
     }
 }
