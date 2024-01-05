d040e50ed89f75b44697c6df98dbdb2a8666d31e
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-01-05 09:00:13 +0900

    [level unrated] Title: 등차수열의 특정한 항만 더하기, Time: 0.02 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -1,16 +1,10 @@
 class Solution {
     public int solution(int a, int d, boolean[] included) {
         int answer = 0;
-        int size = included.length;
-        int[] arr = new int[size];
         
-        for (int i = 0; i < size; i++) {
-            arr[i] = a + d * i;
-        }
-        
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < included.length; i++) {
             if (included[i]) {
-                answer += arr[i];
+                answer += a + d * i;
             }
         }
         
