a6be5f0ce74e2547c47528f5ca7026444d5468d8
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-01-09 10:22:52 +0900

    Time: 0 ms (100.00%), Space: 41.8 MB (6.61%) - LeetHub

--- LeetCode/Easy/0872-leaf-similar-trees/0872-leaf-similar-trees.java
+++ LeetCode/Easy/0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -15,8 +15,8 @@
  */
 class Solution {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        List<Integer> leaves1 = new ArrayList();
-        List<Integer> leaves2 = new ArrayList();
+        List<Integer> leaves1 = new ArrayList<>();
+        List<Integer> leaves2 = new ArrayList<>();
         dfs(root1, leaves1);
         dfs(root2, leaves2);
         return leaves1.equals(leaves2);
@@ -24,8 +24,9 @@ class Solution {
 
     public void dfs(TreeNode node, List<Integer> leafValues) {
         if (node != null) {
-            if (node.left == null && node.right == null)
+            if (node.left == null && node.right == null) {
                 leafValues.add(node.val);
+            }
             dfs(node.left, leafValues);
             dfs(node.right, leafValues);
         }
