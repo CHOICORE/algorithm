a9d3da1772c210caeaeaa992544ec913b468e2b9
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-01-11 08:21:28 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,15 +1,14 @@
 class Solution {
     public int solution(int a, int b, int c) {
         int answer = 0;
-
+        
         if (a == b && b == c) {
-            answer = (a + b + c) * ((a * a) + (b * b) + (c * c)) * ((a * a * a) + (b * b * b) + (c * c * c));
-        } else if (a == b || b == c || a == c) {
-            answer = (a + b + c) * ((a * a) + (b * b) + (c * c));
+            return (int) ((a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2))
+                    * (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3)));
+        } else if (a == b || a == c || b == c) {
+            return (int) ((a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)));
         } else {
-            answer = a + b + c;
+            return a + b + c;
         }
-
-        return answer;
     }
 }
