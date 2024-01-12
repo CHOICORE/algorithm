9148a393e9f0d6cf71e0622c57dca2391e5266e5
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-01-12 20:11:03 +0900

    [level Lv.0] Title: 원소들의 곱과 합, Time: 0.02 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/Lv.0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,20 +1,14 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int[] num_list) {
-        
-        int answer = -1;
+        int multiply = 1;
+        int sum = 0;
 
-        int sum = Arrays.stream(num_list).sum();
-
-        int multiply = Arrays.stream(num_list).reduce(1, (a, b) -> a * b);
-
-        if (multiply < sum * sum) {
-            answer = 1;
-        } else if (multiply > sum * sum) {
-            answer = 0;
+        for (int i : num_list) {
+            multiply *= i;
+            sum += i;
         }
+        sum = sum * sum;
 
-        return answer;
+        return (multiply < sum) ? 1 : 0;
     }
 }
