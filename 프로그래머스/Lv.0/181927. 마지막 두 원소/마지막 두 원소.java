0aceab1b30c53601f0afb6190b9a3917ad5725c6
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-01-16 10:18:03 +0900

    [level Lv.0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/Lv.0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -2,16 +2,19 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = Arrays.copyOf(num_list, num_list.length + 1);
-        int last = num_list[num_list.length - 1];
-        int beforeLast = num_list[num_list.length - 2];
+        int len = num_list.length;
+
+        int[] answer = Arrays.copyOf(num_list, len + 1);
+
+        int last = num_list[len - 1];
+        int beforeLast = num_list[len - 2];
 
         if (last > beforeLast) {
-            answer[num_list.length] = last - beforeLast;;
+            answer[len] = last - beforeLast;
         } else {
-            answer[num_list.length] = last * 2;
+            answer[len] = last * 2;
         }
-        
+
         return answer;
     }
 }
