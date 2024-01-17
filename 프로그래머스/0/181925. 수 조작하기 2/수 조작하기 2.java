35830642753f23924f89d915a794a9a0fbe3d999
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-01-18 08:44:23 +0900

    [level 0] Title: 수 조작하기 2, Time: 8.63 ms, Memory: 95.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(int[] numLog) {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 1; i < numLog.length; i++) {
+            int diff = numLog[i] - numLog[i - 1];
+            switch (diff) {
+                case 1 -> answer.append("w");
+                case -1 -> answer.append("s");
+                case 10 -> answer.append("d");
+                case -10 -> answer.append("a");
+            }
+        }
+
+        return answer.toString();
+    }
+}
