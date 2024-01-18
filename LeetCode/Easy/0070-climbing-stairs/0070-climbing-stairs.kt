70e078bc28cfe402416260af48d97eb68448f79f
CHOICORE <choijaehyeong93@gmail.com>
2024-01-18 19:35:51 +0900

    Time: 123 ms (66.71%), Space: 33.5 MB (20.03%) - LeetHub

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.kt
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.kt
@@ -1,13 +1,15 @@
 class Solution {
     fun climbStairs(n: Int): Int {
-        val m = IntArray(10_000)
-        m[1] = 1
-        m[2] = 2
+        if (n <= 1) return 1
+        var last = 1
+        var beforeLast = 2
 
         for (i in 3..n) {
-            m[i] = m[i - 1] + m[i - 2]
+            val tmp = last + beforeLast
+            last = beforeLast
+            beforeLast = tmp
         }
 
-        return m[n]
+        return beforeLast
     }
 }
