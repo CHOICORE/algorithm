ead3ab4a1ee58bc3c23e6ac74858e3496738f15e
CHOICORE <choijaehyeong93@gmail.com>
2024-01-18 19:36:57 +0900

    Time: 0 ms (100.00%), Space: 42.4 MB (5.02%) - LeetHub

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
+        int[] m = new int[10_000];
+        m[1] = 1;
+        m[2] = 2;
 
-        // DP(Dynamic Programming) 배열 초기화
-        int[] dp = new int[n+1]; // i번째 계단까지 오는 데 필요한 경우의 수 초기화      
-        dp[0] = 1; // 0번째 계단까지 오는 경우의 수 (아무것도 안 한 경우)
-        dp[1] = 1; // 1번째 계단까지 오는 경우의 수 (한 칸 올라간 경우)
-
-        // dp 배열 채우기
-        for (int i = 2; i <= n; i++) {
-            // i번째 계단까지 오는 경우의 수는 (i-1)번째 계단에서 1칸 오른 경우의 수와 (i-2)번째 계단에서 2칸 오른 경우의 수를 더한 것과 같다.
-            dp[i] = dp[i-1] + dp[i-2]; 
+        for (int i = 3; i <= n; i++) {
+            m[i] = m[i - 1] + m[i - 2];
         }
 
-        return dp[n]; // n번째 계단까지 오는 경우의 수 반환
+        return m[n];
     }
 }
