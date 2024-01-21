0d79ffef9cc09e86b5ff8418e651b95ea7deb626
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-01-21 18:45:02 +0900

    Time: 137 ms (72.44%), Space: 34.1 MB (41.19%) - LeetHub

--- LeetCode/Medium/0198-house-robber/0198-house-robber.kt
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.kt
@@ -1,15 +1,13 @@
 class Solution {
     fun rob(nums: IntArray): Int {
-        val n = nums.size
-        if (n < 2) return nums[0]
-
-        val dp = IntArray(n)
-        dp[0] = nums[0]
-        dp[1] = max(nums[0].toDouble(), nums[1].toDouble()).toInt()
-        for (i in 2 until n) {
-            dp[i] = max(dp[i - 1].toDouble(), (dp[i - 2] + nums[i]).toDouble()).toInt()
+        var last = 0
+        var prev = 0
+        var current = 0
+        for (index in nums.indices) {
+            current = (prev + nums[index]).coerceAtLeast(last)
+            prev = last
+            last = current
         }
-
-        return dp[n - 1]
+        return last;
     }
 }
