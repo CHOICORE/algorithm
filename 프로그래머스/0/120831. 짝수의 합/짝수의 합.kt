dad76ec1d2614bb02c2fb187519207a1618e9dbc
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-01-26 20:02:47 +0900

    [level 0] Title: 짝수의 합, Time: 8.14 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.kt
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.kt
@@ -1,15 +1,3 @@
 class Solution {
-    fun solution(n: Int): Int {
-        var answer: Int = 0
-        var sum = n
-        
-        while (sum > 0) {
-            if (sum % 2 == 0) {
-                answer += sum
-            }
-            sum--
-        }
-
-        return answer
-    }
+    fun solution(n: Int): Int = (0..n).filter { it % 2 == 0 }.sum()
 }
