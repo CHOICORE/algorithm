768419a1291b89e777c685a25d5e45b209c30067
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-01-26 19:45:59 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.08 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,19 +1,13 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-        int[] answer = new int[2];
-
-        int numer3 = denom1 * denom2;
-        int denom3 = numer1 * denom2 + numer2 * denom1;
-        int max = 1;
-
-        for (int i = 1; i <= numer3 && i <= denom3; i++) {
+        int denom3 = denom1 * denom2;
+        int numer3 = denom1 * numer2 + denom2 * numer1;
+        for (int i = denom3; i >= 1; i--) {
             if (numer3 % i == 0 && denom3 % i == 0) {
-                max = i;
+                numer3 /= i;
+                denom3 /= i;
             }
         }
-        answer[0] = denom3 / max;
-        answer[1] = numer3 / max;
-
-        return answer;
+        return new int[]{numer3, denom3};
     }
 }
