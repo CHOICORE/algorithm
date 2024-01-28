efb38a09d83aefbd7e9890a49cd814b0a548b470
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-01-28 12:40:25 +0900

    Time: 17 ms (99.71%), Space: 64.8 MB (5.20%) - LeetHub

--- LeetCode/Hard/1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
+++ LeetCode/Hard/1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
@@ -1,30 +1,51 @@
 class Solution {
+    static final int BASE_SUM = -10_000_000;
+    static byte[] map = new byte[20_000_001];
+
     public int numSubmatrixSumTarget(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
+        int rowLastIdx = matrix.length - 1;
+        int colLastIdx = matrix[0].length - 1;
+        int result = 0;
 
-        for (int row = 0; row < m; row++) {
-            for (int col = 1; col < n; col++) {
-                matrix[row][col] += matrix[row][col - 1];
-            }
-        }
-
-        int count = 0;
-
-        for (int c1 = 0; c1 < n; c1++) {
-            for (int c2 = c1; c2 < n; c2++) {
-                Map<Integer, Integer> map = new HashMap<>();
-                map.put(0, 1);
+        int[][] colSums;
+        if (rowLastIdx <= colLastIdx) {                 // Height <= width
+            colSums = new int[rowLastIdx + 1][colLastIdx + 2];
+            for (int row = 0; row <= rowLastIdx; row++) {
                 int sum = 0;
-
-                for (int[] ints : matrix) {
-                    sum += ints[c2] - (c1 > 0 ? ints[c1 - 1] : 0);
-                    count += map.getOrDefault(sum - target, 0);
-                    map.put(sum, map.getOrDefault(sum, 0) + 1);
-                }
+                for (int col = colLastIdx; col >= 0; col--)
+                    sum = colSums[row][col] = sum + matrix[row][col];
+            }
+        } else {
+            int temp = colLastIdx;
+            colLastIdx = rowLastIdx;
+            rowLastIdx = temp;
+            colSums = new int[rowLastIdx + 1][colLastIdx + 2];
+            for (int row = 0; row <= rowLastIdx; row++) {
+                int[] curSumRow = colSums[row];
+                int sum = 0;
+                for (int col = colLastIdx; col >= 0; col--)
+                    sum = curSumRow[col] = sum + matrix[col][row];
             }
         }
 
-        return count;
+        int[] curRowSums = new int[rowLastIdx + 2];
+        
+        for (int startCol = colLastIdx; startCol >= 0; startCol--) {
+            for (int colWidth = colLastIdx - startCol + 1; colWidth > 0; colWidth--) {
+                int sum = -BASE_SUM;
+                map[target - BASE_SUM] = 1;
+                curRowSums[rowLastIdx + 1] = target - BASE_SUM;
+                for (int row = rowLastIdx; row >= 0; row--) {
+                    sum += colSums[row][startCol] - colSums[row][startCol + colWidth];
+                    result += map[sum];
+                    map[sum + target]++;
+                    curRowSums[row] = sum + target;
+                }
+                for (int row = rowLastIdx + 1; row >= 0; row--)
+                    map[curRowSums[row]] = 0;
+            }
+        }
+
+        return result;
     }
 }
