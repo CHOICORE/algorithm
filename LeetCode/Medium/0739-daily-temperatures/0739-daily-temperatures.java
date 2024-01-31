e677faea5ce09a9ba0a95204d36384c5b2fd69a2
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-01-31 18:00:26 +0900

    Time: 24 ms (86.25%), Space: 60.2 MB (46.77%) - LeetHub

---
+++ LeetCode/Medium/0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        int[] answer = new int[temperatures.length];
+
+        for (int i = temperatures.length - 1; i >= 0; --i) {
+            if (deque.isEmpty()) {
+                deque.offerFirst(i);
+                answer[i] = 0;
+            } else {
+                while (!deque.isEmpty() && temperatures[i] >= temperatures[deque.peekFirst()]) {
+                    deque.pollFirst();
+                }
+
+                if (deque.isEmpty()) {
+                    answer[i] = 0;
+                } else {
+                    answer[i] = deque.peekFirst() - i;
+                }
+
+                deque.offerFirst(i);
+            }
+        }
+
+        return answer;
+    }
+}
