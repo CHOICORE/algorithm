276cac958a807ab859833604d257b07de2e9b1a1
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-02-03 18:36:37 +0900

    Time: 3 ms (100.00%), Space: 42 MB (47.96%) - LeetHub

--- LeetCode/Medium/1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ LeetCode/Medium/1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -1,20 +1,41 @@
 class Solution {
-    public int maxSumAfterPartitioning(int[] arr, int k) {
-        int N = arr.length;
-        int K = k + 1;
-
-        int[] dp = new int[K];
-        Arrays.fill(dp, 0);
-
-        for (int start = N - 1; start >= 0; start--) {
-            int currMax = 0;
-            int end = Math.min(N, start + k);
-
-            for (int i = start; i < end; i++) {
-                currMax = Math.max(currMax, arr[i]);
-                dp[start % K] = Math.max(dp[start % K], dp[(i + 1) % K] + currMax * (i - start + 1));
-            }
+    int sum(int[] arr, int i, int n) {
+        int sum = Integer.MIN_VALUE;
+        int j = i;
+        for (; i <= n; i++) {
+            sum = Math.max(sum, arr[i]);
         }
-        return dp[0];
+        return sum * (n - j + 1);
+    }
+
+    public int maxSum(int[] arr, int k, int n, int[] t) {
+        if (t[n] != -1)
+            return t[n];
+        if (k >= n + 1) {
+            t[n] = sum(arr, 0, n);
+            return t[n];
+        }
+
+        int max = Integer.MIN_VALUE;
+        
+        int answer = Integer.MIN_VALUE;
+        for (int i = 0; i < k; i++) {
+            if (n < k)
+                break;
+            max = Math.max(max, arr[n - i]);
+            answer = Math.max(answer, (i + 1) * max + maxSum(arr, k, n - i - 1, t));
+            t[n] = answer;
+
+        }
+        return answer;
+
+
+    }
+
+    int maxSumAfterPartitioning(int[] arr, int k) {
+        int[] t = new int[arr.length];
+        Arrays.fill(t, -1);
+
+        return maxSum(arr, k, arr.length - 1, t);
     }
 }
