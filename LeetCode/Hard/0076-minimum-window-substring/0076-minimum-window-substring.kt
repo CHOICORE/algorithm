7732558babbe8044a6a06e283cac2211a407c835
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-02-04 12:09:57 +0900

    Time: 168 ms (99.24%), Space: 37.1 MB (94.68%) - LeetHub

--- LeetCode/Hard/0076-minimum-window-substring/0076-minimum-window-substring.kt
+++ LeetCode/Hard/0076-minimum-window-substring/0076-minimum-window-substring.kt
@@ -6,13 +6,13 @@ class Solution {
         val map = IntArray(123)
         var left = 0
         var right = 0
-        var count = t.length
-        var tmp = Int.MAX_VALUE
+        var count: Int = t.length
+        var tmp: Int = Int.MAX_VALUE
         var start = 0
 
-        for (c in t.toCharArray()) map[c.code]++
+        for (c: Char in t.toCharArray()) map[c.code]++
 
-        val ch = s.toCharArray()
+        val ch: CharArray = s.toCharArray()
         while (right < s.length) {
             if (map[ch[right++].code]-- > 0) count--
 
