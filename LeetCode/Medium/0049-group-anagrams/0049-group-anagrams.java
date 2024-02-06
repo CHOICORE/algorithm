4767bc2cf0875a748a3ad622bacbf3ec84afb01b
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-02-06 10:06:56 +0900

    Time: 7 ms (57.75%), Space: 47.5 MB (65.51%) - LeetHub

--- LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.java
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.java
@@ -1,26 +1,12 @@
 class Solution {
-    public String getSignature(String s) {
-        int[] count = new int[26];
-        for (char c : s.toCharArray()) {
-            count[c - 'a']++;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) {
-                sb.append((char) ('a' + i)).append(count[i]);
-            }
-        }
-        return sb.toString();
-    }
-
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> groups = new HashMap<>();
-
-        for (String str : strs) {
-            groups.computeIfAbsent(getSignature(str), k -> new ArrayList<>()).add(str);
+        for (String s : strs) {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+            groups.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
         }
-
         return new ArrayList<>(groups.values());
     }
 }
