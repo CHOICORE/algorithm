64024964ad0e68cc3f24324b19849ed9eb765944
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-02-07 20:33:20 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 11.58 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -1,25 +1,11 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = new int[queries.length];
-        for (int i = 0; i < queries.length; i++) {
-
-            int[] query = queries[i];
-            int s = query[0];
-            int e = query[1];
-            int k = query[2];
-
-            answer[i] = -1;
-            for (int j = s; j < e + 1; j++) {
-                if (arr[j] > k) {
-                    if (answer[i] == -1) {
-                        answer[i] = arr[j];
-                    } else {
-                        answer[i] = Math.min(answer[i], arr[j]);
-                    }
-                }
-            }
-        }
-
-        return answer;
+        return Arrays.stream(queries).mapToInt(query -> IntStream.rangeClosed(query[0], query[1])
+                .map(i -> arr[i])
+                .filter(i -> i > query[2])
+                .min().orElse(-1)).toArray();
     }
 }
