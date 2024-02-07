d2a2dda5cff8689e244220699959f96d755ff97e
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-02-07 11:14:22 +0900

    Time: 16 ms (56.80%), Space: 45.7 MB (40.29%) - LeetHub

---
+++ LeetCode/Medium/0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        PriorityQueue<Map.Entry<Character, Integer>> queue = new PriorityQueue<>(
+                (a, b) -> b.getValue() - a.getValue()
+        );
+
+        queue.addAll(map.entrySet());
+
+        StringBuilder result = new StringBuilder();
+        while (!queue.isEmpty()) {
+            Map.Entry<Character, Integer> entry = queue.poll();
+            result.append(String.valueOf(entry.getKey()).repeat(entry.getValue()));
+        }
+
+        return result.toString();
+    }
+}
