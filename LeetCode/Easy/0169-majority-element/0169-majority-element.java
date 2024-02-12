baf45eb424c18102e163db9334cd045cc123a9a0
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-02-12 11:32:17 +0900

    Time: 1 ms (99.82%), Space: 50.1 MB (62.08%) - LeetHub

--- LeetCode/Easy/0169-majority-element/0169-majority-element.java
+++ LeetCode/Easy/0169-majority-element/0169-majority-element.java
@@ -1,7 +1,15 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        return nums[n / 2];
+        int majority = 0, count = 0;
+        for (int num : nums) {
+            if (num == majority) count++;
+            else count--;
+
+            if (count < 1) {
+                majority = num;
+                count = 1;
+            }
+        }
+        return majority;
     }
 }
