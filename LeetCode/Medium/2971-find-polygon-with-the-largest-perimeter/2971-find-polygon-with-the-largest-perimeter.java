3b0c5caa5b199a634d5d0851b90668148708931c
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-02-15 09:54:14 +0900

    Time: 1 ms (100.00%), Space: 56.1 MB (82.29%) - LeetHub

--- LeetCode/Medium/2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.java
+++ LeetCode/Medium/2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.java
@@ -1,14 +1,27 @@
 class Solution {
     public long largestPerimeter(int[] nums) {
-        Arrays.sort(nums);
+        return helper(nums, nums.length);
+    }
+
+    long helper(int[] nums, int end) {
+        int maxIndex = 0;
         long sum = 0;
-        long answer = -1;
-        for (int num : nums) {
-            if (num < sum) {
-                answer = num + sum;
-            }
-            sum += num;
+        for (int i = 0; i < end; i++) {
+            sum += nums[i];
+
+            if (nums[i] > nums[maxIndex])
+                maxIndex = i;
         }
-        return answer;
+        if ((sum - nums[maxIndex]) > nums[maxIndex])
+            return sum;
+        else {
+            int temp = nums[maxIndex];
+            nums[maxIndex] = nums[end - 1];
+            nums[end - 1] = temp;
+            if (end < 3)
+                return -1;
+            return helper(nums, end - 1);
+        }
+
     }
 }
