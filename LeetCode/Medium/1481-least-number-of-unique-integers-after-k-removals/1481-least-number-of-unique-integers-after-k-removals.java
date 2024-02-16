a76841f238d28c9d6eb8aebc56432ca04575513f
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-02-16 10:13:11 +0900

    Time: 25 ms (99.88%), Space: 57.1 MB (69.70%) - LeetHub

--- LeetCode/Medium/1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
+++ LeetCode/Medium/1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
@@ -1,32 +1,23 @@
 class Solution {
     public int findLeastNumOfUniqueInts(int[] arr, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i : arr) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
-        }
-
-        int n = arr.length;
-        
-        int[] countOfFrequencies = new int[n + 1];
-        
-        for (int count : map.values()) {
-            countOfFrequencies[count]++;
-        }
-        
-        int remainingUniqueElements = map.size();
-        
-        for (int i = 1; i <= n; i++) {
-            int numElementsToRemove = Math.min(k / i, countOfFrequencies[i]);
-            
-            k -= (i * numElementsToRemove);
-            
-            remainingUniqueElements -= numElementsToRemove;
-            
-            if (k < i) {
-                return remainingUniqueElements;
+        Arrays.sort(arr);
+        int[] node = new int[arr.length + 1];
+        int count = 0;
+        int length = 1;
+        for (int i = 0; i < arr.length; i++) {
+            if (i + 1 < arr.length && arr[i] == arr[i + 1]) {
+                length++;
+            } else {
+                count++;
+                node[length]++;
+                length = 1;
             }
         }
-        
-        return 0;
+        for (int i = 1; i < node.length; i++) {
+            int remove = Math.min(k / i, node[i]);
+            count -= remove;
+            k -= remove * i;
+        }
+        return count;
     }
 }
