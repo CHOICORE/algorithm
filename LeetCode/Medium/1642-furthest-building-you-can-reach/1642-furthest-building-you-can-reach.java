ec3b234d57fe2e9139bf2dec85eb19149a5cb202
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-02-17 10:20:01 +0900

    Time: 8 ms (99.73%), Space: 56.3 MB (95.93%) - LeetHub

--- LeetCode/Medium/1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
+++ LeetCode/Medium/1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
@@ -1,25 +1,37 @@
 class Solution {
 	public int furthestBuilding(int[] heights, int bricks, int ladders) {
-		PriorityQueue<Integer> bricksUsed = new PriorityQueue<>(Collections.reverseOrder());
-		int i;
-		for (i = 0; i < heights.length - 1; i++) {
-			if (heights[i + 1] <= heights[i])
-				continue;
 
-			int diff = heights[i + 1] - heights[i];
+		int len = heights.length;
+		int maxBricksNeeded = 0;
+		for (int i = 1; i < len; i++)
+			if (heights[i] > heights[i - 1])
+				maxBricksNeeded += heights[i] - heights[i - 1];
+		if (bricks >= maxBricksNeeded) return len - 1;
 
-			if (diff <= bricks) {
-				bricks -= diff;
-				bricksUsed.add(diff);
-			} else if (ladders > 0) {
-				if (!bricksUsed.isEmpty() && bricksUsed.peek() > diff) {
-					bricks += (bricksUsed.remove() - diff);
-					bricksUsed.add(diff);
+		PriorityQueue<Integer> bricksUsed = new PriorityQueue<>((i1, i2) -> i2 - i1);
+		int max = 0;
+		for (int i = max; i < len; i++) {
+			max = i;
+			if (i < len - 1 && heights[i + 1] > heights[i]) {
+				int needed = heights[i + 1] - heights[i];
+				if (bricks < needed) {
+					if (ladders > 0) {
+						if (!bricksUsed.isEmpty()) {
+							int height = bricksUsed.peek();
+							if (height > needed) {
+								bricksUsed.poll();
+								bricks += height - needed;
+								bricksUsed.offer(needed);
+							}
+						}
+						ladders--;
+					} else break;
+				} else {
+					bricks -= needed;
+					bricksUsed.offer(needed);
 				}
-				ladders--;
-			} else
-				break;
+			}
 		}
-		return i;
+		return max;
 	}
 }
