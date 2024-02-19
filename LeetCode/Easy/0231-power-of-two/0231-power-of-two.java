554db35a20c7efdec8c94af01325c2090be89c09
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-02-19 10:07:02 +0900

    Time: 0 ms (100.00%), Space: 40.7 MB (71.58%) - LeetHub

--- LeetCode/Easy/0231-power-of-two/0231-power-of-two.java
+++ LeetCode/Easy/0231-power-of-two/0231-power-of-two.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        switch (n) {
-            case 0:
-                return false;
-            case 1:
-                return true;
-            default:
-                if (n % 2 == 0) {
-                    return isPowerOfTwo(n / 2);
-                } else {
-                    return false;
-                }
+        
+        if (n <= 0) {
+            return false;
         }
+        if (n == 1) {
+            return true;
+        }
+        if (n % 2 == 1) {
+            return false;
+        }
+        return isPowerOfTwo(n / 2);
+
     }
 }
