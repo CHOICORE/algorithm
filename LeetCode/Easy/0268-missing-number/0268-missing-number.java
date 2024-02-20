c4e4a39de8f7bcc752de6eb1b802a2324d077b25
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-02-20 15:33:56 +0900

    Time: 0 ms (100.00%), Space: 45.2 MB (61.43%) - LeetHub

--- LeetCode/Easy/0268-missing-number/0268-missing-number.java
+++ LeetCode/Easy/0268-missing-number/0268-missing-number.java
@@ -1,14 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        int[] temp = new int[n + 1];
-        Arrays.fill(temp, -1);
+        int total = (n * (n + 1)) / 2;
+        int sum = 0;
         for (int num : nums) {
-            temp[num] = num;
+            sum += num;
         }
-        for (int i = 0; i < temp.length; i++) {
-            if (temp[i] == -1) return i;
-        }
-        return 0;
+        return total - sum;
     }
 }
