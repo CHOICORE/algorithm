0599a9be21d54d31d776a0e0950317cf11496a10
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-02-22 17:49:31 +0900

    Time: 2 ms (99.85%), Space: 54.2 MB (21.53%) - LeetHub

---
+++ LeetCode/Easy/0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -0,0 +1,20 @@
+class Solution {
+	public int findJudge(int n, int[][] trust) {
+		int[] trusting = new int[n + 1];
+		int[] trusted = new int[n + 1];
+
+		for (int[] ints : trust) {
+			trusting[ints[0]]++;
+			trusted[ints[1]]++;
+		}
+
+		int answer = -1;
+
+		for (int i = 1; i <= n; i++) {
+			if (trusting[i] == 0 && trusted[i] == n - 1)
+				answer = i;
+		}
+
+		return answer;
+	}
+}
