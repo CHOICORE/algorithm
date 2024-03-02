c47a135ef4fa67d32255d8fd2918481ceb92dcd2
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-03-02 18:28:42 +0900

    [level 0] Title: 카운트 업, Time: 1.76 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,14 +1,7 @@
+import java.util.stream.IntStream;
+
 class Solution {
 	public int[] solution(int start_num, int end_num) {
-		int[] answer = new int[end_num - start_num + 1];
-
-		int i = 0;
-		while (start_num <= end_num) {
-			answer[i] = start_num;
-			i++;
-			start_num++;
-		}
-
-		return answer;
+		return IntStream.rangeClosed(start_num, end_num).toArray();
 	}
 }
