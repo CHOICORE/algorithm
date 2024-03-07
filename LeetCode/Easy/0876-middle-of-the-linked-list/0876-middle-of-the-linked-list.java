6a62547e4961eff6371c2861943f889071662169
CHOICORE <choijaehyeong93@gmail.com>
2024-03-07 12:44:23 +0900

    Time: 0 ms (100.00%), Space: 40.9 MB (74.00%) - LeetHub

--- LeetCode/Easy/0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ LeetCode/Easy/0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -1,25 +1,26 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast != null && fast.next != null) {
-            
-            slow = slow.next;
-            fast = fast.next.next;
+        ListNode temp = head;
+        int size = 0;
+        while (temp != null) {
+            temp = temp.next;
+            size++;
         }
-
-        return slow;
+        temp = head;
+        for (int i = 0; i < size / 2; i++) {
+            temp = temp.next;
+        }
+        head = temp;
+        return head;
     }
 }
