6fd4537e3d9111e1821abb4429e01be18dffab26
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-03-13 10:28:09 +0900

    Time: 0 ms (100.00%), Space: 40.7 MB (43.66%) - LeetHub

--- LeetCode/Easy/2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ LeetCode/Easy/2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -1,22 +1,8 @@
 class Solution {
     public int pivotInteger(int n) {
-        for (int i = 1; i <= n; i++) {
-            int l = 0;
-            int r = 0;
-
-            for (int j = 1; j <= i; j++) {
-                l += j;
-            }
-
-            for (int k = i; k <= n; k++) {
-                r += k;
-            }
-
-            if (l == r) {
-                return i;
-            }
-        }
-
-        return -1;
+        int y = n * (n + 1) / 2;
+        int x = (int) Math.sqrt(y);
+        if (x * x == y) return x;
+        else return -1;
     }
 }
