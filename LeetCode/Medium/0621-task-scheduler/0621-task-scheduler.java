866754c0e4d05fdd6798d0d9b2bee46c4f349613
CHOICORE <choijaehyeong93@gmail.com>
2024-03-19 11:25:26 +0900

    Time: 25 ms (43.79%), Space: 45.7 MB (56.73%) - LeetHub

--- LeetCode/Medium/0621-task-scheduler/0621-task-scheduler.java
+++ LeetCode/Medium/0621-task-scheduler/0621-task-scheduler.java
@@ -1,21 +1,32 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int[] map = new int[26];
-        for (char task : tasks)
-            map[task - 'A']++;
+        int[] freq = new int[26];
+        for (char ch : tasks) {
+            freq[ch - 'A']++;
+        }
 
-        int maxCount = 0, numOfMaxCount = 0;
-        for (int num : map) {
-            if (num > maxCount) {
-                maxCount = num;
-                numOfMaxCount = 1;
-            } else if (num == maxCount) {
-                numOfMaxCount++;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                pq.offer(freq[i]);
             }
         }
 
-        int res = (n + 1) * (maxCount - 1) + numOfMaxCount;
-
-        return Math.max(res, tasks.length);
+        int time = 0;
+        while (!pq.isEmpty()) {
+            int cycle = n + 1;
+            List<Integer> store = new ArrayList<>();
+            int taskCount = 0;
+            while (cycle-- > 0 && !pq.isEmpty()) {
+                int currentFreq = pq.poll();
+                if (currentFreq > 1) {
+                    store.add(currentFreq - 1);
+                }
+                taskCount++;
+            }
+            store.forEach(pq::offer);
+            time += (pq.isEmpty() ? taskCount : n + 1);
+        }
+        return time;
     }
 }
