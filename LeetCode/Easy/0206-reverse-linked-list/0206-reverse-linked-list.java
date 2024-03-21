5e006e23b6771cbc8f6b016ebd62cfe01c498904
CHOICORE <choijaehyeong93@gmail.com>
2024-03-21 12:17:15 +0900

    Time: 0 ms (100.00%), Space: 42.5 MB (40.27%) - LeetHub

--- LeetCode/Easy/0206-reverse-linked-list/0206-reverse-linked-list.java
+++ LeetCode/Easy/0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -1,24 +1,27 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
-        
-        while ( head != null ){
-            
-            ListNode current = head;
-            head = head.next;
-            current.next = prev;
-            prev = current;
+        ListNode cur = head;
+        ListNode next = null;
+
+        while (cur != null) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
         }
-        return prev;
+
+        head = prev;
+        return head;
     }
 }
