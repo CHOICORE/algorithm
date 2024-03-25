1289f1e4f62d84cfa6c9b6b75ef63a6d6941d08d
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-03-25 10:23:28 +0900

    Time: 4 ms (95.02%), Space: 53.4 MB (82.52%) - LeetHub

--- LeetCode/Medium/0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ LeetCode/Medium/0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -1,33 +1,21 @@
-import java.util.AbstractList;
-
 class Solution {
-    private List<Integer> answer;
-
+    
     public List<Integer> findDuplicates(int[] nums) {
-        return new AbstractList<>() {
-            public Integer get(int index) {
-                init();
-                return answer.get(index);
-            }
+        
+        List<Integer> answer = new ArrayList<>();
+        
+        int n = nums.length;
+        
+        if (n <= 1) return answer;
 
-            public int size() {
-                init();
-                return answer.size();
-            }
+        boolean[] visited = new boolean[(int) Math.pow(10, 5)];
 
-            private void init() {
-                if (answer != null) return;
-                answer = new ArrayList<>();
-                int t;
-                for (int i = 0; i < nums.length; i++) {
-                    t = Math.abs(nums[i]);
-                    if (nums[t - 1] < 0) {
-                        answer.add(t);
-                    } else {
-                        nums[t - 1] *= -1;
-                    }
-                }
-            }
-        };
+        for (int num : nums) {
+            if (visited[num]) answer.add(num);
+            else visited[num] = true;
+            
+        }
+        
+        return answer;
     }
 }
