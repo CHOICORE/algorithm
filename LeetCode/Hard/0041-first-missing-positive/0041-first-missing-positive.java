547ca4dc634c2e7367224a539d60c15e22353ec1
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-03-26 11:24:13 +0900

    Time: 0 ms (100.00%), Space: 55 MB (61.84%) - LeetHub

--- LeetCode/Hard/0041-first-missing-positive/0041-first-missing-positive.java
+++ LeetCode/Hard/0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,19 +1,22 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int n = nums.length;
         
+        int n = nums.length;
         boolean[] found = new boolean[n + 1];
+        
         for (int num : nums) {
             if (num > 0 && num <= n) {
                 found[num] = true;
 
             }
         }
+        
         for (int i = 1; i <= n; i++) {
             if (!found[i]) {
                 return i;
             }
         }
+        
         return n + 1;
     }
 }
