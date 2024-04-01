9347797df89205d3275b92c26a95deb5ad9b3dc8
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-04-01 10:00:29 +0900

    Time: 124 ms (97.51%), Space: 34.8 MB (72.41%) - LeetHub

---
+++ LeetCode/Easy/0058-length-of-last-word/0058-length-of-last-word.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun lengthOfLastWord(s: String): Int {
+        var length = 0
+        for (i in s.length - 1 downTo 0) {
+            if (s[i] != ' ') length++
+            else if (length != 0) break
+        }
+        return length
+    }
+}
