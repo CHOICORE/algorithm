e928e3828ad26c99543a71e32807ce66961f7aa0
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-04-05 10:03:06 +0900

    Time: 1 ms (100.00%), Space: 42.1 MB (57.36%) - LeetHub

--- LeetCode/Easy/1544-make-the-string-great/1544-make-the-string-great.java
+++ LeetCode/Easy/1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,7 +1,6 @@
 class Solution {
     public String makeGood(String s) {
-        if (s.isEmpty() || s.length() == 1)
-            return s;
+        if (s.isEmpty() || s.length() == 1) return s;
 
         StringBuilder sb = new StringBuilder();
 
@@ -10,16 +9,13 @@ class Solution {
         int i = 0;
 
         while (i < sb.length() - 1) {
-            char leftChar = sb.charAt(i);
-            char rightChar = sb.charAt(i + 1);
+            char left = sb.charAt(i);
+            char right = sb.charAt(i + 1);
 
-            if (Math.abs(leftChar - rightChar) == 32) {
+            if (Math.abs(left - right) == 32) {
                 sb.delete(i, i + 2);
-                if (i > 0)
-                    i--;
-            } else {
-                i++;
-            }
+                if (i > 0) i--;
+            } else i++;
         }
 
         return sb.toString();
