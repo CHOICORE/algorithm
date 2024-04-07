6dbbe580355fa494f7e2c4a0152800f2f551c57b
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-04-07 18:13:14 +0900

    Time: 6 ms (5.02%), Space: 41.5 MB (19.71%) - LeetHub

--- LeetCode/Medium/0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ LeetCode/Medium/0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,36 +1,31 @@
 class Solution {
     public boolean checkValidString(String s) {
         int n = s.length();
-        int[][] memo = new int[n][n];
-        for (int[] row : memo) {
-            Arrays.fill(row, -1);
-        }
-        return isValidString(0, 0, s, memo);
-    }
+        boolean[][] dp = new boolean[n + 1][n + 1];
+        
+        dp[n][0] = true;
 
-    private boolean isValidString(int index, int openCount, String str, int[][] memo) {
-        if (index == str.length()) {
-            return (openCount == 0);
-        }
-        if (memo[index][openCount] != -1) {
-            return memo[index][openCount] == 1;
-        }
-        boolean isValid = false;
-        if (str.charAt(index) == '*') {
-            isValid |= isValidString(index + 1, openCount + 1, str, memo);
-            if (openCount > 0) {
-                isValid |= isValidString(index + 1, openCount - 1, str, memo);
-            }
-            isValid |= isValidString(index + 1, openCount, str, memo);
-        } else {
-            if (str.charAt(index) == '(') {
-                isValid = isValidString(index + 1, openCount + 1, str, memo);
-            } else if (openCount > 0) {
-                isValid = isValidString(index + 1, openCount - 1, str, memo);
+        for (int index = n - 1; index >= 0; index--) {
+            for (int openBracket = 0; openBracket < n; openBracket++) {
+                boolean isValid = false;
+                
+                if (s.charAt(index) == '*') {
+                    isValid |= dp[index + 1][openBracket + 1];
+                    if (openBracket > 0) {
+                        isValid |= dp[index + 1][openBracket - 1];
+                    }
+                    isValid |= dp[index + 1][openBracket];
+                } else {
+                    if (s.charAt(index) == '(') {
+                        isValid |= dp[index + 1][openBracket + 1];
+                    } else if (openBracket > 0) {
+                        isValid |= dp[index + 1][openBracket - 1];
+                    }
+                }
+                dp[index][openBracket] = isValid;
             }
         }
 
-        memo[index][openCount] = isValid ? 1 : 0;
-        return isValid;
+        return dp[0][0];
     }
 }
