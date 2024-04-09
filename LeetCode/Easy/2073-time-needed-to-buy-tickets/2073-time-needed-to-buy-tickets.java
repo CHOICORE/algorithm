c62832e905feb6c064b68b949a2d681f63bbde3e
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-04-09 11:22:09 +0900

    Time: 0 ms (100.00%), Space: 41.2 MB (55.58%) - LeetHub

--- LeetCode/Easy/2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ LeetCode/Easy/2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,26 +1,13 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-        Queue<Integer> queue = new LinkedList<>();
-
-        for (int i = 0; i < tickets.length; i++) {
-            queue.add(i);
-        }
-
         int time = 0;
 
-        while (!queue.isEmpty()) {
-            time++;
-
-            int front = queue.poll();
-
-            tickets[front]--;
-
-            if (k == front && tickets[front] == 0) {
-                return time;
-            }
-            
-            if (tickets[front] != 0) {
-                queue.add(front);
+        for (int i = 0; i < tickets.length; i++) {
+            if (i <= k) {
+                time += Math.min(tickets[k], tickets[i]);
+            } else {
+                
+                time += Math.min(tickets[k] - 1, tickets[i]);
             }
         }
 
