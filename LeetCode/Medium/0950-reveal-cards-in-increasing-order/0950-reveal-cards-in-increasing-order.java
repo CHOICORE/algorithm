e7befc6b992779dbadca54c2cf2829f9357e7fd5
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-04-10 09:42:42 +0900

    Time: 2 ms (100.00%), Space: 42.9 MB (97.57%) - LeetHub

--- LeetCode/Medium/0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
+++ LeetCode/Medium/0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
@@ -2,24 +2,30 @@ class Solution {
     public int[] deckRevealedIncreasing(int[] deck) {
         int length = deck.length;
         int[] result = new int[length];
-        boolean skip = false;
-        int i = 0;
-        int r = 0;
 
         Arrays.sort(deck);
 
-        while (i < length) {
+        return everyOther(deck, result, 0, 0, false);
+    }
+
+    private int[] everyOther(int[] deck, int[] result, int i, int r, boolean skip) {
+        int length = deck.length;
+
+        if (i == length) {
+            return result;
+        }
+
+        while (r < length) {
             if (result[r] == 0) {
-                
                 if (!skip) {
                     result[r] = deck[i];
                     i++;
                 }
-                
                 skip = !skip;
             }
-            r = (r + 1) % length;
+            r++;
         }
-        return result;
+
+        return everyOther(deck, result, i, 0, skip);
     }
 }
