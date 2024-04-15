7752ca75e0870ae004f880a7af97a4f44ad8a0b9
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-04-15 14:14:24 +0900

    Time: 0 ms (100.00%), Space: 41.2 MB (41.25%) - LeetHub

--- LeetCode/Medium/0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ LeetCode/Medium/0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -14,19 +14,15 @@
  * }
  */
 class Solution {
+    int dfs(TreeNode root, int sum) {
+        if (root == null) return 0;
+        sum = sum * 10 + root.val;
+        if (root.left == null && root.right == null) return sum;
+        return dfs(root.left, sum) + dfs(root.right, sum);
+    }
+
     public int sumNumbers(TreeNode root) {
         return dfs(root, 0);
     }
 
-    private int dfs(TreeNode node, int pathSum) {
-        if (node == null)
-            return 0;
-
-        pathSum = pathSum * 10 + node.val;
-
-        if (node.left == null && node.right == null)
-            return pathSum;
-
-        return dfs(node.left, pathSum) + dfs(node.right, pathSum);
-    }
 }
