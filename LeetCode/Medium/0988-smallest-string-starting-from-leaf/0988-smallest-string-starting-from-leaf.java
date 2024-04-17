a35b6dadeff6fc19da37eaa91627efd4e0220e7f
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-04-17 12:48:22 +0900

    Time: 2 ms (70.44%), Space: 44.6 MB (52.41%) - LeetHub

--- LeetCode/Medium/0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
+++ LeetCode/Medium/0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
@@ -14,50 +14,26 @@
  * }
  */
 class Solution {
-
-    char[] chs = null;
-    int l = 0;
+    String answer = "";
+    public void traversal(TreeNode root, StringBuilder sb) {
+        if (root == null) {
+            return;
+        }
+        sb.insert(0, (char) (root.val + 97));
+        if (root.left == null && root.right == null) {
+            if (answer.equals("")) {
+                answer = sb.toString();
+            } else {
+                answer = answer.compareTo(sb.toString()) > 0 ? sb.toString() : answer;
+            }
+        }
+        traversal(root.left, sb);
+        traversal(root.right, sb);
+        sb.deleteCharAt(0);
+    }
 
     public String smallestFromLeaf(TreeNode root) {
-        int d = depth(root);
-        solve(root, new char[d], d - 1);
-        return new String(chs, l, d - l);
-    }
-
-    private int depth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        return Math.max(depth(root.left), depth(root.right)) + 1;
-    }
-
-    private void solve(TreeNode node, char[] t, int i) {
-        if (node == null) {
-            return;
-        }
-        t[i] = (char) ('a' + node.val);
-        if (node.left == null && node.right == null) {
-            if (chs == null) {
-                chs = Arrays.copyOf(t, t.length);
-                l = i;
-            } else {
-                for (int k = 0, d = Math.max(l, i); k + d < t.length; k++) {
-                    if (chs[l + k] < t[i + k]) {
-                        return;
-                    } else if (chs[l + k] > t[i + k]) {
-                        chs = Arrays.copyOf(t, t.length);
-                        l = i;
-                        return;
-                    }
-                }
-                if (l < i) {
-                    chs = Arrays.copyOf(t, t.length);
-                    l = i;
-                }
-            }
-            return;
-        }
-        solve(node.left, t, i - 1);
-        solve(node.right, t, i - 1);
+        traversal(root, new StringBuilder());
+        return answer;
     }
 }
