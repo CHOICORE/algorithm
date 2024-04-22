e5288b21b1229176df4aae886840b29961cfbba2
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-04-22 09:43:58 +0900

    Time: 8 ms (98.78%), Space: 44.7 MB (98.30%) - LeetHub

--- LeetCode/Medium/0752-open-the-lock/0752-open-the-lock.java
+++ LeetCode/Medium/0752-open-the-lock/0752-open-the-lock.java
@@ -1,72 +1,43 @@
 class Solution {
     public int openLock(String[] deadends, String target) {
-        Map<Character, Character> nextSlot = Map.of(
-                '0', '1',
-                '1', '2',
-                '2', '3',
-                '3', '4',
-                '4', '5',
-                '5', '6',
-                '6', '7',
-                '7', '8',
-                '8', '9',
-                '9', '0'
-        );
-        Map<Character, Character> prevSlot = Map.of(
-                '0', '9',
-                '1', '0',
-                '2', '1',
-                '3', '2',
-                '4', '3',
-                '5', '4',
-                '6', '5',
-                '7', '6',
-                '8', '7',
-                '9', '8'
-        );
-
-        Set<String> visitedCombinations = new HashSet<>(Arrays.asList(deadends));
-        Queue<String> pendingCombinations = new LinkedList<String>();
-
-        int turns = 0;
-
-
-        if (visitedCombinations.contains("0000")) {
-            return -1;
+        int[] pow10 = {1, 10, 100, 1000};
+        int[] visit = new int[10000];
+        for (String dead : deadends) {
+            visit[Integer.parseInt(dead)] = 2;
         }
-
-        pendingCombinations.add("0000");
-        visitedCombinations.add("0000");
-
-        while (!pendingCombinations.isEmpty()) {
-            int currLevelNodesCount = pendingCombinations.size();
-            for (int i = 0; i < currLevelNodesCount; i++) {
-                String currentCombination = pendingCombinations.poll();
-
-
-                if (currentCombination.equals(target)) {
-                    return turns;
-                }
-
-                for (int wheel = 0; wheel < 4; wheel += 1) {
-                    StringBuilder newCombination = new StringBuilder(currentCombination);
-                    newCombination.setCharAt(wheel, nextSlot.get(newCombination.charAt(wheel)));
-
-                    if (!visitedCombinations.contains(newCombination.toString())) {
-                        pendingCombinations.add(newCombination.toString());
-                        visitedCombinations.add(newCombination.toString());
-                    }
-
-                    newCombination = new StringBuilder(currentCombination);
-                    newCombination.setCharAt(wheel, prevSlot.get(newCombination.charAt(wheel)));
-
-                    if (!visitedCombinations.contains(newCombination.toString())) {
-                        pendingCombinations.add(newCombination.toString());
-                        visitedCombinations.add(newCombination.toString());
+        int src = 0, dest = Integer.parseInt(target), steps = 0, dir = 1;
+        if (visit[src] == 2 || visit[dest] == 2) return -1;
+        if (src == dest) return 0;
+        Queue<Integer> forward = new LinkedList<>(), backward = new LinkedList<>();
+        forward.add(src);
+        visit[src] = 1;
+        backward.add(dest);
+        visit[dest] = -1;
+        while (!forward.isEmpty() && !backward.isEmpty()) {
+            if (forward.size() > backward.size()) {
+                Queue<Integer> tmp = forward;
+                forward = backward;
+                backward = tmp;
+                dir = -dir;
+            }
+            steps++;
+            int size = forward.size();
+            while (size-- > 0) {
+                int cur = forward.poll();
+                for (int p : pow10) {
+                    int d = (cur / p) % 10;
+                    for (int i = -1; i <= 1; i += 2) {
+                        int z = d + i;
+                        z = z == -1 ? 9 : (z == 10 ? 0 : z);
+                        int next = cur + (z - d) * p;
+                        if (visit[next] == -dir) return steps;
+                        if (visit[next] == 0) {
+                            forward.add(next);
+                            visit[next] = dir;
+                        }
                     }
                 }
             }
-            turns += 1;
         }
         return -1;
     }
