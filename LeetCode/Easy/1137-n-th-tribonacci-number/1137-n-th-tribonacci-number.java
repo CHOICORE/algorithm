d7f844a78d70a9c18e5beb4430007e2b467770f7
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-04-24 10:09:20 +0900

    Time: 0 ms (100.00%), Space: 39.8 MB (92.34%) - LeetHub

--- LeetCode/Easy/1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ LeetCode/Easy/1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,8 +1,5 @@
 class Solution {
-    public int tribonacci(int n) {
-        if (n == 0) return 0;
-        if (n == 1 || n == 2) return 1;
-        int[] dp = new int[n + 1];
+    public static int helper(int[] dp, int n) {
         dp[0] = 0;
         dp[1] = 1;
         dp[2] = 1;
@@ -10,5 +7,17 @@ class Solution {
             dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
         return dp[n];
+    }
+
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1 || n == 2) {
+            return 1;
+        }
+
+        int[] dp = new int[n + 1];
+
+        return helper(dp, n);
     }
 }
