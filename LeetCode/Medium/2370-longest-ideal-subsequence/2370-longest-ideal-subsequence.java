aec9af60d4456b3513260e1867a3fa7f18d8f377
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-04-25 17:22:24 +0900

    Time: 14 ms (99.21%), Space: 45.1 MB (47.62%) - LeetHub

--- LeetCode/Medium/2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.java
+++ LeetCode/Medium/2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.java
@@ -1,40 +1,30 @@
 class Solution {
+
     public int longestIdealString(String s, int k) {
-        int N = s.length();
+        int[] dp = new int[27];
+        int n = s.length();
 
-        int[][] dp = new int[N][26];
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(dp[i], -1);
-        }
+        for (int i = n - 1; i >= 0; i--) {
+            char cc = s.charAt(i);
+            int idx = cc - 'a';
+            int max = Integer.MIN_VALUE;
 
-        int res = 0;
-        for (int c = 0; c < 26; c++) {
-            res = Math.max(res, dfs(N - 1, c, dp, s, k));
-        }
-        return res;
-    }
+            int left = Math.max((idx - k), 0);
+            int right = Math.min((idx + k), 26);
 
-    public int dfs(int i, int c, int[][] dp, String s, int k) {
-        if (dp[i][c] != -1) {
-            return dp[i][c];
-        }
-
-        dp[i][c] = 0;
-        boolean match = c == (s.charAt(i) - 'a');
-        if (match) {
-            dp[i][c] = 1;
-        }
-        
-        if (i > 0) {
-            dp[i][c] = dfs(i - 1, c, dp, s, k);
-            if (match) {
-                for (int p = 0; p < 26; p++) {
-                    if (Math.abs(c - p) <= k) {
-                        dp[i][c] = Math.max(dp[i][c], dfs(i - 1, p, dp, s, k) + 1);
-                    }
-                }
+            for (int j = left; j <= right; j++) {
+                max = Math.max(max, dp[j]);
             }
+
+            dp[idx] = max + 1;
         }
-        return dp[i][c];
+
+        int max = Integer.MIN_VALUE;
+
+        for (int ele : dp) {
+            max = Math.max(ele, max);
+        }
+
+        return max;
     }
 }
