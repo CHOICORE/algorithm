fcb367b61a480f07fcbbc669f7eab0d5a0f2de50
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-04-30 13:58:11 +0900

    Time: 7 ms (100.00%), Space: 45.7 MB (9.43%) - LeetHub

--- LeetCode/Medium/1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.java
+++ LeetCode/Medium/1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.java
@@ -1,27 +1,24 @@
 class Solution {
+    static final int TOTAL = 1 << 10;
     public long wonderfulSubstrings(String word) {
-        int N = word.length();
-
-        Map<Integer, Integer> freq = new HashMap<>();
-
-        freq.put(0, 1);
-
-        int mask = 0;
-        long answer = 0L;
-        for (int i = 0; i < N; i++) {
+        long wonderful = 0;
+        long[] map = new long[TOTAL];
+        map[0] = 1L;
+        int value = 0;
+        int length = word.length();
+        for (int i = 0; i < length; i++) {
             char c = word.charAt(i);
-            int bit = c - 'a';
-
-            mask ^= (1 << bit);
-
-            answer += freq.getOrDefault(mask, 0);
-
-            freq.put(mask, freq.getOrDefault(mask, 0) + 1);
-            
-            for (int odd_c = 0; odd_c < 10; odd_c++) {
-                answer += freq.getOrDefault(mask ^ (1 << odd_c), 0);
+            int index = c - 'a';
+            value ^= (1 << index);
+            map[value]++;
+        }
+        for (int i = 0; i < TOTAL; i++) {
+            wonderful += map[i] * (map[i] - 1) / 2;
+            for (int j = 1; j <= i; j <<= 1) {
+                if ((i & j) == j)
+                    wonderful += map[i] * map[i - j];
             }
         }
-        return answer;
+        return wonderful;
     }
 }
