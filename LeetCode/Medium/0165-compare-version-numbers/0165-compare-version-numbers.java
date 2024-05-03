00a38b0c705ed02e32e3a459a247c40316092f4b
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-05-03 10:45:31 +0900

    Time: 0 ms (100.00%), Space: 41.3 MB (70.46%) - LeetHub

--- LeetCode/Medium/0165-compare-version-numbers/0165-compare-version-numbers.java
+++ LeetCode/Medium/0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,14 +1,21 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        var v1 = version1.split("\\.");
-        var v2 = version2.split("\\.");
-
-        for (var i = 0; i < Math.max(v1.length, v2.length); i++) {
-            var num1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
-            var num2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
-
-            if (num1 < num2) return -1;
-            if (num1 > num2) return 1;
+        int n = version1.length(), m = version2.length();
+        int i = 0, j = 0;
+        while (i < n || j < m) {
+            int x = 0;
+            for (; i < n && version1.charAt(i) != '.'; ++i) {
+                x = x * 10 + version1.charAt(i) - '0';
+            }
+            ++i;
+            int y = 0;
+            for (; j < m && version2.charAt(j) != '.'; ++j) {
+                y = y * 10 + version2.charAt(j) - '0';
+            }
+            ++j;
+            if (x != y) {
+                return x > y ? 1 : -1;
+            }
         }
         return 0;
     }
