03ada1f5c3ef605bd4819b2afed7166716cac6b2
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-05-04 10:59:48 +0900

    Time: 318 ms (95.24%), Space: 48.7 MB (100.00%) - LeetHub

--- LeetCode/Medium/0881-boats-to-save-people/0881-boats-to-save-people.kt
+++ LeetCode/Medium/0881-boats-to-save-people/0881-boats-to-save-people.kt
@@ -1,25 +1,29 @@
 class Solution {
     fun numRescueBoats(people: IntArray, limit: Int): Int {
-        val buckets = IntArray(limit + 1)
-        for (weight in people) {
-            buckets[weight]++
+        val n = people.size
+        if (n < 2) {
+            return n
         }
+        if (limit < 1) {
+            return 0
+        }
+
+        Arrays.sort(people)
+
+        var boots = 0
 
         var start = 0
-        var end = buckets.size - 1
-        var boats = 0
+        var end = n - 1
         while (start <= end) {
-            while (start <= end && buckets[start] <= 0) start++
-            while (start <= end && buckets[end] <= 0) end--
-
-            if (buckets[start] <= 0 && buckets[end] <= 0) break
-
-            boats++
-            if (start + end <= limit) {
-                buckets[start]--
+            if (people[end] + people[start] <= limit) {
+                start++
+                end--
+            } else {
+                end--
             }
-            buckets[end]--
+            boots++
         }
-        return boats
+
+        return boots
     }
 }
