2b89854578567dc2d975f9496c3e5a2fac32af58
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-05-14 12:40:04 +0900

    Time: 11 ms (100.00%), Space: 40.7 MB (89.51%) - LeetHub

--- LeetCode/Medium/1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
+++ LeetCode/Medium/1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -1,63 +1,57 @@
 class Solution {
-    private final int[] DIRECTIONS = new int[]{0, 1, 0, -1, 0};
+    public int max = 0;
+
+    public void ways(int[][] grid, int r, int c, int gold) {
+        if (gold > max) {
+            max = gold;
+        }
+        if (grid[r][c] == 0) {
+            return;
+        }
+        int a = grid[r][c];
+        grid[r][c] = 0;
+        if (r - 1 >= 0) {
+            ways(grid, r - 1, c, gold + a);
+        }
+        if (r + 1 <= grid.length - 1) {
+            ways(grid, r + 1, c, gold + a);
+        }
+        if (c + 1 <= grid[0].length - 1) {
+            ways(grid, r, c + 1, gold + a);
+        }
+        if (c - 1 >= 0) {
+            ways(grid, r, c - 1, gold + a);
+        }
+        grid[r][c] = a;
+        return;
+
+    }
+
+    public int gridWithNoZeros(int[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 0) {
+                    return -1;
+                } else
+                    count += grid[i][j];
+            }
+        }
+        return count;
+    }
+
 
     public int getMaximumGold(int[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-        
-        int totalGold = 0;
-        for (int[] row : grid) {
-            for (int cell : row) {
-                totalGold += cell;
+
+        int count = gridWithNoZeros(grid);
+        if (count != -1) return count;
+
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                ways(grid, i, j, 0);
             }
         }
-        
-        int maxGold = 0;
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
-                if (grid[row][col] != 0) {
-                    maxGold = Math.max(maxGold, bfsBacktrack(grid, rows, cols, row, col));
-                    if (maxGold == totalGold) {
-                        return totalGold;
-                    }
-                }
-            }
-        }
-        return maxGold;
-    }
-    
-    private int bfsBacktrack(int[][] grid, int rows, int cols, int row, int col) {
-        Queue<Pair<int[], Set<String>>> queue = new ArrayDeque<>();
-        Set<String> visited = new HashSet<>();
-        int maxGold = 0;
-        visited.add(row + "," + col);
-        queue.offer(new Pair<>(new int[]{row, col, grid[row][col]}, visited));
-
-        while (!queue.isEmpty()) {
-            Pair<int[], Set<String>> current = queue.poll();
-            int currRow = current.getKey()[0];
-            int currCol = current.getKey()[1];
-            int currGold = current.getKey()[2];
-            Set<String> currVis = current.getValue();
-            maxGold = Math.max(maxGold, currGold);
-            
-            for (int direction = 0; direction < 4; direction++) {
-                int nextRow = currRow + DIRECTIONS[direction];
-                int nextCol = currCol + DIRECTIONS[direction + 1];
-                
-                if (nextRow >= 0 && nextRow < rows && nextCol >= 0 && nextCol < cols &&
-                        grid[nextRow][nextCol] != 0 &&
-                        !currVis.contains(nextRow + "," + nextCol)) {
-
-                    currVis.add(nextRow + "," + nextCol);
-                    Set<String> copyVis = new HashSet<>(currVis);
-                    queue.offer(new Pair<>(new int[]{nextRow, nextCol,
-                            currGold + grid[nextRow][nextCol]}, copyVis));
-                    currVis.remove(nextRow + "," + nextCol);
-
-                }
-            }
-        }
-        return maxGold;
+        return max;
     }
 }
