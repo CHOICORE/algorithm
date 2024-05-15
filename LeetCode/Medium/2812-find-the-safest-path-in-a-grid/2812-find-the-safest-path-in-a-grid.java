ecbcfa7e4a976983590409a510195bc06e2839b2
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-05-15 09:11:15 +0900

    Time: 75 ms (100.00%), Space: 78.5 MB (6.56%) - LeetHub

--- LeetCode/Medium/2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
+++ LeetCode/Medium/2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
@@ -1,62 +1,73 @@
 class Solution {
-    final int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
     public int maximumSafenessFactor(List<List<Integer>> grid) {
         int n = grid.size();
-        int[][] mat = new int[n][n];
-        Queue<int[]> multiSourceQueue = new LinkedList<>();
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        if (grid.getFirst().getFirst() == 1 || grid.get(n - 1).get(n - 1) == 1) return 0;
+        int[][] cost = new int[n][n];
+        for (var v : cost) Arrays.fill(v, Integer.MAX_VALUE);
+        bfs(cost, grid, n);
+        int l = 1, r = n * n;
+        int ans = 0;
+        while (l <= r) {
+            int mid = (r - l) / 2 + l;
+            if (possible(0, 0, cost, mid, n, new boolean[n][n])) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    public boolean possible(int i, int j, int[][] cost, int mid, int n, boolean[][] visited) {
+        if (i < 0 || j < 0 || i >= n || j >= n) return false;
+        if (cost[i][j] == Integer.MAX_VALUE || cost[i][j] < mid) return false;
+        if (i == n - 1 && j == n - 1) return true;
+        if (visited[i][j]) return false;
+        visited[i][j] = true;
+        int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        boolean ans = false;
+        for (var v : dir) {
+            int ii = i + v[0];
+            int jj = j + v[1];
+            ans |= possible(ii, jj, cost, mid, n, visited);
+            if (ans) return true;
+        }
+        return ans;
+    }
+
+    public void bfs(int[][] cost, List<List<Integer>> grid, int n) {
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[n][n];
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid.size(); j++) {
                 if (grid.get(i).get(j) == 1) {
-                    multiSourceQueue.add(new int[]{i, j});
-                    mat[i][j] = 0;
-                } else {
-                    mat[i][j] = -1;
+                    q.add(new int[]{i, j});
+                    visited[i][j] = true;
                 }
             }
         }
-        
-        while (!multiSourceQueue.isEmpty()) {
-            int size = multiSourceQueue.size();
-            while (size-- > 0) {
-                int[] curr = multiSourceQueue.poll();
-                for (int[] d : dir) {
-                    int di = curr[0] + d[0];
-                    int dj = curr[1] + d[1];
-                    int val = mat[curr[0]][curr[1]];
-                    if (isValidCell(mat, di, dj) && mat[di][dj] == -1) {
-                        mat[di][dj] = val + 1;
-                        multiSourceQueue.add(new int[]{di, dj});
+        int level = 1;
+        int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        while (!q.isEmpty()) {
+            int len = q.size();
+            for (int i = 0; i < len; i++) {
+                var v = q.poll();
+                for (var val : dir) {
+                    int ii = v[0] + val[0];
+                    int jj = v[1] + val[1];
+                    if (isValid(ii, jj, n) && !visited[ii][jj]) {
+                        q.add(new int[]{ii, jj});
+                        cost[ii][jj] = Math.min(cost[ii][jj], level);
+                        visited[ii][jj] = true;
                     }
                 }
             }
+            level++;
         }
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[2] - a[2]);
-        pq.add(new int[]{0, 0, mat[0][0]});
-        mat[0][0] = -1;
-        
-        while (!pq.isEmpty()) {
-            int[] curr = pq.poll();
-            if (curr[0] == n - 1 && curr[1] == n - 1) {
-                return curr[2];
-            }
-            for (int[] d : dir) {
-                int di = d[0] + curr[0];
-                int dj = d[1] + curr[1];
-                if (isValidCell(mat, di, dj) && mat[di][dj] != -1) {
-                    pq.add(new int[]{di, dj, Math.min(curr[2], mat[di][dj])});
-                    mat[di][dj] = -1;
-                }
-            }
-        }
-
-        return -1;
     }
-    
-    private boolean isValidCell(int[][] mat, int i, int j) {
-        int n = mat.length;
-        return i >= 0 && j >= 0 && i < n && j < n;
+
+    public boolean isValid(int i, int j, int n) {
+        return (i >= 0 && j >= 0 && i < n && j < n);
     }
 }
