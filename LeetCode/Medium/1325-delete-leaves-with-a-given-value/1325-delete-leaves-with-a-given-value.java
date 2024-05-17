ed7d6a5a35533e4a0886d3e0efb4716f8b77ff97
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-05-17 14:28:00 +0900

    Time: 0 ms (100.00%), Space: 44.1 MB (58.31%) - LeetHub

--- LeetCode/Medium/1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
+++ LeetCode/Medium/1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
@@ -15,18 +15,14 @@
  */
 class Solution {
     public TreeNode removeLeafNodes(TreeNode root, int target) {
-        if (root == null) {
+        if (root == null)
             return null;
-        }
-        
-        root.left = removeLeafNodes(root.left, target);
-        
-        root.right = removeLeafNodes(root.right, target);
-        
-        if (root.left == null && root.right == null && root.val == target) {
+        TreeNode left = removeLeafNodes(root.left, target);
+        TreeNode right = removeLeafNodes(root.right, target);
+        if (left == null && right == null && root.val == target)
             return null;
-        }
-        
+        root.left = left;
+        root.right = right;
         return root;
     }
 }
