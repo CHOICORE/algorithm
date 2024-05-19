c370038e2e663471065343688669c6411fbab145
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-05-19 12:53:59 +0900

    Time: 1 ms (100.00%), Space: 54.8 MB (47.33%) - LeetHub

--- LeetCode/Hard/3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.java
+++ LeetCode/Hard/3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.java
@@ -1,27 +1,24 @@
 class Solution {
     public long maximumValueSum(int[] nums, int k, int[][] edges) {
-        long[][] memo = new long[nums.length][2];
-        for (long[] row : memo) {
-            Arrays.fill(row, -1);
+        long sum = 0;
+        long minExtra = 1_000_000;
+        int count = 0;
+
+        for (int val : nums) {
+            if ((val ^ k) > val) {
+                sum += val ^ k;
+                minExtra = Math.min(minExtra, (val ^ k) - val);
+                count++;
+            } else {
+                sum += val;
+                minExtra = Math.min(minExtra, val - (val ^ k));
+            }
         }
-        return maxSumOfNodes(0, 1, nums, k, memo);
-    }
 
-    long maxSumOfNodes(int index, int isEven, int[] nums, int k,
-                               long[][] memo) {
-        if (index == nums.length) {
-
-
-            return isEven == 1 ? 0 : Integer.MIN_VALUE;
+        if (count % 2 == 0) {
+            return sum;
+        } else {
+            return sum - minExtra;
         }
-        if (memo[index][isEven] != -1) {
-            return memo[index][isEven];
-        }
-        
-        long noXorDone = nums[index] + maxSumOfNodes(index + 1, isEven, nums, k, memo);
-        long xorDone = (nums[index] ^ k) +
-                maxSumOfNodes(index + 1, isEven ^ 1, nums, k, memo);
-
-        return memo[index][isEven] = Math.max(xorDone, noXorDone);
     }
 }
