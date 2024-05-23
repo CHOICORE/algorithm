3ef8600d69ed32a85d93efd9b4868650a4b1f132
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-05-23 11:33:44 +0900

    Time: 3 ms (100.00%), Space: 43.6 MB (94.63%) - LeetHub

--- LeetCode/Medium/2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.java
+++ LeetCode/Medium/2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.java
@@ -1,43 +1,23 @@
 class Solution {
     public int beautifulSubsets(int[] nums, int k) {
-        int totalCount = 1;
-        Map<Integer, Map<Integer, Integer>> freqMap = new TreeMap<>();
-        
-        for (int num : nums) {
-            Map<Integer, Integer> fr = freqMap.getOrDefault(num % k, new TreeMap<>());
-            fr.put(num, fr.getOrDefault(num, 0) + 1);
-            freqMap.put(num % k, fr);
-        }
-        
-        for (Map.Entry<Integer, Map<Integer, Integer>> entry : freqMap.entrySet()) {
-            ArrayList<Pair<Integer, Integer>> subsets = new ArrayList<Pair<Integer, Integer>>(
-                    entry.getValue().entrySet().size());
-            for (Map.Entry<Integer, Integer> subsetEntry : entry.getValue().entrySet()) {
-                subsets.add(
-                        new Pair<>(subsetEntry.getKey(), subsetEntry.getValue()));
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int num : nums) m.put(num, m.getOrDefault(num, 0) + 1);
+        int r = 1, p, pp;
+        for (Map.Entry<Integer, Integer> e : m.entrySet()) {
+            int cur = e.getKey();
+
+            if (m.containsKey(cur - k)) continue;
+
+            p = 0;
+
+            while (m.containsKey(cur)) {
+                pp = p;
+                p = ((1 << m.get(cur)) - 1) * r;
+                r += pp;
+                cur += k;
             }
-            totalCount *= countBeautifulSubsets(subsets, subsets.size(), k, 0);
+            r += p;
         }
-
-        return totalCount - 1;
-    }
-
-    private int countBeautifulSubsets(ArrayList<Pair<Integer, Integer>> subsets,
-                                      int numSubsets, int difference, int i) {
-        if (i == numSubsets) {
-            return 1;
-        }
-        
-        int skip = countBeautifulSubsets(subsets, numSubsets, difference, i + 1);
-        int take = (1 << subsets.get(i).getValue()) - 1;
-        
-        if (i + 1 < numSubsets &&
-                subsets.get(i + 1).getKey() - subsets.get(i).getKey() == difference) {
-            take *= countBeautifulSubsets(subsets, numSubsets, difference, i + 2);
-        } else {
-            take *= countBeautifulSubsets(subsets, numSubsets, difference, i + 1);
-        }
-
-        return skip + take;
+        return r - 1;
     }
 }
