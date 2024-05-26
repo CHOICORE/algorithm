17c33c82c91201797d76edf1748c2c543dcdeaa5
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-05-26 09:50:56 +0900

    Time: 2 ms (100.00%), Space: 40.6 MB (93.86%) - LeetHub

--- LeetCode/Hard/0552-student-attendance-record-ii/0552-student-attendance-record-ii.java
+++ LeetCode/Hard/0552-student-attendance-record-ii/0552-student-attendance-record-ii.java
@@ -1,34 +1,52 @@
 class Solution {
-    private final static int MOD = (int) 1e9 + 7;
-    private int[][][] memo;
+    static final int mod = (int) 1e9 + 7;
 
     public int checkRecord(int n) {
-        memo = new int[n + 1][2][3];
-        for (int[][] array2D : memo) {
-            for (int[] array1D : array2D) {
-                Arrays.fill(array1D, -1);
+        long[][] mat = new long[][]{
+                {1, 1, 1, 0, 0, 0},
+                {1, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0},
+                {1, 1, 1, 1, 1, 1},
+                {0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 1, 0}
+        };
+
+        long[][] res = pow(mat, n);
+        long ans = 0;
+
+        long[] initial = {1, 0, 0, 0, 0, 0};
+        for (int i = 0; i <= 5; i++) {
+            long sum = 0;
+            for (int j = 0; j <= 5; j++) {
+                sum = (sum + res[i][j] * initial[j]) % mod;
+            }
+            ans = (ans + sum) % mod;
+        }
+        return (int) (ans);
+    }
+
+    public long[][] pow(long[][] mat, int n) {
+        long[][] result = {{1, 0, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0}, {0, 0, 1, 0, 0, 0}, {0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 1, 0}, {0, 0, 0, 0, 0, 1}};
+        while (n > 0) {
+            if ((n % 2) == 1) {
+                result = multiply(result, mat);
+            }
+            n /= 2;
+            mat = multiply(mat, mat);
+        }
+        return result;
+    }
+
+    public long[][] multiply(long[][] a, long[][] b) {
+        long[][] c = new long[6][6];
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j <= 5; j++) {
+                c[i][j] = 0;
+                for (int k = 0; k <= 5; k++) {
+                    c[i][j] = (c[i][j] + a[i][k] * b[k][j]) % mod;
+                }
             }
         }
-        return eligibleCombinations(n, 0, 0);
+        return c;
     }
-
-    private int eligibleCombinations(int n, int totalAbsences, int consecutiveLates) {
-        if (totalAbsences >= 2 || consecutiveLates >= 3) {
-            return 0;
-        }
-        if (n == 0) {
-            return 1;
-        }
-        if (memo[n][totalAbsences][consecutiveLates] != -1) {
-            return memo[n][totalAbsences][consecutiveLates];
-        }
-
-        int count = 0;
-        count = eligibleCombinations(n - 1, totalAbsences, 0) % MOD;
-        count = (count + eligibleCombinations(n - 1, totalAbsences + 1, 0)) % MOD;
-        count = (count + eligibleCombinations(n - 1, totalAbsences, consecutiveLates + 1)) % MOD;
-
-        return memo[n][totalAbsences][consecutiveLates] = count;
-    }
-
 }
