145b9b14b09e4a9b016284fb2d17b519f726df4e
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-06-10 12:29:49 +0900

    Time: 5 ms (11.65%), Space: 41.1 MB (80.38%) - LeetHub

--- LeetCode/Easy/1051-height-checker/1051-height-checker.java
+++ LeetCode/Easy/1051-height-checker/1051-height-checker.java
@@ -1,42 +1,26 @@
 class Solution {
-    private void heapify(int[] arr, int n, int i) {
-        int largest = i;
-        int left = 2 * i + 1;
-        int right = 2 * i + 2;
+    private void countingSort(int[] arr) {
+        Map<Integer, Integer> counts = new HashMap<>();
+        int min = Arrays.stream(arr).min().getAsInt();
+        int max = Arrays.stream(arr).max().getAsInt();
         
-        if (left < n && arr[left] > arr[largest]) {
-            largest = left;
+        for (int val : arr) {
+            counts.put(val, counts.getOrDefault(val, 0) + 1);
         }
-        
-        if (right < n && arr[right] > arr[largest]) {
-            largest = right;
-        }
-        
-        if (largest != i) {
-            int swap = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = swap;
-            heapify(arr, n, largest);
-        }
-    }
 
-    private void heapSort(int[] arr) {
-        int n = arr.length;
-        for (int i = n / 2 - 1; i >= 0; i--) {
-            heapify(arr, n, i);
-        }
-        
-        for (int i = n - 1; i >= 0; i--) {
-            int swap = arr[0];
-            arr[0] = arr[i];
-            arr[i] = swap;
-            heapify(arr, i, 0);
+        int index = 0;
+        for (int val = min; val <= max; ++val) {
+            while (counts.getOrDefault(val, 0) > 0) {
+                arr[index] = val;
+                index += 1;
+                counts.put(val, counts.get(val) - 1);
+            }
         }
     }
 
     public int heightChecker(int[] heights) {
         int[] sortedHeights = heights.clone();
-        heapSort(sortedHeights);
+        countingSort(sortedHeights);
 
         int count = 0;
         for (int i = 0; i < sortedHeights.length; ++i) {
