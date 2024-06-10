535e18a3d253ca9774dac9a357cae4cc098eeb63
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-06-10 12:28:41 +0900

    Time: 0 ms (100.00%), Space: 40.8 MB (95.55%) - LeetHub

--- LeetCode/Easy/1051-height-checker/1051-height-checker.java
+++ LeetCode/Easy/1051-height-checker/1051-height-checker.java
@@ -1,20 +1,50 @@
 class Solution {
-    private void bubbleSort(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; ++i) {
-            for (int j = 0; j < n - i - 1; ++j) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
+    private void merge(int[] arr, int left, int mid, int right, int[] tempArr) {
+        int start2 = mid + 1;
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        if (n1 >= 0) System.arraycopy(arr, left, tempArr, left, n1);
+        if (n2 >= 0) System.arraycopy(arr, start2, tempArr, start2, n2);
+        
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (tempArr[left + i] <= tempArr[start2 + j]) {
+                arr[k] = tempArr[left + i];
+                i += 1;
+            } else {
+                arr[k] = tempArr[start2 + j];
+                j += 1;
             }
+            k += 1;
         }
+        
+        while (i < n1) {
+            arr[k] = tempArr[left + i];
+            i += 1;
+            k += 1;
+        }
+        while (j < n2) {
+            arr[k] = tempArr[start2 + j];
+            j += 1;
+            k += 1;
+        }
+    }
+    
+    private void mergeSort(int[] arr, int left, int right, int[] tempArr) {
+        if (left >= right) {
+            return;
+        }
+        int mid = (left + right) / 2;
+        mergeSort(arr, left, mid, tempArr);
+        mergeSort(arr, mid + 1, right, tempArr);
+        merge(arr, left, mid, right, tempArr);
     }
 
     public int heightChecker(int[] heights) {
         int[] sortedHeights = heights.clone();
-        bubbleSort(sortedHeights);
+        int[] tempArray = new int[heights.length];
+        mergeSort(sortedHeights, 0, sortedHeights.length - 1, tempArray);
 
         int count = 0;
         for (int i = 0; i < sortedHeights.length; ++i) {
