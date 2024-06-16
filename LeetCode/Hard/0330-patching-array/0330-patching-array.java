920d02571d78682130710795849bf5d9c9c085a4
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-06-16 09:18:59 +0900

    Time: 0 ms (100.00%), Space: 44.5 MB (6.79%) - LeetHub

--- LeetCode/Hard/0330-patching-array/0330-patching-array.java
+++ LeetCode/Hard/0330-patching-array/0330-patching-array.java
@@ -1,32 +1,17 @@
 class Solution {
     public int minPatches(int[] nums, int n) {
-        int x = 0;
-        long reachable = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            int curr = nums[i];
-            long c = curr;
-
-            if (c <= reachable + 1) {
-                reachable += curr;
+        int patches = 0;
+        long x = 1;
+        int length = nums.length, index = 0;
+        while (x <= n) {
+            if (index < length && nums[index] <= x) {
+                x += nums[index];
+                index++;
             } else {
-                x++;
-                i--;
-                long willAdd = reachable + 1;
-                reachable += willAdd;
-            }
-
-            if (reachable >= n) {
-                return x;
+                x *= 2;
+                patches++;
             }
         }
-
-        while (reachable < n) {
-            x++;
-            long willAdd = reachable + 1;
-            reachable += willAdd;
-        }
-
-        return x;
+        return patches;
     }
 }
