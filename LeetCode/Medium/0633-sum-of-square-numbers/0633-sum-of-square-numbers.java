2ab0a6c88b7d04941348748caed61a07c038c5a7
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-06-17 12:38:46 +0900

    Time: 0 ms (100.00%), Space: 40.4 MB (35.29%) - LeetHub

--- LeetCode/Medium/0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
+++ LeetCode/Medium/0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -1,10 +1,16 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
-        for (long a = 0; a * a <= c; a++) {
-            double b = Math.sqrt(c - a * a);
-            if (b == (int) b)
-                return true;
+        for (int i = 2; i * i <= c; i++) {
+            if (c % i == 0) {
+                int count = 0;
+                while (c % i == 0) {
+                    count++;
+                    c /= i;
+                }
+                if (i % 4 == 3 && count % 2 != 0)
+                    return false;
+            }
         }
-        return false;
+        return c % 4 != 3;
     }
 }
