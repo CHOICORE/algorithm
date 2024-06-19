a9515247c88c88cf45ceb871828c839a47b0ff5b
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-06-19 14:30:24 +0900

    Time: 9 ms (100.00%), Space: 55.8 MB (89.56%) - LeetHub

--- LeetCode/Medium/1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ LeetCode/Medium/1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,43 +1,55 @@
 class Solution {
-    private int getNumOfBouquets(int[] bloomDay, int mid, int k) {
-        int numOfBouquets = 0;
-        int count = 0;
+    int[] arr;
+    int m, k;
 
-        for (int j : bloomDay) {
-            if (j <= mid) {
-                count++;
-            } else {
-                count = 0;
-            }
+    public int minDays(int[] arr, int m, int k) {
+        if (m * k < 0 || m * k > arr.length)
+            return -1;
 
-            if (count == k) {
-                numOfBouquets++;
-                count = 0;
-            }
+        this.m = m;
+        this.k = k;
+
+        this.arr = arr;
+
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++)
+            max = Math.max(max, arr[i]);
+
+        int st = 1, en = max, md;
+
+        while (st <= en) {
+            md = (st + en) / 2;
+            if (count(md)) {
+                en = md - 1;
+            } else
+                st = md + 1;
         }
 
-        return numOfBouquets;
+        return st;
     }
 
-    public int minDays(int[] bloomDay, int m, int k) {
-        int start = 0;
-        int end = 0;
-        for (int day : bloomDay) {
-            end = Math.max(end, day);
-        }
+    boolean count(int val) {
 
-        int minDays = -1;
-        while (start <= end) {
-            int mid = (start + end) / 2;
+        int temp = 0, temp2 = 0;
 
-            if (getNumOfBouquets(bloomDay, mid, k) >= m) {
-                minDays = mid;
-                end = mid - 1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= val) {
+                temp++;
+                if (temp == k) {
+                    temp = 0;
+                    temp2++;
+                    if (temp2 == m)
+                        return true;
+                }
             } else {
-                start = mid + 1;
+                if ((arr.length - i) / k < m - temp2)
+                    return false;
+                temp = 0;
             }
-        }
 
-        return minDays;
+        }
+        return false;
+
     }
 }
