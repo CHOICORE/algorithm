c181df1114c1f1ba620d5d7c8c61db7d06f9ef35
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-06-22 15:14:28 +0900

    Time: 50 ms (5.05%), Space: 55 MB (36.41%) - LeetHub

---
+++ LeetCode/Medium/1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        int currSum = 0, subarrays = 0;
+        Map<Integer, Integer> prefixSum = new HashMap<>();
+        prefixSum.put(currSum, 1);
+
+        for (int num : nums) {
+            currSum += num % 2;
+            if (prefixSum.containsKey(currSum - k)) {
+                subarrays = subarrays + prefixSum.get(currSum - k);
+            }
+            prefixSum.put(currSum, prefixSum.getOrDefault(currSum, 0) + 1);
+        }
+
+        return subarrays;
+    }
+}
