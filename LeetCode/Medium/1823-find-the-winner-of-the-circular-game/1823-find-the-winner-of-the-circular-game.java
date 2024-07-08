e16a6005b7ee62b4d8ab1557bf089db146af7200
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-07-08 09:20:13 +0900

    Time: 0 ms (100.00%), Space: 40.7 MB (42.70%) - LeetHub

--- LeetCode/Medium/1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ LeetCode/Medium/1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,20 +1,6 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        List<Integer> circle = new LinkedList<Integer>();
-        for (int i = 1; i <= n; i++) {
-            circle.add(i);
-        }
-        
-        int startIndex = 0;
-        
-        while (circle.size() > 1) {
-            int removalIndex = (startIndex + k - 1) % circle.size();
-            
-            circle.remove(removalIndex);
-            
-            startIndex = removalIndex;
-        }
-
-        return circle.getFirst();
+        if (n == 1) return 1;
+        return (findTheWinner(n - 1, k) + k - 1) % n + 1;
     }
 }
