ccead919e203f29918f71ad44ccbeefc20dff34a
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-07-11 10:23:21 +0900

    Time: 0 ms (100.00%), Space: 41.4 MB (83.90%) - LeetHub

--- LeetCode/Medium/1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.java
+++ LeetCode/Medium/1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.java
@@ -1,27 +1,27 @@
 class Solution {
+    int i = 0;
+
     public String reverseParentheses(String s) {
-        Stack<Integer> openParenthesesIndices = new Stack<>();
-        StringBuilder result = new StringBuilder();
-
-        for (char currentChar : s.toCharArray()) {
-            if (currentChar == '(') {
-                openParenthesesIndices.push(result.length());
-            } else if (currentChar == ')') {
-                int start = openParenthesesIndices.pop();
-                reverse(result, start, result.length() - 1);
-            } else {
-                result.append(currentChar);
-            }
-        }
-
-        return result.toString();
+        char[] ar = s.toCharArray();
+        return helper(ar);
     }
 
-    private void reverse(StringBuilder sb, int start, int end) {
-        while (start < end) {
-            char temp = sb.charAt(start);
-            sb.setCharAt(start++, sb.charAt(end));
-            sb.setCharAt(end--, temp);
+    public String helper(char[] s) {
+        StringBuilder sb = new StringBuilder();
+
+        while (i < s.length) {
+            if (s[i] == ')') {
+                i++;
+                return sb.reverse().toString();
+            } else if (s[i] == '(') {
+                i++;
+                sb.append(helper(s));
+            } else {
+                sb.append(s[i]);
+                i++;
+            }
         }
+        return sb.toString();
+
     }
 }
