eb032085e722be1d4e209bd0e2f5ede27d80cf9f
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-07-14 13:16:29 +0900

    Time: 2 ms (100.00%), Space: 41.6 MB (70.59%) - LeetHub

--- LeetCode/Hard/0726-number-of-atoms/0726-number-of-atoms.java
+++ LeetCode/Hard/0726-number-of-atoms/0726-number-of-atoms.java
@@ -1,79 +1,43 @@
 class Solution {
-    private int index = 0;
-
     public String countOfAtoms(String formula) {
-        Map<String, Integer> finalMap = parseFormula(formula);
-
-        TreeMap<String, Integer> sortedMap = new TreeMap<>(finalMap);
-
-        StringBuilder ans = new StringBuilder();
-        for (String atom : sortedMap.keySet()) {
-            ans.append(atom);
-            if (sortedMap.get(atom) > 1) {
-                ans.append(sortedMap.get(atom));
-            }
-        }
-
-        return ans.toString();
-    }
-
-    private Map<String, Integer> parseFormula(String formula) {
-        Map<String, Integer> currMap = new HashMap<>();
-
-        while (index < formula.length() && formula.charAt(index) != ')') {
-            if (formula.charAt(index) == '(') {
-                index++;
-                Map<String, Integer> nestedMap = parseFormula(formula);
-                for (String atom : nestedMap.keySet()) {
-                    currMap.put(
-                            atom,
-                            currMap.getOrDefault(atom, 0) + nestedMap.get(atom)
-                    );
+        Map<String, Integer> map = new HashMap<>();
+        int[] stack = new int[1000];
+        int top = 0, multiplier = 1, freq = 0;
+        char[] c = formula.toCharArray();
+        for (int i = c.length - 1; i >= 0; i--) {
+            if (c[i] >= 'a' && c[i] <= 'z') {
+                int end = i--;
+                while (i >= 0 && c[i] >= 'a' && c[i] <= 'z') i--;
+                String key = new String(c, i, end - i + 1);
+                map.put(key, map.getOrDefault(key, 0) + Math.max(freq, 1) * multiplier);
+                freq = 0;
+            } else if (c[i] >= 'A' && c[i] <= 'Z') {
+                String key = new String(c, i, 1);
+                map.put(key, map.getOrDefault(key, 0) + Math.max(freq, 1) * multiplier);
+                freq = 0;
+            } else if (c[i] >= '0' && c[i] <= '9') {
+                freq = c[i] - '0';
+                int p = 10;
+                while (i - 1 >= 0 && c[i - 1] >= '0' && c[i - 1] <= '9') {
+                    freq += p * (c[--i] - '0');
+                    p *= 10;
                 }
+            } else if (c[i] == ')') {
+                stack[top++] = multiplier;
+                multiplier *= Math.max(freq, 1);
+                freq = 0;
             } else {
-                StringBuilder currAtom = new StringBuilder();
-                do {
-                    currAtom.append(formula.charAt(index));
-                    index++;
-                } while (index < formula.length() &&
-                        Character.isLowerCase(formula.charAt(index)));
-
-                StringBuilder currCount = new StringBuilder();
-                while (
-                        index < formula.length() &&
-                                Character.isDigit(formula.charAt(index))
-                ) {
-                    currCount.append(formula.charAt(index));
-                    index++;
-                }
-
-                if (currCount.isEmpty()) {
-                    currMap.put(
-                            currAtom.toString(),
-                            currMap.getOrDefault(currAtom.toString(), 0) + 1
-                    );
-                } else {
-                    currMap.put(
-                            currAtom.toString(),
-                            currMap.getOrDefault(currAtom.toString(), 0) +
-                                    Integer.parseInt(currCount.toString())
-                    );
-                }
+                multiplier = stack[--top];
             }
         }
-
-        index++;
-        StringBuilder multiplier = new StringBuilder();
-        while (
-                index < formula.length() && Character.isDigit(formula.charAt(index))
-        ) {
-            multiplier.append(formula.charAt(index));
-            index++;
+        List<String> keys = new ArrayList<>(map.keySet());
+        Collections.sort(keys);
+        StringBuilder sb = new StringBuilder();
+        for (String key : keys) {
+            sb.append(key);
+            int f = map.get(key);
+            if (f > 1) sb.append(f);
         }
-        if (!multiplier.isEmpty()) {
-            currMap.replaceAll((a, v) -> currMap.get(a) * Integer.parseInt(multiplier.toString()));
-        }
-
-        return currMap;
+        return sb.toString();
     }
 }
