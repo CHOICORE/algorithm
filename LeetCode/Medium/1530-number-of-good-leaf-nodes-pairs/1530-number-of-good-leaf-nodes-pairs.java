ade68d2116d3a53a6486643f91d26a1148c3ec81
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-07-18 10:22:08 +0900

    Time: 2 ms (95.76%), Space: 44.5 MB (83.73%) - LeetHub

--- LeetCode/Medium/1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.java
+++ LeetCode/Medium/1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.java
@@ -15,58 +15,35 @@
  */
 class Solution {
     public int countPairs(TreeNode root, int distance) {
-        Map<TreeNode, List<TreeNode>> graph = new HashMap<>();
-        Set<TreeNode> leafNodes = new HashSet<>();
-
-        traverseTree(root, null, graph, leafNodes);
-
-        int ans = 0;
-
-        for (TreeNode leaf : leafNodes) {
-            Queue<TreeNode> bfsQueue = new LinkedList<>();
-            Set<TreeNode> seen = new HashSet<>();
-            bfsQueue.add(leaf);
-            seen.add(leaf);
-            for (int i = 0; i <= distance; i++) {
-                int size = bfsQueue.size();
-                for (int j = 0; j < size; j++) {
-                    TreeNode currNode = bfsQueue.remove();
-                    if (leafNodes.contains(currNode) && currNode != leaf) {
-                        ans++;
-                    }
-                    if (graph.containsKey(currNode)) {
-                        for (TreeNode neighbor : graph.get(currNode)) {
-                            if (!seen.contains(neighbor)) {
-                                bfsQueue.add(neighbor);
-                                seen.add(neighbor);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return ans / 2;
+        int[] result = new int[1];
+        dfs(root, distance, result);
+        return result[0];
     }
 
-    private void traverseTree(
-            TreeNode currNode,
-            TreeNode prevNode,
-            Map<TreeNode, List<TreeNode>> graph,
-            Set<TreeNode> leafNodes
-    ) {
-        if (currNode == null) {
-            return;
+    private int[] dfs(TreeNode node, int distance, int[] result) {
+        if (node == null) {
+            return new int[distance + 1];
         }
-        if (currNode.left == null && currNode.right == null) {
-            leafNodes.add(currNode);
+        if (node.left == null && node.right == null) {
+            int[] leafDistance = new int[distance + 1];
+            leafDistance[1] = 1;
+            return leafDistance;
         }
-        if (prevNode != null) {
-            graph.computeIfAbsent(prevNode, k -> new ArrayList<TreeNode>());
-            graph.get(prevNode).add(currNode);
-            graph.computeIfAbsent(currNode, k -> new ArrayList<TreeNode>());
-            graph.get(currNode).add(prevNode);
+
+        int[] left = dfs(node.left, distance, result);
+        int[] right = dfs(node.right, distance, result);
+
+        //calculate result
+        for (int i = 1; i <= distance; i++) {
+            for (int j = 1; j <= distance - i; j++) {
+                result[0] += left[i] * right[j];
+            }
         }
-        traverseTree(currNode.left, currNode, graph, leafNodes);
-        traverseTree(currNode.right, currNode, graph, leafNodes);
+
+        int[] leafDistance = new int[distance + 1];
+        for (int i = 1; i < distance; i++) {
+            leafDistance[i + 1] = left[i] + right[i];
+        }
+        return leafDistance;
     }
 }
