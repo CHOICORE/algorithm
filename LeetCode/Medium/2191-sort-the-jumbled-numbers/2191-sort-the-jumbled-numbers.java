d8f7186e4defd767893ff30d8c6952790b20fe69
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-07-24 10:48:50 +0900

    Time: 29 ms (99.02%), Space: 55.8 MB (34.31%) - LeetHub

--- LeetCode/Medium/2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.java
+++ LeetCode/Medium/2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.java
@@ -1,23 +1,48 @@
 class Solution {
-    public int[] sortJumbled(int[] mapping, int[] nums) {
-        List<Integer[]> storePairs = new ArrayList<>();
-
-        for (int i = 0; i < nums.length; ++i) {
-            String number = Integer.toString(nums[i]);
-            StringBuilder formed = new StringBuilder();
-            for (int j = 0; j < number.length(); ++j) {
-                formed.append(mapping[number.charAt(j) - '0']);
+    static int[] countSort(int[] arr, int place, int[] mapping) {
+        int n = arr.length;
+        if (n <= 1) return arr;
+        int[] result = new int[n];
+        int[] freq = new int[10];
+        for (int x : arr) {
+            if (x / place != 0 || x == 0) {
+                int val = (x / place) % 10;
+                freq[mapping[val]]++;
+            } else {
+                freq[0]++;
             }
-            int mappedValue = Integer.parseInt(formed.toString());
-            storePairs.add(new Integer[]{mappedValue, i});
         }
 
+        for (int i = 1; i < 10; i++) {
+            freq[i] += freq[i - 1];
+        }
 
-        storePairs.sort(Comparator.comparing(o -> o[0]));
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] / place != 0 || arr[i] == 0) {
+                int val = (arr[i] / place) % 10;
 
-        int[] answer = new int[nums.length];
-        for (int i = 0; i < storePairs.size(); i++) {
-            answer[i] = nums[storePairs.get(i)[1]];
+                result[freq[mapping[val]] - 1] = arr[i];
+
+                freq[mapping[val]]--;
+            } else {
+                result[freq[0] - 1] = arr[i];
+                freq[0]--;
+            }
+        }
+
+        return result;
+    }
+
+    public int[] sortJumbled(int[] mapping, int[] nums) {
+        int max = -1;
+
+        for (int x : nums) {
+            if (x > max) max = x;
+        }
+
+        int[] answer = nums;
+        for (int place = 1; max / place > 0; place *= 10) {
+            answer = countSort(answer, place, mapping);
         }
         return answer;
     }
