02b90f75fd05ba8dc606a823d671f9124acf856b
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-07-25 10:16:35 +0900

    Time: 4 ms (98.10%), Space: 55.4 MB (84.25%) - LeetHub

--- LeetCode/Medium/0912-sort-an-array/0912-sort-an-array.java
+++ LeetCode/Medium/0912-sort-an-array/0912-sort-an-array.java
@@ -1,15 +1,16 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        var map = new int[100_001];
+        int[] map = new int[100_001];
+        for (int num : nums) {
+            map[num + 50_000]++;
+        }
 
-        for (var n : nums)
-            map[n + 50_000]++;
-
-        var k = 0;
-        for (var i = 0; i < 100_001; i++)
+        int k = 0;
+        for (int i = 0; i < 100_001; i++) {
             for (var j = 0; j < map[i]; j++)
                 nums[k++] = i - 50_000;
-
+        }
+        
         return nums;
     }
 }
