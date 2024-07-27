bfabebc22c97d080697a754f4cd089bab8a4bda2
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-07-27 12:34:07 +0900

    Time: 11 ms (100.00%), Space: 46.3 MB (11.01%) - LeetHub

--- LeetCode/Medium/2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.java
+++ LeetCode/Medium/2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.java
@@ -1,51 +1,33 @@
 class Solution {
-    public long minimumCost(
-            String source,
-            String target,
-            char[] original,
-            char[] changed,
-            int[] cost
-    ) {
-        long totalCost = 0;
-        
-        long[][] minCost = new long[26][26];
-        for (long[] row : minCost) {
-            Arrays.fill(row, Integer.MAX_VALUE);
+    public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
+        int[][] dis = new int[26][26];
+        for (int i = 0; i < 26; i++) {
+            Arrays.fill(dis[i], Integer.MAX_VALUE);
+            dis[i][i] = 0;
         }
-        
-        for (int i = 0; i < original.length; ++i) {
-            int startChar = original[i] - 'a';
-            int endChar = changed[i] - 'a';
-            minCost[startChar][endChar] = Math.min(
-                    minCost[startChar][endChar],
-                    (long) cost[i]
-            );
+        for (int i = 0; i < cost.length; i++) {
+            dis[original[i] - 'a'][changed[i] - 'a'] = Math.min(dis[original[i] - 'a'][changed[i] - 'a'], cost[i]);
         }
-        
-        for (int k = 0; k < 26; ++k) {
-            for (int i = 0; i < 26; ++i) {
-                for (int j = 0; j < 26; ++j) {
-                    minCost[i][j] = Math.min(
-                            minCost[i][j],
-                            minCost[i][k] + minCost[k][j]
-                    );
+        for (int k = 0; k < 26; k++) {
+            for (int i = 0; i < 26; i++)
+                if (dis[i][k] < Integer.MAX_VALUE) {
+                    for (int j = 0; j < 26; j++) {
+                        if (dis[k][j] < Integer.MAX_VALUE) {
+                            dis[i][j] = Math.min(dis[i][j], dis[i][k] + dis[k][j]);
+                        }
+                    }
                 }
+        }
+        long answer = 0L;
+        for (int i = 0; i < source.length(); i++) {
+            int c1 = source.charAt(i) - 'a';
+            int c2 = target.charAt(i) - 'a';
+            if (dis[c1][c2] == Integer.MAX_VALUE) {
+                return -1L;
+            } else {
+                answer += dis[c1][c2];
             }
         }
-        
-        for (int i = 0; i < source.length(); ++i) {
-            if (source.charAt(i) == target.charAt(i)) {
-                continue;
-            }
-            int sourceChar = source.charAt(i) - 'a';
-            int targetChar = target.charAt(i) - 'a';
-            
-            if (minCost[sourceChar][targetChar] >= Integer.MAX_VALUE) {
-                return -1;
-            }
-            totalCost += minCost[sourceChar][targetChar];
-        }
-
-        return totalCost;
+        return answer;
     }
 }
