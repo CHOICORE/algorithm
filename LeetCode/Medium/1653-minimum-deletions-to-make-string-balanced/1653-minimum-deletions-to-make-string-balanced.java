47b0693268cefcf7e7fbcff0693d553897810f78
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-07-30 11:27:04 +0900

    Time: 11 ms (100.00%), Space: 45.4 MB (39.55%) - LeetHub

--- LeetCode/Medium/1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ LeetCode/Medium/1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -1,26 +1,20 @@
 class Solution {
     public int minimumDeletions(String s) {
-        int n = s.length();
-        int[] countA = new int[n];
-        int[] countB = new int[n];
-        int bCount = 0;
-
-        for (int i = 0; i < n; i++) {
-            countB[i] = bCount;
-            if (s.charAt(i) == 'b') bCount++;
+        char[] letters = s.toCharArray();
+        int a = 0;
+        int b = 0;
+        int output = Integer.MAX_VALUE;
+        for (char letter : letters) {
+            a += ('b' - letter);
         }
 
-        int aCount = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            countA[i] = aCount;
-            if (s.charAt(i) == 'a') aCount++;
+        for (int i = 0; i < s.length(); i++) {
+            output = Math.min(output, a + b);
+            a -= ('b' - letters[i]);
+            b += (letters[i] - 'a');
         }
 
-        int minDeletions = n;
-        for (int i = 0; i < n; i++) {
-            minDeletions = Math.min(minDeletions, countA[i] + countB[i]);
-        }
-
-        return minDeletions;
+        output = Math.min(output, a + b);
+        return output;
     }
 }
