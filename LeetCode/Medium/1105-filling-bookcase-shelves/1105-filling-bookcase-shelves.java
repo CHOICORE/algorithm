5eabc8265b4538daef22aeee14aa58e7e99fc848
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-07-31 09:22:41 +0900

    Time: 0 ms (100.00%), Space: 41.8 MB (88.15%) - LeetHub

--- LeetCode/Medium/1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.java
+++ LeetCode/Medium/1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.java
@@ -1,53 +1,17 @@
 class Solution {
     public int minHeightShelves(int[][] books, int shelfWidth) {
-        int[][] memo = new int[books.length][shelfWidth + 1];
-        return dpHelper(books, shelfWidth, memo, 0, shelfWidth, 0);
-    }
-
-    private int dpHelper(
-            int[][] books,
-            int shelfWidth,
-            int[][] memo,
-            int i,
-            int remainingShelfWidth,
-            int maxHeight
-    ) {
-        int[] currentBook = books[i];
-        int maxHeightUpdated = Math.max(maxHeight, currentBook[1]);
-        if (i == books.length - 1) {
-            if (remainingShelfWidth >= currentBook[0]) return maxHeightUpdated;
-            return maxHeight + currentBook[1];
-        }
-        if (memo[i][remainingShelfWidth] != 0) {
-            return memo[i][remainingShelfWidth];
-        } else {
-            int option1Height =
-                    maxHeight +
-                            dpHelper(
-                                    books,
-                                    shelfWidth,
-                                    memo,
-                                    i + 1,
-                                    shelfWidth - currentBook[0],
-                                    currentBook[1]
-                            );
-            if (remainingShelfWidth >= currentBook[0]) {
-                int option2Height = dpHelper(
-                        books,
-                        shelfWidth,
-                        memo,
-                        i + 1,
-                        remainingShelfWidth - currentBook[0],
-                        maxHeightUpdated
-                );
-                memo[i][remainingShelfWidth] = Math.min(
-                        option1Height,
-                        option2Height
-                );
-                return memo[i][remainingShelfWidth];
+        int[] dp = new int[books.length + 1];
+        dp[books.length] = 0;
+        for (int i = books.length - 1; i >= 0; i--) {
+            int height = books[i][1];
+            int width = books[i][0];
+            dp[i] = height + dp[i + 1];
+            for (int j = i + 1; j < books.length && (width + books[j][0] <= shelfWidth); j++) {
+                height = Math.max(height, books[j][1]);
+                width += books[j][0];
+                dp[i] = Math.min(dp[i], height + dp[j + 1]);
             }
-            memo[i][remainingShelfWidth] = option1Height;
-            return memo[i][remainingShelfWidth];
         }
+        return dp[0];
     }
 }
