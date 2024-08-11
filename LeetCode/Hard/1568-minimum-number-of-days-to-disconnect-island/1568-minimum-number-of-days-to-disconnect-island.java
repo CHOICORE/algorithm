bbb8ec973a78573d76dad5308fc6988218d54fd5
Choi Jae-hyeong <41681972+CHOICORE@users.noreply.github.com>
2024-08-11 13:53:16 +0900

    Time: 1 ms (100%), Space: 42 MB (98.48%) - LeetHub

--- LeetCode/Hard/1568-minimum-number-of-days-to-disconnect-island/1568-minimum-number-of-days-to-disconnect-island.java
+++ LeetCode/Hard/1568-minimum-number-of-days-to-disconnect-island/1568-minimum-number-of-days-to-disconnect-island.java
@@ -1,86 +1,66 @@
 class Solution {
-    static final int[][] DIRECTIONS = {
-            {0, 1},
-            {0, -1},
-            {1, 0},
-            {-1, 0},
-    };
+    int time;
+    int[][] visited;
+    int[][] low;
+    int[] d = new int[]{0, 1, 0, -1, 0};
+    boolean flag;
 
     public int minDays(int[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-
-        int initialIslandCount = countIslands(grid);
-
-        if (initialIslandCount != 1) {
-            return 0;
-        }
-
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
-                if (grid[row][col] == 0) continue;
-
-                grid[row][col] = 0;
-                int newIslandCount = countIslands(grid);
-
-                if (newIslandCount != 1) return 1;
-
-                grid[row][col] = 1;
-            }
-        }
-
-        return 2;
-    }
-
-    int countIslands(int[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-        boolean[][] visited = new boolean[rows][cols];
-        int islandCount = 0;
-
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
-                if (!visited[row][col] && grid[row][col] == 1) {
-                    exploreIsland(grid, row, col, visited);
-                    islandCount++;
+        int n = grid.length;
+        int m = grid[0].length;
+        flag = false;
+        visited = new int[n][m];
+        low = new int[n][m];
+        time = 1;
+        boolean hasArt = false;
+        boolean found = false;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    if (found)
+                        return 0;
+                    found = true;
+                    art(i, j, grid, -100, -100);
                 }
             }
         }
-        return islandCount;
+
+        if (time == 1) {
+            return 0;
+        }
+        if (time == 2) {
+            return 1;
+        }
+        if (flag) {
+            return 1;
+        } else
+            return 2;
     }
 
-    void exploreIsland(
-            int[][] grid,
-            int row,
-            int col,
-            boolean[][] visited
-    ) {
-        visited[row][col] = true;
+    public void art(int row, int col, int[][] grid, int parRow, int parCol) {
+        grid[row][col] = 0;
+        visited[row][col] = time;
+        low[row][col] = time;
+        time++;
+        int child = 0;
+        for (int i = 0; i < 4; i++) {
+            int x = d[i] + row;
+            int y = d[i + 1] + col;
 
-        for (int[] direction : DIRECTIONS) {
-            int newRow = row + direction[0];
-            int newCol = col + direction[1];
-            if (isValidLandCell(grid, newRow, newCol, visited)) {
-                exploreIsland(grid, newRow, newCol, visited);
+            if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || (x == parRow && y == parCol) || (visited[x][y] == 0 && grid[x][y] == 0))
+                continue;
+            if (visited[x][y] == 0) {
+                child++;
+                art(x, y, grid, row, col);
+                low[row][col] = Math.min(low[row][col], low[x][y]);
+                if (low[x][y] >= visited[row][col] && (parRow != -100 && parCol != -100))
+                    flag = true;
+            } else {
+                low[row][col] = Math.min(low[row][col], visited[x][y]);
             }
         }
-    }
 
-    boolean isValidLandCell(
-            int[][] grid,
-            int row,
-            int col,
-            boolean[][] visited
-    ) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-        return (
-                row >= 0 &&
-                        col >= 0 &&
-                        row < rows &&
-                        col < cols &&
-                        grid[row][col] == 1 &&
-                        !visited[row][col]
-        );
+        if (parRow == -100 && parCol == -100 && child > 1)
+            flag = true;
     }
 }
