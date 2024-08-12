9695492e59a3818045f9a6e837e683b3eccd6ddd
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-08-12 13:23:24 +0900

    Time: 23 ms (7.34%), Space: 46.7 MB (76.31%) - LeetHub

--- LeetCode/Easy/0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
+++ LeetCode/Easy/0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -1,89 +1,38 @@
 class KthLargest {
-    int[] a;
-    int size = 0;
-    int k = 0;
-
-    private int left(int i) {
-        return i * 2;
-    }
-
-    private int right(int i) {
-        return i * 2 + 1;
-    }
-
-    private int parent(int i) {
-        return i / 2;
-    }
-
-    private void swap(int i, int j) {
-        int tmp = a[i];
-        a[i] = a[j];
-        a[j] = tmp;
-    }
-
-    private void siftdown(int i) {
-        siftdown(i, a.length);
-    }
-
-    private void siftdown(int i, int n) {
-        while (2 * i < n) {
-            int j = i;
-            if (a[2 * i] < a[j]) {
-                j = 2 * i;
-            }
-            if (2 * i + 1 < n && a[2 * i + 1] < a[j]) {
-                j = 2 * i + 1;
-            }
-            if (i == j) {
-                break;
-            }
-            swap(i, j);
-            i = j;
-        }
-    }
-
-    private void siftup(int i) {
-        while (i / 2 >= 1) {
-            if (a[i / 2] <= a[i]) {
-                break;
-            }
-            swap(i / 2, i);
-            i = i / 2;
-        }
-    }
+    List<Integer> stream;
+    int k;
 
     public KthLargest(int k, int[] nums) {
+        stream = new ArrayList<Integer>(nums.length);
         this.k = k;
-        a = new int[k + 1];
-        heapifyAndInsert(nums);
-    }
 
-    private void heapifyAndInsert(int[] nums) {
-        size = Math.min(nums.length, k);
-        for (int i = 0; i < size; i++) {
-            a[i + 1] = nums[i];
+        for (int num : nums) {
+            stream.add(num);
         }
-        int currK = Math.min(nums.length, k);
-        for (int i = currK / 2; i >= 1; i--) {
-            siftdown(i, currK + 1);
-        }
-        for (int i = size; i < nums.length; i++) {
-            add(nums[i]);
-        }
+
+        Collections.sort(stream);
     }
 
     public int add(int val) {
-        if (size == k) {
-            if (val > a[1]) {
-                a[1] = val;
-                siftdown(1);
+        int index = getIndex(val);
+        stream.add(index, val);
+        return stream.get(stream.size() - k);
+    }
+
+    private int getIndex(int val) {
+        int left = 0;
+        int right = stream.size() - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int midElement = stream.get(mid);
+            if (midElement == val) return mid;
+            if (midElement > val) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
-        } else {
-            size++;
-            a[size] = val;
-            siftup(size);
         }
-        return a[1];
+        return left;
     }
 }
 
