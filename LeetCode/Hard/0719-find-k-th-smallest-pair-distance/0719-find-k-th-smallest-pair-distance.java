f5b12d908a1dddcb6105a37464bea5b615c27c87
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-08-14 19:18:17 +0900

    Time: 8 ms (81.12%), Space: 44.7 MB (21.77%) - LeetHub

--- LeetCode/Hard/0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
+++ LeetCode/Hard/0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
@@ -1,68 +1,31 @@
 class Solution {
     public int smallestDistancePair(int[] nums, int k) {
         Arrays.sort(nums);
-        int arraySize = nums.length;
-        int maxElement = nums[arraySize - 1];
-
-        int prefixCountSize = maxElement * 2;
-
-        int[] prefixCount = new int[prefixCountSize];
-        int[] valueCount = new int[maxElement + 1];
-
-        int prefixIndex = 0;
-        for (int value = 0; value < prefixCountSize; ++value) {
-            while (prefixIndex < arraySize && nums[prefixIndex] <= value) {
-                ++prefixIndex;
-            }
-            prefixCount[value] = prefixIndex;
-        }
-        for (int num : nums) {
-            ++valueCount[num];
-        }
-
-        int left = 0;
-        int right = maxElement;
-        while (left < right) {
-            int middle = (left + right) / 2;
-
-            int count = countPairs(nums, prefixCount, valueCount, middle);
-
-            if (count < k) {
-                left = middle + 1;
+        int low = 0;
+        int high = nums[nums.length - 1] - nums[0];
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int count = countPairsWithSumMid(nums, mid);
+            if (count >= k) {
+                high = mid;
             } else {
-                right = middle;
+                low = mid + 1;
             }
         }
-        return left;
+        return low;
     }
 
-    private int countPairs(
-            int[] nums,
-            int[] prefixCount,
-            int[] valueCount,
-            int maxDistance
-    ) {
+    int countPairsWithSumMid(int[] nums, int sum) {
         int count = 0;
-        int arraySize = nums.length;
-        int index = 0;
-        while (index < arraySize) {
-            int currentValue = nums[index];
-            int valueCountForCurrent = valueCount[currentValue];
-            int pairsWithLargerValues =
-                    prefixCount[Math.min(
-                            currentValue + maxDistance,
-                            prefixCount.length - 1
-                    )] -
-                            prefixCount[currentValue];
-            int pairsWithSameValues =
-                    (valueCountForCurrent * (valueCountForCurrent - 1)) / 2;
-            count +=
-                    pairsWithLargerValues * valueCountForCurrent + pairsWithSameValues;
-            while (index + 1 < arraySize && nums[index] == nums[index + 1]) {
-                ++index;
+        int left = 0;
+
+        for (int right = 1; right < nums.length; right++) {
+            while (nums[right] - nums[left] > sum) {
+                left++;
             }
-            ++index;
+            count += right - left;
         }
         return count;
+
     }
 }
