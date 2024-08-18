99cb333adcace4220bdf7ba37ad80e0acb4f4dc6
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-08-18 11:45:02 +0900

    Time: 11 ms (27.69%), Space: 43.7 MB (34.36%) - LeetHub

--- LeetCode/Medium/0264-ugly-number-ii/0264-ugly-number-ii.java
+++ LeetCode/Medium/0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -1,16 +1,24 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        TreeSet<Long> uglyNumbersSet = new TreeSet<>();
-        uglyNumbersSet.add(1L);
+        return new Ugly().nums[n - 1];
+    }
 
-        Long currentUgly = 1L;
-        for (int i = 0; i < n; i++) {
-            currentUgly = uglyNumbersSet.pollFirst();
-            uglyNumbersSet.add(currentUgly * 2);
-            uglyNumbersSet.add(currentUgly * 3);
-            uglyNumbersSet.add(currentUgly * 5);
+    static class Ugly {
+        public int[] nums = new int[1_690];
+
+        Ugly() {
+            nums[0] = 1;
+            int ugly, i2 = 0, i3 = 0, i5 = 0;
+
+            for (int i = 1; i < 1690; i++) {
+                ugly = Math.min(Math.min(nums[i2] * 2, nums[i3] * 3), nums[i5] * 5);
+                nums[i] = ugly;
+
+                if (ugly == nums[i2] * 2) ++i2;
+                if (ugly == nums[i3] * 3) ++i3;
+                if (ugly == nums[i5] * 5) ++i5;
+
+            }
         }
-
-        return currentUgly.intValue();
     }
 }
