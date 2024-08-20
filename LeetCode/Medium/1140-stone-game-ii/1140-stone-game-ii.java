e7027e5ec33da2eff58f1c229dab1c88cbcb7475
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-08-20 09:33:36 +0900

    Time: 19 ms (25%), Space: 42.4 MB (68.93%) - LeetHub

--- LeetCode/Medium/1140-stone-game-ii/1140-stone-game-ii.java
+++ LeetCode/Medium/1140-stone-game-ii/1140-stone-game-ii.java
@@ -1,23 +1,33 @@
 class Solution {
     public int stoneGameII(int[] piles) {
-        int len = piles.length;
-        int[] preSum = Arrays.copyOf(piles, len);
-        for (int i = len - 2; i >= 0; i--)
-            preSum[i] += preSum[i + 1];
-        return dfs(preSum, 1, 0, new int[len][len]);
-    }
+        int length = piles.length;
+        int[][] dp = new int[length + 1][length + 1];
 
-    private int dfs(int[] preSum, int m, int p, int[][] memo) {
-        if (p + 2 * m >= preSum.length)
-            return preSum[p];
-        if (memo[p][m] > 0)
-            return memo[p][m];
-        int result = 0, take = 0;
-        for (int i = 1; i <= 2 * m; i++) {
-            take = preSum[p] - preSum[p + i];
-            result = Math.max(result, take + preSum[p + i] - dfs(preSum, Math.max(i, m), p + i, memo));
+        int[] suffixSum = new int[length + 1];
+        for (int i = length - 1; i >= 0; i--) {
+            suffixSum[i] = suffixSum[i + 1] + piles[i];
         }
-        memo[p][m] = result;
-        return result;
+
+        for (int i = 0; i <= length; i++) {
+            dp[i][length] = suffixSum[i];
+        }
+
+        for (int index = length - 1; index >= 0; index--) {
+            for (int maxTillNow = length - 1; maxTillNow >= 1; maxTillNow--) {
+                for (
+                        int X = 1;
+                        X <= 2 * maxTillNow && index + X <= length;
+                        X++
+                ) {
+                    dp[index][maxTillNow] = Math.max(
+                            dp[index][maxTillNow],
+                            suffixSum[index] -
+                                    dp[index + X][Math.max(maxTillNow, X)]
+                    );
+                }
+            }
+        }
+        
+        return dp[0][1];
     }
 }
