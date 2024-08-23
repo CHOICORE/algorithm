f45804887446b77eb65b86e14bd45692aef14221
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-08-23 16:13:52 +0900

    Time: 1 ms (100%), Space: 41.7 MB (78.9%) - LeetHub

--- LeetCode/Medium/0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.java
+++ LeetCode/Medium/0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.java
@@ -1,56 +1,136 @@
 class Solution {
     public String fractionAddition(String expression) {
-        int num = 0;
-        int denom = 1;
+        List<Fraction> allFractions = parseFractions(expression);
+        Fraction baseFraction = allFractions.getFirst();
 
-        int i = 0;
-        while (i < expression.length()) {
-            int currNum = 0;
-            int currDenom = 0;
+        for (int i = 1; i < allFractions.size(); i++) {
+            Fraction actedUponFraction = allFractions.get(i);
 
-            boolean isNegative = false;
-            
-            if (expression.charAt(i) == '-' || expression.charAt(i) == '+') {
-                if (expression.charAt(i) == '-') {
-                    isNegative = true;
-                }
-                i++;
-            }
-            
-            while (Character.isDigit(expression.charAt(i))) {
-                int val = expression.charAt(i) - '0';
-                currNum = currNum * 10 + val;
-                i++;
-            }
+            int baseDenominator = baseFraction.denominator;
+            baseFraction.numerator *= actedUponFraction.denominator;
+            baseFraction.denominator *= actedUponFraction.denominator;
+            actedUponFraction.numerator *= baseDenominator;
+            actedUponFraction.denominator *= baseDenominator;
 
-            if (isNegative) currNum *= -1;
-
-            //skip divisor
-            i++;
-            
-            while (
-                    i < expression.length() &&
-                            Character.isDigit(expression.charAt(i))
-            ) {
-                int val = expression.charAt(i) - '0';
-                currDenom = currDenom * 10 + val;
-                i++;
-            }
-            
-            num = num * currDenom + currNum * denom;
-            denom = denom * currDenom;
+            baseFraction.add(actedUponFraction);
+            baseFraction.reduce();
         }
-
-        int gcd = Math.abs(FindGCD(num, denom));
-        
-        num /= gcd;
-        denom /= gcd;
-
-        return num + "/" + denom;
+        return baseFraction.toString();
     }
 
-    private int FindGCD(int a, int b) {
-        if (a == 0) return b;
-        return FindGCD(b % a, a);
+    public List<Fraction> parseFractions(String expression) {
+        List<Fraction> allFractions = new ArrayList<>();
+        int i = 0;
+        do {
+            Fraction fraction = new Fraction();
+            char currentChar = expression.charAt(i);
+
+            if (currentChar == '-') {
+                fraction.isNegative = true;
+                i = i + 1;
+            }
+            if (currentChar == '+') {
+                i = i + 1;
+            }
+            currentChar = expression.charAt(i);
+            do {
+                fraction.placeNumerator(currentChar);
+                i = i + 1;
+                currentChar = expression.charAt(i);
+            } while (currentChar != '/');
+            i = i + 1;
+
+            currentChar = expression.charAt(i);
+            do {
+                fraction.placeDenominator(currentChar);
+                i = i + 1;
+
+                if (i >= expression.length()) {
+                    break;
+                }
+                currentChar = expression.charAt(i);
+
+            } while (currentChar != '+' && currentChar != '-');
+
+            allFractions.add(fraction);
+
+        } while (i < expression.length());
+
+        return allFractions;
+    }
+
+    static class Fraction {
+        public boolean isNegative;
+        public int numerator;
+        public int denominator;
+
+        public void placeNumerator(char n) {
+            if (numerator > 0) {
+                numerator *= 10;
+            }
+            numerator += Character.getNumericValue(n);
+        }
+
+        public void placeDenominator(char n) {
+            if (denominator > 0) {
+                denominator *= 10;
+            }
+            denominator += Character.getNumericValue(n);
+        }
+
+        public int getSignedNumerator() {
+            return isNegative ? -numerator : numerator;
+        }
+
+        public void add(Fraction other) {
+            int finalResult = this.getSignedNumerator() +
+                    other.getSignedNumerator();
+
+            this.isNegative = (finalResult < 0);
+            this.numerator = Math.abs(finalResult);
+
+            if (finalResult == 0) {
+                this.denominator = 1;
+            }
+        }
+
+        public void reduce() {
+            if (denominator == 1) {
+                return;
+            }
+            if (denominator % numerator == 0) {
+                denominator = denominator / numerator;
+                numerator = 1;
+                return;
+            }
+            if (numerator % denominator == 0) {
+                numerator = numerator / denominator;
+                denominator = 1;
+                return;
+            }
+            int multipleCap = Math.min(numerator, denominator) / 2;
+
+            for (int i = 2; i <= multipleCap; i++) {
+                // If not evenly divisible, continue.
+                if (numerator % i != 0 || denominator % i != 0) {
+                    continue;
+                }
+                numerator = numerator / i;
+                denominator = denominator / i;
+                multipleCap = Math.min(numerator, denominator) / 2;
+                i = 1;
+            }
+        }
+
+        public String toString() {
+            StringBuilder str = new StringBuilder();
+            if (isNegative) {
+                str.append('-');
+            }
+            str.append(numerator);
+            str.append('/');
+            str.append(denominator);
+            return str.toString();
+        }
     }
 }
