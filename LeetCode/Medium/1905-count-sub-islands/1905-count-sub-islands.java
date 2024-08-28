787d3598e43b48fa1dda48f7c7f7c01b45c436b8
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-08-28 11:00:53 +0900

    Time: 31 ms (27.05%), Space: 90.4 MB (8.95%) - LeetHub

--- LeetCode/Medium/1905-count-sub-islands/1905-count-sub-islands.java
+++ LeetCode/Medium/1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,9 +1,9 @@
 class Solution {
     private final int[][] directions = {
-            {0, 1},
-            {1, 0},
-            {0, -1},
-            {-1, 0},
+            { 0, 1 },
+            { 1, 0 },
+            { 0, -1 },
+            { -1, 0 },
     };
     
     private boolean isCellLand(int x, int y, int[][] grid) {
@@ -19,39 +19,34 @@ class Solution {
     ) {
         int totalRows = grid2.length;
         int totalCols = grid2[0].length;
-
         boolean isSubIsland = true;
-
-        Queue<int[]> pendingCells = new LinkedList<>();
-        pendingCells.offer(new int[]{x, y});
-        visited[x][y] = true;
         
-        while (!pendingCells.isEmpty()) {
-            int[] curr = pendingCells.poll();
-            int currX = curr[0];
-            int currY = curr[1];
-            
-            if (!isCellLand(currX, currY, grid1)) {
-                isSubIsland = false;
-            }
-
-            for (int[] direction : directions) {
-                int nextX = currX + direction[0];
-                int nextY = currY + direction[1];
-                if (
-                        nextX >= 0 &&
-                                nextY >= 0 &&
-                                nextX < totalRows &&
-                                nextY < totalCols &&
-                                !visited[nextX][nextY] &&
-                                isCellLand(nextX, nextY, grid2)
-                ) {
-                    pendingCells.offer(new int[]{nextX, nextY});
-                    visited[nextX][nextY] = true;
-                }
+        if (!isCellLand(x, y, grid1)) {
+            isSubIsland = false;
+        }
+        
+        for (int[] direction : directions) {
+            int nextX = x + direction[0];
+            int nextY = y + direction[1];
+            if (
+                    nextX >= 0 &&
+                            nextY >= 0 &&
+                            nextX < totalRows &&
+                            nextY < totalCols &&
+                            !visited[nextX][nextY] &&
+                            isCellLand(nextX, nextY, grid2)
+            ) {
+                visited[nextX][nextY] = true;
+                boolean nextCellIsPartOfSubIsland = isSubIsland(
+                        nextX,
+                        nextY,
+                        grid1,
+                        grid2,
+                        visited
+                );
+                isSubIsland = isSubIsland && nextCellIsPartOfSubIsland;
             }
         }
-
         return isSubIsland;
     }
 
@@ -64,12 +59,11 @@ class Solution {
         
         for (int x = 0; x < totalRows; ++x) {
             for (int y = 0; y < totalCols; ++y) {
-                if (
-                        !visited[x][y] &&
-                                isCellLand(x, y, grid2) &&
-                                isSubIsland(x, y, grid1, grid2, visited)
-                ) {
-                    subIslandCounts += 1;
+                if (!visited[x][y] && isCellLand(x, y, grid2)) {
+                    visited[x][y] = true;
+                    if (isSubIsland(x, y, grid1, grid2, visited)) {
+                        subIslandCounts += 1;
+                    }
                 }
             }
         }
