3f197e42565ae4e798e23885368a35017872f44b
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-08-28 11:01:04 +0900

    Time: 15 ms (99.63%), Space: 78.4 MB (78.36%) - LeetHub

--- LeetCode/Medium/1905-count-sub-islands/1905-count-sub-islands.java
+++ LeetCode/Medium/1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,72 +1,46 @@
 class Solution {
-    private final int[][] directions = {
-            { 0, 1 },
-            { 1, 0 },
-            { 0, -1 },
-            { -1, 0 },
-    };
-    
-    private boolean isCellLand(int x, int y, int[][] grid) {
-        return grid[x][y] == 1;
-    }
-    
-    private boolean isSubIsland(
-            int x,
-            int y,
-            int[][] grid1,
-            int[][] grid2,
-            boolean[][] visited
-    ) {
-        int totalRows = grid2.length;
-        int totalCols = grid2[0].length;
-        boolean isSubIsland = true;
-        
-        if (!isCellLand(x, y, grid1)) {
-            isSubIsland = false;
-        }
-        
-        for (int[] direction : directions) {
-            int nextX = x + direction[0];
-            int nextY = y + direction[1];
-            if (
-                    nextX >= 0 &&
-                            nextY >= 0 &&
-                            nextX < totalRows &&
-                            nextY < totalCols &&
-                            !visited[nextX][nextY] &&
-                            isCellLand(nextX, nextY, grid2)
-            ) {
-                visited[nextX][nextY] = true;
-                boolean nextCellIsPartOfSubIsland = isSubIsland(
-                        nextX,
-                        nextY,
-                        grid1,
-                        grid2,
-                        visited
-                );
-                isSubIsland = isSubIsland && nextCellIsPartOfSubIsland;
-            }
-        }
-        return isSubIsland;
-    }
+    int n, m;
+    int[][] isValid;
+    int[] dx = new int[]{-1, 0, 0, 1};
+    int[] dy = new int[]{0, -1, 1, 0};
 
     public int countSubIslands(int[][] grid1, int[][] grid2) {
-        int totalRows = grid2.length;
-        int totalCols = grid2[0].length;
-
-        boolean[][] visited = new boolean[totalRows][totalCols];
-        int subIslandCounts = 0;
-        
-        for (int x = 0; x < totalRows; ++x) {
-            for (int y = 0; y < totalCols; ++y) {
-                if (!visited[x][y] && isCellLand(x, y, grid2)) {
-                    visited[x][y] = true;
-                    if (isSubIsland(x, y, grid1, grid2, visited)) {
-                        subIslandCounts += 1;
+        n = grid1.length;
+        m = grid1[0].length;
+        isValid = new int[n][m];
+        int num = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (isValid[i][j] == 0 && grid2[i][j] == 1) {
+                    if (isSubIslands(i, j, grid1, grid2)) {
+                        num++;
                     }
                 }
             }
         }
-        return subIslandCounts;
+        return num;
+    }
+
+    private boolean isSubIslands(int r, int c, int[][] grid1, int[][] grid2) {
+        if (grid1[r][c] != 1) {
+            isValid[r][c] = -1;
+            return false;
+        }
+        isValid[r][c] = 1;
+        for (int k = 0; k < 4; k++) {
+            int nr = r + dx[k];
+            int nc = c + dy[k];
+            if (nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
+            if (isValid[nr][nc] == -1) {
+                isValid[r][c] = -1;
+                return false;
+            }
+            if (isValid[nr][nc] == 1) continue;
+            if (grid2[nr][nc] == 1 && !isSubIslands(nr, nc, grid1, grid2)) {
+                isValid[r][c] = -1;
+                return false;
+            }
+        }
+        return true;
     }
 }
