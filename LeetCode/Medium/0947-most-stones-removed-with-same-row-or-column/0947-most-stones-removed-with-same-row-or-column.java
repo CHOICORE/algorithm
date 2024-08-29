412f075ea4fcda274711dbdd3c2d108754951455
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-08-29 14:26:29 +0900

    Time: 2 ms (100%), Space: 45 MB (37.69%) - LeetHub

--- LeetCode/Medium/0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ LeetCode/Medium/0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,67 +1,34 @@
 class Solution {
 
+    public int numOfIslands = 0;
+
     public int removeStones(int[][] stones) {
-        int maxRow = 0;
-        int maxCol = 0;
-        int i, j;
-        int n = stones.length;
-        for (i = 0; i < n; i++) {
-            maxRow = Math.max(maxRow, stones[i][0]);
-            maxCol = Math.max(maxCol, stones[i][1]);
+        int[] parent = new int[20_003];
+        for (int[] stone : stones) {
+            unionSets(stone[0] + 1, stone[1] + 10_002, parent);
         }
-        int m = maxRow + maxCol + 1;
-        DisjointSet ds = new DisjointSet(m);
-        int[] res = new int[m + 1];
-        for (i = 0; i < n; i++) {
-            int row = stones[i][0];
-            int col = stones[i][1] + maxRow + 1;
-            ds.unionSet(row, col);
-            res[row] = 1;
-            res[col] = 1;
-        }
-        int com = 0;
-        for (i = 0; i <= m; i++) {
-            if (res[i] == 1) {
-                if (ds.findUPar(i) == i) {
-                    com++;
-                }
-            }
-        }
-        return n - com;
+        return stones.length - numOfIslands;
     }
 
-    static class DisjointSet {
-
-        int[] rank;
-        int[] parent;
-
-        public DisjointSet(int n) {
-            rank = new int[n + 1];
-            parent = new int[n + 1];
-            for (int i = 0; i <= n; i++) {
-                rank[i] = 0;
-                parent[i] = i;
-            }
+    public void unionSets(int a, int b, int[] parent) {
+        int parA = findParent(a, parent), parB = findParent(b, parent);
+        if (parA != parB) {
+            parent[parB] = parA;
+            numOfIslands--;
         }
+        return;
+    }
 
-        public int findUPar(int u) {
-            if (parent[u] == u) {
-                return u;
-            }
-            return parent[u] = findUPar(parent[u]);
+    public int findParent(int node, int[] parent) {
+        if (parent[node] == 0) {
+            parent[node] = node;
+            numOfIslands++;
         }
-
-        public void unionSet(int u, int v) {
-            u = findUPar(u);
-            v = findUPar(v);
-            if (rank[u] < rank[v]) {
-                parent[u] = v;
-            } else if (rank[v] < rank[u]) {
-                parent[v] = u;
-            } else {
-                parent[v] = u;
-                rank[u]++;
-            }
+        if (parent[node] == node) {
+            return node;
         }
+        int par = findParent(parent[node], parent);
+        parent[node] = par;
+        return par;
     }
 }
