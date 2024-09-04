1344e9ba0524412272c6d257ceb5858a94ddce11
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-09-04 13:57:58 +0900

    Time: 10 ms (100%), Space: 49.8 MB (43.23%) - LeetHub

--- LeetCode/Medium/0874-walking-robot-simulation/0874-walking-robot-simulation.java
+++ LeetCode/Medium/0874-walking-robot-simulation/0874-walking-robot-simulation.java
@@ -1,51 +1,75 @@
 class Solution {
 
-    private static final int HASH_MULTIPLIER = 60_001;
-
     public int robotSim(int[] commands, int[][] obstacles) {
-        Set<Integer> obstacleSet = new HashSet<>();
+        Robot robot = new Robot();
+        Set<Obstacle> obstacleSet = new HashSet<>();
         for (int[] obstacle : obstacles) {
-            obstacleSet.add(hashCoordinates(obstacle[0], obstacle[1]));
+            obstacleSet.add(new Obstacle(obstacle[0], obstacle[1]));
         }
-
-        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
-        int[] currentPosition = {0, 0};
-        int maxDistanceSquared = 0;
-        int currentDirection = 0;
-
         for (int command : commands) {
-            if (command == -1) {
-                currentDirection = (currentDirection + 1) % 4;
-                continue;
-            }
-            if (command == -2) {
-                currentDirection = (currentDirection + 3) % 4;
-                continue;
-            }
+            robot.handleCommand(command, obstacleSet);
+        }
+        return robot.maxDistance;
+    }
 
-            int[] direction = directions[currentDirection];
-            for (int step = 0; step < command; step++) {
-                int nextX = currentPosition[0] + direction[0];
-                int nextY = currentPosition[1] + direction[1];
-                if (obstacleSet.contains(hashCoordinates(nextX, nextY))) {
-                    break;
-                }
-                currentPosition[0] = nextX;
-                currentPosition[1] = nextY;
-            }
+    private static class Robot {
 
-            maxDistanceSquared = Math.max(
-                maxDistanceSquared,
-                currentPosition[0] * currentPosition[0] +
-                    currentPosition[1] * currentPosition[1]
-            );
+        public int x = 0, y = 0, dir = 0, maxDistance = 0;
+        public Set<Obstacle> obstacles;
+
+        Robot() {
         }
 
-        return maxDistanceSquared;
+        private void handleCommand(int command, Set<Obstacle> obstacles) {
+            switch (command) {
+                case -2:
+                    dir = (dir + 3) % 4;
+                    return;
+                case -1:
+                    dir = (dir + 1) % 4;
+                    return;
+                default: {
+                    while (command-- > 0) {
+                        int newX = x, newY = y;
+                        switch (dir) {
+                            case 0:
+                                ++newY;
+                                break;
+                            case 1:
+                                ++newX;
+                                break;
+                            case 2:
+                                --newY;
+                                break;
+                            default:
+                                --newX;
+                        }
+                        if (!obstacles.contains(new Obstacle(newX, newY))) {
+                            x = newX;
+                            y = newY;
+                        }
+                    }
+                    maxDistance = Math.max(x * x + y * y, maxDistance);
+                }
+            }
+        }
     }
-    
-    private int hashCoordinates(int x, int y) {
-        return x + HASH_MULTIPLIER * y;
+
+    private static class Obstacle {
+
+        public int x, y;
+
+        public Obstacle(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public boolean equals(Object other) {
+            return x == ((Obstacle) other).x && y == ((Obstacle) other).y;
+        }
+
+        public int hashCode() {
+            return x + y * 413;
+        }
     }
 }
