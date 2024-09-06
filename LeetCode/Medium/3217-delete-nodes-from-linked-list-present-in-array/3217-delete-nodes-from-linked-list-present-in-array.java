d8e56e1375b627a09a041d88177118d30344c769
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-09-06 15:12:43 +0900

    Time: 3 ms (100%), Space: 59.9 MB (99.97%) - LeetHub

--- LeetCode/Medium/3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.java
+++ LeetCode/Medium/3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.java
@@ -9,30 +9,34 @@
  * }
  */
 class Solution {
-
+    
     public ListNode modifiedList(int[] nums, ListNode head) {
-        Set<Integer> valuesToRemove = new HashSet<>();
-        for (int num : nums) {
-            valuesToRemove.add(num);
-        }
-        
-        while (head != null && valuesToRemove.contains(head.val)) {
-            head = head.next;
-        }
-        
-        if (head == null) {
-            return null;
-        }
-        
-        ListNode current = head;
-        while (current.next != null) {
-            if (valuesToRemove.contains(current.next.val)) {
-                current.next = current.next.next;
-            } else {
-                current = current.next;
+        int max = 0;
+        for (int n : nums) {
+            if (n > max) {
+                max = n;
             }
         }
+        boolean[] seen = new boolean[max + 1];
 
+        for (int n : nums) {
+            seen[n] = true;
+        }
+        while (head != null && head.val <= max && seen[head.val]) {
+            head = head.next;
+        }
+        if (head == null) {
+            return head;
+        }
+        ListNode curr = head.next, prev = head;
+        while (curr != null) {
+            if (curr.val <= max && seen[curr.val]) {
+                prev.next = curr.next;
+            } else {
+                prev = curr;
+            }
+            curr = curr.next;
+        }
         return head;
     }
 }
