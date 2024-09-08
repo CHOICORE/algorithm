500efb7db0e4acd4333d56a9cdb71ef2b6d8ea1c
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-09-08 15:42:01 +0900

    Time: 0 ms (100%), Space: 43.6 MB (14.09%) - LeetHub

--- LeetCode/Medium/0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
+++ LeetCode/Medium/0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
@@ -8,33 +8,42 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-public class Solution {
-    int x = 0;
-
-    int count(ListNode root) {
-        ListNode temp = root;
-        while (temp != null) {
-            temp = temp.next;
-            x++;
-        }
-        return x;
-    }
-
+class Solution {
     public ListNode[] splitListToParts(ListNode head, int k) {
-        int n = count(head);
-        int x = n / k, y = n % k;
-        ListNode[] v = new ListNode[k];
-        ListNode a = head, b = null;
-        for (int i = 0; i < k; i++) {
-            v[i] = head;
-            for (int j = 0; j < x + (i < y ? 1 : 0); j++) {
-                b = head;
-                head = head.next;
-            }
-            if (b != null) {
-                b.next = null;
-            }
+        ListNode[] ans = new ListNode[k];
+        
+        int size = 0;
+        ListNode current = head;
+        while (current != null) {
+            size++;
+            current = current.next;
         }
-        return v;
+        
+        int splitSize = size / k;
+        
+        int numRemainingParts = size % k;
+
+        current = head;
+        for (int i = 0; i < k; i++) {
+            // create the i-th part
+            ListNode newPart = new ListNode(0);
+            ListNode tail = newPart;
+
+            int currentSize = splitSize;
+            if (numRemainingParts > 0) {
+                numRemainingParts--;
+                currentSize++;
+            }
+            int j = 0;
+            while (j < currentSize) {
+                tail.next = new ListNode(current.val);
+                tail = tail.next;
+                j++;
+                current = current.next;
+            }
+            ans[i] = newPart.next;
+        }
+
+        return ans;
     }
 }
