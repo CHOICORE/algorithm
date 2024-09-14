cee0d9b4fa3ee64a7eae7b5226dc3c7ee26960a2
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-09-14 15:43:56 +0900

    Time: 4 ms (22.47%), Space: 59.3 MB (69.66%) - LeetHub

---
+++ LeetCode/Medium/2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public int longestSubarray(int[] nums) {
+        int maxVal = 0, answer = 0, currentStreak = 0;
+
+        for (int num : nums) {
+            if (maxVal < num) {
+                maxVal = num;
+                answer = currentStreak = 0;
+            }
+
+            if (maxVal == num) {
+                currentStreak++;
+            } else {
+                currentStreak = 0;
+            }
+
+            answer = Math.max(answer, currentStreak);
+        }
+        return answer;
+    }
+}
