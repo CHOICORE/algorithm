16796143db81ac5a585aac0eaacde750ea4e4553
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-09-20 13:21:29 +0900

    Time: 218 ms (28.96%), Space: 45 MB (32.13%) - LeetHub

---
+++ LeetCode/Hard/0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String shortestPalindrome(String s) {
+        int length = s.length();
+        String reversedString = new StringBuilder(s).reverse().toString();
+
+        for (int i = 0; i < length; i++) {
+            if (
+                s.substring(0, length - i).equals(reversedString.substring(i))
+            ) {
+                return reversedString.substring(0, i) + s;
+            }
+        }
+        return "";
+    }
+}
