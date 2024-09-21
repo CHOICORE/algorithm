3d9d7033a17c752727209d0afabf0004da82c019
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-09-21 16:52:47 +0900

    [level 2] Title: 기능개발, Time: 3.07 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,16 +1,32 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] complete = new int[100];
-        int day = -1;
+        int complete = 100;
+
+        Queue<Integer> taken = new LinkedList<>();
+
         for (int i = 0; i < progresses.length; i++) {
-            while (progresses[i] + (day * speeds[i]) < 100) {
-                day++;
-            }
-            complete[day]++;
+            taken.add((complete - progresses[i] - 1) / speeds[i] + 1);
         }
-        return Arrays.stream(complete).filter(i -> i != 0).toArray();
+
+        List<Integer> deploys = new ArrayList<>();
+        while (!taken.isEmpty()) {
+            int count = 1;
+            int head = taken.poll();
+
+            while (!taken.isEmpty() && taken.peek() <= head) {
+                taken.poll();
+                count++;
+            }
+
+            deploys.add(count);
+        }
+
+        return deploys
+            .stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
+
     }
 }
