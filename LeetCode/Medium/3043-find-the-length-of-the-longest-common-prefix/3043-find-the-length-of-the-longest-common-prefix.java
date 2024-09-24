7f8de70cd6998971ed75ebba32c776251b3a660d
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-09-24 14:43:43 +0900

    Time: 34 ms (91.99%), Space: 55.1 MB (92.46%) - LeetHub

--- LeetCode/Medium/3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
+++ LeetCode/Medium/3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
@@ -1,28 +1,55 @@
-import java.util.HashSet;
+class TrieNode {
+    TrieNode[] children = new TrieNode[10];
+}
+
+class Trie {
+
+    TrieNode root = new TrieNode();
+
+    void insert(int num) {
+        TrieNode node = root;
+        String numStr = Integer.toString(num);
+        for (char digit : numStr.toCharArray()) {
+            int idx = digit - '0';
+            if (node.children[idx] == null) {
+                node.children[idx] = new TrieNode();
+            }
+            node = node.children[idx];
+        }
+    }
+
+    int findLongestPrefix(int num) {
+        TrieNode node = root;
+        String numStr = Integer.toString(num);
+        int len = 0;
+
+        for (char digit : numStr.toCharArray()) {
+            int idx = digit - '0';
+            if (node.children[idx] != null) {
+                len++;
+                node = node.children[idx];
+            } else {
+                break;
+            }
+        }
+        return len;
+    }
+}
 
 class Solution {
+
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
-        Set<Integer> arr1Prefixes = new HashSet<Integer>();
-        
-        for (int val : arr1) {
-            while (!arr1Prefixes.contains(val) && val > 0) {
-                arr1Prefixes.add(val);
-                val /= 10;
-            }
+        Trie trie = new Trie();
+
+        for (int num : arr1) {
+            trie.insert(num);
         }
 
         int longestPrefix = 0;
-        
-        for (int val : arr2) {
-            while (!arr1Prefixes.contains(val) && val > 0) {
-                val /= 10;
-            }
-            if (val > 0) {
-                longestPrefix = Math.max(
-                    longestPrefix,
-                    (int) Math.log10(val) + 1
-                );
-            }
+
+        for (int num : arr2) {
+            int len = trie.findLongestPrefix(num);
+            longestPrefix = Math.max(longestPrefix, len);
         }
 
         return longestPrefix;
