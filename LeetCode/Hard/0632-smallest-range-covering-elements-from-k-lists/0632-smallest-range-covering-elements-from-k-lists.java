cda5cd9e519e98723921f2ef9ccb83838d913d11
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-10-13 11:20:59 +0900

    Time: 39 ms (27.87%), Space: 52 MB (10.57%) - LeetHub

--- LeetCode/Hard/0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
+++ LeetCode/Hard/0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
@@ -1,34 +1,42 @@
 class Solution {
     public int[] smallestRange(List<List<Integer>> nums) {
-        int k = nums.size();
-        int[] indices = new int[k];
-        int[] range = new int[]{0, Integer.MAX_VALUE};
+        List<int[]> merged = new ArrayList<>();
 
-        while (true) {
-            int curMin = Integer.MAX_VALUE, curMax =
-                    Integer.MIN_VALUE, minListIndex = 0;
-
-            for (int i = 0; i < k; i++) {
-                int currentElement = nums.get(i).get(indices[i]);
-
-                if (currentElement < curMin) {
-                    curMin = currentElement;
-                    minListIndex = i;
-                }
-
-                if (currentElement > curMax) {
-                    curMax = currentElement;
-                }
+        for (int i = 0; i < nums.size(); i++) {
+            for (int num : nums.get(i)) {
+                merged.add(new int[]{num, i});
             }
-
-            if (curMax - curMin < range[1] - range[0]) {
-                range[0] = curMin;
-                range[1] = curMax;
-            }
-
-            if (++indices[minListIndex] == nums.get(minListIndex).size()) break;
         }
 
-        return range;
+        merged.sort(Comparator.comparingInt(a -> a[0]));
+
+        Map<Integer, Integer> freq = new HashMap<>();
+        int left = 0, count = 0;
+        int rangeStart = 0, rangeEnd = Integer.MAX_VALUE;
+
+        for (int[] ints : merged) {
+            freq.put(
+                    ints[1],
+                    freq.getOrDefault(ints[1], 0) + 1
+            );
+            if (freq.get(ints[1]) == 1) count++;
+
+            while (count == nums.size()) {
+                int curRange = ints[0] - merged.get(left)[0];
+                if (curRange < rangeEnd - rangeStart) {
+                    rangeStart = merged.get(left)[0];
+                    rangeEnd = ints[0];
+                }
+
+                freq.put(
+                        merged.get(left)[1],
+                        freq.get(merged.get(left)[1]) - 1
+                );
+                if (freq.get(merged.get(left)[1]) == 0) count--;
+                left++;
+            }
+        }
+
+        return new int[]{rangeStart, rangeEnd};
     }
 }
