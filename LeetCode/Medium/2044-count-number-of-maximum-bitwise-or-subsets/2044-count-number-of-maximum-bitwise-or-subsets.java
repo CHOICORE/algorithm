58be40e011006c1b6439ad9fdc03186e5966fccd
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-10-18 15:04:42 +0900

    Time: 16 ms (48.29%), Space: 61.4 MB (5.26%) - LeetHub

--- LeetCode/Medium/2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.java
+++ LeetCode/Medium/2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.java
@@ -1,31 +1,48 @@
 class Solution {
     public int countMaxOrSubsets(int[] nums) {
+        int n = nums.length;
         int maxOrValue = 0;
+
         for (int num : nums) {
             maxOrValue |= num;
         }
-        return countSubsets(nums, 0, 0, maxOrValue);
+
+        Integer[][] memo = new Integer[n][maxOrValue + 1];
+
+        return countSubsetsRecursive(nums, 0, 0, maxOrValue, memo);
     }
 
-    private int countSubsets(
+    private int countSubsetsRecursive(
             int[] nums,
             int index,
             int currentOr,
-            int targetOr
+            int targetOr,
+            Integer[][] memo
     ) {
         if (index == nums.length) {
             return (currentOr == targetOr) ? 1 : 0;
         }
 
-        int countWithout = countSubsets(nums, index + 1, currentOr, targetOr);
+        if (memo[index][currentOr] != null) {
+            return memo[index][currentOr];
+        }
 
-        int countWith = countSubsets(
+        int countWithout = countSubsetsRecursive(
+                nums,
+                index + 1,
+                currentOr,
+                targetOr,
+                memo
+        );
+
+        int countWith = countSubsetsRecursive(
                 nums,
                 index + 1,
                 currentOr | nums[index],
-                targetOr
+                targetOr,
+                memo
         );
         
-        return countWithout + countWith;
+        return memo[index][currentOr] = countWithout + countWith;
     }
 }
