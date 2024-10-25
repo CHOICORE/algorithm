0914357efa54413312e1aa2de19a4f7be8d7dbf2
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-10-25 10:15:14 +0900

    Time: 23 ms (99.59%), Space: 56.1 MB (40.41%) - LeetHub

--- LeetCode/Medium/1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
+++ LeetCode/Medium/1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
@@ -1,29 +1,55 @@
+class Trie {
+    Node root;
+
+    Trie() {
+        root = new Node();
+    }
+
+    public void insert(String s) {
+        Node curr = root;
+        for (char ch : s.toCharArray()) {
+            int idx = ch - 'a';
+            if (ch == '/') idx = 26;
+            if (curr.children[idx] == null) curr.children[idx] = new Node();
+            curr = curr.children[idx];
+        }
+        curr = curr.children[26] = new Node();
+        curr.eow = true;
+    }
+
+    public boolean search(String s) {
+        Node curr = root;
+        for (char ch : s.toCharArray()) {
+            int idx = ch - 'a';
+            if (ch == '/') idx = 26;
+            if (curr.children[idx] == null) return false;
+            curr = curr.children[idx];
+            if (curr.eow) return true;
+        }
+        return false;
+    }
+
+    class Node {
+        boolean eow;
+        Node[] children;
+
+        Node() {
+            children = new Node[27];
+        }
+    }
+}
+
 class Solution {
     public List<String> removeSubfolders(String[] folder) {
-        Set<String> folderSet = new HashSet<>(Arrays.asList(folder));
-        List<String> result = new ArrayList<>();
-
-        for (String f : folder) {
-            boolean isSubFolder = false;
-            String prefix = f;
-
-            while (!prefix.isEmpty()) {
-                int pos = prefix.lastIndexOf('/');
-                if (pos == -1) break;
-
-                prefix = prefix.substring(0, pos);
-
-                if (folderSet.contains(prefix)) {
-                    isSubFolder = true;
-                    break;
-                }
-            }
-            
-            if (!isSubFolder) {
-                result.add(f);
+        Trie trie = new Trie();
+        Arrays.sort(folder, (a, b) -> a.length() - b.length());
+        List<String> answer = new ArrayList<>();
+        for (String s : folder) {
+            if (!trie.search(s)) {
+                answer.add(s);
+                trie.insert(s);
             }
         }
-
-        return result;
+        return answer;
     }
 }
