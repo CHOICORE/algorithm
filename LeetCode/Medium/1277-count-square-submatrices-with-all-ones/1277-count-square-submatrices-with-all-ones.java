00f660a6aab5dbae36aef8e0422a0b9476c6e2ec
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-10-27 09:57:29 +0900

    Time: 6 ms (70.73%), Space: 54.9 MB (63.01%) - LeetHub

--- LeetCode/Medium/1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ LeetCode/Medium/1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -1,38 +1,21 @@
 class Solution {
 
-    public int solve(int i, int j, int[][] grid, int[][] dp) {
-        if (i >= grid.length || j >= grid[0].length) {
-            return 0;
-        }
-        if (grid[i][j] == 0) {
-            return 0;
-        }
-
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        int right = solve(i, j + 1, grid, dp);
-        int diagonal = solve(i + 1, j + 1, grid, dp);
-        int below = solve(i + 1, j, grid, dp);
-
-        return dp[i][j] = 1 + Math.min(right, Math.min(diagonal, below));
-    }
-
-    public int countSquares(int[][] grid) {
+    public int countSquares(int[][] matrix) {
+        int row = matrix.length, col = matrix[0].length;
+        int[][] dp = new int[row + 1][col + 1];
         int answer = 0;
-        int[][] dp = new int[grid.length][grid[0].length];
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                Arrays.fill(dp[i], -1);
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == 1) {
+                    dp[i + 1][j + 1] = Math.min(
+                            Math.min(dp[i][j + 1], dp[i + 1][j]),
+                            dp[i][j]
+                    ) +
+                            1;
+                    answer += dp[i + 1][j + 1];
+                }
             }
         }
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                answer += solve(i, j, grid, dp);
-            }
-        }
-
         return answer;
     }
 }
