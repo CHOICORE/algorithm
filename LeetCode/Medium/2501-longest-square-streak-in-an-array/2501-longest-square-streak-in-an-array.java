c962716dcc32fff2e67e216c0839eb766cbf9a7c
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-10-28 10:13:35 +0900

    Time: 39 ms (84.38%), Space: 57.6 MB (87.5%) - LeetHub

--- LeetCode/Medium/2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.java
+++ LeetCode/Medium/2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.java
@@ -1,44 +1,27 @@
 class Solution {
     public int longestSquareStreak(int[] nums) {
-        Arrays.sort(nums);
-        Set<Integer> processedNumbers = new HashSet<>();
         int longestStreak = 0;
-        
-        for (int current : nums) {
-            if (processedNumbers.contains(current)) continue;
 
-            int streak = current;
-            int streakLength = 1;
-            
-            while (true) {
-                if ((long) streak * (long) streak > 1e5) break;
-                if (binarySearch(nums, streak * streak)) {
-                    streak *= streak;
-                    processedNumbers.add(streak);
-                    streakLength++;
-                } else {
-                    break;
-                }
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        for (int num : nums) {
+            uniqueNumbers.add(num);
+        }
+
+        for (int startNumber : nums) {
+            int currentStreak = 0;
+            long current = startNumber;
+
+            while (uniqueNumbers.contains((int) current)) {
+                currentStreak++;
+
+                if (current * current > 1e5) break;
+
+                current *= current;
             }
-            longestStreak = Math.max(longestStreak, streakLength);
+
+            longestStreak = Math.max(longestStreak, currentStreak);
         }
         
         return longestStreak < 2 ? -1 : longestStreak;
-    }
-    
-    private boolean binarySearch(int[] nums, int target) {
-        if (target < 0) return false;
-
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) return true;
-            if (nums[mid] > target) right = mid - 1;
-            else left = mid + 1;
-        }
-
-        return false;
     }
 }
