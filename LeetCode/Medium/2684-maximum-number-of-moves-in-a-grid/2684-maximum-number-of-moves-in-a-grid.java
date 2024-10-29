7b462462fd4a0af236cb676c4bcb9863b14a61be
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-10-29 14:57:34 +0900

    Time: 5 ms (100%), Space: 56.6 MB (27.54%) - LeetHub

--- LeetCode/Medium/2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.java
+++ LeetCode/Medium/2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.java
@@ -1,46 +1,42 @@
 class Solution {
-    private final int[] dirs = {-1, 0, 1};
-
     public int maxMoves(int[][] grid) {
-        int M = grid.length, N = grid[0].length;
-
-        Queue<int[]> q = new LinkedList<>();
-        boolean[][] vis = new boolean[M][N];
-
-        for (int i = 0; i < M; i++) {
-            vis[i][0] = true;
-            q.offer(new int[]{i, 0, 0});
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] dp = new int[n][m];
+        for (int[] r : dp) {
+            Arrays.fill(r, -1);
         }
 
         int maxMoves = 0;
-        while (!q.isEmpty()) {
-            int sz = q.size();
+        for (int i = 0; i < n; i++) {
 
-            while (sz-- > 0) {
-                int[] v = q.poll();
+            maxMoves = Math.max(maxMoves, solve(i, 0, n, m, grid, dp));
 
-                int row = v[0], col = v[1], count = v[2];
-
-                maxMoves = Math.max(maxMoves, count);
-
-                for (int dir : dirs) {
-                    int newRow = row + dir, newCol = col + 1;
-
-                    if (
-                            newRow >= 0 &&
-                                    newCol >= 0 &&
-                                    newRow < M &&
-                                    newCol < N &&
-                                    !vis[newRow][newCol] &&
-                                    grid[row][col] < grid[newRow][newCol]
-                    ) {
-                        vis[newRow][newCol] = true;
-                        q.offer(new int[]{newRow, newCol, count + 1});
-                    }
-                }
-            }
         }
 
         return maxMoves;
+    }
+
+    private int solve(int i, int j, int n, int m, int[][] grid, int[][] dp) {
+        if (i < 0 || j < 0 || i >= n || j >= m) {
+            return 0;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        int ans = 0;
+        if (i - 1 >= 0 && j + 1 < m && grid[i][j] < grid[i - 1][j + 1]) {
+            ans = Math.max(ans, 1 + solve(i - 1, j + 1, n, m, grid, dp));
+        }
+        if (j + 1 < m && grid[i][j] < grid[i][j + 1]) {
+            ans = Math.max(ans, 1 + solve(i, j + 1, n, m, grid, dp));
+        }
+        if (i + 1 < n && j + 1 < m && grid[i][j] < grid[i + 1][j + 1]) {
+            ans = Math.max(ans, 1 + solve(i + 1, j + 1, n, m, grid, dp));
+        }
+
+        dp[i][j] = ans;
+        return dp[i][j];
     }
 }
