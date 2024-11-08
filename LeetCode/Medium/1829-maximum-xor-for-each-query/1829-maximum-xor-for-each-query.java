bf6c2dbc722a228435c2cedf272e80b8c0834c9f
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-11-08 15:48:03 +0900

    Time: 3 ms (78.95%), Space: 58.8 MB (61.43%) - LeetHub

--- LeetCode/Medium/1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.java
+++ LeetCode/Medium/1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.java
@@ -1,18 +1,19 @@
-class Solution {
+public class Solution {
     public int[] getMaximumXor(int[] nums, int maximumBit) {
-        int xorProduct = 0;
+        int n = nums.length;
+        int maxVal = (1 << maximumBit) - 1;
+        int[] result = new int[n];
+        int xor = 0;
+
         for (int num : nums) {
-            xorProduct = xorProduct ^ num;
-        }
-        int[] ans = new int[nums.length];
-
-        int mask = (1 << maximumBit) - 1;
-
-        for (int i = 0; i < nums.length; i++) {
-            ans[i] = xorProduct ^ mask;
-            xorProduct = xorProduct ^ nums[nums.length - 1 - i];
+            xor ^= num;
         }
 
-        return ans;
+        for (int i = 0; i < n; i++) {
+            result[i] = xor ^ maxVal;
+            xor ^= nums[n - 1 - i];
+        }
+
+        return result;
     }
 }
