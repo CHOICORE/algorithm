fcc7cef6118498a8f6fe60f0f0044acd5e30dbf1
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-11-09 14:28:56 +0900

    Time: 1 ms (87.5%), Space: 41.4 MB (5.77%) - LeetHub

--- LeetCode/Medium/3133-minimum-array-end/3133-minimum-array-end.java
+++ LeetCode/Medium/3133-minimum-array-end/3133-minimum-array-end.java
@@ -1,8 +1,36 @@
-class Solution {
+public class Solution {
     public long minEnd(int n, int x) {
-        long result = x;
-        while (--n > 0) {
-            result = (result + 1) | x;
+        long result = 0, bit;
+        --n;
+
+        int[] binaryX = new int[64];
+        int[] binaryN = new int[64];
+
+        long longN = n;
+
+        for (int i = 0; i < 64; ++i) {
+            bit = ((long) x >> i) & 1;
+            binaryX[i] = (int) bit;
+
+            bit = (longN >> i) & 1;
+            binaryN[i] = (int) bit;
+        }
+
+        int posX = 0, posN = 0;
+
+        while (posX < 64) {
+            while (binaryX[posX] != 0) {
+                ++posX;
+            }
+            binaryX[posX] = binaryN[posN];
+            ++posX;
+            ++posN;
+        }
+
+        for (int i = 0; i < 64; ++i) {
+            if (binaryX[i] == 1) {
+                result += (long) Math.pow(2, i);
+            }
         }
 
         return result;
