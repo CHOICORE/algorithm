1cce862f9822e59f31a8da5ebf310e260188b662
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-11-13 22:29:41 +0900

    Time: 65 ms (42.75%), Space: 57.4 MB (26.26%) - LeetHub

---
+++ LeetCode/Medium/2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
@@ -0,0 +1,37 @@
+class Solution {
+    long lowerBound(int[] nums, int low, int high, int element) {
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+            if (nums[mid] >= element) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        long ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+
+            long low = lowerBound(
+                    nums,
+                    i + 1,
+                    nums.length - 1,
+                    lower - nums[i]
+            );
+
+            long high = lowerBound(
+                    nums,
+                    i + 1,
+                    nums.length - 1,
+                    upper - nums[i] + 1
+            );
+
+            ans += (high - low);
+        }
+        return ans;
+    }
+}
