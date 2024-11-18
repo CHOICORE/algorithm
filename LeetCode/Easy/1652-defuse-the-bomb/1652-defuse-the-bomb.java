60f59fbfa2cb32259436074be53b7496da02e403
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-11-18 15:17:15 +0900

    Time: 0 ms (100%), Space: 42.1 MB (55.38%) - LeetHub

--- LeetCode/Easy/1652-defuse-the-bomb/1652-defuse-the-bomb.java
+++ LeetCode/Easy/1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
         int[] result = new int[code.length];
-        if (k == 0) {
-            return result;
+        if (k == 0) return result;
+        int start = 1, end = k, sum = 0;
+        if (k < 0) {
+            start = code.length - Math.abs(k);
+            end = code.length - 1;
         }
-        for (int i = 0; i < result.length; i++) {
-            if (k > 0) {
-                for (int j = i + 1; j < i + k + 1; j++) {
-                    result[i] += code[j % code.length];
-                }
-            } else {
-                for (int j = i - Math.abs(k); j < i; j++) {
-                    result[i] += code[(j + code.length) % code.length];
-                }
-            }
+        for (int i = start; i <= end; i++) sum += code[i];
+        for (int i = 0; i < code.length; i++) {
+            result[i] = sum;
+            sum -= code[(start) % code.length];
+            sum += code[(end + 1) % code.length];
+            start++;
+            end++;
         }
         return result;
     }
