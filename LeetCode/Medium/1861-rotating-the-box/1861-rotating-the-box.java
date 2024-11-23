feb60657f682d3f44de0fcdf579418132ef13d8b
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-11-23 12:15:06 +0900

    Time: 7 ms (79.67%), Space: 79.7 MB (36.21%) - LeetHub

--- LeetCode/Medium/1861-rotating-the-box/1861-rotating-the-box.java
+++ LeetCode/Medium/1861-rotating-the-box/1861-rotating-the-box.java
@@ -2,35 +2,65 @@ class Solution {
     public char[][] rotateTheBox(char[][] box) {
         int m = box.length;
         int n = box[0].length;
-        char[][] result = new char[n][m];
+        char[][] ans = new char[n][m];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                result[i][j] = box[j][i];
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m / 2; j++) {
-                char temp = result[i][j];
-                result[i][j] = result[i][m - 1 - j];
-                result[i][m - 1 - j] = temp;
-            }
-        }
-
-        for (int j = 0; j < m; j++) {
-            int lowestRowWithEmptyCell = n - 1;
-            for (int i = n - 1; i >= 0; i--) {
-                if (result[i][j] == '#') {
-                    result[i][j] = '.';
-                    result[lowestRowWithEmptyCell][j] = '#';
-                    lowestRowWithEmptyCell--;
-                }
-                if (result[i][j] == '*') {
-                    lowestRowWithEmptyCell = i - 1;
+        for (int i = 0; i < m; i++) {
+            int pos = n - 1;
+            for (int j = pos; j >= 0; j--) {
+                switch (box[i][j]) {
+                    case '#':
+                        ans[j][m - 1 - i] = '.';
+                        ans[pos][m - 1 - i] = '#';
+                        pos -= 1;
+                        break;
+                    case '*':
+                        ans[j][m - 1 - i] = '*';
+                        pos = j - 1;
+                        break;
+                    default:
+                        ans[j][m - 1 - i] = '.';
+                        break;
                 }
             }
+            while (pos >= 0) {
+                ans[pos--][m - 1 - i] = '.';
+            }
         }
-        return result;
+        return ans;
+    }
+
+    private void moveRight(char[][] box) {
+        for (int i = 0; i < box.length; i++) {
+            int pos = box[i].length - 1;
+            for (int j = pos; j >= 0; j--) {
+                switch (box[i][j]) {
+                    case '#':
+                        box[i][j] = '.';
+                        box[i][pos--] = '#';
+                        break;
+                    case '*':
+                        pos = j - 1;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            while (pos >= 0) {
+                box[i][pos--] = '.';
+            }
+        }
+    }
+
+    private char[][] rotate(char[][] box) {
+        int m = box.length;
+        int n = box[0].length;
+        char[][] ans = new char[n][m];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[j][m - i - 1] = box[i][j];
+            }
+        }
+        return ans;
     }
 }
