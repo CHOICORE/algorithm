0a78f51f459d2abfdbafb0d07c2739cd9108dd00
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-11-25 15:03:03 +0900

    Time: 6 ms (97.46%), Space: 43.2 MB (77.24%) - LeetHub

--- LeetCode/Hard/0773-sliding-puzzle/0773-sliding-puzzle.java
+++ LeetCode/Hard/0773-sliding-puzzle/0773-sliding-puzzle.java
@@ -1,45 +1,55 @@
 class Solution {
-    private final int[][] directions = {
-        {1, 3},
-        {0, 2, 4},
-        {1, 5},
-        {0, 4},
-        {3, 5, 1},
-        {4, 2},
-    };
 
     public int slidingPuzzle(int[][] board) {
+        int[][] directions = new int[][]{
+            {1, 3},
+            {0, 2, 4},
+            {1, 5},
+            {0, 4},
+            {1, 3, 5},
+            {2, 4},
+        };
+
+        String target = "123450";
         StringBuilder startState = new StringBuilder();
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 3; j++) {
-                startState.append(board[i][j]);
+
+        for (int[] ints : board) {
+            for (int j = 0; j < board[0].length; j++) {
+                startState.append(ints[j]);
             }
         }
 
-        Map<String, Integer> visited = new HashMap<>();
+        Set<String> visited = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+        queue.add(startState.toString());
+        visited.add(startState.toString());
 
-        dfs(startState.toString(), visited, startState.indexOf("0"), 0);
+        int moves = 0;
 
-        return visited.getOrDefault("123450", -1);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                String currentState = queue.poll();
+
+                if (currentState.equals(target)) {
+                    return moves;
+                }
+
+                int zeroPos = currentState.indexOf('0');
+                for (int newPos : directions[zeroPos]) {
+                    String nextState = swap(currentState, zeroPos, newPos);
+
+                    if (visited.contains(nextState)) continue;
+
+                    visited.add(nextState);
+                    queue.add(nextState);
+                }
+            }
+            moves++;
+        }
+        return -1;
     }
 
-    private void dfs(
-        String state,
-        Map<String, Integer> visited,
-        int zeroPos,
-        int moves
-    ) {
-        if (visited.containsKey(state) && visited.get(state) <= moves) {
-            return;
-        }
-        visited.put(state, moves);
-
-        for (int nextPos : directions[zeroPos]) {
-            String newState = swap(state, zeroPos, nextPos);
-            dfs(newState, visited, nextPos, moves + 1);
-        }
-    }
-    
     private String swap(String str, int i, int j) {
         StringBuilder sb = new StringBuilder(str);
         sb.setCharAt(i, str.charAt(j));
