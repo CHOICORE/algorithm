c98de1671ab07e658cbd409ef28e3158deae67f4
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-12-06 11:37:50 +0900

    Time: 46 ms (77.72%), Space: 46.8 MB (5%) - LeetHub

--- LeetCode/Medium/2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.java
+++ LeetCode/Medium/2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.java
@@ -1,37 +1,18 @@
 class Solution {
-
     public int maxCount(int[] banned, int n, int maxSum) {
-        Arrays.sort(banned);
-
+        Set<Integer> bannedSet = new HashSet<>();
+        for (int num : banned) {
+            bannedSet.add(num);
+        }
         int count = 0;
-
         for (int num = 1; num <= n; num++) {
-            if (customBinarySearch(banned, num)) continue;
-
+            if (bannedSet.contains(num))
+                continue;
+            if (maxSum - num < 0)
+                return count;
             maxSum -= num;
-
-            if (maxSum < 0) break;
-
             count++;
         }
         return count;
-    }
-    
-    private boolean customBinarySearch(int[] arr, int target) {
-        int left = 0;
-        int right = arr.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (arr[mid] == target) return true;
-
-            if (arr[mid] > target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return false;
     }
 }
