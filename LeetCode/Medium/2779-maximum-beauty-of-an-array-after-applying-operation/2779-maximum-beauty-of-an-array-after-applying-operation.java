6d60b7f24bef125978509320817a48a805fd0500
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-12-11 13:15:45 +0900

    Time: 39 ms (75.27%), Space: 60.3 MB (50%) - LeetHub

--- LeetCode/Medium/2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.java
+++ LeetCode/Medium/2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.java
@@ -1,27 +1,15 @@
 class Solution {
     public int maximumBeauty(int[] nums, int k) {
         Arrays.sort(nums);
+        int right = 0;
         int maxBeauty = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            int upperBound = findUpperBound(nums, nums[i] + 2 * k);
-            maxBeauty = Math.max(maxBeauty, upperBound - i + 1);
+        for (int left = 0; left < nums.length; left++) {
+            while (right < nums.length && nums[right] - nums[left] <= 2 * k) {
+                right++;
+            }
+            maxBeauty = Math.max(maxBeauty, right - left);
         }
         return maxBeauty;
-    }
-
-    private int findUpperBound(int[] arr, int val) {
-        int low = 0, high = arr.length - 1, result = 0;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (arr[mid] <= val) {
-                result = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        return result;
     }
 }
