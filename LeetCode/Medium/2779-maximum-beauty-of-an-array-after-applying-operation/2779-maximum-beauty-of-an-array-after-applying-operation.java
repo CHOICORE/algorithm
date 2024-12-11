0d7bfda94f55ac61bf2c2aa098a95e643d51b7a8
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-12-11 13:16:07 +0900

    Time: 4 ms (100%), Space: 60.7 MB (20.79%) - LeetHub

--- LeetCode/Medium/2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.java
+++ LeetCode/Medium/2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.java
@@ -1,15 +1,25 @@
 class Solution {
     public int maximumBeauty(int[] nums, int k) {
-        Arrays.sort(nums);
-        int right = 0;
-        int maxBeauty = 0;
+        if (nums.length == 1) return 1;
 
-        for (int left = 0; left < nums.length; left++) {
-            while (right < nums.length && nums[right] - nums[left] <= 2 * k) {
-                right++;
-            }
-            maxBeauty = Math.max(maxBeauty, right - left);
+        int maxBeauty = 0;
+        int maxValue = 0;
+
+        for (int num : nums) maxValue = Math.max(maxValue, num);
+
+        int[] count = new int[maxValue + 1];
+
+        for (int num : nums) {
+            count[Math.max(num - k, 0)]++;
+            count[Math.min(num + k + 1, maxValue)]--;
         }
+
+        int currentSum = 0;
+        for (int val : count) {
+            currentSum += val;
+            maxBeauty = Math.max(maxBeauty, currentSum);
+        }
+
         return maxBeauty;
     }
 }
