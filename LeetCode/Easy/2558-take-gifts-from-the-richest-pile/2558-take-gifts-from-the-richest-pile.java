91c6dad5bf6829477aac87b700cfa980c549c2a2
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-12-12 17:12:15 +0900

    Time: 6 ms (65.07%), Space: 42.8 MB (21.79%) - LeetHub

--- LeetCode/Easy/2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.java
+++ LeetCode/Easy/2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.java
@@ -1,28 +1,19 @@
 class Solution {
-
     public long pickGifts(int[] gifts, int k) {
-        int n = gifts.length;
-
-        for (int i = 0; i < k; i++) {
-            int richestPileIndex = 0;
-
-            for (
-                    int currentPileIndex = 0;
-                    currentPileIndex < n;
-                    currentPileIndex++
-            ) {
-                if (gifts[richestPileIndex] < gifts[currentPileIndex]) {
-                    richestPileIndex = currentPileIndex;
-                }
-            }
-
-            gifts[richestPileIndex] = (int) Math.floor(
-                    Math.sqrt(gifts[richestPileIndex])
-            );
+        Queue<Long> q = new PriorityQueue<>(Collections.reverseOrder());
+        for (long i : gifts) {
+            q.add(i);
         }
 
-        return Arrays.stream(gifts)
-                .mapToLong(gift -> gift)
-                .sum();
+        while (k-- > 0) {
+            long a = q.poll();
+            q.add((long) Math.floor(Math.sqrt(a)));
+        }
+
+        long s = 0;
+        while (!q.isEmpty()) {
+            s += q.poll();
+        }
+        return s;
     }
 }
