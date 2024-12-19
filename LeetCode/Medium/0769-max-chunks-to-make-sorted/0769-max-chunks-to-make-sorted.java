4047001cc460297406373a5cc5e2a35f31587685
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-12-19 13:09:50 +0900

    Time: 0 ms (100%), Space: 41 MB (34.84%) - LeetHub

--- LeetCode/Medium/0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.java
+++ LeetCode/Medium/0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.java
@@ -1,24 +1,17 @@
 class Solution {
+
     public int maxChunksToSorted(int[] arr) {
         int n = arr.length;
-        int[] prefixMax = arr.clone();
-        int[] suffixMin = arr.clone();
+        int chunks = 0, prefixSum = 0, sortedPrefixSum = 0;
 
-        for (int i = 1; i < n; i++) {
-            prefixMax[i] = Math.max(prefixMax[i - 1], prefixMax[i]);
-        }
-
-        for (int i = n - 2; i >= 0; i--) {
-            suffixMin[i] = Math.min(suffixMin[i + 1], suffixMin[i]);
-        }
-
-        int chunks = 0;
         for (int i = 0; i < n; i++) {
-            if (i == 0 || suffixMin[i] > prefixMax[i - 1]) {
+            prefixSum += arr[i];
+            sortedPrefixSum += i;
+            
+            if (prefixSum == sortedPrefixSum) {
                 chunks++;
             }
         }
-
         return chunks;
     }
 }
