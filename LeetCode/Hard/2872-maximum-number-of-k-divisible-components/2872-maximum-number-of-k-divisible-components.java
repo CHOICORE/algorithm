998376681e50c9f2fad5dc3498be810fdf895320
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-12-21 16:36:12 +0900

    Time: 123 ms (6.49%), Space: 61.3 MB (74.03%) - LeetHub

--- LeetCode/Hard/2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.java
+++ LeetCode/Hard/2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.java
@@ -6,55 +6,60 @@ class Solution {
             int[] values,
             int k
     ) {
-        List<Integer>[] adjList = new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            adjList[i] = new ArrayList<>();
-        }
+        if (n < 2) return 1;
+
+        int componentCount = 0;
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+
         for (int[] edge : edges) {
-            int node1 = edge[0];
-            int node2 = edge[1];
-            adjList[node1].add(node2);
-            adjList[node2].add(node1);
+            int node1 = edge[0], node2 = edge[1];
+            graph.computeIfAbsent(node1, key -> new HashSet<>()).add(node2);
+            graph.computeIfAbsent(node2, key -> new HashSet<>()).add(node1);
         }
 
-        int[] componentCount = new int[1];
+        long[] longValues = new long[values.length];
+        for (int i = 0; i < values.length; i++) {
+            longValues[i] = values[i];
+        }
 
-        dfs(0, -1, adjList, values, k, componentCount);
-
-        return componentCount[0];
-    }
-
-    private int dfs(
-            int currentNode,
-            int parentNode,
-            List<Integer>[] adjList,
-            int[] nodeValues,
-            int k,
-            int[] componentCount
-    ) {
-        int sum = 0;
-
-        for (int neighborNode : adjList[currentNode]) {
-            if (neighborNode != parentNode) {
-                sum += dfs(
-                        neighborNode,
-                        currentNode,
-                        adjList,
-                        nodeValues,
-                        k,
-                        componentCount
-                );
-                sum %= k;
+        Queue<Integer> queue = new LinkedList<>();
+        for (Map.Entry<Integer, Set<Integer>> entry : graph.entrySet()) {
+            if (entry.getValue().size() == 1) {
+                queue.add(entry.getKey());
             }
         }
 
-        sum += nodeValues[currentNode];
-        sum %= k;
+        while (!queue.isEmpty()) {
+            int currentNode = queue.poll();
 
-        if (sum == 0) {
-            componentCount[0]++;
+            int neighborNode = -1;
+            if (
+                    graph.get(currentNode) != null &&
+                            !graph.get(currentNode).isEmpty()
+            ) {
+                neighborNode = graph.get(currentNode).iterator().next();
+            }
+
+            if (neighborNode >= 0) {
+                graph.get(neighborNode).remove(currentNode);
+                graph.get(currentNode).remove(neighborNode);
+            }
+
+            if (longValues[currentNode] % k == 0) {
+                componentCount++;
+            } else if (neighborNode >= 0) {
+                longValues[neighborNode] += longValues[currentNode];
+            }
+            
+            if (
+                    neighborNode >= 0 &&
+                            graph.get(neighborNode) != null &&
+                            graph.get(neighborNode).size() == 1
+            ) {
+                queue.add(neighborNode);
+            }
         }
-        
-        return sum;
+
+        return componentCount;
     }
 }
