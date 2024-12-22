d2a839d33b72bef3ec6ea70a41b79b877d14ab14
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-12-22 13:48:33 +0900

    Time: 43 ms (74.36%), Space: 71.9 MB (78.2%) - LeetHub

--- LeetCode/Hard/2940-find-building-where-alice-and-bob-can-meet/2940-find-building-where-alice-and-bob-can-meet.java
+++ LeetCode/Hard/2940-find-building-where-alice-and-bob-can-meet/2940-find-building-where-alice-and-bob-can-meet.java
@@ -1,69 +1,53 @@
 class Solution {
 
     public int[] leftmostBuildingQueries(int[] heights, int[][] queries) {
-        List<Pair<Integer, Integer>> monoStack = new ArrayList<>();
-        int[] result = new int[queries.length];
-        Arrays.fill(result, -1);
-        List<List<Pair<Integer, Integer>>> newQueries = new ArrayList<>(
+        List<List<List<Integer>>> storeQueries = new ArrayList<>(
                 heights.length
         );
+        for (int i = 0; i < heights.length; i++)
+            storeQueries.add(
+                    new ArrayList<>()
+            );
+        PriorityQueue<List<Integer>> maxIndex = new PriorityQueue<>(
+                (a, b) -> a.get(0) - b.get(0)
+        );
+        int[] result = new int[queries.length];
+        Arrays.fill(result, -1);
 
-        for (int i = 0; i < heights.length; i++) {
-            newQueries.add(new ArrayList<>());
-        }
 
-        for (int i = 0; i < queries.length; i++) {
-            int a = queries[i][0];
-            int b = queries[i][1];
-            if (a > b) {
-                int temp = a;
-                a = b;
-                b = temp;
-            }
-            if (heights[b] > heights[a] || a == b) {
-                result[i] = b;
+        for (int currQuery = 0; currQuery < queries.length; currQuery++) {
+            int a = queries[currQuery][0], b = queries[currQuery][1];
+            if (a < b && heights[a] < heights[b]) {
+                result[currQuery] = b;
+            } else if (a > b && heights[a] > heights[b]) {
+                result[currQuery] = a;
+            } else if (a == b) {
+                result[currQuery] = a;
             } else {
-                newQueries.get(b).add(new Pair<>(heights[a], i));
+                storeQueries
+                        .get(Math.max(a, b))
+                        .add(
+                                Arrays.asList(
+                                        Math.max(heights[a], heights[b]),
+                                        currQuery
+                                )
+                        );
             }
         }
 
-        for (int i = heights.length - 1; i >= 0; i--) {
-            int monoStackSize = monoStack.size();
-            for (Pair<Integer, Integer> pair : newQueries.get(i)) {
-                int position = search(pair.getKey(), monoStack);
-                if (position < monoStackSize && position >= 0) {
-                    result[pair.getValue()] = monoStack
-                            .get(position)
-                            .getValue();
-                }
-            }
 
+        for (int index = 0; index < heights.length; index++) {
             while (
-                    !monoStack.isEmpty() &&
-                            monoStack.get(monoStack.size() - 1).getKey() <= heights[i]
+                    !maxIndex.isEmpty() && maxIndex.peek().get(0) < heights[index]
             ) {
-                monoStack.remove(monoStack.size() - 1);
+                result[maxIndex.peek().get(1)] = index;
+                maxIndex.poll();
             }
-
-            monoStack.add(new Pair<>(heights[i], i));
+            
+            for (List<Integer> element : storeQueries.get(index)) {
+                maxIndex.offer(element);
+            }
         }
-
         return result;
-    }
-
-    private int search(int height, List<Pair<Integer, Integer>> monoStack) {
-        int left = 0;
-        int right = monoStack.size() - 1;
-        int ans = -1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (monoStack.get(mid).getKey() > height) {
-                ans = Math.max(ans, mid);
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return ans;
     }
 }
