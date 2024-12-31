0c5ed33d9b3237b8d2c63160a9a4daf9e3129f46
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-12-31 13:46:32 +0900

    Time: 1 ms (79.45%), Space: 41.7 MB (50.02%) - LeetHub

--- LeetCode/Medium/0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ LeetCode/Medium/0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,35 +1,21 @@
 class Solution {
-    Set<Integer> isTravelNeeded = new HashSet<>();
-
-    private int solve(int[] dp, int[] days, int[] costs, int currDay) {
-        if (currDay > days[days.length - 1]) {
-            return 0;
-        }
-
-        if (!isTravelNeeded.contains(currDay)) {
-            return solve(dp, days, costs, currDay + 1);
-        }
-
-        if (dp[currDay] != -1) {
-            return dp[currDay];
-        }
-
-        int oneDay = costs[0] + solve(dp, days, costs, currDay + 1);
-        int sevenDay = costs[1] + solve(dp, days, costs, currDay + 7);
-        int thirtyDay = costs[2] + solve(dp, days, costs, currDay + 30);
-
-        return dp[currDay] = Math.min(oneDay, Math.min(sevenDay, thirtyDay));
-    }
-
     public int mincostTickets(int[] days, int[] costs) {
         int lastDay = days[days.length - 1];
         int[] dp = new int[lastDay + 1];
-        Arrays.fill(dp, -1);
-        
-        for (int day : days) {
-            isTravelNeeded.add(day);
+        Arrays.fill(dp, 0);
+
+        int i = 0;
+        for (int day = 1; day <= lastDay; day++) {
+            if (day < days[i]) {
+                dp[day] = dp[day - 1];
+            } else {
+                i++;
+                dp[day] = Math.min(dp[day - 1] + costs[0],
+                        Math.min(dp[Math.max(0, day - 7)] + costs[1],
+                                dp[Math.max(0, day - 30)] + costs[2]));
+            }
         }
 
-        return solve(dp, days, costs, 1);
+        return dp[lastDay];
     }
 }
