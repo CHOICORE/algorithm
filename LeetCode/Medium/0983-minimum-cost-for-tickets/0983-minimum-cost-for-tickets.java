e42ce9941cff085079236565d204deb59cbb2913
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2024-12-31 13:45:54 +0900

    Time: 2 ms (45.44%), Space: 42 MB (20.64%) - LeetHub

--- LeetCode/Medium/0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ LeetCode/Medium/0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,22 +1,35 @@
 class Solution {
-    public int mincostTickets(int[] days, int[] costs) {
-        // dp 배열 초기화
-        int[] dp = new int[366];
-        int dayIndex = 0;
-        for (int i = 1; i <= 365; i++) {
-            if (dayIndex == days.length) {
-                break;
-            }
-            if (i == days[dayIndex]) {
-                int dayAgoCost = dp[Math.max(0, i-1)] + costs[0];
-                int weekCost = dp[Math.max(0, i-7)] + costs[1];
-                int monthAgoCost = dp[Math.max(0, i-30)] + costs[2];
-                dp[i] = Math.min(dayAgoCost, Math.min(weekCost, monthAgoCost));
-                dayIndex++;
-            } else {
-                dp[i] = dp[i-1];
-            }
+    Set<Integer> isTravelNeeded = new HashSet<>();
+
+    private int solve(int[] dp, int[] days, int[] costs, int currDay) {
+        if (currDay > days[days.length - 1]) {
+            return 0;
         }
-        return dp[days[days.length-1]];
+
+        if (!isTravelNeeded.contains(currDay)) {
+            return solve(dp, days, costs, currDay + 1);
+        }
+
+        if (dp[currDay] != -1) {
+            return dp[currDay];
+        }
+
+        int oneDay = costs[0] + solve(dp, days, costs, currDay + 1);
+        int sevenDay = costs[1] + solve(dp, days, costs, currDay + 7);
+        int thirtyDay = costs[2] + solve(dp, days, costs, currDay + 30);
+
+        return dp[currDay] = Math.min(oneDay, Math.min(sevenDay, thirtyDay));
+    }
+
+    public int mincostTickets(int[] days, int[] costs) {
+        int lastDay = days[days.length - 1];
+        int[] dp = new int[lastDay + 1];
+        Arrays.fill(dp, -1);
+        
+        for (int day : days) {
+            isTravelNeeded.add(day);
+        }
+
+        return solve(dp, days, costs, 1);
     }
 }
