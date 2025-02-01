dc4385b580973585a25dd9b0be2241c1385121a5
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-02-01 15:24:13 +0900

    Time: 1 ms (37.58%), Space: 43.4 MB (56.64%) - LeetHub

--- LeetCode/Easy/3151-special-array-i/3151-special-array-i.java
+++ LeetCode/Easy/3151-special-array-i/3151-special-array-i.java
@@ -1,5 +1,4 @@
 class Solution {
-
     public boolean isArraySpecial(int[] nums) {
         for (int index = 0; index < nums.length - 1; index++) {
             if (nums[index] % 2 == nums[index + 1] % 2) {
