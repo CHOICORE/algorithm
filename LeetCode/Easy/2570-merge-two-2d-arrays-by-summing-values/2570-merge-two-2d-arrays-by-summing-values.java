0129baf5d68b3875937f980389555513ccdb3124
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-03-02 09:06:42 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.java
+++ LeetCode/Easy/2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.java
@@ -1,41 +1,26 @@
 class Solution {
-    
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        int i = 0;
-        int j = 0;
-        
-        ArrayList<int[]> arr = new ArrayList<>();
-        
-        while (i < nums1.length && j < nums2.length) {
-            int[] ko = new int[2];
-
+        int n1 = nums1.length, n2 = nums2.length, i = 0, j = 0;
+        ArrayList<int[]> ans = new ArrayList();
+        while (i < n1 && j < n2) {
             if (nums1[i][0] == nums2[j][0]) {
-                ko[0] = nums1[i][0];
-                ko[1] = nums1[i][1] + nums2[j][1];
+                ans.add(new int[]{nums1[i][0], nums1[i][1] + nums2[j][1]});
                 i++;
                 j++;
             } else if (nums1[i][0] < nums2[j][0]) {
-                ko[0] = nums1[i][0];
-                ko[1] = nums1[i][1];
-
+                ans.add(nums1[i]);
                 i++;
             } else {
-                ko[0] = nums2[j][0];
-                ko[1] = nums2[j][1];
-
+                ans.add(nums2[j]);
                 j++;
             }
-
-            arr.add(ko);
         }
-
-        while (i < nums1.length) {
-            arr.add(nums1[i++]);
+        while (i < n1) {
+            ans.add(nums1[i++]);
         }
-        while (j < nums2.length) {
-            arr.add(nums2[j++]);
+        while (j < n2) {
+            ans.add(nums2[j++]);
         }
-
-        return arr.toArray(int[][]::new);
+        return ans.toArray(new int[ans.size()][]);
     }
-}
+}}
