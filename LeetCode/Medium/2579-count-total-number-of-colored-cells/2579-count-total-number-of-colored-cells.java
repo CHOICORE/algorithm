b09e67d4f9cee716ed9ea8a053e62f0a1a8b1d0d
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-03-05 12:30:29 +0900

    Time: 0 ms (100%), Space: 41.2 MB (5.74%) - LeetHub

--- LeetCode/Medium/2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.java
+++ LeetCode/Medium/2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.java
@@ -1,12 +1,5 @@
 class Solution {
     public long coloredCells(int n) {
-        long i = 1;
-        int addend = 4;
-
-        while (--n > 0) {
-            i += addend;
-            addend += 4;
-        }
-        return i;
+        return 1 + (long) n * (n - 1) * 2;
     }
 }
