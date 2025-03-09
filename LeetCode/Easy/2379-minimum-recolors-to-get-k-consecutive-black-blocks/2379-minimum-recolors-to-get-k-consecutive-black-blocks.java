6ec7fa36d067e43cad22afbf729cb242c3d4068f
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-03-09 09:30:43 +0900

    Time: 1 ms (92.09%), Space: 41.6 MB (40.38%) - LeetHub

--- LeetCode/Easy/2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ LeetCode/Easy/2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,24 +1,20 @@
 class Solution {
+
     public int minimumRecolors(String blocks, int k) {
-        Queue<Character> blockQueue = new LinkedList<>();
-        int numWhites = 0;
+        int left = 0, numWhites = 0, numRecolors = Integer.MAX_VALUE;
 
-        for (int i = 0; i < k; i++) {
-            char currentChar = blocks.charAt(i);
-            if (currentChar == 'W') numWhites++;
-            blockQueue.add(currentChar);
-        }
+        for (int right = 0; right < blocks.length(); right++) {
+            if (blocks.charAt(right) == 'W') {
+                numWhites++;
+            }
 
-        int numRecolors = numWhites;
+            if (right - left + 1 == k) {
+                numRecolors = Math.min(numRecolors, numWhites);
 
-        for (int i = k; i < blocks.length(); i++) {
-            if (blockQueue.poll() == 'W') numWhites--;
-
-            char currentChar = blocks.charAt(i);
-            if (currentChar == 'W') numWhites++;
-            blockQueue.add(currentChar);
-            
-            numRecolors = Math.min(numRecolors, numWhites);
+                if (blocks.charAt(left) == 'W') numWhites--;
+                
+                left++;
+            }
         }
         return numRecolors;
     }
