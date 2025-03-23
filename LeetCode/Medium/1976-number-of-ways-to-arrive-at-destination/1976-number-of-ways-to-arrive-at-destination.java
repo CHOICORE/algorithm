43126172151ab5f913a779adaae02dd5d2fa2ea7
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-03-23 12:00:51 +0900

    Time: 196 ms (5.29%), Space: 49.9 MB (33.5%) - LeetHub

--- LeetCode/Medium/1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.java
+++ LeetCode/Medium/1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.java
@@ -1,56 +1,50 @@
 class Solution {
+    private static final int MOD = 1_000_000_007;
 
     public int countPaths(int n, int[][] roads) {
-        final int MOD = 1_000_000_007;
+        long[][][] dp = new long[n][n][2];
 
-        List<List<int[]>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        for (int[] road : roads) {
-            int startNode = road[0], endNode = road[1], travelTime = road[2];
-            graph.get(startNode).add(new int[]{endNode, travelTime});
-            graph.get(endNode).add(new int[]{startNode, travelTime});
-        }
-
-        PriorityQueue<long[]> minHeap = new PriorityQueue<>(
-                Comparator.comparingLong(a -> a[0])
-        );
-
-        long[] shortestTime = new long[n];
-        Arrays.fill(shortestTime, Long.MAX_VALUE);
-        int[] pathCount = new int[n];
-
-        shortestTime[0] = 0;
-        pathCount[0] = 1;
-
-        minHeap.offer(new long[]{0, 0});
-
-        while (!minHeap.isEmpty()) {
-            long[] top = minHeap.poll();
-            long currTime = top[0];
-            int currNode = (int) top[1];
-
-            if (currTime > shortestTime[currNode]) {
-                continue;
-            }
-
-            for (int[] neighbor : graph.get(currNode)) {
-                int neighborNode = neighbor[0], roadTime = neighbor[1];
-
-                if (currTime + roadTime < shortestTime[neighborNode]) {
-                    shortestTime[neighborNode] = currTime + roadTime;
-                    pathCount[neighborNode] = pathCount[currNode];
-                    minHeap.offer(
-                            new long[]{shortestTime[neighborNode], neighborNode}
-                    );
-                } else if (currTime + roadTime == shortestTime[neighborNode]) {
-                    pathCount[neighborNode] =
-                            (pathCount[neighborNode] + pathCount[currNode]) % MOD;
+        for (int src = 0; src < n; src++) {
+            for (int dest = 0; dest < n; dest++) {
+                if (src != dest) {
+                    dp[src][dest][0] = (long) 1e12;
+                    dp[src][dest][1] = 0;
+                } else {
+                    dp[src][dest][0] = 0;
+                    dp[src][dest][1] = 1;
                 }
             }
         }
 
-        return pathCount[n - 1];
+        for (int[] road : roads) {
+            int startNode = road[0], endNode = road[1], travelTime = road[2];
+            dp[startNode][endNode][0] = travelTime;
+            dp[endNode][startNode][0] = travelTime;
+            dp[startNode][endNode][1] = 1;
+            dp[endNode][startNode][1] = 1;
+        }
+
+        for (int mid = 0; mid < n; mid++) {
+            for (int src = 0; src < n; src++) {
+                for (int dest = 0; dest < n; dest++) {
+                    if (src != mid && dest != mid) {
+                        long newTime = dp[src][mid][0] + dp[mid][dest][0];
+
+                        if (newTime < dp[src][dest][0]) {
+                            dp[src][dest][0] = newTime;
+                            dp[src][dest][1] =
+                                    (dp[src][mid][1] * dp[mid][dest][1]) % MOD;
+                        } else if (newTime == dp[src][dest][0]) {
+                            dp[src][dest][1] =
+                                    (dp[src][dest][1] +
+                                            dp[src][mid][1] * dp[mid][dest][1]) %
+                                            MOD;
+                        }
+                    }
+                }
+            }
+        }
+
+        return (int) dp[n - 1][0][1];
     }
 }
