59b12bd438bb91630740a19f931d4e27deda9175
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-03-26 16:51:44 +0900

    Time: 91 ms (35.18%), Space: 75.7 MB (49.75%) - LeetHub

--- LeetCode/Medium/2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.java
+++ LeetCode/Medium/2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.java
@@ -1,10 +1,11 @@
 class Solution {
     public int minOperations(int[][] grid, int x) {
         List<Integer> numsArray = new ArrayList<>();
-        int result = 0;
+        int result = Integer.MAX_VALUE;
 
         for (int[] ints : grid) {
             for (int col = 0; col < grid[0].length; col++) {
+                if (ints[col] % x != grid[0][0] % x) return -1;
                 numsArray.add(ints[col]);
             }
         }
@@ -12,13 +13,25 @@ class Solution {
         Collections.sort(numsArray);
 
         int length = numsArray.size();
-        int finalCommonNumber = numsArray.get(length / 2);
+        int[] prefixSum = new int[length];
+        int[] suffixSum = new int[length];
 
-        for (int number : numsArray) {
-            if (number % x != finalCommonNumber % x) {
-                return -1;
-            }
-            result += Math.abs(finalCommonNumber - number) / x;
+        for (int index = 1; index < length; index++) {
+            prefixSum[index] = prefixSum[index - 1] + numsArray.get(index - 1);
+        }
+
+        for (int index = length - 2; index >= 0; index--) {
+            suffixSum[index] = suffixSum[index + 1] + numsArray.get(index + 1);
+        }
+
+        for (int index = 0; index < length; index++) {
+            int leftOperations =
+                    (numsArray.get(index) * index - prefixSum[index]) / x;
+            int rightOperations =
+                    (suffixSum[index] -
+                            numsArray.get(index) * (length - index - 1)) /
+                            x;
+            result = Math.min(result, leftOperations + rightOperations);
         }
 
         return result;
