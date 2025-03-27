44bc773ef5dbceb1e858a6aba3137f651a9bf196
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-03-27 16:17:21 +0900

    Time: 11 ms (95.51%), Space: 56.1 MB (92.95%) - LeetHub

--- LeetCode/Medium/2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.java
+++ LeetCode/Medium/2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.java
@@ -1,26 +1,36 @@
 class Solution {
     public int minimumIndex(List<Integer> nums) {
-        Map<Integer, Integer> firstMap = new HashMap<>();
-        Map<Integer, Integer> secondMap = new HashMap<>();
-        int n = nums.size();
+        int x = nums.get(0), count = 0, xCount = 0, n = nums.size();
 
         for (int num : nums) {
-            secondMap.put(num, secondMap.getOrDefault(num, 0) + 1);
+            if (num == x) {
+                count++;
+            } else {
+                count--;
+            }
+            if (count == 0) {
+                x = num;
+                count = 1;
+            }
         }
 
-        for (int index = 0; index < n; index++) {
-            int num = nums.get(index);
-            secondMap.put(num, secondMap.get(num) - 1);
-            firstMap.put(num, firstMap.getOrDefault(num, 0) + 1);
+        for (int num : nums) {
+            if (num == x) {
+                xCount++;
+            }
+        }
 
-            if (
-                firstMap.get(num) * 2 > index + 1 &&
-                    secondMap.get(num) * 2 > n - index - 1
-            ) {
+        count = 0;
+        for (int index = 0; index < n; index++) {
+            if (nums.get(index) == x) {
+                count++;
+            }
+            int remainingCount = xCount - count;
+            if (count * 2 > index + 1 && remainingCount * 2 > n - index - 1) {
                 return index;
             }
         }
-        
+
         return -1;
     }
 }
