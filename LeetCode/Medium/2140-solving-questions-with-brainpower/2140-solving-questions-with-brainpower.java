630b63f2f685da331609296139e59508a9c465c7
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-04-01 16:41:46 +0900

    Time: 12 ms (31.25%), Space: 124.9 MB (30.53%) - LeetHub

--- LeetCode/Medium/2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
+++ LeetCode/Medium/2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
@@ -1,18 +1,19 @@
 class Solution {
+    private long helper(int i, int[][] questions, long[] dp, int n) {
+        if (i >= n) return 0;
+        if (dp[i] != -1) return dp[i];
+
+        long take = questions[i][0] + helper(i + questions[i][1] + 1, questions, dp, n);
+        long dont = helper(i + 1, questions, dp, n);
+
+        return dp[i] = Math.max(take, dont);
+
+    }
+
     public long mostPoints(int[][] questions) {
         int n = questions.length;
         long[] dp = new long[n];
-
-        for (int i = n - 1; i >= 0; i--) {
-            dp[i] = questions[i][0];
-            if (i + questions[i][1] + 1 < n) {
-                dp[i] = Math.max(dp[i], (long) questions[i][0] + dp[i + questions[i][1] + 1]);
-            }
-            if (i + 1 < n) {
-                dp[i] = Math.max(dp[i], dp[i + 1]);
-            }
-        }
-
-        return dp[0];
+        Arrays.fill(dp, -1);
+        return helper(0, questions, dp, n);
     }
 }
