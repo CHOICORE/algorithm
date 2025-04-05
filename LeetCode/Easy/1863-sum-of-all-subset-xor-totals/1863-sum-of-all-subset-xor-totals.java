af1ef766ad1dcb41299a0856ba2a46819e1b6617
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-04-05 15:23:09 +0900

    Time: 16 ms (6.94%), Space: 45 MB (9.61%) - LeetHub

--- LeetCode/Easy/1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
+++ LeetCode/Easy/1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -1,11 +1,29 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-        return helper(nums, 0, 0);
+        List<List<Integer>> subsets = new ArrayList<>();
+        generateSubsets(nums, 0, new ArrayList<>(), subsets);
+
+        int result = 0;
+        for (List<Integer> subset : subsets) {
+            int subsetXORTotal = 0;
+            for (int num : subset) {
+                subsetXORTotal ^= num;
+            }
+            result += subsetXORTotal;
+        }
+        return result;
     }
 
-    public int helper(int[] nums, int index, int currValue) {
-        if (index == nums.length) return currValue;
+    private void generateSubsets(int[] nums, int index, List<Integer> subset, List<List<Integer>> subsets) {
+        if (index == nums.length) {
+            subsets.add(new ArrayList<>(subset));
+            return;
+        }
 
-        return helper(nums, index + 1, currValue ^ nums[index]) + helper(nums, index + 1, currValue);
+        subset.add(nums[index]);
+        generateSubsets(nums, index + 1, subset, subsets);
+        subset.remove(subset.size() - 1);
+        
+        generateSubsets(nums, index + 1, subset, subsets);
     }
 }
