91835411a77575b8c4d97f91136deffda258b45d
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-04-08 14:44:03 +0900

    Time: 1 ms (98.39%), Space: 44.8 MB (23.39%) - LeetHub

--- LeetCode/Easy/3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ LeetCode/Easy/3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -1,11 +1,10 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        boolean[] seen = new boolean[128];
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (seen[nums[i]]) {
-                return i / 3 + 1;
-            }
-            seen[nums[i]] = true;
+        int n = nums.length;
+        boolean[] seen = new boolean[101];
+        for (int i = n - 1; i >= 0; i--) {
+            if (seen[nums[i]]) return i / 3 + 1;
+            else seen[nums[i]] = true;
         }
         return 0;
     }
