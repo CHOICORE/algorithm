ae20324bc666239a8569abdac04c2bcdce152b8f
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-05-13 13:36:11 +0900

    Time: 36 ms (96.2%), Space: 45.8 MB (29.11%) - LeetHub

---
+++ LeetCode/Medium/3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    private static final int MOD = (int) 1e9 + 7;
+
+    public int lengthAfterTransformations(String s, int t) {
+        int[] cnt = new int[26];
+        for (char ch : s.toCharArray()) {
+            ++cnt[ch - 'a'];
+        }
+        for (int round = 0; round < t; ++round) {
+            int[] nxt = new int[26];
+            nxt[0] = cnt[25];
+            nxt[1] = (cnt[25] + cnt[0]) % MOD;
+            System.arraycopy(cnt, 1, nxt, 2, 24);
+            cnt = nxt;
+        }
+        int ans = 0;
+        for (int i = 0; i < 26; ++i) {
+            ans = (ans + cnt[i]) % MOD;
+        }
+        return ans;
+    }
+}
