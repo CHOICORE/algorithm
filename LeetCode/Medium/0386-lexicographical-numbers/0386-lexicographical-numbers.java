28fc41a64468d66a8e6d312fa8db528ed45362f9
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-06-08 18:42:01 +0900

    Time: 1 ms (100%), Space: 48.5 MB (58.06%) - LeetHub

--- LeetCode/Medium/0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ LeetCode/Medium/0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,4 +1,4 @@
-class Solution {
+public class Solution {
     public List<Integer> lexicalOrder(int n) {
         List<Integer> lexicographicalNumbers = new ArrayList<>();
         for (int start = 1; start <= 9; ++start) {
@@ -13,9 +13,9 @@ class Solution {
             List<Integer> result
     ) {
         if (currentNumber > limit) return;
-        
+
         result.add(currentNumber);
-        
+
         for (int nextDigit = 0; nextDigit <= 9; ++nextDigit) {
             int nextNumber = currentNumber * 10 + nextDigit;
             if (nextNumber <= limit) {
