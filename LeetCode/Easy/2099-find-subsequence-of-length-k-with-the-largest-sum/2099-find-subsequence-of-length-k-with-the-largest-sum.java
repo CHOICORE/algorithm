abf84241412b0a474aef0c95ae75df855b60c8d4
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-06-28 17:19:27 +0900

    Time: 3 ms (99.08%), Space: 43.7 MB (98.82%) - LeetHub

--- LeetCode/Easy/2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.java
+++ LeetCode/Easy/2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.java
@@ -1,19 +1,33 @@
 class Solution {
     public int[] maxSubsequence(int[] nums, int k) {
         int n = nums.length;
-        int[][] vals = new int[n][2];
-        for (int i = 0; i < n; ++i) {
-            vals[i][0] = i;
-            vals[i][1] = nums[i];
+        int[] sorted = Arrays.copyOf(nums, n);
+
+        Arrays.sort(sorted);
+
+        int threshold = sorted[n - k];
+        int thresholdCnt = 0;
+        for (int i = n - k; i < n; i++) {
+            if (sorted[i] == threshold) {
+                thresholdCnt++;
+            }
         }
 
-        Arrays.sort(vals, (x1, x2) -> Integer.compare(x2[1], x1[1]));
-        Arrays.sort(vals, 0, k, (x1, x2) -> Integer.compare(x1[0], x2[0]));
-        
         int[] r = new int[k];
-        for (int i = 0; i < k; ++i) {
-            r[i] = vals[i][1];
+
+        int p = 0;
+        for (int num : nums) {
+            if (num > threshold) {
+                r[p++] = num;
+            } else if (num == threshold && thresholdCnt > 0) {
+                r[p++] = num;
+                thresholdCnt--;
+            }
+            if (p == k) {
+                break;
+            }
         }
+
         return r;
     }
 }
