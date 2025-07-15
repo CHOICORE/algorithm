8b29bdcb7a774aa2171f5701b6a7bb134064e8b0
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-07-15 13:23:39 +0900

    Time: 1 ms (99.12%), Space: 42 MB (62.25%) - LeetHub

---
+++ LeetCode/Easy/3136-valid-word/3136-valid-word.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValid(String word) {
+        if (word.length() < 3) {
+            return false;
+        }
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+        for (char c : word.toCharArray()) {
+            if (Character.isLetter(c)) {
+                char ch = Character.toLowerCase(c);
+                if (check(ch)) {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            } else if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return hasVowel && hasConsonant;
+    }
+
+    private static boolean check(final char ch) {
+        return ch == 'a' ||
+                ch == 'e' ||
+                ch == 'i' ||
+                ch == 'o' ||
+                ch == 'u';
+    }
+}
