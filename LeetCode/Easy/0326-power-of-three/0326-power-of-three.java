a25032237db7d18ab810eb7781de18d007c38ee1
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-08-13 10:39:46 +0900

    Time: 8 ms (84.41%), Space: 43.9 MB (94.06%) - LeetHub

--- LeetCode/Easy/0326-power-of-three/0326-power-of-three.java
+++ LeetCode/Easy/0326-power-of-three/0326-power-of-three.java
@@ -1,6 +1,11 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        int maxPowerOf3 = 1_162_261_467;
-        return n > 0 && maxPowerOf3 % n == 0;
+        if (n <= 0) {
+            return false;
+        }
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
     }
 }
