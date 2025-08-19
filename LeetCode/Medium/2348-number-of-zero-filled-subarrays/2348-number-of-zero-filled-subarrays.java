bbc2a840557eceb5bac6d13d8143b0804300d1d1
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-08-19 14:26:06 +0900

    Time: 3 ms (94.03%), Space: 62.9 MB (5.97%) - LeetHub

--- LeetCode/Medium/2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
+++ LeetCode/Medium/2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
@@ -1,21 +1,10 @@
 class Solution {
     public long zeroFilledSubarray(int[] nums) {
-        int n = nums.length;
-        long count = 0;
-        int i = 0;
-        while (i < n) {
-            if (nums[i] != 0) {
-                i++;
-                continue;
-            }
-            int j = i + 1;
-            while (j < n && nums[j] == 0) {
-                j++;
-            }
-            int len = j - i;
-            count += (long) len * (len + 1) / 2;
-            i = j;
+        long c = 0, s = 0;
+        for (int num : nums) {
+            s = (num == 0) ? s + 1 : 0;
+            c += s;
         }
-        return count;
+        return c;
     }
 }
