f7f4c00027d4c74dc7566f0009bf86929649707e
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-09-01 16:49:38 +0900

    Time: 355 ms (60.1%), Space: 95.9 MB (51.3%) - LeetHub

--- LeetCode/Medium/1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.java
+++ LeetCode/Medium/1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.java
@@ -1,56 +1,46 @@
 class Solution {
 
     public double maxAverageRatio(int[][] classes, int extraStudents) {
-        List<Double> passRatios = new ArrayList<>();
+        PriorityQueue<double[]> maxHeap = new PriorityQueue<>((a, b) ->
+                Double.compare(b[0], a[0])
+        );
 
-        for (int[] aClass : classes) {
-            double initialRatio =
-                    (double) aClass[0] / aClass[1];
-            passRatios.add(initialRatio);
+        for (int[] singleClass : classes) {
+            int passes = singleClass[0];
+            int totalStudents = singleClass[1];
+            double gain = calculateGain(passes, totalStudents);
+            maxHeap.offer(new double[]{gain, passes, totalStudents});
         }
 
-        while (extraStudents > 0) {
-            List<Double> updatedRatios = new ArrayList<>();
-
-            for (
-                    int classIndex = 0;
-                    classIndex < classes.length;
-                    classIndex++
-            ) {
-                double newRatio =
-                        (double) (classes[classIndex][0] + 1) /
-                                (classes[classIndex][1] + 1);
-                updatedRatios.add(newRatio);
-            }
-
-            int bestClassIndex = 0;
-            double maximumGain = 0;
-
-            for (
-                    int classIndex = 0;
-                    classIndex < updatedRatios.size();
-                    classIndex++
-            ) {
-                double gain =
-                        updatedRatios.get(classIndex) - passRatios.get(classIndex);
-                if (gain > maximumGain) {
-                    bestClassIndex = classIndex;
-                    maximumGain = gain;
-                }
-            }
-
-            passRatios.set(bestClassIndex, updatedRatios.get(bestClassIndex));
-            classes[bestClassIndex][0]++;
-            classes[bestClassIndex][1]++;
-
-            extraStudents--;
+        while (extraStudents-- > 0) {
+            double[] current = maxHeap.poll();
+            double currentGain = current[0];
+            int passes = (int) current[1];
+            int totalStudents = (int) current[2];
+            maxHeap.offer(
+                    new double[]{
+                            calculateGain(passes + 1, totalStudents + 1),
+                            passes + 1,
+                            totalStudents + 1,
+                    }
+            );
         }
         
         double totalPassRatio = 0;
-        for (double passRatio : passRatios) {
-            totalPassRatio += passRatio;
+        while (!maxHeap.isEmpty()) {
+            double[] current = maxHeap.poll();
+            int passes = (int) current[1];
+            int totalStudents = (int) current[2];
+            totalPassRatio += (double) passes / totalStudents;
         }
 
         return totalPassRatio / classes.length;
+    }
+
+    private double calculateGain(int passes, int totalStudents) {
+        return (
+                (double) (passes + 1) / (totalStudents + 1) -
+                        (double) passes / totalStudents
+        );
     }
 }
