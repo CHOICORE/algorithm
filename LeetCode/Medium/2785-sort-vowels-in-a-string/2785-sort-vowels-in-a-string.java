5383c2baa69fed9a68005b42cc57600af1955267
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-09-11 17:04:35 +0900

    Time: 54 ms (47.05%), Space: 48 MB (36.12%) - LeetHub

--- LeetCode/Medium/2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
+++ LeetCode/Medium/2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
@@ -1,32 +1,31 @@
-public class Solution {
-    public String sortVowels(String s) {
-        int[] vowelCount = new int[11];
-        int[] countIndexMap = new int[128];
-        char[] result = s.toCharArray();
-        char[] charMap = "AEIOUaeiou".toCharArray();
-
-        for (int i = 0; i < charMap.length; i++)
-            countIndexMap[charMap[i]] = i + 1;
-
-        for (char c : result)
-            vowelCount[countIndexMap[c]]++;
-        int j = 1;
-        int i = 0;
-
-        while (j < vowelCount.length) {
-            if (vowelCount[j] > 0)
-                while (i < result.length) {
-                    if (countIndexMap[result[i]] == 0) {
-                        i++;
-                        continue;
-                    }
-                    vowelCount[j]--;
-                    result[i++] = charMap[j - 1];
-                    break;
-                }
-            else
-                j++;
-        }
-        return new String(result);
+class Solution {
+    boolean isVowel(Character c) {
+        return c == 'a' || c == 'e' || c == 'o' || c == 'u' || c == 'i'
+                || c == 'A' || c == 'E' || c == 'O' || c == 'U' || c == 'I';
     }
-}
+
+    public String sortVowels(String s) {
+        List<Character> temp = new ArrayList<>();
+
+        for (char c : s.toCharArray()) {
+            if (isVowel(c)) {
+                temp.add(c);
+            }
+        }
+
+        Collections.sort(temp);
+
+        StringBuilder ans = new StringBuilder();
+        int j = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (isVowel(s.charAt(i))) {
+                ans.append(temp.get(j));
+                j++;
+            } else {
+                ans.append(s.charAt(i));
+            }
+        }
+
+        return ans.toString();
+    }
+};
