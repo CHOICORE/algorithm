df0eb7478110e8afd1c553b52f33d94c7570345c
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-10-05 09:42:27 +0900

    Time: 4 ms (97.67%), Space: 45.6 MB (60.62%) - LeetHub

--- LeetCode/Medium/0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ LeetCode/Medium/0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,69 +1,34 @@
 class Solution {
-    private int m, n;
-    private int[][] heights;
-    private boolean[][] visitedP, visitedA;
-    private int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
-    private List<List<Integer>> res = new ArrayList<>();
-
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        m = heights.length;
-        n = heights[0].length;
-        this.heights = heights;
-        visitedP = new boolean[m][n];
-        visitedA = new boolean[m][n];
+        int rows = heights.length, cols = heights[0].length;
+        boolean[][] pac = new boolean[rows][cols];
+        boolean[][] atl = new boolean[rows][cols];
 
-        // 탐색을 위한 큐
-        Queue<int[]> queueP = new LinkedList<>();
-        Queue<int[]> queueA = new LinkedList<>();
-
-        // 탐색을 위해 바다의 가장자리에 있는 좌표를 큐에 추가
-        for (int i = 0; i < m; i++) {
-            queueP.offer(new int[]{i, 0});
-            queueA.offer(new int[]{i, n-1});
-            visitedP[i][0] = true;
-            visitedA[i][n-1] = true;
+        for (int col = 0; col < cols; col++) {
+            dfs(0, col, rows, cols, pac, heights[0][col], heights);
+            dfs(rows - 1, col, rows, cols, atl, heights[rows - 1][col], heights);
         }
-        for (int i = 0; i < n; i++) {
-            queueP.offer(new int[]{0, i});
-            queueA.offer(new int[]{m-1, i});
-            visitedP[0][i] = true;
-            visitedA[m-1][i] = true;
+        for (int row = 0; row < rows; row++) {
+            dfs(row, 0, rows, cols, pac, heights[row][0], heights);
+            dfs(row, cols - 1, rows, cols, atl, heights[row][cols - 1], heights);
         }
-
-        // 큐를 이용한 BFS 탐색
-        bfs(queueP, visitedP);
-        bfs(queueA, visitedA);
-
-        // 결과 리스트 생성
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (visitedP[i][j] && visitedA[i][j]) {
-                    res.add(Arrays.asList(i, j));
-                }
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++) {
+                if (pac[i][j] && atl[i][j])
+                    result.add(Arrays.asList(i, j));
             }
-        }
-
-        return res;
+        return result;
     }
 
-    private void bfs(Queue<int[]> queue, boolean[][] visited) {
-        while (!queue.isEmpty()) {
-            int[] curr = queue.poll();
-            int row = curr[0], col = curr[1];
+    private void dfs(int row, int col, int rows, int cols, boolean[][] visited, int prevHeight, int[][] heights) {
+        if (row < 0 || row >= rows || col < 0 || col >= cols || visited[row][col] || prevHeight > heights[row][col])
+            return;
+        visited[row][col] = true;
+        dfs(row + 1, col, rows, cols, visited, heights[row][col], heights);
+        dfs(row - 1, col, rows, cols, visited, heights[row][col], heights);
+        dfs(row, col + 1, rows, cols, visited, heights[row][col], heights);
+        dfs(row, col - 1, rows, cols, visited, heights[row][col], heights);
 
-            for (int[] d : dir) {
-                int newRow = row + d[0];
-                int newCol = col + d[1];
-
-                // 유효한 좌표인지, 아직 방문하지 않은 육지인지, 현재 위치보다 높이가 높은지 확인
-                if (newRow < 0 || newRow >= m || newCol < 0 || newCol >= n 
-                        || visited[newRow][newCol] || heights[newRow][newCol] < heights[row][col]) {
-                    continue;
-                }
-
-                visited[newRow][newCol] = true;
-                queue.offer(new int[]{newRow, newCol});
-            }
-        }
     }
 }
