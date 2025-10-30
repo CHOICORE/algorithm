3abb7b6d27f573617d7725f5c712fd9b63fd336a
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-10-30 16:43:26 +0900

    Time: 3 ms (100%), Space: 57.2 MB (34.08%) - LeetHub

---
+++ LeetCode/Hard/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minNumberOperations(int[] target) {
+        int n = target.length;
+        int r = target[0];
+        for (int i = 1; i < n; ++i) {
+            r += Math.max(target[i] - target[i - 1], 0);
+        }
+        return r;
+    }
+}
