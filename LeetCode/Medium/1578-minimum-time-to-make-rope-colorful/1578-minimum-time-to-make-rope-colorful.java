a9c3405f0496f375d6b686bc586fe8f8a1ccf440
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-11-03 14:29:36 +0900

    Time: 11 ms (18.49%), Space: 92.1 MB (5.68%) - LeetHub

--- LeetCode/Medium/1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
+++ LeetCode/Medium/1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
@@ -1,24 +1,17 @@
-import java.util.Arrays;
-
-public class Solution {
-    public int minCost(String colorSequence, int[] timeRequired) {
-        int n = colorSequence.length();
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, 0);
-        char previousColor = 'A';
-        int previousTime = 0;
-
-        for (int i = 1; i <= n; i++) {
-            if (colorSequence.charAt(i - 1) == previousColor) {
-                dp[i] = dp[i - 1] + Math.min(timeRequired[i - 1], previousTime);
-                previousTime = Math.max(timeRequired[i - 1], previousTime);
-            } else {
-                dp[i] = dp[i - 1];
-                previousColor = colorSequence.charAt(i - 1);
-                previousTime = timeRequired[i - 1];
+class Solution {
+    public int minCost(String colors, int[] neededTime) {
+        int n = colors.length(), sum = 0;
+        for (int i = 1; i < n; i++) {
+            int maxi = 0;
+            while (i < n && colors.charAt(i) == colors.charAt(i - 1)) {
+                sum += neededTime[i - 1];
+                maxi = Math.max(maxi, neededTime[i - 1]);
+                i++;
             }
+            sum += neededTime[i - 1];
+            maxi = Math.max(maxi, neededTime[i - 1]);
+            if (maxi != 0) sum -= maxi;
         }
-
-        return dp[n];
+        return sum;
     }
 }
