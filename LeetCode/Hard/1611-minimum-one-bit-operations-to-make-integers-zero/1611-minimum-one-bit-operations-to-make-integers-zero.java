74a0596f0c0580eb87cb62a7bd02b02b536af959
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-11-08 14:42:53 +0900

    Time: 0 ms (100%), Space: 42.1 MB (12%) - LeetHub

--- LeetCode/Hard/1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.java
+++ LeetCode/Hard/1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.java
@@ -1,23 +1,16 @@
 class Solution {
     public int minimumOneBitOperations(int n) {
-        int[] bits = new int[32];
-
-        bits[0] = 1;
-        for (int i = 1; i <= 31; i++) {
-            bits[i] = 2 * bits[i - 1] + 1;
+        if (n == 0) {
+            return 0;
         }
 
-        boolean flag = true;
-        int steps = 0;
-
-        for (int i = 31; i >= 0; i--) {
-            int bitSet = (n >> i) & 1;
-            if (bitSet == 0) continue;
-            if (flag) steps += bits[i];
-            else steps -= bits[i];
-            flag = !flag;
+        int k = 0;
+        int curr = 1;
+        while (curr * 2 <= n) {
+            curr *= 2;
+            k++;
         }
 
-        return steps;
+        return (1 << (k + 1)) - 1 - minimumOneBitOperations(n ^ curr);
     }
 }
