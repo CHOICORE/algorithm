bb6ac98dd9e2fa3e254d7c36b817372020d9ec33
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2025-11-28 11:03:21 +0900

    Time: 21 ms (100%), Space: 89.2 MB (35.48%) - LeetHub

--- LeetCode/Hard/2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.java
+++ LeetCode/Hard/2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.java
@@ -1,66 +1,59 @@
 class Solution {
-
     public int maxKDivisibleComponents(
             int n,
             int[][] edges,
             int[] values,
             int k
     ) {
-        if (n < 2) return 1;
-
-        int componentCount = 0;
-        List<List<Integer>> graph = new ArrayList<>();
-        int[] inDegree = new int[n];
-
+        List<Integer>[] adjList = new ArrayList[n];
         for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
+            adjList[i] = new ArrayList<>();
         }
         for (int[] edge : edges) {
-            int node1 = edge[0], node2 = edge[1];
-            graph.get(node1).add(node2);
-            graph.get(node2).add(node1);
-            inDegree[node1]++;
-            inDegree[node2]++;
+            int node1 = edge[0];
+            int node2 = edge[1];
+            adjList[node1].add(node2);
+            adjList[node2].add(node1);
         }
 
-        long[] longValues = new long[n];
-        for (int i = 0; i < n; i++) {
-            longValues[i] = values[i];
-        }
+        int[] componentCount = new int[1];
 
-        Queue<Integer> queue = new LinkedList<>();
-        for (int node = 0; node < n; node++) {
-            if (inDegree[node] == 1) {
-                queue.offer(node);
+        dfs(0, -1, adjList, values, k, componentCount);
+
+        return componentCount[0];
+    }
+
+    private int dfs(
+            int currentNode,
+            int parentNode,
+            List<Integer>[] adjust,
+            int[] nodeValues,
+            int k,
+            int[] componentCount
+    ) {
+        int sum = 0;
+
+        for (int neighborNode : adjust[currentNode]) {
+            if (neighborNode != parentNode) {
+                sum += dfs(
+                        neighborNode,
+                        currentNode,
+                        adjust,
+                        nodeValues,
+                        k,
+                        componentCount
+                );
+                sum %= k;
             }
         }
 
-        while (!queue.isEmpty()) {
-            int currentNode = queue.poll();
-            inDegree[currentNode]--;
+        sum += nodeValues[currentNode];
+        sum %= k;
 
-            long addValue = 0;
-
-            if (longValues[currentNode] % k == 0) {
-                componentCount++;
-            } else {
-                addValue = longValues[currentNode];
-            }
-
-            for (int neighborNode : graph.get(currentNode)) {
-                if (inDegree[neighborNode] == 0) {
-                    continue;
-                }
-
-                inDegree[neighborNode]--;
-                longValues[neighborNode] += addValue;
-                
-                if (inDegree[neighborNode] == 1) {
-                    queue.offer(neighborNode);
-                }
-            }
+        if (sum == 0) {
+            componentCount[0]++;
         }
 
-        return componentCount;
+        return sum;
     }
 }
