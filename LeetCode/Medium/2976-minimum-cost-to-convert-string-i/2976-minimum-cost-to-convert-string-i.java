52ec96a6d2825bee10740e1b95952216b46486ea
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2026-01-29 16:25:28 +0900

    Time: 48 ms (21.15%), Space: 48 MB (49.82%) - LeetHub

--- LeetCode/Medium/2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.java
+++ LeetCode/Medium/2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.java
@@ -1,33 +1,80 @@
 class Solution {
-    public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
-        int[][] dis = new int[26][26];
+    public long minimumCost(
+            String source,
+            String target,
+            char[] original,
+            char[] changed,
+            int[] cost
+    ) {
+        List<int[]>[] adjacencyList = new List[26];
         for (int i = 0; i < 26; i++) {
-            Arrays.fill(dis[i], Integer.MAX_VALUE);
-            dis[i][i] = 0;
+            adjacencyList[i] = new ArrayList<>();
         }
-        for (int i = 0; i < cost.length; i++) {
-            dis[original[i] - 'a'][changed[i] - 'a'] = Math.min(dis[original[i] - 'a'][changed[i] - 'a'], cost[i]);
+
+        int conversionCount = original.length;
+        for (int i = 0; i < conversionCount; i++) {
+            adjacencyList[original[i] - 'a'].add(
+                    new int[]{changed[i] - 'a', cost[i]}
+            );
         }
-        for (int k = 0; k < 26; k++) {
-            for (int i = 0; i < 26; i++)
-                if (dis[i][k] < Integer.MAX_VALUE) {
-                    for (int j = 0; j < 26; j++) {
-                        if (dis[k][j] < Integer.MAX_VALUE) {
-                            dis[i][j] = Math.min(dis[i][j], dis[i][k] + dis[k][j]);
-                        }
-                    }
+
+        long[][] minConversionCosts = new long[26][26];
+        for (int i = 0; i < 26; i++) {
+            minConversionCosts[i] = dijkstra(i, adjacencyList);
+        }
+
+        long totalCost = 0;
+        int stringLength = source.length();
+        for (int i = 0; i < stringLength; i++) {
+            if (source.charAt(i) != target.charAt(i)) {
+                long charConversionCost =
+                        minConversionCosts[source.charAt(i) - 'a'][target.charAt(
+                                i
+                        ) -
+                                'a'];
+                if (charConversionCost == -1) {
+                    return -1;
                 }
-        }
-        long answer = 0L;
-        for (int i = 0; i < source.length(); i++) {
-            int c1 = source.charAt(i) - 'a';
-            int c2 = target.charAt(i) - 'a';
-            if (dis[c1][c2] == Integer.MAX_VALUE) {
-                return -1L;
-            } else {
-                answer += dis[c1][c2];
+                totalCost += charConversionCost;
             }
         }
-        return answer;
+        return totalCost;
+    }
+
+    private long[] dijkstra(int startChar, List<int[]>[] adjacencyList) {
+        PriorityQueue<Pair<Long, Integer>> priorityQueue = new PriorityQueue<>(
+                (e1, e2) -> Long.compare(e1.getKey(), e2.getKey())
+        );
+
+        priorityQueue.add(new Pair<>(0L, startChar));
+
+        long[] minCosts = new long[26];
+        Arrays.fill(minCosts, -1L);
+
+        while (!priorityQueue.isEmpty()) {
+            Pair<Long, Integer> currentPair = priorityQueue.poll();
+            long currentCost = currentPair.getKey();
+            int currentChar = currentPair.getValue();
+
+            if (
+                    minCosts[currentChar] != -1L &&
+                            minCosts[currentChar] < currentCost
+            ) continue;
+
+            for (int[] conversion : adjacencyList[currentChar]) {
+                int targetChar = conversion[0];
+                long conversionCost = conversion[1];
+                long newTotalCost = currentCost + conversionCost;
+
+                if (
+                        minCosts[targetChar] == -1L ||
+                                newTotalCost < minCosts[targetChar]
+                ) {
+                    minCosts[targetChar] = newTotalCost;
+                    priorityQueue.add(new Pair<>(newTotalCost, targetChar));
+                }
+            }
+        }
+        return minCosts;
     }
 }
