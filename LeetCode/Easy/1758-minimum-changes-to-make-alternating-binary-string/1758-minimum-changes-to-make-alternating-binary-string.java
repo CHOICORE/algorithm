589069d004123a716cd564d7e0d1f439445d6ba2
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2026-03-05 16:10:56 +0900

    Time: 4 ms (75.73%), Space: 44 MB (18.42%) - LeetHub

--- LeetCode/Easy/1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ LeetCode/Easy/1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -1,22 +1,18 @@
 class Solution {
     public int minOperations(String s) {
-        char zeroIndex = s.charAt(0);
-        int count1 = count(s, zeroIndex);
-        int count2 = count(s, zeroIndex == '0' ? '1' : '0') + 1;
-        return Math.min(count1, count2);
-    }
-
-    private int count(String s, char prev) {
-        int count = 0;
-        for (int i = 1; i < s.length(); i++) {
-            char current = s.charAt(i);
-            if (current == prev) {
-                count++;
-                prev = prev == '0' ? '1' : '0';
+        int r = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 == 0) {
+                if (s.charAt(i) == '1') {
+                    r++;
+                }
             } else {
-                prev = current;
+                if (s.charAt(i) == '0') {
+                    r++;
+                }
             }
         }
-        return count;
+
+        return Math.min(r, s.length() - r);
     }
 }
