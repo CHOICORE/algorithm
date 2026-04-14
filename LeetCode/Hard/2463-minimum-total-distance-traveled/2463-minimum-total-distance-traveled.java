d9082e2ef2a247023c7dae7cc9e80489a9c48399
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2026-04-14 15:37:52 +0900

    Time: 154 ms (11.29%), Space: 75.4 MB (17.74%) - LeetHub

--- LeetCode/Hard/2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.java
+++ LeetCode/Hard/2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.java
@@ -2,7 +2,7 @@ class Solution {
     public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
         Collections.sort(robot);
         Arrays.sort(factory, Comparator.comparingInt(a -> a[0]));
-        
+
         List<Integer> factoryPositions = new ArrayList<>();
         for (int[] f : factory) {
             for (int i = 0; i < f[1]; i++) {
@@ -14,7 +14,7 @@ class Solution {
         int factoryCount = factoryPositions.size();
         long[][] memo = new long[robotCount][factoryCount];
         for (long[] row : memo) Arrays.fill(row, -1);
-        
+
         return calculateMinDistance(0, 0, robot, factoryPositions, memo);
     }
 
@@ -28,7 +28,7 @@ class Solution {
         if (robotIdx == robot.size()) return 0;
         if (factoryIdx == factoryPositions.size()) return (long) 1e12;
         if (memo[robotIdx][factoryIdx] != -1) return memo[robotIdx][factoryIdx];
-        
+
         long assign =
                 Math.abs(robot.get(robotIdx) - factoryPositions.get(factoryIdx)) +
                         calculateMinDistance(
@@ -38,7 +38,7 @@ class Solution {
                                 factoryPositions,
                                 memo
                         );
-        
+
         long skip = calculateMinDistance(
                 robotIdx,
                 factoryIdx + 1,
