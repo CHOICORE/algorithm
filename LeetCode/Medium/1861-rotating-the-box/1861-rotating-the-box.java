f7b25cc014a4731fae291506b4bbb7852ef8335a
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2026-05-06 16:12:58 +0900

    Time: 8 ms (59.38%), Space: 124.7 MB (60.34%) - LeetHub

--- LeetCode/Medium/1861-rotating-the-box/1861-rotating-the-box.java
+++ LeetCode/Medium/1861-rotating-the-box/1861-rotating-the-box.java
@@ -1,66 +1,25 @@
 class Solution {
-    public char[][] rotateTheBox(char[][] box) {
-        int m = box.length;
-        int n = box[0].length;
-        char[][] ans = new char[n][m];
-
-        for (int i = 0; i < m; i++) {
-            int pos = n - 1;
-            for (int j = pos; j >= 0; j--) {
-                switch (box[i][j]) {
-                    case '#':
-                        ans[j][m - 1 - i] = '.';
-                        ans[pos][m - 1 - i] = '#';
-                        pos -= 1;
-                        break;
-                    case '*':
-                        ans[j][m - 1 - i] = '*';
-                        pos = j - 1;
-                        break;
-                    default:
-                        ans[j][m - 1 - i] = '.';
-                        break;
-                }
-            }
-            while (pos >= 0) {
-                ans[pos--][m - 1 - i] = '.';
+    public char[][] rotateTheBox(char[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        for (int r = 0; r < rows; r++) {
+            int p = 0;
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == '.') {
+                    char temp = grid[r][c];
+                    grid[r][c] = grid[r][p];
+                    grid[r][p] = temp;
+                    p++;
+                } else if (grid[r][c] == '*')
+                    p = c + 1;
             }
         }
-        return ans;
-    }
 
-    private void moveRight(char[][] box) {
-        for (int i = 0; i < box.length; i++) {
-            int pos = box[i].length - 1;
-            for (int j = pos; j >= 0; j--) {
-                switch (box[i][j]) {
-                    case '#':
-                        box[i][j] = '.';
-                        box[i][pos--] = '#';
-                        break;
-                    case '*':
-                        pos = j - 1;
-                        break;
-                    default:
-                        break;
-                }
-            }
-            while (pos >= 0) {
-                box[i][pos--] = '.';
-            }
-        }
-    }
+        char[][] res = new char[cols][rows];
+        for (int r = 0; r < rows; r++)
+            for (int c = 0; c < cols; c++)
+                res[c][rows - 1 - r] = grid[r][c];
 
-    private char[][] rotate(char[][] box) {
-        int m = box.length;
-        int n = box[0].length;
-        char[][] ans = new char[n][m];
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                ans[j][m - i - 1] = box[i][j];
-            }
-        }
-        return ans;
+        return res;
     }
 }
