d43a11c9c682889ff3441763bb63ce6ccd5387b6
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2026-05-12 16:58:16 +0900

    Time: 264 ms (5.77%), Space: 114 MB (40.38%) - LeetHub

--- LeetCode/Hard/1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.java
+++ LeetCode/Hard/1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.java
@@ -1,10 +1,17 @@
 class Solution {
     public int minimumEffort(int[][] tasks) {
-        Arrays.sort(tasks, (a, b) -> a[1] - a[0] - (b[1] - b[0]));
-        int r = 0;
+        Arrays.sort(tasks, (a, b) -> (b[1] - b[0]) - (a[1] - a[0]));
+        int energy = 0;
+        int curr = 0;
         for (int[] task : tasks) {
-            r = Math.max(r + task[0], task[1]);
+            if (curr < task[1]) {
+                energy += task[1] - curr;
+                curr = task[1];
+            }
+
+            curr -= task[0];
         }
-        return r;
+
+        return energy;
     }
 }
