0aede960eaf4927c3bbd8195b7999bfdf5a11b04
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2026-05-19 15:52:12 +0900

    Time: 15 ms (15.62%), Space: 89 MB (10.33%) - LeetHub

--- LeetCode/Easy/2540-minimum-common-value/2540-minimum-common-value.java
+++ LeetCode/Easy/2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,18 +1,17 @@
-class Solution {
+public class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        
-        int i = 0, j = 0;
-        int num1Length = nums1.length;
-        int num2Length = nums2.length;
-        
-        if (nums1[num1Length - 1] < nums2[0] || nums2[num2Length - 1] < nums1[0])
-            return -1;
+        Set<Integer> set1 = new HashSet<>();
 
-        while (i < num1Length && j < num2Length) {
-            if (nums1[i] == nums2[j]) return nums1[i];
-            else if (nums1[i] > nums2[j]) j++;
-            else i++;
+        for (int num : nums1) {
+            set1.add(num);
         }
+
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                return num;
+            }
+        }
+        
         return -1;
     }
 }
