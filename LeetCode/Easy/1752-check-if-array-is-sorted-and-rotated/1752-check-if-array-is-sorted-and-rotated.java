7275c64682d3e5ebb912b8a9ce6b7f07b108b439
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2026-05-24 06:40:34 +0900

    Time: 2 ms (4.7%), Space: 42.9 MB (79.5%) - LeetHub

--- LeetCode/Easy/1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ LeetCode/Easy/1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean check(int[] nums) {
         int n = nums.length;
-
+        
         int[] checkSorted = new int[n];
-
+        
         for (int rotationOffset = 0; rotationOffset < n; ++rotationOffset) {
             int currIndex = 0;
             for (int index = rotationOffset; index < n; ++index) {
@@ -12,7 +12,7 @@ class Solution {
             for (int index = 0; index < rotationOffset; ++index) {
                 checkSorted[currIndex++] = nums[index];
             }
-
+            
             boolean isSorted = true;
             for (int index = 0; index < n - 1; ++index) {
                 if (checkSorted[index] > checkSorted[index + 1]) {
@@ -20,7 +20,7 @@ class Solution {
                     break;
                 }
             }
-
+            
             if (isSorted) {
                 return true;
             }
