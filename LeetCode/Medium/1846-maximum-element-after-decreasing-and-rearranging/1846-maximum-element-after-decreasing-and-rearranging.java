89162ccf22e733e021c55a2bc5a353590863f179
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2026-06-28 19:48:34 +0900

    Time: 7 ms (95.42%), Space: 77.3 MB (92.08%) - LeetHub

--- LeetCode/Medium/1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
+++ LeetCode/Medium/1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
@@ -1,16 +1,14 @@
 class Solution {
     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
-        int l = arr.length;
-        int[] counter = new int[l];
-
-        for (final int j : arr) {
-            ++counter[Math.min(j - 1, l - 1)];
-        }
+        Arrays.sort(arr);
         int ans = 1;
-        for (int i = 1; i < l; i++) {
-            ans = Math.min(i + 1, ans + counter[i]);
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] >= ans + 1) {
+                ans++;
+            }
         }
-
+        
         return ans;
     }
 }
