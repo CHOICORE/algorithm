151b277a3e5a4c384d2fa72a7f3b08c27b11fe4e
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2026-07-08 13:46:20 +0900

    Time: 14 ms (100%), Space: 131.3 MB (23.33%) - LeetHub

--- LeetCode/Medium/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.java
+++ LeetCode/Medium/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.java
@@ -1,36 +1,43 @@
 class Solution {
-    private static final int MOD = 1000000007;
-    private static final int MAX_N = 100001;
-    private static final long[] pow10 = new long[MAX_N];
-    
-    static {
-        pow10[0] = 1;
-        for (int i = 1; i < MAX_N; ++i) {
-            pow10[i] = (pow10[i - 1] * 10) % MOD;
-        }
-    }
-
     public int[] sumAndMultiply(String s, int[][] queries) {
-        int n = s.length();
-        int[] sum = new int[n + 1];
-        long[] x = new long[n + 1];
-        int[] cnt = new int[n + 1];
-        for (int i = 0; i < n; ++i) {
-            int d = s.charAt(i) - '0';
-            sum[i + 1] = sum[i] + d;
-            x[i + 1] = d > 0 ? (x[i] * 10 + d) % MOD : x[i];
-            cnt[i + 1] = cnt[i] + (d > 0 ? 1 : 0);
+        long MOD = 1_000_000_007;
+        int len = s.length();
+        long[] preSum = new long[len + 1];
+        long[] preProduct = new long[len + 1];
+        int[] nonZeroCnt = new int[len + 1];
+        long[] p10 = new long[len + 1];
+
+        p10[0] = 1;
+        for (int i = 0; i < len; i++) {
+            p10[i + 1] = (p10[i] * 10) % MOD;
+
+            int digit = s.charAt(i) - '0';
+            preSum[i + 1] = preSum[i] + digit;
+
+            if (digit == 0) {
+                preProduct[i + 1] = preProduct[i];
+                nonZeroCnt[i + 1] = nonZeroCnt[i];
+            } else {
+                preProduct[i + 1] = (preProduct[i] * 10 + digit) % MOD;
+                nonZeroCnt[i + 1] = nonZeroCnt[i] + 1;
+            }
         }
-        int m = queries.length;
-        int[] q = new int[m];
-        for (int i = 0; i < m; ++i) {
-            int l = queries[i][0];
-            int r = queries[i][1] + 1;
-            int length = cnt[r] - cnt[l];
-            long val_x = (x[r] - ((x[l] * pow10[length]) % MOD) + MOD) % MOD;
-            long val_sum = sum[r] - sum[l];
-            q[i] = (int) ((val_x * val_sum) % MOD);
+
+        int[] r = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int start = queries[i][0];
+            int end = queries[i][1];
+
+            long sum = preSum[end + 1] - preSum[start];
+
+            int cnt = nonZeroCnt[end + 1] - nonZeroCnt[start];
+
+            long subtract = (preProduct[start] * p10[cnt]) % MOD;
+            long x = (preProduct[end + 1] - subtract + MOD) % MOD;
+            
+            r[i] = (int) ((x * sum) % MOD);
         }
-        return q;
+
+        return r;
     }
 }
