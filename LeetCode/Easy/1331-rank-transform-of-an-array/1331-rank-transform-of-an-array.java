c2841d853dec63742607f23470b225c5e23782d7
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2026-07-12 22:20:09 +0900

    Time: 31 ms (59.74%), Space: 75 MB (84.11%) - LeetHub

--- LeetCode/Easy/1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ LeetCode/Easy/1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -1,20 +1,18 @@
 class Solution {
-    public int[] arrayRankTransform(int[] arr) {
-        Map<Integer, List<Integer>> numToIndices = new TreeMap<>();
 
-        for (int i = 0; i < arr.length; i++) {
-            if (!numToIndices.containsKey(arr[i])) {
-                numToIndices.put(arr[i], new ArrayList<>());
-            }
-            numToIndices.get(arr[i]).add(i);
-        }
-        
+    public int[] arrayRankTransform(int[] arr) {
+        HashMap<Integer, Integer> numToRank = new HashMap<>();
+        int[] sortedArr = Arrays.copyOf(arr, arr.length);
+        Arrays.sort(sortedArr);
         int rank = 1;
-        for (int num : numToIndices.keySet()) {
-            for (int index : numToIndices.get(num)) {
-                arr[index] = rank;
+        for (int i = 0; i < sortedArr.length; i++) {
+            if (i > 0 && sortedArr[i] > sortedArr[i - 1]) {
+                rank++;
             }
-            rank++;
+            numToRank.put(sortedArr[i], rank);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = numToRank.get(arr[i]);
         }
         return arr;
     }
