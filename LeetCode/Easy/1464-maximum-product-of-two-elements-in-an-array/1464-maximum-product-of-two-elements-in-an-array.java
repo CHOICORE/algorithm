8f9b98d36d950a0fe98dab4d15985ffb5c0abb3c
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2026-07-27 16:49:33 +0900

    Time: 5 ms (42.28%), Space: 44.4 MB (73.61%) - LeetHub

--- LeetCode/Easy/1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
+++ LeetCode/Easy/1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
@@ -1,25 +1,12 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int largest = nums[0];
-        int secondLargest = nums[1];
-
-        if (secondLargest > largest) {
-            int temp = largest;
-            largest = secondLargest;
-            secondLargest = temp;
-        }
-
-        for (int i = 2; i < nums.length; i++) {
-            if (nums[i] > largest) {
-                secondLargest = largest;
-                largest = nums[i];
-            } else if (nums[i] > secondLargest) {
-                secondLargest = nums[i];
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                ans = Math.max(ans, (nums[i] - 1) * (nums[j] - 1));
             }
         }
-
-        return (secondLargest - 1) * (largest - 1);
-
-
+        
+        return ans;
     }
 }
