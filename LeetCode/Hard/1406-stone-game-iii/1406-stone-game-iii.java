0c025d34cbff7aa7c3f354ff83951852e0216978
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2026-08-03 15:34:42 +0900

    Time: 11 ms (65.34%), Space: 85.6 MB (60.23%) - LeetHub

--- LeetCode/Hard/1406-stone-game-iii/1406-stone-game-iii.java
+++ LeetCode/Hard/1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,26 +1,33 @@
 class Solution {
     public String stoneGameIII(int[] stoneValue) {
         int n = stoneValue.length;
-        int dp[] = {0, 0, 0};
-        int total = 0;
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MIN_VALUE);
+
+        dp[n] = 0;
 
         for (int i = n - 1; i >= 0; i--) {
-            total = total + stoneValue[i];
 
-            int curr = Math.max(total - dp[0], Math.max(total - dp[1], total - dp[2]));
+            int take = 0;
 
-            dp[2] = dp[1];
-            dp[1] = dp[0];
-            dp[0] = curr;
+            for (int k = 0; k < 3 && i + k < n; k++) {
+
+                take += stoneValue[i + k];
+
+                dp[i] = Math.max(
+                        dp[i],
+                        take - dp[i + k + 1]
+                );
+            }
         }
 
-        if (dp[0] > total - dp[0]) {
+        if (dp[0] > 0)
             return "Alice";
-        } else if (dp[0] < total - dp[0]) {
-            return "Bob";
-        } else {
-            return "Tie";
-        }
 
+        if (dp[0] < 0)
+            return "Bob";
+
+        return "Tie";
     }
 }
