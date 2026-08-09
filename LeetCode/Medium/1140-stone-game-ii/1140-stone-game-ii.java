ab9a2e7f814b4c888e4f00f1442e7b5358b5db19
Jae-hyeong Choi <41681972+CHOICORE@users.noreply.github.com>
2026-08-09 20:48:32 +0900

    Time: 2 ms (100%), Space: 44.2 MB (82.63%) - LeetHub

--- LeetCode/Medium/1140-stone-game-ii/1140-stone-game-ii.java
+++ LeetCode/Medium/1140-stone-game-ii/1140-stone-game-ii.java
@@ -1,33 +1,37 @@
 class Solution {
+
     public int stoneGameII(int[] piles) {
-        int length = piles.length;
-        int[][] dp = new int[length + 1][length + 1];
+        int[] suffixSum = Arrays.copyOf(piles, piles.length);
 
-        int[] suffixSum = new int[length + 1];
-        for (int i = length - 1; i >= 0; i--) {
-            suffixSum[i] = suffixSum[i + 1] + piles[i];
+        for (int i = suffixSum.length - 2; i >= 0; i--) {
+            suffixSum[i] += suffixSum[i + 1];
         }
+        return maxStones(suffixSum, 1, 0, new int[piles.length][piles.length]);
+    }
 
-        for (int i = 0; i <= length; i++) {
-            dp[i][length] = suffixSum[i];
+    private int maxStones(
+            int[] suffixSum,
+            int maxTillNow,
+            int currIndex,
+            int[][] memo
+    ) {
+        if (currIndex + 2 * maxTillNow >= suffixSum.length) {
+            return suffixSum[currIndex];
         }
-
-        for (int index = length - 1; index >= 0; index--) {
-            for (int maxTillNow = length - 1; maxTillNow >= 1; maxTillNow--) {
-                for (
-                        int X = 1;
-                        X <= 2 * maxTillNow && index + X <= length;
-                        X++
-                ) {
-                    dp[index][maxTillNow] = Math.max(
-                            dp[index][maxTillNow],
-                            suffixSum[index] -
-                                    dp[index + X][Math.max(maxTillNow, X)]
-                    );
-                }
-            }
+        if (memo[currIndex][maxTillNow] > 0) return memo[currIndex][maxTillNow];
+        int res = Integer.MAX_VALUE;
+        for (int i = 1; i <= 2 * maxTillNow; i++) {
+            res = Math.min(
+                    res,
+                    maxStones(
+                            suffixSum,
+                            Math.max(i, maxTillNow),
+                            currIndex + i,
+                            memo
+                    )
+            );
         }
-        
-        return dp[0][1];
+        memo[currIndex][maxTillNow] = suffixSum[currIndex] - res;
+        return memo[currIndex][maxTillNow];
     }
 }
